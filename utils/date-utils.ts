d43f19ec407f8667a035f34fdd109209d4c9f86a
Jonas Daniels <jonas.daniels@outlook.com>
2022-03-31 12:06:46 +0200

    fix date-utils crash (#40)

--- utils/date-utils.ts
+++ utils/date-utils.ts
@@ -1,7 +1,9 @@
-import { format } from "date-fns";
+import { format, isValid } from "date-fns";
 
 const DATE_TIME_LOCAL_FORMAT = "yyyy-MM-dd'T'HH:mm:ss";
 
 export function toDateTimeLocal(date?: Date) {
-  return date ? format(date, DATE_TIME_LOCAL_FORMAT) : undefined;
+  return date && isValid(date)
+    ? format(date, DATE_TIME_LOCAL_FORMAT)
+    : undefined;
 }
