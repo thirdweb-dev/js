89071be32626e65ac8673a48b99a2670de3fec51
Jonas Daniels <jonas.daniels@outlook.com>
2022-04-14 09:34:08 -0700

    update cta button color + add icon

--- components/shared/GeneralCta.tsx
+++ components/shared/GeneralCta.tsx
@@ -1,7 +1,9 @@
 import { LinkButton } from "./LinkButton";
 import { useWeb3 } from "@3rdweb-sdk/react";
-import { Flex } from "@chakra-ui/react";
+import { Flex, Icon } from "@chakra-ui/react";
 import { useTrack } from "hooks/analytics/useTrack";
+import { IoRocketOutline } from "react-icons/io5";
+import { VscDashboard } from "react-icons/vsc";
 
 interface GeneralCtaProps {
   size?: string;
@@ -15,6 +17,7 @@ export const GeneralCta: React.FC<GeneralCtaProps> = ({ size = "md" }) => {
     <Flex w="100%" justifyContent="center">
       {address ? (
         <LinkButton
+          leftIcon={<Icon as={VscDashboard} />}
           bgColor="primary.500"
           color="white"
           _hover={{ bgColor: "primary.400" }}
@@ -34,9 +37,10 @@ export const GeneralCta: React.FC<GeneralCtaProps> = ({ size = "md" }) => {
         </LinkButton>
       ) : (
         <LinkButton
-          bgColor="primary.500"
+          leftIcon={<Icon as={IoRocketOutline} />}
+          bgColor="purple.500"
           color="white"
-          _hover={{ bgColor: "primary.400" }}
+          _hover={{ bgColor: "purple.600" }}
           px={12}
           onClick={() =>
             trackEvent({
