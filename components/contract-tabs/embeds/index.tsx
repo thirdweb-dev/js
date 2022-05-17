af69677179842caf0194db7755b9ca89bbd89327
Nacho Iacovino <50103937+nachoiacovino@users.noreply.github.com>
2022-05-17 16:35:08 +0200

    Update URI (#123)

--- components/contract-tabs/embeds/index.tsx
+++ components/contract-tabs/embeds/index.tsx
@@ -34,7 +34,7 @@ interface EmbedSetupProps {
   contract?: ValidContractInstance;
 }
 
-const IPFS_URI = "ipfs://Qma17oTrNVQqPTg8v3E5UvHYFbXjbgeqHCocDZtPyUopLX";
+const IPFS_URI = "ipfs://QmWadjbCefyTnh8DMNBJyvZiVhXmFvrFbKzYqzY7WAGEHf";
 
 const getContractEmbedHash = (contract?: ValidContractInstance) => {
   if (contract instanceof NFTDrop) {
