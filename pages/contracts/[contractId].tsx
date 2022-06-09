4f9ec70b2fb381b5b62246cf674d5d5e64d11753
Jonas Daniels <jonas.daniels@outlook.com>
2022-06-09 00:38:03 -0700

    remove suggested extensions (#182)

--- pages/contracts/[contractId].tsx
+++ pages/contracts/[contractId].tsx
@@ -1,10 +1,6 @@
 import {
-  Accordion,
-  AccordionButton,
-  AccordionIcon,
-  AccordionItem,
-  AccordionPanel,
   Box,
+  Center,
   Divider,
   Flex,
   Icon,
@@ -20,27 +16,13 @@ import { ChakraNextImage } from "components/Image";
 import { AppLayout } from "components/app-layouts/app";
 import { ContractDeployForm } from "components/contract-components/contract-deploy-form";
 import { useContractPublishMetadataFromURI } from "components/contract-components/hooks";
-import { SnippetApiResponse } from "components/contract-tabs/code/types";
 import { FeatureIconMap } from "constants/mappings";
 import { useTrack } from "hooks/analytics/useTrack";
 import { useSingleQueryParam } from "hooks/useQueryParam";
 import { useRouter } from "next/router";
 import { ReactElement, useMemo } from "react";
-import {
-  FiArrowLeft,
-  FiCheckCircle,
-  FiExternalLink,
-  FiInfo,
-} from "react-icons/fi";
-import { useQuery } from "react-query";
-import {
-  Card,
-  CodeBlock,
-  Heading,
-  LinkButton,
-  Text,
-  TrackedLink,
-} from "tw-components";
+import { FiArrowLeft, FiCheckCircle, FiExternalLink } from "react-icons/fi";
+import { Card, Heading, LinkButton, Text, TrackedLink } from "tw-components";
 
 const ALWAYS_SUGGESTED = ["ContractMetadata", "Permissions"];
 
@@ -112,13 +94,13 @@ export default function ContractDetailPage() {
   }, [contractId]);
   const features = useContractFeatures(publishMetadataQuery.data?.abi);
 
-  const [enabledFeatures, suggestedFeatures] = useMemo(() => {
+  const [enabledFeatures] = useMemo(() => {
     if (!features) {
-      return [[], []];
+      return [[]];
     }
     const enabled = features.enabledFeatures;
-    const suggested = features.suggestedFeatures;
-    return [enabled, suggested] as const;
+
+    return [enabled] as const;
   }, [features]);
 
   return (
@@ -211,34 +193,16 @@ export default function ContractDetailPage() {
               </Card>
             </Flex>
           )}
-          {suggestedFeatures.length > 0 && (
-            <Flex gap={4} direction="column" as="section">
-              <Box>
-                <Heading size="subtitle.lg">Suggested Extensions</Heading>
-                <Text>
-                  These extensions would likely be useful for this contract, but
-                  are not being implemented.
-                </Text>
-              </Box>
-
-              <Accordion
-                allowToggle
-                allowMultiple
-                defaultIndex={[0]}
-                display="flex"
-                flexDir="column"
-                gap={6}
-              >
-                {suggestedFeatures.map((feature) => (
-                  <SuggestedFeature
-                    key={feature.name}
-                    contractName={publishMetadataQuery.data?.name}
-                    feature={feature}
-                  />
-                ))}
-              </Accordion>
-            </Flex>
-          )}
+          <Center>
+            <LinkButton
+              size="sm"
+              variant="outline"
+              isExternal
+              href="https://portal.thirdweb.com/thirdweb-deploy/contract-extensions"
+            >
+              Learn about thirdweb extensions
+            </LinkButton>
+          </Center>
         </Flex>
       </Flex>
     </Track>
@@ -287,95 +251,3 @@ const EnabledFeature: React.FC<EnabledFeatureProps> = ({ feature }) => {
     </Card>
   );
 };
-
-interface SuggestedFeatureProps {
-  feature: FeatureWithEnabled;
-  contractName?: string;
-}
-
-const SuggestedFeature: React.FC<SuggestedFeatureProps> = ({
-  feature,
-  contractName = "YourContract",
-}) => {
-  const { trackEvent } = useTrack();
-  const codeSnippets = useFeatureCodeSnippet();
-
-  const featureDetails = useMemo(() => {
-    return codeSnippets.data ? codeSnippets.data[feature.name] : undefined;
-  }, [codeSnippets.data, feature.name]);
-
-  const contractFeatureName = feature.docLinks.contracts;
-  const contractFeaturePath = contractFeatureName.startsWith("I")
-    ? "feature/interface"
-    : "feature";
-
-  return (
-    <Card p={0} overflow="hidden">
-      <AccordionItem border="none">
-        <AccordionButton
-          p={4}
-          justifyContent="space-between"
-          _focus={{
-            boxShadow: "none",
-          }}
-          _expanded={{
-            bg: "blackAlpha.50",
-          }}
-        >
-          <Flex gap={2} align="center" justify="space-between">
-            <Flex gap={2} align="center">
-              <Icon boxSize={4} color="blue.500" as={FiInfo} />
-              <Heading textAlign="left" size="subtitle.sm">
-                {feature.name}
-              </Heading>
-            </Flex>
-          </Flex>
-          <AccordionIcon />
-        </AccordionButton>
-
-        <AccordionPanel>
-          {featureDetails && (
-            <Flex direction="column" gap={2}>
-              <Flex gap={4} justify="space-between" align="center">
-                <Heading size="label.md">{featureDetails.summary}</Heading>
-                <LinkButton
-                  size="sm"
-                  href={`https://portal.thirdweb.com/contracts/${feature.docLinks.contracts}`}
-                  isExternal
-                  variant="ghost"
-                  borderRadius="md"
-                  onClick={() =>
-                    trackEvent({
-                      category: "extensions-deploy",
-                      action: "click",
-                      label: feature.name,
-                    })
-                  }
-                >
-                  Learn more
-                </LinkButton>
-              </Flex>
-              <Flex mt={2} gap={3} direction="column">
-                <CodeBlock
-                  language="solidity"
-                  code={`import "@thirdweb-dev/contracts/${contractFeaturePath}/${contractFeatureName}.sol";
-
-contract ${contractName} is ${contractFeatureName} { ... }`}
-                />
-              </Flex>
-            </Flex>
-          )}
-        </AccordionPanel>
-      </AccordionItem>
-    </Card>
-  );
-};
-
-function useFeatureCodeSnippet() {
-  return useQuery(["feature-snippets"], async () => {
-    const res = await fetch(
-      `https://raw.githubusercontent.com/thirdweb-dev/typescript-sdk/main/docs/feature_snippets.json`,
-    );
-    return (await res.json()) as SnippetApiResponse;
-  });
-}
