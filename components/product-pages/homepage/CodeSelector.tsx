7fc1eaaff33a4b5e9a5dfe60808c2b4ecc6058b4
Nacho Iacovino <50103937+nachoiacovino@users.noreply.github.com>
2022-07-26 23:57:05 +0200

    Replit replacement (#303)
    * Working version
    
    * Update buttons
    
    * Update lock
    
    * Fix border

--- components/product-pages/homepage/CodeSelector.tsx
+++ components/product-pages/homepage/CodeSelector.tsx
@@ -1,19 +1,13 @@
 import {
-  AspectRatio,
-  AspectRatioProps,
   Box,
-  Center,
   Flex,
   Icon,
-  PropsOf,
   SimpleGrid,
-  Spinner,
-  chakra,
   useBreakpointValue,
 } from "@chakra-ui/react";
-import useIntersectionObserver from "@react-hook/intersection-observer";
 import { useTrack } from "hooks/analytics/useTrack";
-import { Dispatch, SetStateAction, useRef, useState } from "react";
+import { Language } from "prism-react-renderer";
+import { Dispatch, SetStateAction, useState } from "react";
 import { flushSync } from "react-dom";
 import {
   SiGo,
@@ -25,6 +19,7 @@ import {
 import {
   Button,
   ButtonProps,
+  CodeBlock,
   LinkButton,
   PossibleButtonSize,
 } from "tw-components";
@@ -48,9 +43,68 @@ const LOGO_OPTIONS = {
   },
 } as const;
 
-export type CodeOptions = keyof typeof LOGO_OPTIONS;
+const codeSnippets = {
+  typescript: `import { ThirdwebSDK } from "@thirdweb-dev/sdk";
 
-const isReplitEnabled = true;
+const sdk = new ThirdwebSDK("rinkeby");
+const nftCollection = sdk.getNFTCollection("0xb1c42E0C4289E68f1C337Eb0Da6a38C4c9F3f58e");
+
+const nfts = await nftCollection.getAll();
+console.log(nfts);`,
+  react: `import {
+  ThirdwebNftMedia,
+  useNFTCollection,
+  useNFTs,
+} from "@thirdweb-dev/react";
+
+export default function App() {
+  const nftCollection = useNFTCollection(
+    "0xb1c42E0C4289E68f1C337Eb0Da6a38C4c9F3f58e",
+  );
+  const { data: nfts } = useNFTs(nftCollection);
+
+  return (nfts || []).map((nft) => (
+    <div key={nft.metadata.id.toString()}>
+      <ThirdwebNftMedia metadata={nft.metadata} />
+      <h3>{nft.metadata.name}</h3>
+    </div>
+  ));
+}`,
+  python: `from thirdweb import ThirdwebSDK
+from pprint import pprint
+
+sdk = ThirdwebSDK("rinkeby")
+
+nftCollection = sdk.get_nft_collection(
+    "0xb1c42E0C4289E68f1C337Eb0Da6a38C4c9F3f58e")
+
+nfts = nftCollection.get_all()
+pprint(nfts)
+  `,
+  go: `package main
+
+import (
+  "encoding/json"
+  "fmt"
+  "github.com/thirdweb-dev/go-sdk/thirdweb"
+)
+
+func main() {
+  sdk, _ := thirdweb.NewThirdwebSDK("rinkeby", nil)
+
+  // Add your NFT Collection contract address here
+  address := "0xb1c42E0C4289E68f1C337Eb0Da6a38C4c9F3f58e"
+  nft, _ := sdk.GetNFTCollection(address)
+
+  // Now you can use any of the read-only SDK contract functions
+  nfts, _ := nft.GetAll()
+
+  b, _ := json.MarshalIndent(nfts, "", "  ")
+  fmt.Printf(string(b))
+}`,
+};
+
+export type CodeOptions = keyof typeof LOGO_OPTIONS;
 
 interface CodeOptionButtonProps extends ButtonProps {
   language: CodeOptions;
@@ -145,109 +199,70 @@ export const CodeSelector: React.FC = () => {
           Go
         </CodeOptionButton>
       </SimpleGrid>
-      {isReplitEnabled ? (
-        <LazyLoadedIframe
-          aspectRatioProps={{
-            ratio: { base: 9 / 16, md: 16 / 9 },
-            w: "full",
-            borderRadius: "xl",
-            overflow: "hidden",
-            border: "2px solid",
-            borderColor: "#4953AF",
-            bg: "#1C2333",
-          }}
-          frameBorder="0"
-          width="1200px"
-          height="800px"
-          loading="lazy"
-          src={`https://replit.com/@thirdweb-dev/${activeLanguage}-sdk?lite=true`}
-        />
-      ) : (
-        <Flex
-          gap={{ base: 4, md: 12 }}
-          align="center"
-          direction={{ base: "column", md: "row" }}
-          w="100%"
-          maxW="container.sm"
-        >
-          <LinkButton
-            role="group"
-            borderRadius="md"
-            p={6}
-            variant="gradient"
-            fromcolor="#1D64EF"
-            tocolor="#E0507A"
-            // flexShrink={0}
-            isExternal
-            // variant="solid"
-            colorScheme="primary"
-            w="full"
-            // maxW="300px"
-            href={`https://replit.com/@thirdweb-dev/${activeLanguage}-sdk`}
-            leftIcon={
-              <Icon
-                color="#1D64EF"
-                _groupHover={{ color: "#E0507A" }}
-                as={SiReplit}
-              />
-            }
-          >
-            <Box as="span">Try it on repl.it</Box>
-          </LinkButton>
-        </Flex>
-      )}
-      <LinkButton
-        variant="outline"
-        borderRadius="md"
-        bg="#fff"
-        color="#000"
-        w="full"
-        maxW="container.sm"
-        _hover={{
-          bg: "whiteAlpha.800",
-        }}
-        href="https://portal.thirdweb.com/"
-        isExternal
-        p={6}
-        onClick={() =>
-          trackEvent({
-            category: "code-selector",
-            action: "click",
-            label: "documentation",
-          })
+
+      <CodeBlock
+        w={{ base: "full", md: "80%" }}
+        borderColor="#4953AF"
+        borderWidth="2px"
+        py={4}
+        code={codeSnippets[activeLanguage]}
+        language={
+          activeLanguage === "react" ? "jsx" : (activeLanguage as Language)
         }
+      />
+
+      <Flex
+        gap={{ base: 4, md: 6 }}
+        align="center"
+        direction={{ base: "column", md: "row" }}
+        w="100%"
+        maxW="container.sm"
       >
-        Explore documentation
-      </LinkButton>
+        <LinkButton
+          role="group"
+          borderRadius="md"
+          p={6}
+          variant="gradient"
+          fromcolor="#1D64EF"
+          tocolor="#E0507A"
+          isExternal
+          colorScheme="primary"
+          w="full"
+          href={`https://replit.com/@thirdweb-dev/${activeLanguage}-sdk`}
+          rightIcon={
+            <Icon
+              color="#E0507A"
+              _groupHover={{ color: "#1D64EF" }}
+              as={SiReplit}
+            />
+          }
+        >
+          <Box as="span">Try it on Replit</Box>
+        </LinkButton>
+        <LinkButton
+          variant="outline"
+          borderRadius="md"
+          bg="#fff"
+          color="#000"
+          w="full"
+          maxW="container.sm"
+          _hover={{
+            bg: "whiteAlpha.800",
+          }}
+          href="https://portal.thirdweb.com/"
+          isExternal
+          p={6}
+          onClick={() =>
+            trackEvent({
+              category: "code-selector",
+              action: "click",
+              label: "documentation",
+            })
+          }
+        >
+          Explore documentation
+        </LinkButton>
+      </Flex>
     </>
-  );
-};
-
-const ChakraIframe = chakra("iframe");
-
-type LazyLoadedIframeProps = PropsOf<typeof ChakraIframe> & {
-  aspectRatioProps?: AspectRatioProps;
-};
-
-export const LazyLoadedIframe: React.FC<LazyLoadedIframeProps> = ({
-  aspectRatioProps,
-  ...iframeProps
-}) => {
-  const containerRef = useRef<HTMLDivElement>(null);
-  const lockRef = useRef(false);
-  const { isIntersecting } = useIntersectionObserver(containerRef);
-  if (isIntersecting) {
-    lockRef.current = true;
-  }
-  return (
-    <AspectRatio {...aspectRatioProps} ref={containerRef}>
-      {lockRef.current ? (
-        <ChakraIframe {...iframeProps} />
-      ) : (
-        <Center>
-          <Spinner color="heading" />
-        </Center>
-      )}
-    </AspectRatio>
   );
 };
