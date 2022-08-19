c48d18a0ac7a68f179e7ae1bc542b089ecb8685e
Jonas Daniels <jonas.daniels@outlook.com>
2022-08-18 19:04:14 -0700

    fix masked avatar being low quality (#400)

--- components/contract-components/releaser/masked-avatar.tsx
+++ components/contract-components/releaser/masked-avatar.tsx
@@ -1,76 +1,35 @@
 import { ens, useReleaserProfile } from "../hooks";
-import { Box, BoxProps } from "@chakra-ui/react";
-import { useMemo } from "react";
+import hexagon from "./hexagon.png";
+import { Img, ImgProps } from "@chakra-ui/react";
+import "react";
 
 interface MaskedAvatarProps
-  extends Omit<BoxProps, "as" | "viewBox" | "boxSize"> {
+  extends Omit<ImgProps, "as" | "viewBox" | "boxSize"> {
   src: string;
+  name?: string;
   boxSize?: number;
 }
 
 export const MaskedAvatar: React.FC<MaskedAvatarProps> = ({
   src,
+  name,
   boxSize = 12,
   ...restBoxProps
 }) => {
-  const stdDeviation = useMemo(() => {
-    let d = boxSize / 5;
-    if (d < 2) {
-      d = 2;
-    }
-    if (d > 5) {
-      d = 5;
-    }
-    return d;
-  }, [boxSize]);
-
   return (
-    <>
-      <Box
-        boxSize={boxSize}
-        {...restBoxProps}
-        filter="url(#round)"
-        display="grid"
-        _before={{
-          content: `""`,
-          display: "block",
-          my: "auto",
-          pt: "86.6%",
-          background: `url(${src})`,
-          backgroundSize: "cover",
-          backgroundRepeat: "no-repeat",
-          backgroundPosition: "center",
-          clipPath:
-            "polygon(25% 0%, 75% 0%, 100% 50%, 75% 100%, 25% 100%, 0% 50%)",
-        }}
-      />
-      <Box
-        as="svg"
-        visibility="hidden"
-        position="absolute"
-        width="0"
-        height="0"
-        xmlns="http://www.w3.org/2000/svg"
-        version="1.1"
-      >
-        <defs>
-          <filter id="round">
-            <feGaussianBlur
-              in="SourceGraphic"
-              stdDeviation={stdDeviation}
-              result="blur"
-            />
-            <feColorMatrix
-              in="blur"
-              mode="matrix"
-              values="1 0 0 0 0  0 1 0 0 0  0 0 1 0 0  0 0 0 19 -9"
-              result="goo"
-            />
-            <feComposite in="SourceGraphic" in2="goo" operator="atop" />
-          </filter>
-        </defs>
-      </Box>
-    </>
+    <Img
+      boxSize={boxSize}
+      objectFit="contain"
+      {...restBoxProps}
+      style={{
+        WebkitMaskImage: `url("${hexagon.src}")`,
+        WebkitMaskSize: "cover",
+        mask: `url("${hexagon.src}")`,
+        maskSize: "cover",
+      }}
+      src={src}
+      alt={name || ""}
+    />
   );
 };
 
