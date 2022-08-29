f7480b5a155e2834bd5eb7efe0e17fa7afc6db4f
Jonas Daniels <jonas.daniels@outlook.com>
2022-08-29 01:02:41 -0700

    run the nightly ci release directly in the scripts

--- release-nightly.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-
-# THIS IS MEANT FOR CI ONLY
-# This script is used to build the release packages for the nightly builds.
-
-git checkout -b release-nightly
-yarn changeset version --snapshot nightly
-yarn changeset publish --tag nightly --no-git-tag
