55ebd3f4e8335c92915cb8f6f65097f0a275db38
Joaquim Verges <joaquim.verges@gmail.com>
2022-08-30 16:39:12 -0700

    use latest stable solaba version

--- packages/solana/scripts/install_solana.sh
+++ packages/solana/scripts/install_solana.sh
@@ -2,6 +2,6 @@
 
 set -e
 
-sh -c "$(curl -sSfL https://release.solana.com/v1.10.35/install)"
+sh -c "$(curl -sSfL https://release.solana.com/stable/install)"
 export PATH="/home/runner/.local/share/solana/install/active_release/bin:$PATH"
 solana --version
