1e1135d4d085e400addae2e713eab07f96730dfa
Joaquim Verges <joaquim.verges@gmail.com>
2022-08-30 16:42:40 -0700

    move everything to script

--- packages/solana/scripts/run_tests.sh
+++ packages/solana/scripts/run_tests.sh
@@ -5,3 +5,6 @@ set -e
 sh -c "$(curl -sSfL https://release.solana.com/stable/install)"
 export PATH="/home/runner/.local/share/solana/install/active_release/bin:$PATH"
 solana --version
+yarn run amman:start
+yarn run test:all
+yarn run amman:stop
