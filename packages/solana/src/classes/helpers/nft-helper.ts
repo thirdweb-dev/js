814e8a1280f2e78077a5d77b5972afb916f48cf4
adam-maj <mr.adam.maj@gmail.com>
2022-09-09 13:42:02 -0700

    Update nft helper

--- packages/solana/src/classes/helpers/nft-helper.ts
+++ packages/solana/src/classes/helpers/nft-helper.ts
@@ -1,4 +1,3 @@
-import { METAPLEX_PROGRAM_ID } from "../../constants/addresses";
 import { TransactionResult } from "../../types/common";
 import { NFTMetadata } from "../../types/nft";
 import {
@@ -9,9 +8,8 @@ import {
   SftWithToken,
   token,
 } from "@metaplex-foundation/js";
-import { Metadata } from "@metaplex-foundation/mpl-token-metadata";
 import { getAccount, getAssociatedTokenAddress } from "@solana/spl-token";
-import { ConfirmedSignatureInfo, Connection, PublicKey } from "@solana/web3.js";
+import { Connection, PublicKey } from "@solana/web3.js";
 
 export class NFTHelper {
   private metaplex: Metaplex;
@@ -31,89 +29,6 @@ export class NFTHelper {
       .run();
 
     return this.toNFTMetadata(meta);
-  }
-
-  async getAll(collectionMintAddress: string): Promise<string[]> {
-    const allSignatures: ConfirmedSignatureInfo[] = [];
-    // This returns the first 1000, so we need to loop through until we run out of signatures to get.
-    let signatures = await this.connection.getSignaturesForAddress(
-      new PublicKey(collectionMintAddress),
-    );
-
-    allSignatures.push(...signatures);
-    do {
-      const options = {
-        before: signatures[signatures.length - 1]?.signature,
-      };
-      signatures = await this.connection.getSignaturesForAddress(
-        new PublicKey(collectionMintAddress),
-        options,
-      );
-      allSignatures.push(...signatures);
-    } while (signatures.length > 0);
-
-    const metadataAddresses: PublicKey[] = [];
-    const mintAddresses = new Set<string>();
-
-    // TODO RPC's will throttle this, need to do some optimizations here
-    const batchSize = 1000; // alchemy RPC batch limit
-    for (let i = 0; i < allSignatures.length; i += batchSize) {
-      const batch = allSignatures.slice(
-        i,
-        Math.min(allSignatures.length, i + batchSize),
-      );
-
-      const transactions = await this.connection.getTransactions(
-        batch.map((s) => s.signature),
-      );
-
-      for (const tx of transactions) {
-        if (tx) {
-          const programIds = tx.transaction.message
-            .programIds()
-            .map((p) => p.toString());
-          const accountKeys = tx.transaction.message.accountKeys.map((p) =>
-            p.toString(),
-          );
-          // Only look in transactions that call the Metaplex token metadata program
-          if (programIds.includes(METAPLEX_PROGRAM_ID)) {
-            // Go through all instructions in a given transaction
-            for (const ix of tx.transaction.message.instructions) {
-              // Filter for setAndVerify or verify instructions in the Metaplex token metadata program
-              if (
-                (ix.data === "K" || ix.data === "S" || ix.data === "X") &&
-                accountKeys[ix.programIdIndex] === METAPLEX_PROGRAM_ID
-              ) {
-                const metadataAddressIndex = ix.accounts[0];
-                const metadata_address =
-                  tx.transaction.message.accountKeys[metadataAddressIndex];
-                metadataAddresses.push(metadata_address);
-              }
-            }
-          }
-        }
-      }
-    }
-
-    const metadataAccounts = await Promise.all(
-      metadataAddresses.map((a) => {
-        try {
-          return this.connection.getAccountInfo(a);
-        } catch (e) {
-          console.log(e);
-          return undefined;
-        }
-      }),
-    );
-
-    for (const account of metadataAccounts) {
-      if (account) {
-        const [metadata] = Metadata.deserialize(account.data);
-        mintAddresses.add(metadata.mint.toBase58());
-      }
-    }
-
-    return Array.from(mintAddresses);
   }
 
   async transfer(
--- packages/solana/src/contracts/nft-collection.ts
+++ packages/solana/src/contracts/nft-collection.ts
@@ -1,5 +1,6 @@
 import { NFTHelper } from "../classes/helpers/nft-helper";
 import { UserWallet } from "../classes/user-wallet";
+import { METAPLEX_PROGRAM_ID } from "../constants/addresses";
 import { TransactionResult } from "../types/common";
 import {
   NFTCollectionMetadata,
@@ -7,7 +8,8 @@ import {
   NFTMetadataInput,
 } from "../types/nft";
 import { Metaplex } from "@metaplex-foundation/js";
-import { PublicKey } from "@solana/web3.js";
+import { Metadata } from "@metaplex-foundation/mpl-token-metadata";
+import { ConfirmedSignatureInfo, PublicKey } from "@solana/web3.js";
 import { IStorage } from "@thirdweb-dev/storage";
 
 export class NFTCollection {
@@ -44,7 +46,86 @@ export class NFTCollection {
   }
 
   async getAll(collectionMintAddress: string): Promise<string[]> {
-    return this.nft.getAll(collectionMintAddress);
+    const allSignatures: ConfirmedSignatureInfo[] = [];
+    // This returns the first 1000, so we need to loop through until we run out of signatures to get.
+    let signatures = await this.metaplex.connection.getSignaturesForAddress(
+      new PublicKey(collectionMintAddress),
+    );
+
+    allSignatures.push(...signatures);
+    do {
+      const options = {
+        before: signatures[signatures.length - 1]?.signature,
+      };
+      signatures = await this.metaplex.connection.getSignaturesForAddress(
+        new PublicKey(collectionMintAddress),
+        options,
+      );
+      allSignatures.push(...signatures);
+    } while (signatures.length > 0);
+
+    const metadataAddresses: PublicKey[] = [];
+    const mintAddresses = new Set<string>();
+
+    // TODO RPC's will throttle this, need to do some optimizations here
+    const batchSize = 1000; // alchemy RPC batch limit
+    for (let i = 0; i < allSignatures.length; i += batchSize) {
+      const batch = allSignatures.slice(
+        i,
+        Math.min(allSignatures.length, i + batchSize),
+      );
+
+      const transactions = await this.metaplex.connection.getTransactions(
+        batch.map((s) => s.signature),
+      );
+
+      for (const tx of transactions) {
+        if (tx) {
+          const programIds = tx.transaction.message
+            .programIds()
+            .map((p) => p.toString());
+          const accountKeys = tx.transaction.message.accountKeys.map((p) =>
+            p.toString(),
+          );
+          // Only look in transactions that call the Metaplex token metadata program
+          if (programIds.includes(METAPLEX_PROGRAM_ID)) {
+            // Go through all instructions in a given transaction
+            for (const ix of tx.transaction.message.instructions) {
+              // Filter for setAndVerify or verify instructions in the Metaplex token metadata program
+              if (
+                (ix.data === "K" || ix.data === "S" || ix.data === "X") &&
+                accountKeys[ix.programIdIndex] === METAPLEX_PROGRAM_ID
+              ) {
+                const metadataAddressIndex = ix.accounts[0];
+                const metadata_address =
+                  tx.transaction.message.accountKeys[metadataAddressIndex];
+                metadataAddresses.push(metadata_address);
+              }
+            }
+          }
+        }
+      }
+    }
+
+    const metadataAccounts = await Promise.all(
+      metadataAddresses.map((a) => {
+        try {
+          return this.metaplex.connection.getAccountInfo(a);
+        } catch (e) {
+          console.log(e);
+          return undefined;
+        }
+      }),
+    );
+
+    for (const account of metadataAccounts) {
+      if (account) {
+        const [metadata] = Metadata.deserialize(account.data);
+        mintAddresses.add(metadata.mint.toBase58());
+      }
+    }
+
+    return Array.from(mintAddresses);
   }
 
   async balanceOf(walletAddress: string, mintAddress: string): Promise<bigint> {
