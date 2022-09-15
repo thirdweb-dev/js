ed89fc9027d29cb704364f34305ca57130135f2a
Jarrod Watts <jarrodwatts16@gmail.com>
2022-09-15 21:55:14 +1000

    Change docs urls from /contracts to /extensions

--- packages/cli/src/common/feature-detector.ts
+++ packages/cli/src/common/feature-detector.ts
@@ -70,13 +70,13 @@ export async function detectExtensions(options: any) {
       const enabledFeatures: Feature[] = features.enabledFeatures.map(
         (feature) => ({
           name: feature.name,
-          reference: `https://portal.thirdweb.com/contracts/${feature.docLinks.contracts}`,
+          reference: `https://portal.thirdweb.com/extensions/${feature.name.toLowerCase()}`,
         }),
       );
       const suggestedFeatures: Feature[] = features.suggestedFeatures.map(
         (feature) => ({
           name: feature.name,
-          reference: `https://portal.thirdweb.com/contracts/${feature.docLinks.contracts}`,
+          reference: `https://portal.thirdweb.com/extensions/${feature.name.toLowerCase()}`,
         }),
       );
 
