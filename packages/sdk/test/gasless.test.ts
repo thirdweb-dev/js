3b877ba221acfd85f80b99e1bc382055217f0a39
Yash <72552910+kumaryash90@users.noreply.github.com>
2022-09-22 10:17:00 +0530

    [SDK] Ozdefender EOA forwarder option (#174)
    Co-authored-by: Joaquim Verges <joaquim.verges@gmail.com>

--- packages/sdk/test/gasless.test.ts
+++ packages/sdk/test/gasless.test.ts
@@ -24,14 +24,21 @@ describe("Gasless Forwarder", async () => {
   });
 
   it.skip("should use sdk with openzeppelin defender", async () => {
-    // const provider = ethers.getDefaultProvider(RPC_URL);
-    // const wallet = Wallet.createRandom().connect(provider);
-    // const sdk = new ThirdwebSDK(wallet, {
-    //   gasless: {
-    //     openzeppelin: {
-    //       relayerUrl: process.env.OZ_DEFENDER_RELAYER_URL as string,
-    //     },
-    //   },
-    // });
+    const NFT_COLLECTION_ADDRESS = "0x24A5aB2878B63716B001aa9AbE816c2662192B12";
+    const provider = ethers.getDefaultProvider(RPC_URL);
+    const wallet = Wallet.createRandom().connect(provider);
+    const sdk = new ThirdwebSDK(wallet, {
+      gasless: {
+        openzeppelin: {
+          relayerUrl: "<Enter URL>", 
+        },
+      },
+    });
+
+    // const nftCollection = await sdk.getNFTCollection(NFT_COLLECTION_ADDRESS);
+    // await nftCollection.mintTo("0xEBed8e37a32660dbCeeeC19cCBb952b7d214f008", "uri");
+
+    const pack = await sdk.getPack("0x3A85026217Ca7DA710bf42bbD641991632DAB685");
+    await pack.open(0, 0);
   });
 });
