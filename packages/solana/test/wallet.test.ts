9bf3c34348970632f970e978c54398d1cb5ed887
Joaquim Verges <joaquim.verges@gmail.com>
2022-09-23 18:50:13 -0700

    add assert in test

--- packages/solana/test/wallet.test.ts
+++ packages/solana/test/wallet.test.ts
@@ -1,12 +1,9 @@
-import { UserWallet } from "../../sdk/src";
 import { sdk } from "./before-setup";
-import { Keypair } from "@solana/web3.js";
 import { expect } from "chai";
 
 describe("Wallet", async () => {
   it("balance", async () => {
     const balance = await sdk.wallet.getBalance();
-    console.log("balance", balance);
-    //expect(balance.displayValue).to.eq("My Token");
+    expect(balance.displayValue).to.eq("100.000000000");
   });
 });
