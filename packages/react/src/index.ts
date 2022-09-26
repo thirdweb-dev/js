5ce2542f10f19ad26626444de56dad3f5ec7a1ae
Jonas Daniels <jonas.daniels@outlook.com>
2022-09-26 15:44:43 -0700

    ethereum

--- packages/react/src/index.ts
+++ packages/react/src/index.ts
@@ -1,2 +1,2 @@
-// at the moment we'll re-export everything from the evm package (only!)
-export * from "./evm";
+// at the moment we'll re-export everything from the ethereum package (only!)
+export * from "./ethereum";
