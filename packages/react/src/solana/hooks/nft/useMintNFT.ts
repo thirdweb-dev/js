196195e2f9368eddad2979d05dd8de73a1aa08fd
Jonas Daniels <jonas.daniels@outlook.com>
2022-09-27 03:27:32 -0700

    [React] - fix small discrepancies (#215)

--- packages/react/src/solana/hooks/nft/useMintNFT.ts
+++ packages/react/src/solana/hooks/nft/useMintNFT.ts
@@ -1,5 +1,6 @@
 import { createSOLProgramQueryKey } from "../../../core/query-utils/query-key";
 import { RequiredParam } from "../../../core/types/shared";
+import { MintNFTParams } from "../../../evm/types";
 import { useMutation, useQueryClient } from "@tanstack/react-query";
 import type { NFTCollection, NFTMetadataInput } from "@thirdweb-dev/solana";
 import invariant from "tiny-invariant";
@@ -7,9 +8,10 @@ import invariant from "tiny-invariant";
 export function useMintNFT(program: RequiredParam<NFTCollection>) {
   const queryClient = useQueryClient();
   return useMutation(
-    async (metadata: NFTMetadataInput) => {
+    async (data: MintNFTParams) => {
       invariant(program, "program is required");
-      return await program.mint(metadata);
+      // TODO fix this once consolidated
+      return await program.mintTo(data.to, data.metadata as NFTMetadataInput);
     },
     {
       onSettled: () =>
