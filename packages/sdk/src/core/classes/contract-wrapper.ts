acbabc8999b2b7b6e0eb89cae97e2f1ef7143501
Yash <72552910+kumaryash90@users.noreply.github.com>
2022-09-27 19:05:29 +0530

    [SDK] - Relayer gas override (#206)
    Co-authored-by: Jarrod Watts <35651410+jarrodwatts@users.noreply.github.com>

--- packages/sdk/src/core/classes/contract-wrapper.ts
+++ packages/sdk/src/core/classes/contract-wrapper.ts
@@ -450,6 +450,14 @@ export class ContractWrapper<
       gas = BigNumber.from(500000);
     }
 
+    // check for gas override in callOverrides
+    if (
+      callOverrides.gasLimit &&
+      BigNumber.from(callOverrides.gasLimit).gt(gas)
+    ) {
+      gas = BigNumber.from(callOverrides.gasLimit);
+    }
+
     const tx: GaslessTransaction = {
       from,
       to,
@@ -637,10 +645,14 @@ export class ContractWrapper<
     invariant(signer, "provider is not set");
     invariant(provider, "provider is not set");
     const forwarderAddress =
-      this.options.gasless.openzeppelin.relayerForwarderAddress 
-      || (this.options.gasless.openzeppelin.useEOAForwarder 
-          ? CONTRACT_ADDRESSES[transaction.chainId as keyof typeof CONTRACT_ADDRESSES].openzeppelinForwarderEOA 
-          : CONTRACT_ADDRESSES[transaction.chainId as keyof typeof CONTRACT_ADDRESSES].openzeppelinForwarder);
+      this.options.gasless.openzeppelin.relayerForwarderAddress ||
+      (this.options.gasless.openzeppelin.useEOAForwarder
+        ? CONTRACT_ADDRESSES[
+            transaction.chainId as keyof typeof CONTRACT_ADDRESSES
+          ].openzeppelinForwarderEOA
+        : CONTRACT_ADDRESSES[
+            transaction.chainId as keyof typeof CONTRACT_ADDRESSES
+          ].openzeppelinForwarder);
 
     const forwarder = new Contract(forwarderAddress, ForwarderABI, provider);
     const nonce = await getAndIncrementNonce(forwarder, "getNonce", [
