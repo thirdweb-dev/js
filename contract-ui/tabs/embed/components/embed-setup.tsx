5c8d1c3154dadbadeaf33dd1716604a093aa5c62
Nacho Iacovino <50103937+nachoiacovino@users.noreply.github.com>
2022-09-29 20:12:12 +0200

    Fix maxClaimable for erc1155 embed (#521)

--- contract-ui/tabs/embed/components/embed-setup.tsx
+++ contract-ui/tabs/embed/components/embed-setup.tsx
@@ -30,7 +30,7 @@ interface EmbedSetupProps {
   contractType?: string | null;
 }
 
-const IPFS_URI = "ipfs://QmbHM2tUJPsDxKo7wWGbu9Sca4Y8FRmtnJVNY2Mfots2yQ";
+const IPFS_URI = "ipfs://QmPaVYdGue8zEXFKqrtVHpvzBvufM1DYzw5n1of3KVPG88";
 
 interface IframeSrcOptions {
   rpcUrl: string;
