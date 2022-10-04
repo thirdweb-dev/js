3bad26dd20c214872b2121c83decbbb200bbd8cf
Jonas Daniels <jonas.daniels@outlook.com>
2022-10-04 10:05:14 -0700

    [React] - enable permissions enumerable feature

---
+++ .changeset/ninety-panthers-reflect.md
@@ -0,0 +1,5 @@
+---
+"@thirdweb-dev/sdk": patch
+---
+
+[EVM] - make PermissiosEnumerable feature available
--- packages/sdk/src/evm/constants/thirdweb-features.ts
+++ packages/sdk/src/evm/constants/thirdweb-features.ts
@@ -1,6 +1,7 @@
 import IAppURI from "@thirdweb-dev/contracts-js/dist/abis/IAppURI.json";
 import IContractMetadataAbi from "@thirdweb-dev/contracts-js/dist/abis/IContractMetadata.json";
 import IPermissionsAbi from "@thirdweb-dev/contracts-js/dist/abis/IPermissions.json";
+import IPermissionsEnumerableAbi from "@thirdweb-dev/contracts-js/dist/abis/IPermissionsEnumerable.json";
 import IThirdwebPlatformFeeAbi from "@thirdweb-dev/contracts-js/dist/abis/IPlatformFee.json";
 import IThirdwebPrimarySaleAbi from "@thirdweb-dev/contracts-js/dist/abis/IPrimarySale.json";
 import IThirdwebRoyaltyAbi from "@thirdweb-dev/contracts-js/dist/abis/IRoyalty.json";
@@ -44,9 +45,20 @@ export const FEATURE_PERMISSIONS = {
   namespace: "roles",
   docLinks: {
     sdk: "sdk.contractroles",
-    contracts: "PermissionsEnumerable",
+    contracts: "Permissions",
   },
   abis: [IPermissionsAbi],
+  features: {},
+} as const;
+
+export const FEATURE_PERMISSIONS_ENUMERABLE = {
+  name: "PermissionsEnumerable",
+  namespace: "roles",
+  docLinks: {
+    sdk: "sdk.contractroles",
+    contracts: "PermissionsEnumerable",
+  },
+  abis: [IPermissionsEnumerableAbi],
   features: {},
 } as const;
 
