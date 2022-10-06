76593b72bc35aa699e1e08f121a2983816981317
Jonas Daniels <jonas.daniels@outlook.com>
2022-10-06 00:35:47 -0700

    blow away the global cache

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -26,7 +26,7 @@ import React, {
 import { generateBreakpointTypographyCssVars } from "tw-components/utils/typography";
 import { isBrowser } from "utils/isBrowser";
 
-const __CACHE_BUSTER = "v3.0.0-0";
+const __CACHE_BUSTER = "v3.1.0";
 
 export function bigNumberReplacer(_key: string, value: any) {
   // if we find a BigNumber then make it into a string (since that is safe)
