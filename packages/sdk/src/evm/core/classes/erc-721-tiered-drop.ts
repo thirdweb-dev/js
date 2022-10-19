252de2220f2e848b19773bbdc11f2f39b063d32c
adam-maj <mr.adam.maj@gmail.com>
2022-10-19 11:26:36 -0700

    Add lazy mint with tier function

--- packages/sdk/src/evm/core/classes/erc-721-tiered-drop.ts
+++ packages/sdk/src/evm/core/classes/erc-721-tiered-drop.ts
@@ -1,18 +1,30 @@
-import { TransactionResultWithId } from "..";
+import { Erc721, TransactionResultWithId } from "..";
 import { NFTMetadata, NFTMetadataOrUri } from "../../../core/schema/nft";
+import { getBaseUriFromBatch, uploadOrExtractURIs } from "../../common/nft";
 import { FEATURE_NFT_TIERED_DROP } from "../../constants/erc721-features";
 import { UploadProgressEvent } from "../../types/events";
 import { DetectableFeature } from "../interfaces/DetectableFeature";
 import { ContractWrapper } from "./contract-wrapper";
 import type { LazyMintWithTier } from "@thirdweb-dev/contracts-js";
+import { TokensLazyMintedEvent } from "@thirdweb-dev/contracts-js/dist/declarations/src/TieredDrop";
+import { ThirdwebStorage } from "@thirdweb-dev/storage";
+import { ethers } from "ethers";
 
 export class Erc721TieredDrop implements DetectableFeature {
   featureName = FEATURE_NFT_TIERED_DROP.name;
 
   private contractWrapper: ContractWrapper<LazyMintWithTier>;
+  private erc721: Erc721;
+  private storage: ThirdwebStorage;
 
-  constructor(contractWrapper: ContractWrapper<LazyMintWithTier>) {
+  constructor(
+    erc721: Erc721,
+    contractWrapper: ContractWrapper<LazyMintWithTier>,
+    storage: ThirdwebStorage,
+  ) {
+    this.erc721 = erc721;
     this.contractWrapper = contractWrapper;
+    this.storage = storage;
   }
 
   public async lazyMintWithTier(
@@ -21,5 +33,38 @@ export class Erc721TieredDrop implements DetectableFeature {
     options?: {
       onProgress: (event: UploadProgressEvent) => void;
     },
-  ): Promise<TransactionResultWithId<NFTMetadata>[]> {}
+  ): Promise<TransactionResultWithId<NFTMetadata>[]> {
+    // TODO: Change this to on extension
+    const startFileNumber = await this.erc721.nextTokenIdToMint();
+    const batch = await uploadOrExtractURIs(
+      metadatas,
+      this.storage,
+      startFileNumber.toNumber(),
+      options,
+    );
+    const baseUri = getBaseUriFromBatch(batch);
+
+    const receipt = await this.contractWrapper.sendTransaction("lazyMint", [
+      batch.length,
+      baseUri.endsWith("/") ? baseUri : `${baseUri}/`,
+      tier,
+      ethers.utils.toUtf8Bytes(""),
+    ]);
+
+    const event = this.contractWrapper.parseLogs<TokensLazyMintedEvent>(
+      "TokensLazyMinted",
+      receipt?.logs,
+    );
+    const startingIndex = event[0].args.startTokenId;
+    const endingIndex = event[0].args.endTokenId;
+    const results: TransactionResultWithId<NFTMetadata>[] = [];
+    for (let id = startingIndex; id.lte(endingIndex); id = id.add(1)) {
+      results.push({
+        id,
+        receipt,
+        data: () => this.erc721.getTokenMetadata(id),
+      });
+    }
+    return results;
+  }
 }
