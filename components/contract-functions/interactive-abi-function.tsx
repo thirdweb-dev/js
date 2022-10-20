65999b6433160e76addc6727272bd634de12789b
Nacho Iacovino <50103937+nachoiacovino@users.noreply.github.com>
2022-10-20 20:27:46 +0200

    Fix bool in contract explorer (#593)
    * Fix bool in contract explorer
    
    * Remove log

--- components/contract-functions/interactive-abi-function.tsx
+++ components/contract-functions/interactive-abi-function.tsx
@@ -202,7 +202,13 @@ export const InteractiveAbiFunction: React.FC<InteractiveAbiFunctionProps> = ({
           if (d.params) {
             mutate(
               formatContractCall(
-                d.params.map((p) => p.value),
+                d.params.map((p) =>
+                  p.type === "bool"
+                    ? p.value === "false"
+                      ? false
+                      : true
+                    : p.value,
+                ),
                 utils.parseEther(d.value),
               ),
             );
