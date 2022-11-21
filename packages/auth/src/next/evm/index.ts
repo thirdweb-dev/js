72589ec234d86939b67b0ca8b3b789f2e9c54cc2
Adam Majmudar <64697628+adam-maj@users.noreply.github.com>
2022-11-21 12:46:18 -0500

    Call user callback in getUser (#397)

--- packages/auth/src/next/evm/index.ts
+++ packages/auth/src/next/evm/index.ts
@@ -70,7 +70,13 @@ export function ThirdwebAuth(cfg: ThirdwebAuthConfig) {
     if (token) {
       try {
         const address = await sdk.auth.authenticate(domain, token);
-        user = { address };
+
+        let data = {};
+        if (ctx.callbacks?.user) {
+          data = await ctx.callbacks.user(address);
+        }
+
+        user = { ...data, address };
       } catch {
         // No-op
       }
