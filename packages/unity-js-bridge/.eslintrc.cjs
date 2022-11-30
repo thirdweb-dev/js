7fae5a072d8b083e837357f4522e4b399e7b67ae
Jonas Daniels <jonas.daniels@outlook.com>
2022-11-29 16:34:20 -0800

    de-escalate the eslint

--- packages/unity-js-bridge/.eslintrc.cjs
+++ packages/unity-js-bridge/.eslintrc.cjs
@@ -1,16 +1,4 @@
 module.exports = {
   root: true,
   extends: ["thirdweb"],
-  rules: {
-    "import/no-cycle": "off",
-  },
-  // allow all imports from within tests
-  overrides: [
-    {
-      files: "./test/**/*",
-      rules: {
-        "@typescript-eslint/no-restricted-imports": "off",
-      },
-    },
-  ],
 };
