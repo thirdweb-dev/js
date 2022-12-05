ee6f2cc0b08e7f71069a8c7c2d42c1b941576111
adam-maj <mr.adam.maj@gmail.com>
2022-12-05 16:44:31 -0500

    Fix linter

--- packages/cli/src/create/helpers/has-forge.ts
+++ packages/cli/src/create/helpers/has-forge.ts
@@ -1,10 +1,10 @@
 import { exec } from "child_process";
 
 export async function hasForge() {
-  // eslint-disable @typescript-eslint/no-unused-vars
-  return await new Promise((resolve, _reject) => {
-    // eslint-disable @typescript-eslint/no-unused-vars
-    exec("forge --version", (error, _stdout, _stderr) => {
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  return await new Promise((resolve, reject) => {
+    // eslint-disable-next-line @typescript-eslint/no-unused-vars
+    exec("forge --version", (error, stdout, stderr) => {
       if (error) {
         resolve(false);
       }
