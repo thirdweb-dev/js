18b38a289f46bb0475cfa9cf4930a3ce72bfed48
Joaquim Verges <joaquim.verges@gmail.com>
2022-12-08 16:02:22 +1300

    improve sending large amount of Txs

--- packages/sdk/src/solana/utils/transactions.ts
+++ packages/sdk/src/solana/utils/transactions.ts
@@ -29,28 +29,43 @@ export async function sendMultipartTransaction(
   // make the connected wallet sign all transactions
   const signedTx = await metaplex.identity().signAllTransactions(txns);
 
-  // send the signed transactions
-  let signatures = [];
-  for (const tx of signedTx) {
-    const signature = await metaplex.connection.sendRawTransaction(
-      tx.serialize(),
+  // send the signed transactions in batches
+  const batchSize = 200; // sends 200 raw tx in parallel
+  const batches = [];
+  for (let i = 0; i < signedTx.length; i += batchSize) {
+    batches.push(signedTx.slice(i, i + batchSize));
+  }
+  let signatures: string[][] = [];
+  for (const txs of batches) {
+    const signature = await Promise.all(
+      txs.map((tx) => metaplex.connection.sendRawTransaction(tx.serialize())),
     );
     signatures.push(signature);
   }
 
-  // wait for confirmations in parallel
-  const confirmations = await Promise.all(
-    signatures.map((sig) => {
-      return metaplex.rpc().confirmTransaction(sig, {
-        blockhash: block.blockhash,
-        lastValidBlockHeight: block.lastValidBlockHeight,
-      });
-    }),
-  );
+  // wait for confirmations in parallel batches
+  let confirmations = [];
+  for (let i = 0; i < signatures.length; i += 1) {
+    await delay(1000); // add delay to avoid rate limiting
+    const sigs = signatures[i];
+    const confirmationBatch = await Promise.all(
+      sigs.map((sig) => {
+        return metaplex.rpc().confirmTransaction(sig, {
+          blockhash: block.blockhash,
+          lastValidBlockHeight: block.lastValidBlockHeight,
+        });
+      }),
+    );
+    confirmations.push(...confirmationBatch);
+  }
 
   if (confirmations.length === 0) {
     throw new Error("Transaction failed");
   }
 
-  return signatures.map((signature) => ({ signature }));
+  return signatures.flatMap((sig) => sig).map((signature) => ({ signature }));
+}
+
+async function delay(delayMs: number) {
+  await new Promise((resolve) => setTimeout(resolve, delayMs));
 }
