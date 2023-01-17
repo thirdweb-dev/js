181268c235d49e13a77c8ca7f52b7cf8f0a0a8d2
Manan Tank <manantankm@gmail.com>
2023-01-18 00:42:29 +0530

    homepage: infinite looping logo carousel (#807)
    * homepage: infinite looping logo carousel
    
    * split partners logo among two rows on mobile
    
    * remove lazy loading from partner logos
    
    * Fix partner logo cut off on safari
    
    * fade-in logo carousel at the container edge

--- components/partners/carousel.tsx
+++ components/partners/carousel.tsx
@@ -1,54 +1,102 @@
 import { PartnerLogo } from "./partner-logo";
-import { Box, Container, GridItem, SimpleGrid } from "@chakra-ui/react";
+import styles from "./partner-logo.module.css";
+import { Box, LayoutProps } from "@chakra-ui/react";
+import React from "react";
+
+const gap = { base: "40px", lg: "60px" };
+
+const MarqueeGroup: React.FC<{
+  ariaHidden: boolean;
+  animationDirection?: "normal" | "reverse";
+  children: React.ReactNode;
+}> = ({ ariaHidden, children, animationDirection = "normal" }) => {
+  return (
+    <Box
+      display="flex"
+      gap={gap}
+      overflow="hidden"
+      flexShrink={0}
+      aria-hidden={ariaHidden}
+      className={styles.marqueeGroup}
+      sx={{
+        animationDirection,
+      }}
+    >
+      {children}
+    </Box>
+  );
+};
+
+const Marquee: React.FC<{
+  children: React.ReactNode;
+  animationDirection?: "normal" | "reverse";
+  display?: LayoutProps["display"];
+}> = ({ children, display, animationDirection }) => {
+  return (
+    <Box gap={gap} overflow="hidden" py={4} display={display}>
+      <MarqueeGroup animationDirection={animationDirection} ariaHidden={false}>
+        {children}
+      </MarqueeGroup>
+      <MarqueeGroup animationDirection={animationDirection} ariaHidden={true}>
+        {children}
+      </MarqueeGroup>
+    </Box>
+  );
+};
 
 export const PartnerCarousel: React.FC = () => {
   return (
-    <Box zIndex={10} position="relative" pointerEvents="none" userSelect="none">
-      <Container position="relative" maxW="container.page">
-        <SimpleGrid
-          columns={{ base: 3, md: 6 }}
-          py={12}
-          px={{ base: 4, md: 0 }}
-          gap={{ base: 8, md: 12 }}
-        >
-          <GridItem colSpan={1}>
-            <PartnerLogo partner="rarible" />
-          </GridItem>
-          <GridItem colSpan={1}>
-            <PartnerLogo partner="fractal" />
-          </GridItem>
-          <GridItem colSpan={1}>
-            <PartnerLogo partner="buildspace" />
-          </GridItem>
-          <GridItem colSpan={1}>
-            <PartnerLogo partner="shopify" />
-          </GridItem>
-          <GridItem colSpan={1}>
-            <PartnerLogo partner="paradigm" />
-          </GridItem>
-          <GridItem colSpan={1}>
-            <PartnerLogo partner="unlock" />
-          </GridItem>
-          <GridItem colSpan={1}>
-            <PartnerLogo partner="minted" />
-          </GridItem>
-          <GridItem colSpan={1}>
-            <PartnerLogo partner="nyfw" />
-          </GridItem>
-          <GridItem colSpan={1}>
-            <PartnerLogo partner="gala_games" />
-          </GridItem>
-          <GridItem colSpan={1}>
-            <PartnerLogo partner="mirror" />
-          </GridItem>
-          <GridItem colSpan={1}>
-            <PartnerLogo partner="heroic_story" />
-          </GridItem>
-          <GridItem colSpan={1}>
-            <PartnerLogo partner="layer3" />
-          </GridItem>
-        </SimpleGrid>
-      </Container>
+    <Box
+      zIndex={10}
+      position="relative"
+      pointerEvents="none"
+      userSelect="none"
+      // slightly larger than container.lg
+      maxW="1200px"
+      mx="auto"
+      sx={{
+        "mask-image": `linear-gradient(to right, hsl(0 0% 0% / 0), hsl(0 0% 0% / 1) 10%, hsl(0 0% 0% / 1) 90%, hsl(0 0% 0% / 0));`,
+      }}
+      pt={10}
+      mb={{ base: 20, md: 24 }}
+    >
+      {/* desktop - 1 row with all logos */}
+      <Marquee display={{ base: "none", lg: "flex" }}>
+        <PartnerLogo partner="rarible" />
+        <PartnerLogo partner="fractal" />
+        <PartnerLogo partner="buildspace" />
+        <PartnerLogo partner="shopify" />
+        <PartnerLogo partner="paradigm" />
+        <PartnerLogo partner="unlock" />
+        <PartnerLogo partner="minted" />
+        <PartnerLogo partner="nyfw" />
+        <PartnerLogo partner="gala_games" />
+        <PartnerLogo partner="mirror" />
+        <PartnerLogo partner="heroic_story" />
+        <PartnerLogo partner="layer3" />
+      </Marquee>
+
+      {/* mobile - 2 rows with logos split in two rows */}
+      <Marquee display={{ base: "flex", lg: "none" }}>
+        <PartnerLogo partner="rarible" />
+        <PartnerLogo partner="fractal" />
+        <PartnerLogo partner="buildspace" />
+        <PartnerLogo partner="shopify" />
+        <PartnerLogo partner="paradigm" />
+        <PartnerLogo partner="unlock" />
+      </Marquee>
+
+      <Marquee
+        animationDirection="reverse"
+        display={{ base: "flex", lg: "none" }}
+      >
+        <PartnerLogo partner="minted" />
+        <PartnerLogo partner="nyfw" />
+        <PartnerLogo partner="gala_games" />
+        <PartnerLogo partner="mirror" />
+        <PartnerLogo partner="heroic_story" />
+        <PartnerLogo partner="layer3" />
+      </Marquee>
     </Box>
   );
 };
--- components/partners/partner-logo.tsx
+++ components/partners/partner-logo.tsx
@@ -1,4 +1,3 @@
-import { AspectRatio } from "@chakra-ui/react";
 import { ChakraNextImage } from "components/Image";
 
 const PARTNER_LOGO_MAP = {
@@ -59,19 +58,18 @@ interface PartnerLogoProps {
 }
 export const PartnerLogo: React.FC<PartnerLogoProps> = ({ partner }) => {
   return (
-    <AspectRatio
-      opacity=".8"
+    <ChakraNextImage
+      opacity="0.8"
       pointerEvents="none"
-      w="full"
-      ratio={16 / 9}
+      w="auto"
+      maxW="150px"
+      h={{ base: "40px", md: "50px" }}
+      loading="eager"
       filter={PARTNER_LOGO_MAP[partner].filter}
-    >
-      <ChakraNextImage
-        src={PARTNER_LOGO_MAP[partner].img}
-        alt={partner}
-        sizes="(max-width: 768px) 25vw,
+      src={PARTNER_LOGO_MAP[partner].img}
+      alt={partner}
+      sizes="(max-width: 768px) 25vw,
               10vw"
-      />
-    </AspectRatio>
+    />
   );
 };
