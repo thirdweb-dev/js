729e81361ea4cc0f5f7dcbf1ee7a1afb7691d184
adam-maj <mr.adam.maj@gmail.com>
2023-01-27 16:55:50 -0500

    Update wallets

--- packages/wallets/src/evm/wallets/private-key.ts
+++ packages/wallets/src/evm/wallets/private-key.ts
@@ -2,14 +2,12 @@ import { AbstractWallet } from "./abstract";
 import { ethers } from "ethers";
 
 export class PrivateKeyWallet extends AbstractWallet {
-  #signer: ethers.Signer;
-
   constructor(privateKey: string) {
     super();
-    this.#signer = new ethers.Wallet(privateKey);
+    this.signer = new ethers.Wallet(privateKey);
   }
 
   async getSigner(): Promise<ethers.Signer> {
-    return this.#signer;
+    return this.signer as ethers.Signer;
   }
 }
