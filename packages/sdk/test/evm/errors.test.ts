87291fa04b5165937789946e9b82c260b6192eaa
adam-maj <mr.adam.maj@gmail.com>
2023-02-05 00:23:39 -0500

    Uncomment fail

--- packages/sdk/test/evm/errors.test.ts
+++ packages/sdk/test/evm/errors.test.ts
@@ -13,7 +13,7 @@ describe("Error Handling", async () => {
         name: "Should Fail Collection",
         primary_sale_recipient: "0x0000000000000000000000000000000000000000",
       });
-      // expect.fail();
+      expect.fail();
     } catch (err) {
       expect(err.message).to.contain(
         "You have insufficient funds in your account to execute this transaction.",
