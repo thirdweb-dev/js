f42d10ec86daab55543d8e43ec31021852971a93
Jonas Daniels <jonas.daniels@outlook.com>
2023-02-11 16:17:38 -0800

    remove TODO

--- packages/react/src/core/constants.ts
+++ packages/react/src/core/constants.ts
@@ -1,6 +1,5 @@
 // eslint-disable-next-line turbo/no-undeclared-env-vars
 export const __DEV__ = process.env.NODE_ENV !== "production";
 
-// TODO update this to a more specific api key
 export const DEFAULT_API_KEY =
   "f9e23156ccfc6173c5267eaabf320fb692ffab1c2d572095a58091cbb67590cd";
