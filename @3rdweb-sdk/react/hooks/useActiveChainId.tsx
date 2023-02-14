42077cea8e28de3a6243512795321afa6cb89a05
Jonas Daniels <jonas.daniels@outlook.com>
2023-02-14 09:49:17 -0800

    fix re-setting contractInfo to null for no reason

--- @3rdweb-sdk/react/hooks/useActiveChainId.tsx
+++ @3rdweb-sdk/react/hooks/useActiveChainId.tsx
@@ -1,6 +1,12 @@
 import { Chain } from "@thirdweb-dev/chains";
 import { useRouter } from "next/router";
-import { createContext, useContext, useEffect, useState } from "react";
+import {
+  createContext,
+  useCallback,
+  useContext,
+  useEffect,
+  useState,
+} from "react";
 import invariant from "tiny-invariant";
 import { getSolNetworkFromNetworkPath } from "utils/solanaUtils";
 
@@ -30,10 +36,23 @@ export function EVMContractInfoProvider(props: {
 
   // keep the state in sync with the props.value
   // this is required when route changes and page goes from fallback to actual page
-  useEffect(() => {
-    setValue(props.value);
-  }, [props.value]);
 
+  const _setValue = useCallback((info?: EVMContractInfo) => {
+    setValue((prevValue) => {
+      if (
+        prevValue?.chainSlug === info?.chainSlug &&
+        prevValue?.contractAddress === info?.contractAddress &&
+        info?.chain === null
+      ) {
+        return prevValue;
+      }
+      return info;
+    });
+  }, []);
+
+  useEffect(() => {
+    _setValue(props.value);
+  }, [_setValue, props.value]);
   // when we move from rendering the fallback page to the actual page, props.value is correct value but state lags behind with undefined value
   // in that case we use the props.value instead of providing undefined and update the state with effect afterwards
   // this only happens ONCE, when the page is not yet created on server. once it is created and cached. value will always be defined
@@ -41,7 +60,7 @@ export function EVMContractInfoProvider(props: {
 
   return (
     <EVMContractInfoContext.Provider value={providedValue}>
-      <SetEVMContractInfoContext.Provider value={setValue}>
+      <SetEVMContractInfoContext.Provider value={_setValue}>
         {props.children}
       </SetEVMContractInfoContext.Provider>
     </EVMContractInfoContext.Provider>
