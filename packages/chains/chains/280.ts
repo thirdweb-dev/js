f580b8ac06534df24b0194cbc632b4a8fd447611
Joaquim Verges <joaquim.verges@gmail.com>
2023-02-17 17:24:16 -0800

    [React] fix react provider readonly chain mismatch (#599)

--- packages/chains/chains/280.ts
+++ packages/chains/chains/280.ts
@@ -1,36 +1,45 @@
 export default {
-  "name": "zkSync alpha testnet",
+  "name": "zkSync Era Testnet",
   "chain": "ETH",
   "rpc": [
-    "https://zksync-alpha-testnet.rpc.thirdweb.com/${THIRDWEB_API_KEY}",
+    "https://zksync-era-testnet.rpc.thirdweb.com/${THIRDWEB_API_KEY}",
     "https://zksync2-testnet.zksync.dev"
   ],
   "faucets": [
-    "https://portal.zksync.io/faucet"
+    "https://goerli.portal.zksync.io/faucet"
   ],
   "nativeCurrency": {
     "name": "Ether",
     "symbol": "ETH",
     "decimals": 18
   },
-  "infoURL": "https://v2-docs.zksync.io/",
+  "infoURL": "https://era.zksync.io/docs/",
   "shortName": "zksync-goerli",
   "chainId": 280,
   "networkId": 280,
   "icon": {
-    "url": "ipfs://QmdwQDr6vmBtXmK2TmknkEuZNoaDqTasFdZdu3DRw8b2wt",
-    "width": 1000,
-    "height": 1628,
-    "format": "png"
+    "url": "ipfs://Qma6H9xd8Ydah1bAFnmDuau1jeMh5NjGEL8tpdnjLbJ7m2",
+    "width": 512,
+    "height": 512,
+    "format": "svg"
   },
   "explorers": [
     {
-      "name": "blockscout",
-      "url": "https://zksync2-testnet.zkscan.io",
-      "icon": "blockscout",
+      "name": "zkSync Era Block Explorer",
+      "url": "https://goerli.explorer.zksync.io",
+      "icon": "zksync-era",
       "standard": "EIP3091"
     }
   ],
+  "parent": {
+    "type": "L2",
+    "chain": "eip155-1",
+    "bridges": [
+      {
+        "url": "https://goerli.portal.zksync.io/bridge"
+      }
+    ]
+  },
   "testnet": true,
-  "slug": "zksync-alpha-testnet"
+  "slug": "zksync-era-testnet"
 } as const;
