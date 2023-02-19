50a4a14805a5ad9b92cea76c4532bc11b7454474
Jonas Daniels <jonas.daniels@outlook.com>
2023-02-18 17:38:37 -0800

    re-order logic to be more readable

--- packages/sdk/src/evm/lib/static-batch-rpc.ts
+++ packages/sdk/src/evm/lib/static-batch-rpc.ts
@@ -66,18 +66,22 @@ export class StaticJsonRpcBatchProvider extends providers.StaticJsonRpcProvider 
         // on whether it was a success or error
         batch.forEach((inflightRequest_, index) => {
           const payload = result[index];
-          // if there is no payload, reject the request
-          if (!payload) {
-            inflightRequest_.reject(new Error("No response for request"));
-          } else {
+
+          // there may *not* be a payload for a given request (typically RPC error level)
+          if (payload) {
+            // if there is a payload, check for an error
             if (payload.error) {
               const error = new Error(payload.error.message);
               (error as any).code = payload.error.code;
               (error as any).data = payload.error.data;
               inflightRequest_.reject(error);
             } else {
+              // if there's no error resolve the request
               inflightRequest_.resolve(payload.result);
             }
+          } else {
+            // if there is no payload, reject the request
+            inflightRequest_.reject(new Error("No response for request"));
           }
         });
       },
