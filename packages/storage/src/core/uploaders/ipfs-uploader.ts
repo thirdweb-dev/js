a783a26a0b0c26780adb5ddd6234dc7b04b3d380
adam-maj <mr.adam.maj@gmail.com>
2023-02-27 23:03:09 -0800

    Fix test cases

--- packages/storage/src/core/uploaders/ipfs-uploader.ts
+++ packages/storage/src/core/uploaders/ipfs-uploader.ts
@@ -57,7 +57,7 @@ export class IpfsUploader implements IStorageUploader<IpfsUploadBatchOptions> {
 
     const cid = await getCIDForUpload(
       data,
-      fileNames,
+      fileNames.map((name) => decodeURIComponent(name)),
       !options?.uploadWithoutDirectory,
     );
     if (await isUploaded(cid)) {
