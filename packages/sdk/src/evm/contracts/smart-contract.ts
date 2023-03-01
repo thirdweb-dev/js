93694c6328f712a234a6dc645711b8b77c1eb9e5
Jonas Daniels <jonas.daniels@outlook.com>
2023-02-28 17:07:33 -0800

    [SDK] - add `extensions` getter

--- packages/sdk/src/evm/contracts/smart-contract.ts
+++ packages/sdk/src/evm/contracts/smart-contract.ts
@@ -294,7 +294,11 @@ export class SmartContract<TContract extends BaseContract = BaseContract>
   }
 
   private detectAppURI() {
-    if (detectContractFeature<AppURI>(this.contractWrapper, "AppURI") || detectContractFeature<ContractMetadata, this.metadata>(this.contractWrapper, "ContractMetadata")) {
+    if (
+      (detectContractFeature<AppURI>(this.contractWrapper, "AppURI") ||
+        detectContractFeature < ContractMetadata,
+      this.metadata > (this.contractWrapper, "ContractMetadata"))
+    ) {
       return new ContractAppURI(this.contractWrapper, this.metadata);
     }
     return undefined;
