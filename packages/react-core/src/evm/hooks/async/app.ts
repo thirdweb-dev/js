8d5b418e78fcf692f72aed5fe49358e40720d80c
Nacho Iacovino <50103937+nachoiacovino@users.noreply.github.com>
2023-02-28 16:51:59 -0800

    [React] Fix appURI hook invariant (#650)

--- packages/react-core/src/evm/hooks/async/app.ts
+++ packages/react-core/src/evm/hooks/async/app.ts
@@ -2,16 +2,15 @@ import {
   RequiredParam,
   requiredParamInvariant,
 } from "../../../core/query-utils/required-param";
+import { useSDKChainId } from "../../providers/thirdweb-sdk-provider";
 import {
-  cacheKeys, invalidateContractAndBalances,
+  cacheKeys,
+  invalidateContractAndBalances,
 } from "../../utils/cache-keys";
 import { useQueryWithNetwork } from "../query-utils/useQueryWithNetwork";
-import {
-  SmartContract,
-} from "@thirdweb-dev/sdk";
 import { useMutation, useQueryClient } from "@tanstack/react-query";
+import { SmartContract } from "@thirdweb-dev/sdk";
 import invariant from "tiny-invariant";
-import { useSDKChainId } from "../../providers/thirdweb-sdk-provider";
 
 /**
  * Get App URI
@@ -33,10 +32,7 @@ export function useAppURI<TContract extends SmartContract>(
     cacheKeys.contract.app.get(contract?.getAddress()),
     async () => {
       requiredParamInvariant(contract, "Contract is required");
-      invariant(
-        "appURI" in contract && contract.appURI,
-        "Contract does not support appURI",
-      );
+      invariant(contract.appURI, "Contract does not support appURI");
       return await contract.appURI.get();
     },
     {
@@ -47,7 +43,7 @@ export function useAppURI<TContract extends SmartContract>(
 
 /**
  * Set App URI
- * 
+ *
  * @example
  * ```jsx
  * const Component = () => {
@@ -76,19 +72,14 @@ export function useAppURI<TContract extends SmartContract>(
  * @twfeature AppURI | ContractMetadata
  * @beta
  */
-export function useSetAppURI(
-  contract: RequiredParam<SmartContract>,
-) {
+export function useSetAppURI(contract: RequiredParam<SmartContract>) {
   const queryClient = useQueryClient();
   const contractAddress = contract?.getAddress();
   const activeChainId = useSDKChainId();
   return useMutation(
     (params: { uri: string }) => {
       requiredParamInvariant(contract, "Contract is required");
-      invariant(
-        "appURI" in contract && contract.appURI,
-        "Contract does not support appURI",
-      );
+      invariant(contract.appURI, "Contract does not support appURI");
       return contract.appURI.set(params.uri);
     },
     {
