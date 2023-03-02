e081075b2cebc7edad855481366bd9864ddba102
Adam Majmudar <64697628+adam-maj@users.noreply.github.com>
2023-03-01 16:28:40 -0800

    [SDK] Expose get and set functions on transaction and tx.sign() (#663)

--- packages/sdk/test/evm/transactions.test.ts
+++ packages/sdk/test/evm/transactions.test.ts
@@ -1,6 +1,8 @@
+import { ThirdwebSDK } from "../../src/evm";
 import { sdk, signers } from "./before-setup";
 import { SignerWithAddress } from "@nomiclabs/hardhat-ethers/signers";
 import { expect } from "chai";
+import { ethers } from "ethers";
 
 describe("Transactions", async () => {
   let adminWallet: SignerWithAddress;
@@ -28,6 +30,38 @@ describe("Transactions", async () => {
       true,
     ]);
     await tx.execute();
+
+    isApproved = await contract.isApproved(
+      adminWallet.address,
+      samWallet.address,
+    );
+    expect(isApproved).to.equal(true);
+  });
+
+  it("Should successfully sign and send transaction", async () => {
+    const wallet = new ethers.Wallet(
+      "0xac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80",
+      adminWallet.provider,
+    );
+    const signerSdk = new ThirdwebSDK(wallet);
+    const address = await signerSdk.deployer.deployNFTCollection({
+      name: "NFT",
+      primary_sale_recipient: adminWallet.address,
+    });
+    const contract = await signerSdk.getContract(address, "nft-collection");
+
+    let isApproved = await contract.isApproved(
+      adminWallet.address,
+      samWallet.address,
+    );
+    expect(isApproved).to.equal(false);
+
+    const tx = await contract.setApprovalForAll.prepare(
+      samWallet.address,
+      true,
+    );
+    const signedTx = await tx.sign();
+    await sdk.getProvider().sendTransaction(signedTx);
 
     isApproved = await contract.isApproved(
       adminWallet.address,
