4355518afea68cd8026d3ab8a0144c15d66b9e24
Jonas Daniels <jonas.daniels@outlook.com>
2023-03-07 10:57:43 -0800

    [chains] - update chains (#685)

---
+++ packages/chains/data/overrides/123.mjs
@@ -0,0 +1,14 @@
+// @ts-check
+/** @typedef { import("../../src/types").Chain } Chain */
+
+/** @type {Partial<Chain>} */
+export default {
+  chainId: 123,
+  testnet: true,
+  icon: {
+    url: "ipfs://QmQg8aqyeaMfHvjzFDtZkb8dUNRYhFezPp8UYVc1HnLpRW/green.png",
+    format: "png",
+    width: 512,
+    height: 512,
+  },
+};
