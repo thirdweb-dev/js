770083ab0af79871ebf2bfe5332a82758a7dd518
Jonas Daniels <jonas.daniels@outlook.com>
2023-03-08 01:19:40 -0800

    le sitemap (#953)

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,5 +1,7 @@
+// @ts-check
+
 // eslint-disable-next-line @typescript-eslint/no-var-requires
-const { allChains } = require("@thirdweb-dev/chains");
+const { allChains, getChainByChainId } = require("@thirdweb-dev/chains");
 
 /** @type {import('next-sitemap').IConfig} */
 module.exports = {
@@ -37,13 +39,52 @@ module.exports = {
     };
   },
   additionalPaths: async (config) => {
-    return allChains.map((chain) => {
-      return {
-        loc: `/${chain.slug}`,
-        changefreq: config.changefreq,
-        priority: config.priority,
-        lastmod: config.autoLastmod ? new Date().toISOString() : undefined,
-      };
-    });
+    return [
+      ...allChains.map((chain) => {
+        return {
+          loc: `/${chain.slug}`,
+          changefreq: config.changefreq,
+          priority: config.priority,
+          lastmod: config.autoLastmod ? new Date().toISOString() : undefined,
+        };
+      }),
+      ...(await createSearchRecordSitemaps(config)),
+    ];
   },
 };
+/**
+ * @param {{ changefreq?: any; priority?: any; autoLastmod?: any; }} config
+ */
+async function createSearchRecordSitemaps(config) {
+  if (!process.env.TYPESENSE_CONTRACT_EXPORT_API_KEY) {
+    return [];
+  }
+  const response = await fetch(
+    "https://search.thirdweb.com/collections/contracts/documents/export?filter_by=testnet:false",
+    {
+      headers: {
+        "x-typesense-api-key": process.env.TYPESENSE_CONTRACT_EXPORT_API_KEY,
+      },
+    },
+  );
+  const data = await response.text();
+  return data
+    .split("\n")
+    .map((l) => {
+      try {
+        const parsedLine = JSON.parse(l);
+        const parsedLineChain = getChainByChainId(
+          parseInt(parsedLine.chain_id),
+        );
+        return {
+          loc: `/${parsedLineChain.slug}/${parsedLine.contract_address}`,
+          changefreq: config.changefreq,
+          priority: config.priority,
+          lastmod: config.autoLastmod ? new Date().toISOString() : undefined,
+        };
+      } catch (e) {
+        return null;
+      }
+    })
+    .filter(Boolean);
+}
