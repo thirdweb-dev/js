240d6500c5fbbb33713f100834a5aeec21eb5014
Jonas Daniels <jonas.daniels@outlook.com>
2023-03-08 21:52:28 -0800

    Update contract.tsx

--- pages/api/og/contract.tsx
+++ pages/api/og/contract.tsx
@@ -148,7 +148,7 @@ export default async function handler(req: NextRequest) {
       </div>
     ),
     {
-      debug: true,
+      debug: false,
       width: 1200,
       height: 630,
       fonts: [
