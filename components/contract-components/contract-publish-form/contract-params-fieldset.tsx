045f850740a7edd7c297e96b0a052dd83c84e030
Nacho Iacovino <50103937+nachoiacovino@users.noreply.github.com>
2023-03-14 02:05:14 +0100

    Fix placeholder overlap on input/button (#975)

--- components/contract-components/contract-publish-form/contract-params-fieldset.tsx
+++ components/contract-components/contract-publish-form/contract-params-fieldset.tsx
@@ -108,7 +108,9 @@ export const ContractParamsFieldset: React.FC<ContractParamsFieldsetProps> = ({
                             param.type === "address" ? "string" : param.type
                           }
                           placeholder={
-                            isMobile
+                            isMobile ||
+                            paramTemplateValues?.[0]?.value ===
+                              "{{trusted_forwarders}}"
                               ? "Pre-filled value."
                               : "This value will be pre-filled in the deploy form."
                           }
