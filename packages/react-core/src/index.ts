21eee8216972c323fa7328f18172129ef9c21fa7
Manan Tank <manantankm@gmail.com>
2023-03-14 12:16:30 +0530

    re-export thirdweb-dev/sdk types from react-core

--- packages/react-core/src/index.ts
+++ packages/react-core/src/index.ts
@@ -1,2 +1,12 @@
 // main entry point = evm
 export * from "./evm";
+
+// export types from thirdweb-dev/sdk
+export type {
+  NFTMetadataInput,
+  NFT,
+  NFTMetadata,
+  NFTMetadataOrUri,
+} from "@thirdweb-dev/sdk";
+export type { CurrencyValue, TokenMetadata } from "@thirdweb-dev/sdk";
+export type { QueryAllParams } from "@thirdweb-dev/sdk";
