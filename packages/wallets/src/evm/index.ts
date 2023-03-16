7a7c8515ae93f3a8c790536b3bb9c504a9e14b69
Jonas Daniels <jonas.daniels@outlook.com>
2023-03-16 16:28:36 -0700

    [tooling] - switch to pnpm for faster CI

--- packages/wallets/src/evm/index.ts
+++ packages/wallets/src/evm/index.ts
@@ -26,7 +26,6 @@ export * from "./wallets/coinbase-wallet";
 export * from "./wallets/device-wallet";
 export * from "./wallets/ethers";
 export * from "./wallets/injected";
-export * from "./wallets/magic-auth";
 export * from "./wallets/metamask";
 export * from "./wallets/private-key";
 export * from "./wallets/wallet-connect";
