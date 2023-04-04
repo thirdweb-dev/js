a777df75c0242b3ac5911fc6537bd6c4dd035753
ciaranightingale <52419674+ciaranightingale@users.noreply.github.com>
2023-04-04 23:32:16 +0100

    install nightly - fix portal links (#1069)
    * install nightly
    
    * remove storage & chains
    
    * type error

--- utils/date-utils.ts
+++ utils/date-utils.ts
@@ -2,7 +2,11 @@ import { format, isValid } from "date-fns";
 
 const DATE_TIME_LOCAL_FORMAT = "yyyy-MM-dd'T'HH:mm";
 
-export function toDateTimeLocal(date?: Date) {
+export function toDateTimeLocal(date?: Date | number) {
+  if (typeof date === "number") {
+    date = new Date(date * 1000);
+  };
+
   return date && isValid(date)
     ? format(date, DATE_TIME_LOCAL_FORMAT)
     : undefined;
