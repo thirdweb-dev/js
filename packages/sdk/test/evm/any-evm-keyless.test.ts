47d68f82fff37465fab7e1dffaa38e7fdfc4601d
Yash <kumaryashcse@gmail.com>
2023-04-07 21:57:05 +0530

    add tests

--- packages/sdk/test/evm/any-evm-keyless.test.ts
+++ packages/sdk/test/evm/any-evm-keyless.test.ts
@@ -3,6 +3,7 @@ import { SmartContract } from "../../src/evm/contracts/smart-contract";
 import { signers } from "./before-setup";
 import { SignerWithAddress } from "@nomiclabs/hardhat-ethers/signers";
 import { expect, assert } from "chai";
+import { ethers } from "ethers";
 
 describe("Any EVM Keyless Deploy", async () => {
   let contract: SmartContract;
@@ -54,6 +55,44 @@ describe("Any EVM Keyless Deploy", async () => {
     return tieredDrop;
   }
 
+  async function deployMarketplaceV3() {
+    const mockPublisher = process.env.contractPublisherAddress;
+    process.env.contractPublisherAddress =
+      "0x664244560eBa21Bf82d7150C791bE1AbcD5B4cd7";
+    const walletAddress = await sdk.wallet.getAddress();
+
+    // This needs to match the published contract for the currently used ABI
+    const publishUri = "ipfs://QmP2QPzc81zg5rqhU9u7cDeSyD4aZH8RdVF8Nuh6rCCgxV";
+    // console.log(
+    //   "transactions: ",
+    //   await sdk.deployer.getTransactionsForDeploy(publishUri),
+    // );
+    // transactionCount = (await sdk.deployer.getTransactionsForDeploy(publishUri))
+    //   .length;
+
+    const address = await sdk.deployer.deployContractFromUri(
+      publishUri,
+      [
+        walletAddress, // defaultAdmin
+        "ipfs://QmP2QPzc81zg5rqhU9u7cDeSyD4aZH8RdVF8Nuh6rCCgxV", // contractUri
+        [], // trustedForwarders
+        walletAddress, // platform fee recipient
+        0, // platform fee bps
+      ],
+      {
+        forceDirectDeploy: false,
+        // notifier(status, contractType) {
+        //   notificationCounter += 1;
+        // },
+      },
+    );
+
+    process.env.contractPublisherAddress = mockPublisher;
+    const marketplace = await sdk.getContract(address);
+
+    return marketplace;
+  }
+
   before(async () => {
     [adminWallet, claimerWallet] = signers;
     sdk = new ThirdwebSDK(adminWallet);
@@ -63,17 +102,33 @@ describe("Any EVM Keyless Deploy", async () => {
     sdk.updateSignerOrProvider(adminWallet);
   });
 
-  it("correct count of logs and transactions", async () => {
-    notificationCounter = 0;
-    transactionCount = 0;
-    contract = await deployTieredDrop();
-    expect(notificationCounter).to.equal(8);
-    expect(transactionCount).to.equal(4);
+  // it("correct count of logs and transactions", async () => {
+  //   notificationCounter = 0;
+  //   transactionCount = 0;
+  //   contract = await deployTieredDrop();
+  //   expect(notificationCounter).to.equal(8);
+  //   expect(transactionCount).to.equal(4);
 
-    notificationCounter = 0;
-    transactionCount = 0;
-    contract = await deployTieredDrop();
-    expect(notificationCounter).to.equal(2);
-    expect(transactionCount).to.equal(1);
+  //   notificationCounter = 0;
+  //   transactionCount = 0;
+  //   contract = await deployTieredDrop();
+  //   expect(notificationCounter).to.equal(2);
+  //   expect(transactionCount).to.equal(1);
+  // });
+
+  it("deploy marketplacev3", async () => {
+    const marketplace = await deployMarketplaceV3();
+    const plugins = await marketplace.call("getAllPlugins");
+    console.log("plugins: ", plugins);
+    console.log("plugins ^");
+
+    const allPlugins = plugins.map((item: any) => item.pluginAddress);
+    const pluginsAddresses = Array.from(new Set(allPlugins));
+
+    expect(pluginsAddresses.length).to.equal(3);
+
+    pluginsAddresses.forEach((address) => {
+      expect(address).to.not.equal(ethers.constants.AddressZero);
+    });
   });
 });
