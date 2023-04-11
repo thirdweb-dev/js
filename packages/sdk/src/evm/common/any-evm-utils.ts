dfe7fc74f01baf1a06f84bb12ade002da1e7e824
Yash <kumaryashcse@gmail.com>
2023-04-11 05:41:21 +0530

    clear deploymentPresets cache in case of error

--- packages/sdk/src/evm/common/any-evm-utils.ts
+++ packages/sdk/src/evm/common/any-evm-utils.ts
@@ -507,78 +507,82 @@ export async function getDeploymentInfo(
   );
   const pluginMetadata = await getMetadataForPlugins(metadataUri, storage);
 
-  // if pluginMetadata is not empty, then it's a plugin-pattern router contract
-  if (pluginMetadata.length > 0) {
-    // get deployment info for all plugins
-    const pluginDeploymentInfo = await Promise.all(
-      pluginMetadata.map(async (metadata) => {
-        const info = await computeDeploymentInfo(
-          "plugin",
-          provider,
-          storage,
-          create2FactoryAddress,
-          { metadata: metadata },
-        );
-        return info;
-      }),
-    );
-
-    // create constructor param input for PluginMap
-    const mapInput: Plugin[] = [];
-    pluginMetadata.forEach((metadata, index) => {
-      const input = generatePluginFunctions(
-        pluginDeploymentInfo[index].transaction.predictedAddress,
-        metadata.abi,
+  try {
+    // if pluginMetadata is not empty, then it's a plugin-pattern router contract
+    if (pluginMetadata.length > 0) {
+      // get deployment info for all plugins
+      const pluginDeploymentInfo = await Promise.all(
+        pluginMetadata.map(async (metadata) => {
+          const info = await computeDeploymentInfo(
+            "plugin",
+            provider,
+            storage,
+            create2FactoryAddress,
+            { metadata: metadata },
+          );
+          return info;
+        }),
       );
-      mapInput.push(...input);
-    });
 
-    // get PluginMap deployment transaction
-    const pluginMapTransaction = await computeDeploymentInfo(
-      "plugin",
+      // create constructor param input for PluginMap
+      const mapInput: Plugin[] = [];
+      pluginMetadata.forEach((metadata, index) => {
+        const input = generatePluginFunctions(
+          pluginDeploymentInfo[index].transaction.predictedAddress,
+          metadata.abi,
+        );
+        mapInput.push(...input);
+      });
+
+      // get PluginMap deployment transaction
+      const pluginMapTransaction = await computeDeploymentInfo(
+        "plugin",
+        provider,
+        storage,
+        create2FactoryAddress,
+        {
+          contractName: "PluginMap",
+          constructorParams: { _pluginsToAdd: { value: mapInput } },
+        },
+      );
+
+      // address of PluginMap is input for MarketplaceV3's constructor
+      customParams["_pluginMap"] = {
+        value: pluginMapTransaction.transaction.predictedAddress,
+      };
+
+      finalDeploymentInfo.push(...pluginDeploymentInfo, pluginMapTransaction);
+    }
+
+    const implementationDeployInfo = await computeDeploymentInfo(
+      "implementation",
       provider,
       storage,
       create2FactoryAddress,
       {
-        contractName: "PluginMap",
-        constructorParams: { _pluginsToAdd: { value: mapInput } },
+        metadata: compilerMetadata,
+        constructorParams: customParams,
       },
     );
 
-    // address of PluginMap is input for MarketplaceV3's constructor
-    customParams["_pluginMap"] = {
-      value: pluginMapTransaction.transaction.predictedAddress,
-    };
+    // get clone factory
+    const factoryInfo = await computeDeploymentInfo(
+      "infra",
+      provider,
+      storage,
+      create2FactoryAddress,
+      { contractName: "TWCloneFactory" },
+    );
 
-    finalDeploymentInfo.push(...pluginDeploymentInfo, pluginMapTransaction);
+    finalDeploymentInfo.push(factoryInfo);
+    finalDeploymentInfo.push(...Object.values(deploymentPresets));
+    finalDeploymentInfo.push(implementationDeployInfo);
+
+    deploymentPresets = {};
+  } catch (error) {
+    deploymentPresets = {};
+    throw new Error(`Couldn't get deployment info: ${error}`);
   }
-
-  const implementationDeployInfo = await computeDeploymentInfo(
-    "implementation",
-    provider,
-    storage,
-    create2FactoryAddress,
-    {
-      metadata: compilerMetadata,
-      constructorParams: customParams,
-    },
-  );
-
-  // get clone factory
-  const factoryInfo = await computeDeploymentInfo(
-    "infra",
-    provider,
-    storage,
-    create2FactoryAddress,
-    { contractName: "TWCloneFactory" },
-  );
-
-  finalDeploymentInfo.push(factoryInfo);
-  finalDeploymentInfo.push(...Object.values(deploymentPresets));
-  finalDeploymentInfo.push(implementationDeployInfo);
-
-  deploymentPresets = {};
-
   return finalDeploymentInfo;
 }
 
