5cf5c257be0a177e1fb0b25aa56718e4dfc21e75
Manan Tank <manantankm@gmail.com>
2023-05-03 23:51:32 +0530

    Update export local wallet flow

--- packages/react/src/wallet/wallets/localWallet/ExportLocalWallet.tsx
+++ packages/react/src/wallet/wallets/localWallet/ExportLocalWallet.tsx
@@ -12,8 +12,14 @@ import { EyeClosedIcon, EyeOpenIcon } from "@radix-ui/react-icons";
 import { FormFieldWithIconButton } from "../../../components/formFields";
 import { useEffect, useState } from "react";
 import { shortenAddress } from "../../../evm/utils/addresses";
-import { LocalWallet } from "@thirdweb-dev/wallets";
-import { GenericWalletIcon } from "../../ConnectWallet/icons/GenericWalletIcon";
+import type { LocalWallet } from "@thirdweb-dev/wallets";
+import type { WalletData } from "@thirdweb-dev/wallets/evm/wallets/local-wallet";
+import { Img } from "../../../components/Img";
+import { Spinner } from "../../../components/Spinner";
+import { Flex } from "../../../components/basic";
+
+const localWalletIcon =
+  "ipfs://QmeAJVqn17aDNQhjEU3kcWVZCFBrfta8LzaDGkS8Egdiyk/local-wallet-desktop.svg";
 
 export const ExportLocalWallet: React.FC<{
   onBack: () => void;
@@ -24,6 +30,9 @@ export const ExportLocalWallet: React.FC<{
   const [showPassword, setShowPassword] = useState(false);
   const [isWrongPassword, setIsWrongPassword] = useState(false);
   const [address, setAddress] = useState("");
+  const [savedData, setSavedData] = useState<WalletData | null | "loading">(
+    "loading",
+  );
 
   useEffect(() => {
     props.localWallet.getAddress().then((add) => {
@@ -31,33 +40,70 @@ export const ExportLocalWallet: React.FC<{
     });
   }, [props.localWallet]);
 
+  useEffect(() => {
+    if (!address) {
+      return;
+    }
+    props.localWallet.getSavedData().then((data) => {
+      if (data) {
+        if (data.address === address) {
+          setSavedData(data);
+        } else {
+          setSavedData(null);
+        }
+      } else {
+        setSavedData(null);
+      }
+    });
+  }, [props.localWallet, address]);
+
   const exportFromLocalStorage = async () => {
-    if (!props.localWallet) {
+    if (!props.localWallet || savedData === "loading") {
       throw new Error("invalid state");
     }
 
-    try {
-      // await props.localWallet.import({
-      //   encryptedJson: walletData.data,
-      //   password,
-      // });
-
-      const json = await props.localWallet.export({
-        strategy: "encryptedJson",
-        password,
-      });
-
-      downloadAsFile(JSON.parse(json), "wallet.json", "application/json");
+    if (savedData) {
+      downloadAsFile(
+        JSON.parse(savedData.data),
+        "wallet.json",
+        "application/json",
+      );
       props.onExport();
-    } catch (e) {
-      setIsWrongPassword(true);
-      return;
+    } else {
+      try {
+        const json = await props.localWallet.export({
+          strategy: "encryptedJson",
+          password,
+        });
+
+        downloadAsFile(JSON.parse(json), "wallet.json", "application/json");
+        props.onExport();
+      } catch (e) {
+        setIsWrongPassword(true);
+        return;
+      }
     }
   };
 
+  if (savedData === "loading") {
+    return (
+      <Flex
+        justifyContent="center"
+        alignItems="center"
+        style={{
+          height: "300px",
+        }}
+      >
+        <Spinner size="md" color="link" />
+      </Flex>
+    );
+  }
+
+  const disabled = isWrongPassword;
+
   return (
     <>
-      <GenericWalletIcon size={iconSize.xl} />
+      <Img src={localWalletIcon} width={iconSize.xl} height={iconSize.xl} />
       <Spacer y="lg" />
       <ModalTitle
         style={{
@@ -91,41 +137,51 @@ export const ExportLocalWallet: React.FC<{
 
         <Spacer y="lg" />
 
-        {/* Hidden Account Address as Username */}
-        <input
-          type="text"
-          name="username"
-          autoComplete="off"
-          value={address}
-          disabled
-          style={{ display: "none" }}
-        />
+        {!savedData && (
+          <>
+            {/* Hidden Account Address as Username */}
+            <input
+              type="text"
+              name="username"
+              autoComplete="off"
+              value={address}
+              disabled
+              style={{ display: "none" }}
+            />
 
-        {/* password */}
-        <FormFieldWithIconButton
-          noSave
-          required
-          name="current-password"
-          autocomplete="current-password"
-          id="current-password"
-          onChange={(value) => {
-            setPassword(value);
-            setIsWrongPassword(false);
-          }}
-          right={{
-            onClick: () => setShowPassword(!showPassword),
-            icon: showPassword ? <EyeClosedIcon /> : <EyeOpenIcon />,
-          }}
-          label="Password"
-          type={showPassword ? "text" : "password"}
-          value={password}
-          error={isWrongPassword ? "Wrong Password" : ""}
-        />
-
-        <Spacer y="xl" />
+            {/* password */}
+            <FormFieldWithIconButton
+              noSave
+              required
+              name="current-password"
+              autocomplete="current-password"
+              id="current-password"
+              onChange={(value) => {
+                setPassword(value);
+                setIsWrongPassword(false);
+              }}
+              right={{
+                onClick: () => setShowPassword(!showPassword),
+                icon: showPassword ? <EyeClosedIcon /> : <EyeOpenIcon />,
+              }}
+              label="Password"
+              type={showPassword ? "text" : "password"}
+              value={password}
+              error={isWrongPassword ? "Wrong Password" : ""}
+            />
+            <Spacer y="xl" />
+          </>
+        )}
 
         <FormFooter>
-          <Button disabled={isWrongPassword} variant="inverted" type="submit">
+          <Button
+            disabled={disabled}
+            variant="inverted"
+            style={{
+              opacity: disabled ? 0.5 : 1,
+            }}
+            type="submit"
+          >
             Export
           </Button>
         </FormFooter>
