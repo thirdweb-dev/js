a3abf68cfd66d75ed3da6794f1e847d0fa6d5633
Yash <kumaryashcse@gmail.com>
2023-05-11 01:07:04 +0530

    addresses

--- packages/sdk/src/evm/zksync/constants/addresses.ts
+++ packages/sdk/src/evm/zksync/constants/addresses.ts
@@ -21,10 +21,10 @@ export const CONTRACT_ADDRESSES: Record<
 export const IMPLEMENTATIONS: Record<number, Record<string, string>> = {
   [280]: {
     DropERC721: "0xA8E28D98203848401A4f924358e6c337153D0f04",
-    DropERC1155: "",
-    DropERC20: "",
+    DropERC1155: "0xf3C7d3F0AA374a2D32489929e24D3e9313Aec8bb",
+    DropERC20: "0x4027561E163a420c4e5Db46E07EBd581CAa8Bb62",
     SignatureDrop: "",
-    Marketplace: "",
+    Marketplace: "0xA77041a7A34A67D7285c8d3550110e441009436e",
     MarketplaceV3: "",
   },
 };
