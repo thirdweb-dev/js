28744ef98c5c8b62c0d25ea7e9c7c0af92460024
Yash <kumaryashcse@gmail.com>
2023-05-11 01:58:39 +0530

    marketplace-v3 address

--- packages/sdk/src/evm/zksync/constants/addresses.ts
+++ packages/sdk/src/evm/zksync/constants/addresses.ts
@@ -25,7 +25,7 @@ export const IMPLEMENTATIONS: Record<number, Record<string, string>> = {
     DropERC20: "0x4027561E163a420c4e5Db46E07EBd581CAa8Bb62",
     SignatureDrop: "",
     Marketplace: "0xA77041a7A34A67D7285c8d3550110e441009436e",
-    MarketplaceV3: "",
+    MarketplaceV3: "0x292c324920d4120F928dA6f04548442fa10B8B0f",
   },
 };
 
