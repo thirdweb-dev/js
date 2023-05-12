9886c858d9c8d0f677aba6572dbf5cc6c876edf2
Yash <72552910+kumaryash90@users.noreply.github.com>
2023-05-13 03:54:31 +0530

    [SDK] Add support for ZKSync Era Testnet (#988)

--- packages/sdk/src/evm/core/classes/contract-deployer.ts
+++ packages/sdk/src/evm/core/classes/contract-deployer.ts
@@ -4,6 +4,7 @@ import {
 } from "../../common";
 import {
   computeCloneFactoryAddress,
+  convertParamValues,
   createTransactionBatches,
   deployContractDeterministic,
   deployWithThrowawayDeployer,
@@ -62,13 +63,7 @@ import { ContractRegistry } from "./registry";
 import { RPCConnectionHandler } from "./rpc-connection-handler";
 import { DeployTransaction, Transaction } from "./transactions";
 import { ThirdwebStorage } from "@thirdweb-dev/storage";
-import {
-  BigNumber,
-  BytesLike,
-  Contract,
-  ContractInterface,
-  ethers,
-} from "ethers";
+import { BytesLike, Contract, ContractInterface, ethers } from "ethers";
 import { EventEmitter } from "eventemitter3";
 import invariant from "tiny-invariant";
 import { z } from "zod";
@@ -643,6 +638,7 @@ export class ContractDeployer extends RPCConnectionHandler {
       const encodedInitializer = Contract.getInterface(
         implementationAbi,
       ).encodeFunctionData(initializerFunction, initializerArgs);
+
       const { TWProxy__factory } = await import(
         "@thirdweb-dev/contracts-js/factories/TWProxy__factory"
       );
@@ -775,7 +771,7 @@ export class ContractDeployer extends RPCConnectionHandler {
           extendedMetadata.factoryDeploymentData
             .implementationInitializerFunction,
         ).map((p) => p.type);
-        const paramValues = this.convertParamValues(
+        const paramValues = convertParamValues(
           initializerParamTypes,
           constructorParamValues,
         );
@@ -907,7 +903,7 @@ export class ContractDeployer extends RPCConnectionHandler {
       const constructorParamTypes = extractConstructorParamsFromAbi(
         compilerMetadata.abi,
       ).map((p) => p.type);
-      const paramValues = this.convertParamValues(
+      const paramValues = convertParamValues(
         constructorParamTypes,
         constructorParamValues,
       );
@@ -1106,44 +1102,5 @@ export class ContractDeployer extends RPCConnectionHandler {
   private hasLocalFactory() {
     // eslint-disable-next-line turbo/no-undeclared-env-vars
     return process.env.factoryAddress !== undefined;
-  }
-
-  private convertParamValues(
-    constructorParamTypes: string[],
-    constructorParamValues: any[],
-  ) {
-    // check that both arrays are same length
-    if (constructorParamTypes.length !== constructorParamValues.length) {
-      throw Error(
-        `Passed the wrong number of constructor arguments: ${constructorParamValues.length}, expected ${constructorParamTypes.length}`,
-      );
-    }
-    return constructorParamTypes.map((p, index) => {
-      if (p === "tuple" || p.endsWith("[]")) {
-        if (typeof constructorParamValues[index] === "string") {
-          return JSON.parse(constructorParamValues[index]);
-        } else {
-          return constructorParamValues[index];
-        }
-      }
-      if (p === "bytes32") {
-        invariant(
-          ethers.utils.isHexString(constructorParamValues[index]),
-          `Could not parse bytes32 value. Expected valid hex string but got "${constructorParamValues[index]}".`,
-        );
-        return ethers.utils.hexZeroPad(constructorParamValues[index], 32);
-      }
-      if (p.startsWith("bytes")) {
-        invariant(
-          ethers.utils.isHexString(constructorParamValues[index]),
-          `Could not parse bytes value. Expected valid hex string but got "${constructorParamValues[index]}".`,
-        );
-        return constructorParamValues[index];
-      }
-      if (p.startsWith("uint") || p.startsWith("int")) {
-        return BigNumber.from(constructorParamValues[index].toString());
-      }
-      return constructorParamValues[index];
-    });
   }
 }
---
+++ packages/sdk/src/evm/zksync/zksync-deploy-utils.ts
@@ -0,0 +1,133 @@
+import * as zk from "zksync-web3";
+import { twProxyArtifactZK } from "./temp-artifact/TWProxy";
+import {
+  convertParamValues,
+  extractFunctionParamsFromAbi,
+  fetchAndCacheDeployMetadata,
+} from "../common";
+import { BytesLike, Contract, Signer, ethers } from "ethers";
+import { ThirdwebStorage } from "@thirdweb-dev/storage";
+import { DeployOptions } from "../types";
+import { ThirdwebSDK } from "../core";
+import { getImplementation } from "./constants/addresses";
+import { DeploymentTransaction } from "../types/any-evm/deploy-data";
+
+export async function zkDeployContractFromUri(
+  publishMetadataUri: string,
+  constructorParamValues: any[],
+  signer: Signer,
+  storage: ThirdwebStorage,
+  chainId: number,
+  options?: DeployOptions,
+): Promise<string> {
+  const { compilerMetadata, extendedMetadata } =
+    await fetchAndCacheDeployMetadata(publishMetadataUri, storage);
+  const forceDirectDeploy = options?.forceDirectDeploy || false;
+
+  if (
+    extendedMetadata &&
+    extendedMetadata.factoryDeploymentData &&
+    (extendedMetadata.isDeployableViaProxy ||
+      extendedMetadata.isDeployableViaFactory) &&
+    !forceDirectDeploy
+  ) {
+    const implementationAddress = getImplementation(
+      chainId,
+      compilerMetadata.name,
+    );
+    if (!implementationAddress) {
+      throw new Error("Contract not supported yet.");
+    }
+
+    const initializerParamTypes = extractFunctionParamsFromAbi(
+      compilerMetadata.abi,
+      extendedMetadata.factoryDeploymentData.implementationInitializerFunction,
+    ).map((p) => p.type);
+
+    const paramValues = convertParamValues(
+      initializerParamTypes,
+      constructorParamValues,
+    );
+
+    const encodedInitializer = Contract.getInterface(
+      compilerMetadata.abi,
+    ).encodeFunctionData(
+      extendedMetadata.factoryDeploymentData.implementationInitializerFunction,
+      paramValues,
+    );
+
+    const proxyFactory = new zk.ContractFactory(
+      twProxyArtifactZK.abi,
+      twProxyArtifactZK.bytecode as BytesLike,
+      signer as zk.Signer,
+      "create",
+    );
+    const proxy = await proxyFactory.deploy(
+      implementationAddress,
+      encodedInitializer,
+    );
+
+    await proxy.deployed();
+
+    // register on multichain registry
+    await registerContractOnMultiChainRegistry(
+      proxy.address,
+      chainId,
+      compilerMetadata.metadataUri,
+    );
+
+    return proxy.address;
+  } else {
+    throw new Error("Contract not supported yet.");
+  }
+}
+
+export async function getZkTransactionsForDeploy(): Promise<
+  DeploymentTransaction[]
+> {
+  let transactions: DeploymentTransaction[] = [];
+
+  transactions.push({
+    contractType: "proxy",
+    addresses: [],
+  });
+  return transactions;
+}
+
+async function registerContractOnMultiChainRegistry(
+  address: string,
+  chainId: number,
+  metadataURI: string,
+) {
+  try {
+    // random wallet is fine here, we're doing gasless calls
+    const wallet = ethers.Wallet.createRandom();
+    const sdk = ThirdwebSDK.fromPrivateKey(wallet.privateKey, "polygon", {
+      gasless: {
+        openzeppelin: {
+          relayerUrl:
+            "https://api.defender.openzeppelin.com/autotasks/dad61716-3624-46c9-874f-0e73f15f04d5/runs/webhook/7d6a1834-dd33-4b7b-8af4-b6b4719a0b97/FdHMqyF3p6MGHw6K2nkLsv",
+          relayerForwarderAddress: "0xEbc1977d1aC2fe1F6DAaF584E2957F7c436fcdEF",
+        },
+        experimentalChainlessSupport: true,
+      },
+    });
+    const existingMeta = await sdk.multiChainRegistry.getContractMetadataURI(
+      chainId,
+      address,
+    );
+    if (existingMeta && existingMeta !== "") {
+      return true;
+    }
+    // add to multichain registry with metadata uri unlocks the contract on SDK/dashboard for everyone
+    await sdk.multiChainRegistry.addContract({
+      address,
+      chainId,
+      metadataURI,
+    });
+    return true;
+  } catch (e) {
+    console.log("Error registering contract on multi chain registry", e);
+    return false;
+  }
+}
