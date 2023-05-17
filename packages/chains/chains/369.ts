bba828cc09a07346855e788fe781ffd58bc45825
ikethirdweb <jorge@thirdweb.com>
2023-05-18 01:36:42 +0200

    [RN] clear unusued type

--- packages/chains/chains/369.ts
+++ packages/chains/chains/369.ts
@@ -1,6 +1,6 @@
 import type { Chain } from "../src/types";
 export default {
-  "name": "PulseChain",
+  "name": "PulseChain Mainnet",
   "shortName": "pls",
   "chain": "PLS",
   "chainId": 369,
@@ -8,22 +8,10 @@ export default {
   "infoURL": "https://pulsechain.com/",
   "rpc": [
     "https://pulsechain.rpc.thirdweb.com/${THIRDWEB_API_KEY}",
-    "https://rpc.pulsechain.com/",
-    "wss://rpc.pulsechain.com/"
-  ],
-  "features": [
-    {
-      "name": "EIP155"
-    },
-    {
-      "name": "EIP1559"
-    }
+    "https://rpc.mainnet.pulsechain.com/",
+    "wss://rpc.mainnet.pulsechain.com/"
   ],
   "faucets": [],
-  "ens": {
-    "registry": "0x00000000000C2E074eC69A0dFb2997BA6C7d2e1e"
-  },
-  "status": "incubating",
   "nativeCurrency": {
     "name": "Pulse",
     "symbol": "PLS",
