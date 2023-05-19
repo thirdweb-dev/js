bb8c74f9be58ee9e074cc556513b501434a901ab
Joaquim Verges <joaquim.verges@gmail.com>
2023-05-19 10:39:27 -0700

    fix buffer

--- packages/wallets/src/evm/connectors/smart-wallet/lib/base-api.ts
+++ packages/wallets/src/evm/connectors/smart-wallet/lib/base-api.ts
@@ -15,6 +15,11 @@ import {
 import { TransactionDetailsForUserOp } from "./transaction-details";
 import { getUserOpHashV06 } from "./utils";
 import { DUMMY_PAYMASTER_AND_DATA, SIG_SIZE } from "./paymaster";
+import { Buffer } from "buffer";
+
+if (typeof window !== undefined) {
+  window.Buffer = Buffer;
+}
 
 export interface BaseApiParams {
   provider: providers.Provider;
