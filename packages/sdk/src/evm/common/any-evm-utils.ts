d0bbc25a39c7c9b70968d4b26b34a1b82370deaf
Yash <kumaryashcse@gmail.com>
2023-05-23 01:26:51 +0530

    default to eip-155 for any error

--- packages/sdk/src/evm/common/any-evm-utils.ts
+++ packages/sdk/src/evm/common/any-evm-utils.ts
@@ -131,7 +131,7 @@ export async function isEIP155Enforced(
     if (matchError(errorMsg) || matchError(errorJson)) {
       return true;
     }
-    return false;
+    return true;
   }
   return false;
 }
