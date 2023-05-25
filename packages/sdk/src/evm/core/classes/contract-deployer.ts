ee4c7de25cb63f99f33b90da8e26293bbfbe6f3e
Manan Tank <manantankm@gmail.com>
2023-05-26 03:35:33 +0530

    [sdk] reduce circular deps (#1071)

--- packages/sdk/src/evm/core/classes/contract-deployer.ts
+++ packages/sdk/src/evm/core/classes/contract-deployer.ts
@@ -54,11 +54,11 @@ import {
   VoteContractDeployMetadata,
 } from "../../types";
 import { ThirdwebSDK } from "../sdk";
+import { NetworkInput } from "../types";
 import {
   DeploySchemaForPrebuiltContractType,
-  NetworkInput,
   PrebuiltContractType,
-} from "../types";
+} from "../../contracts";
 import { ContractFactory } from "./factory";
 import { ContractRegistry } from "./registry";
 import { RPCConnectionHandler } from "./rpc-connection-handler";
