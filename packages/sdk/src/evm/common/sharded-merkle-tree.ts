6aee0413439b9ca408879bbb3c38c538c89d01af
Manan Tank <manantankm@gmail.com>
2023-06-02 03:37:36 +0530

    [sdk] Reduce circular dependencies (#1112)

--- packages/sdk/src/evm/common/sharded-merkle-tree.ts
+++ packages/sdk/src/evm/common/sharded-merkle-tree.ts
@@ -12,7 +12,7 @@ import { fetchCurrencyMetadata } from "./currency/fetchCurrencyMetadata";
 import { ThirdwebStorage } from "@thirdweb-dev/storage";
 import { ethers, utils } from "ethers";
 import { MerkleTree } from "merkletreejs";
-import { parseSnapshotInputs } from "./snapshots";
+import { parseSnapshotInputs } from "./parseSnapshotInputs";
 
 // shard using the first 2 hex character of the address
 // this splits the merkle tree into 256 shards
