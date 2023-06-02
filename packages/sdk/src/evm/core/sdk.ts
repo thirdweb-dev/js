ebbde7a34d1bb1548ed11cf723cdc0a0cde83296
Yash <kumaryashcse@gmail.com>
2023-06-03 03:10:10 +0530

    check standard deploy type

--- packages/sdk/src/evm/core/sdk.ts
+++ packages/sdk/src/evm/core/sdk.ts
@@ -1691,7 +1691,8 @@ export class ContractDeployer extends RPCConnectionHandler {
       if (
         extendedMetadata &&
         extendedMetadata.factoryDeploymentData &&
-        !forceDirectDeploy
+        !forceDirectDeploy &&
+        extendedMetadata.deployType !== "standard"
       ) {
         if (extendedMetadata.deployType === "customFactory") {
           return await this.deployViaCustomFactory.prepare(
