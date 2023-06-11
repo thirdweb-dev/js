1f02a4743ce751f6d2cdfaab61e0c8c5fa94f92c
Andrey <zertsalov96@yandex.ru>
2023-06-11 15:00:40 +0100

    add zerion as default wallet

--- packages/react/src/wallet/wallets/defaultWallets.ts
+++ packages/react/src/wallet/wallets/defaultWallets.ts
@@ -2,9 +2,11 @@ import { WalletConfig } from "@thirdweb-dev/react-core";
 import { coinbaseWallet } from "./coinbase/coinbaseWallet";
 import { metamaskWallet } from "./metamask/metamaskWallet";
 import { walletConnectV1 } from "./walletConnectV1";
+import { zerionWallet } from "./zerion/zerionWallet";
 
 export const defaultWallets: WalletConfig<any>[] = [
   metamaskWallet(),
   coinbaseWallet(),
   walletConnectV1(),
+  zerionWallet(),
 ];
