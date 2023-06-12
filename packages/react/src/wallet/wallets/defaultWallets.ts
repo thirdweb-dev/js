d79643c5b2010a14ade3091d221f87d0a98868bd
Andrey <zertsalov96@yandex.ru>
2023-06-12 17:45:54 +0100

    remove zerion from default wallets

--- packages/react/src/wallet/wallets/defaultWallets.ts
+++ packages/react/src/wallet/wallets/defaultWallets.ts
@@ -2,11 +2,9 @@ import { WalletConfig } from "@thirdweb-dev/react-core";
 import { coinbaseWallet } from "./coinbase/coinbaseWallet";
 import { metamaskWallet } from "./metamask/metamaskWallet";
 import { walletConnectV1 } from "./walletConnectV1";
-import { zerionWallet } from "./zerion/zerionWallet";
 
 export const defaultWallets: WalletConfig<any>[] = [
   metamaskWallet(),
   coinbaseWallet(),
   walletConnectV1(),
-  zerionWallet(),
 ];
