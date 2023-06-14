7803d1f2da71341071248b9dd77b3ee355775227
Kien Ngo <kenseverus@gmail.com>
2023-06-14 00:30:47 -0400

    Fix

--- packages/sdk/src/evm/contracts/index.ts
+++ packages/sdk/src/evm/contracts/index.ts
@@ -23,7 +23,7 @@ import { ethers } from "ethers";
 import type { SmartContract as SmartContractType } from "./smart-contract";
 import { getCompositeABIfromRelease } from "../common/plugin/getCompositeABIfromRelease";
 import { getCompositePluginABI } from "../common/plugin/getCompositePluginABI";
-import { TOKEN_INITIALIZER_ROLES } from "./TokenInitializerRoles";
+import { CONTRACT_ROLES } from "./contractRoles";
 
 type InitalizeParams = [
   network: NetworkInput,
@@ -525,7 +525,7 @@ export const TokenInitializer = {
   name: "TokenERC20" as const,
   contractType: "token" as const,
   schema: TokenErc20ContractSchema,
-  roles: TOKEN_INITIALIZER_ROLES,
+  roles: CONTRACT_ROLES,
   initialize: async (
     ...[network, address, storage, options]: InitalizeParams
   ) => {
