ee3118678f1871a7812e1ad837f2868756a573c6
Marfuen <marfuen98@gmail.com>
2023-06-15 09:57:47 -0700

    update web3py

--- packages/cli/src/core/detection/libraries/web3py.ts
+++ packages/cli/src/core/detection/libraries/web3py.ts
@@ -12,7 +12,7 @@ export default class Web3PyDetector implements LibraryDetector {
     );
 
     return (
-      !!dependencies.find(dep => dep === "web3py") || !!devDependencies.find(dep => dep === "web3py") || false
+      !!dependencies.find(dep => dep === "web3") || !!devDependencies.find(dep => dep === "web3") || false
     );
   }
 }
