86d79154c04a8f665834c56f9cfeed691f355825
Marfuen <marfuen98@gmail.com>
2023-06-15 17:55:35 -0700

    make expo detection more specific

--- packages/cli/src/core/detection/frameworks/expo.ts
+++ packages/cli/src/core/detection/frameworks/expo.ts
@@ -16,15 +16,9 @@ export default class ExpoDetector implements FrameworkDetector {
     const packageJson = readFileSync(packageJsonPath);
     const { dependencies, devDependencies } = parsePackageJson(packageJson);
 
-    const additionalFilesToCheck = ["/app.json", "/app.config.js"];
-    const additionalFilesExist = additionalFilesToCheck.some((file) =>
-      existsSync(path + file),
-    );
-
     const expoDependencyExists =
       !!dependencies["expo"] ||
       !!devDependencies["expo"] ||
-      additionalFilesExist ||
       false;
 
     return expoDependencyExists;
