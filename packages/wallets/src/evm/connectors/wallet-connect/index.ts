7d61a9674e110400971e72e5eaf5154a41057576
ikethirdweb <jorge@thirdweb.com>
2023-06-21 11:31:01 -0400

    [RC/Wallets] Improve error messages for wc and auth

--- packages/wallets/src/evm/connectors/wallet-connect/index.ts
+++ packages/wallets/src/evm/connectors/wallet-connect/index.ts
@@ -236,8 +236,9 @@ export class WalletConnectConnector extends WagmiConnector<
 
   async switchChain(chainId: number) {
     const chain = this.chains.find((chain_) => chain_.chainId === chainId);
+
     if (!chain) {
-      throw new SwitchChainError(new Error("chain not found on connector."));
+      throw new SwitchChainError("Chain not found on connector.");
     }
 
     try {
