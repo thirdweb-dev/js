1ce8558df47186bcba5ee8564fdb04583bf115dd
iketw <121973632+iketw@users.noreply.github.com>
2023-06-21 12:28:16 -0400

    [Wallets] Fix switchChain in WCV2 connector (#1214)

--- packages/wallets/src/evm/connectors/wallet-connect/index.ts
+++ packages/wallets/src/evm/connectors/wallet-connect/index.ts
@@ -250,17 +250,18 @@ export class WalletConnectConnector extends WagmiConnector<
       const isChainApproved = namespaceChains.includes(chainId);
 
       if (!isChainApproved && namespaceMethods.includes(ADD_ETH_CHAIN_METHOD)) {
+        const blockExplorerUrls = chain.explorers?.length
+          ? { blockExplorerUrls: [chain.explorers[0].url] }
+          : {};
         await provider.request({
           method: ADD_ETH_CHAIN_METHOD,
           params: [
             {
               chainId: utils.hexValue(chain.chainId),
-              blockExplorerUrls: [
-                chain.explorers?.length ? chain.explorers[0] : undefined,
-              ],
               chainName: chain.name,
               nativeCurrency: chain.nativeCurrency,
               rpcUrls: [...chain.rpc],
+              ...blockExplorerUrls,
             },
           ],
         });
@@ -282,6 +283,7 @@ export class WalletConnectConnector extends WagmiConnector<
       if (/user rejected request/i.test(message)) {
         throw new UserRejectedRequestError(error);
       }
+
       throw new SwitchChainError(error);
     }
   }
