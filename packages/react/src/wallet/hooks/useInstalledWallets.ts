0e8896a968e7f5c3fea69b5a3b9ce01d2f74f229
Junya Kono <junya03dance@gmail.com>
2023-06-21 23:26:15 +0900

    update: Add zerion to useInstalledWallets

--- packages/react/src/wallet/hooks/useInstalledWallets.ts
+++ packages/react/src/wallet/hooks/useInstalledWallets.ts
@@ -3,18 +3,20 @@ import { assertWindowEthereum } from "@thirdweb-dev/wallets";
 export function useInstalledWallets() {
   let isMetamaskInstalled = false;
   let isCoinbaseWalletInstalled = false;
+  let isZerionWalletInstalled = false;
 
   if (assertWindowEthereum(globalThis.window)) {
     isMetamaskInstalled = globalThis.window.ethereum?.isMetaMask;
     isCoinbaseWalletInstalled =
       globalThis.window.ethereum?.isCoinbaseWallet ||
-      globalThis.window.ethereum?.providers?.some((p) => p.isCoinbaseWallet) ||
-      false;
+      globalThis.window.ethereum?.providers?.some((p) => p.isCoinbaseWallet);
+    isZerionWalletInstalled = globalThis.window.ethereum?.isZerion;
   }
 
   const installedWallets = {
     metamask: !!isMetamaskInstalled,
     coinbaseWallet: !!isCoinbaseWalletInstalled,
+    zerionWallet: !!isZerionWalletInstalled,
   };
 
   return installedWallets;
