517b370d6758f2a0f6a6b9094c8d148638b2080d
Yash <kumaryashcse@gmail.com>
2023-06-27 01:31:32 +0530

    test custom factory

--- packages/sdk/test/evm/new-publish-deploy.test.ts
+++ packages/sdk/test/evm/new-publish-deploy.test.ts
@@ -1,8 +1,13 @@
 import { SignerWithAddress } from "@nomiclabs/hardhat-ethers/signers";
 import { fastForwardTime, jsonProvider, sdk, signers } from "./before-setup";
 import { expect, assert } from "chai";
-import { ThirdwebSDK } from "../../src/evm";
+import {
+  ThirdwebSDK,
+  fetchAndCacheDeployMetadata,
+  fetchExtendedReleaseMetadata,
+} from "../../src/evm";
 import { BigNumber } from "ethers";
+import { mockUploadWithCID } from "./mock/MockStorage";
 
 describe("New Publish Flow", async () => {
   let adminWallet: SignerWithAddress,
@@ -17,7 +22,9 @@ describe("New Publish Flow", async () => {
   beforeEach(async () => {
     await jsonProvider.send("hardhat_reset", []);
     [adminWallet, samWallet, bobWallet, , , , , w4] = signers;
-    realSDK = new ThirdwebSDK(adminWallet);
+    realSDK = new ThirdwebSDK(adminWallet, {
+      gatewayUrls: ["https://gateway.ipfscdn.io/ipfs/"],
+    });
   });
 
   describe("Deploy newly published contract", async () => {
@@ -56,7 +63,90 @@ describe("New Publish Flow", async () => {
       expect(deployer).to.equal(adminWallet.address);
     });
 
-    it("should deploy with custom factory", async () => {});
+    it("should deploy with custom factory", async () => {
+      // Re-upload with mock storage, enable all networks
+      const { compilerMetadata, extendedMetadata } =
+        await fetchAndCacheDeployMetadata(
+          customFactoryPublishUri,
+          realSDK.storage,
+        );
+      await mockUploadWithCID(
+        compilerMetadata.metadataUri.replace("ipfs://", ""),
+        JSON.stringify(compilerMetadata.metadata),
+      );
+      await mockUploadWithCID("bytecode1", compilerMetadata.bytecode);
+      await mockUploadWithCID(
+        "cid1",
+        JSON.stringify({
+          ...extendedMetadata,
+          metadataUri: compilerMetadata.metadataUri,
+          bytecodeUri: "bytecode1",
+          networksForDeployment: {
+            allNetworks: true,
+            networksEnabled: [],
+          },
+          version: "1.0.2",
+        }),
+      );
+
+      // deploy custom-factory `SplitMain`
+      const customFactoryAddress = await sdk.deployer.deployContractFromUri(
+        "cid1",
+        [],
+      );
+
+      // Re-upload contract metadata with mock storage
+      // set custom-factory address on hardhat chain
+      const contractMetadata = await fetchAndCacheDeployMetadata(
+        publishUriWithCustomFactoryDeploy,
+        realSDK.storage,
+      );
+      await mockUploadWithCID(
+        contractMetadata.compilerMetadata.metadataUri.replace("ipfs://", ""),
+        JSON.stringify(contractMetadata.compilerMetadata.metadata),
+      );
+      await mockUploadWithCID(
+        "bytecode2",
+        contractMetadata.compilerMetadata.bytecode,
+      );
+      await mockUploadWithCID(
+        "cid2",
+        JSON.stringify({
+          ...contractMetadata.extendedMetadata,
+          metadataUri: contractMetadata.compilerMetadata.metadataUri,
+          bytecodeUri: "bytecode2",
+          factoryDeploymentData: {
+            ...contractMetadata.extendedMetadata?.factoryDeploymentData,
+            implementationAddresses: {},
+            customFactoryInput: {
+              factoryFunction: "createSplit",
+              params: [],
+              customFactoryAddresses: {
+                "31337": customFactoryAddress,
+              },
+            },
+          },
+          networksForDeployment: {
+            allNetworks: true,
+            networksEnabled: [],
+          },
+          version: "1.0.3",
+        }),
+      );
+
+      // deploy contract via custom-factory
+      const contractAddress = await sdk.deployer.deployContractFromUri("cid2", [
+        [await bobWallet.getAddress(), await adminWallet.getAddress()],
+        [500000, 500000],
+        0,
+        await adminWallet.getAddress(),
+      ]);
+
+      const splitContract = await sdk.getContract(contractAddress);
+      const deployingAddress = await splitContract.call("splitMain");
+
+      expect(deployingAddress).to.equal(customFactoryAddress);
+    });
 
     it("should deploy with auto factory", async () => {});
   });
