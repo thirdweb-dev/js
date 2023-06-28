a222bb344b0cd9bbd8356c425cb2466325551021
Nacho Iacovino <50103937+nachoiacovino@users.noreply.github.com>
2023-06-28 02:17:39 +0200

    Add environment as query param on build tab (#1342)

--- contract-ui/tabs/code/components/code-overview.tsx
+++ contract-ui/tabs/code/components/code-overview.tsx
@@ -47,6 +47,8 @@ import {
 import { DASHBOARD_THIRDWEB_API_KEY } from "constants/rpc";
 import { constants } from "ethers";
 import { useSupportedChain } from "hooks/chains/configureChains";
+import { useSingleQueryParam } from "hooks/useQueryParam";
+import { useRouter } from "next/router";
 import { useMemo, useState } from "react";
 import { Button, Card, Heading, Link, Text, TrackedLink } from "tw-components";
 
@@ -305,7 +307,14 @@ export const CodeOverview: React.FC<CodeOverviewProps> = ({
   chain,
   noSidebar = false,
 }) => {
-  const [environment, setEnvironment] = useState<CodeEnvironment>("javascript");
+  const defaultEnvironment = useSingleQueryParam(
+    "environment",
+  ) as CodeEnvironment;
+  const [environment, setEnvironment] = useState<CodeEnvironment>(
+    defaultEnvironment || "javascript",
+  );
+  const router = useRouter();
+
   const [tab, setTab] = useState("write");
   const { data } = useFeatureContractCodeSnippetQuery(environment);
   const enabledExtensions = useContractEnabledExtensions(abi);
@@ -753,9 +762,13 @@ export const CodeOverview: React.FC<CodeOverviewProps> = ({
           <Flex flexDir="column" gap={2}>
             <Text>Choose a language:</Text>
             <Select
-              onChange={(e) =>
-                setEnvironment(e.target.value as CodeEnvironment)
-              }
+              onChange={(e) => {
+                router.push(
+                  `/${chainName}/${contractAddress}/code?environment=${e.target.value}`,
+                );
+                setEnvironment(e.target.value as CodeEnvironment);
+              }}
+              value={environment}
             >
               <option value="javascript">JavaScript</option>
               <option value="react">React</option>
