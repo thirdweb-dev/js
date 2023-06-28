9bf0d38a5a0eb70eb81dabbfddb0e67da97361c6
Jonas Daniels <jonas.daniels@outlook.com>
2023-06-28 00:40:51 -0700

    remove blocknumber handler, irrelevant

--- packages/sdk-core/src/read/chain/blockNumber.ts
+++
@@ -1,6 +0,0 @@
-import type { Chain } from "@thirdweb-dev/chains";
-import { createClient } from "../client";
-
-export function getBlockNumber({ chain }: { chain: Chain }) {
-  return createClient(chain).getBlockNumber();
-}
