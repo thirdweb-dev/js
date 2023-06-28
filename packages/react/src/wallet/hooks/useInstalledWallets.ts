d794b8f9f41dbd7e461ef07d0808c5b6d2f85515
Junya Kono <junya03dance@gmail.com>
2023-06-29 01:50:36 +0900

    update: Add zerion to useInstalledWallets (#1210)
    Co-authored-by: Jonas Daniels <jonas.daniels@outlook.com>

--- packages/react/src/wallet/hooks/useInstalledWallets.ts
+++ packages/react/src/wallet/hooks/useInstalledWallets.ts
@@ -3,22 +3,22 @@ import { assertWindowEthereum } from "@thirdweb-dev/wallets";
 export function useInstalledWallets() {
   let isMetamaskInstalled = false;
   let isCoinbaseWalletInstalled = false;
-  let isTrustWalletInstalled: boolean | undefined = false;
+  let isZerionWalletInstalled = false;
+  let isTrustWalletInstalled = false;
 
   if (assertWindowEthereum(globalThis.window)) {
-    isMetamaskInstalled = globalThis.window.ethereum?.isMetaMask;
+    isMetamaskInstalled = !!globalThis.window.ethereum?.isMetaMask;
     isCoinbaseWalletInstalled =
-      globalThis.window.ethereum?.isCoinbaseWallet ||
-      globalThis.window.ethereum?.providers?.some((p) => p.isCoinbaseWallet) ||
-      false;
-    isTrustWalletInstalled = globalThis.window.ethereum?.isTrust;
+      !!globalThis.window.ethereum?.isCoinbaseWallet ||
+      !!globalThis.window.ethereum?.providers?.some((p) => p.isCoinbaseWallet);
+    isZerionWalletInstalled = !!globalThis.window.ethereum?.isZerion;
+    isTrustWalletInstalled = !!globalThis.window.ethereum?.isTrust;
   }
 
-  const installedWallets = {
-    metamask: !!isMetamaskInstalled,
-    coinbaseWallet: !!isCoinbaseWalletInstalled,
-    trustWallet: !!isTrustWalletInstalled,
+  return {
+    metamask: isMetamaskInstalled,
+    coinbaseWallet: isCoinbaseWalletInstalled,
+    trustWallet: isTrustWalletInstalled,
+    zerionWallet: isZerionWalletInstalled,
   };
-
-  return installedWallets;
 }
