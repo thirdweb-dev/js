3c2f844cbb0aaa0273fafb3a0b6dfbaab51dedd8
ikethirdweb <jorge@thirdweb.com>
2023-06-30 08:25:32 -0400

    Improve Label customization

--- packages/react-native/src/evm/components/base/Label.tsx
+++ packages/react-native/src/evm/components/base/Label.tsx
@@ -1,21 +1,41 @@
-import Box from "./Box";
+import { useAppTheme } from "../../styles/hooks";
 import Text from "./Text";
+import { StyleSheet } from "react-native";
 
 type LabelProps = {
   text: string;
-};
-export const Label = ({ text }: LabelProps) => {
+} & (typeof Text)["arguments"];
+
+export const Label = ({ text, ...props }: LabelProps) => {
+  const theme = useAppTheme();
   return (
-    <Box
-      justifyContent="center"
-      flexDirection="row"
-      alignItems="center"
-      paddingVertical="xxs"
-      paddingHorizontal="xs"
-      borderRadius="lg"
-      backgroundColor="labelBackground"
+    // <Box
+    //   justifyContent="center"
+    //   flexDirection="row"
+    //   alignItems="center"
+    //   paddingVertical="xxs"
+    //   paddingHorizontal="xs"
+    //   borderRadius="lg"
+    //   backgroundColor="labelBackground"
+    // >
+    <Text
+      variant="bodySmall"
+      style={{
+        ...style.text,
+        backgroundColor: theme.colors.labelBackground,
+        ...props,
+      }}
     >
-      <Text variant="bodyLarge">{text}</Text>
-    </Box>
+      {text}
+    </Text>
   );
 };
+
+const style = StyleSheet.create({
+  text: {
+    textAlign: "center",
+    paddingVertical: 8,
+    paddingHorizontal: 3,
+    borderRadius: 8,
+  },
+});
