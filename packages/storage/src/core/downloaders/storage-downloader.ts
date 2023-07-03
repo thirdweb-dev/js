afd9acbd8e33de1206a38d4ae0236b3e2483296d
Danny Friday <nessup@gmail.com>
2023-07-03 19:52:27 -0400

    Fix status

--- packages/storage/src/core/downloaders/storage-downloader.ts
+++ packages/storage/src/core/downloaders/storage-downloader.ts
@@ -49,7 +49,7 @@ export class StorageDownloader implements IStorageDownloader {
     }
 
     if (resOrErr instanceof Response) {
-      if (resOrErr.status === 403) {
+      if (resOrErr.status === 410) {
         // Don't retry if the content is blacklisted
         console.error(
           `Request to ${resolvedUri} failed with status 403 - Forbidden. This content is probably blacklisted. Search VirusTotal for this URL: ${resolvedUri} `
