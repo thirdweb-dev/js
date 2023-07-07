e303f18093933826f54ff62eac668355c0ef05e3
ikethirdweb <jorge@thirdweb.com>
2023-07-06 20:18:41 -0400

    [RN] Improve svg rendering

--- packages/react-native/src/evm/components/base/ImageSvgUri.tsx
+++ packages/react-native/src/evm/components/base/ImageSvgUri.tsx
@@ -1,4 +1,5 @@
 import { resolveIpfsUri } from "@thirdweb-dev/react-core";
+import { useState } from "react";
 import { Image } from "react-native";
 import { SvgUri } from "react-native-svg";
 
@@ -13,15 +14,29 @@ const ImageSvgUri = ({
   height: number;
   imageAlt?: string;
 }) => {
-  const isSvg = imageUrl.toLowerCase().endsWith(".svg");
+  const isSvg =
+    imageUrl.toLowerCase().endsWith(".svg") ||
+    !imageUrl.toLowerCase().endsWith(".png") ||
+    !imageUrl.toLowerCase().endsWith(".jpg");
   const resolvedImageUrl = resolveIpfsUri(imageUrl) || "";
+
+  const [error, setError] = useState(false);
 
   if (!resolvedImageUrl || resolvedImageUrl === "") {
     return null;
   }
 
-  if (isSvg) {
-    return <SvgUri width={width} height={height} uri={resolvedImageUrl} />;
+  // always try to render svg if we cannot detect the url extension
+  // if error then try to render regular image
+  if (isSvg || error) {
+    return (
+      <SvgUri
+        width={width}
+        height={height}
+        uri={resolvedImageUrl}
+        onError={() => setError(true)}
+      />
+    );
   } else {
     return (
       <Image
