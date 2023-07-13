07c7d97c288063f7a5a65819e37ebe3f51095faf
Jonas Daniels <jonas.daniels@outlook.com>
2023-07-13 13:51:55 -0700

    [service-utils] - initially add package (#1333)
    Co-authored-by: Tair Asim <hi@tairasim.com>

---
+++ packages/service-utils/src/auth/types.ts
@@ -0,0 +1,68 @@
+import { KVNamespace, ExecutionContext } from "@cloudflare/workers-types";
+import { ServiceName } from "../types";
+
+export interface AuthOptions {
+  clientId: string;
+  secretHash?: string;
+  bundleId?: string;
+  origin?: string;
+}
+
+export interface AuthorizeCFWorkerOptions {
+  ctx: ExecutionContext;
+  kvStore: KVNamespace<string>;
+  authOptions: AuthOptions;
+  serviceConfig: ServiceConfiguration;
+  validations: AuthorizationValidations;
+}
+
+export interface AuthorizeNodeServiceOptions {
+  authOptions: AuthOptions;
+  serviceConfig: ServiceConfiguration;
+  validations: AuthorizationValidations;
+}
+
+export interface ServiceConfiguration {
+  apiUrl: string;
+  scope: ServiceName;
+  serviceKey: string;
+  cachedKey?: ApiKey;
+  cacheTtl?: number;
+  onRefetchComplete?: (key: ApiKey) => void;
+}
+
+export interface AuthorizationValidations {
+  serviceTargetAddresses?: string[];
+  serviceAction?: string;
+}
+
+export interface AuthorizationResponse {
+  authorized: boolean;
+  errorMessage?: string;
+  errorCode?: string;
+  statusCode?: number;
+  data?: ApiKey;
+}
+
+export interface ApiResponse {
+  data: ApiKey | null;
+  error: {
+    code: string;
+    statusCode: number;
+    message: string;
+  };
+}
+
+export interface ApiKey {
+  id: string;
+  key: string;
+  secretHash: string;
+  walletAddresses: string[];
+  domains: string[];
+  bundleIds: string[];
+  services: {
+    name: string;
+    targetAddresses: string[];
+    actions: string[];
+  }[];
+}
