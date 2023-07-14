7bb14ccf2f84e2e65ea1b63f944eefdf9c6db1aa
Jonas Daniels <jonas.daniels@outlook.com>
2023-07-14 00:00:08 -0700

    [service-utils] - rewrite

---
+++ packages/service-utils/src/core/authorize/client.ts
@@ -0,0 +1,73 @@
+import type { ApiKeyMetadata } from "../api";
+import type { AuthorizationResult } from "./types";
+
+type ClientAuthorizationPayload = {
+  secretKeyHash: string | null;
+  bundleId: string | null;
+  origin: string | null;
+};
+
+export function authorizeClient(
+  authOptions: ClientAuthorizationPayload,
+  apiKeyMeta: ApiKeyMetadata,
+): AuthorizationResult {
+  const { origin, secretKeyHash: providedSecretHash } = authOptions;
+  const { domains, secretHash } = apiKeyMeta;
+
+  if (providedSecretHash) {
+    if (secretHash !== providedSecretHash) {
+      return {
+        authorized: false,
+        errorMessage: "The secret is invalid.",
+        errorCode: "SECRET_INVALID",
+        status: 401,
+      };
+    }
+    return {
+      authorized: true,
+      apiKeyMeta,
+    };
+  }
+
+  // validate domains
+  if (origin) {
+    if (
+      // find matching domain, or if all domains allowed
+      domains.find((d) => {
+        if (d === "*") {
+          return true;
+        }
+
+        // If the allowedDomain has a wildcard,
+        // we'll check that the ending of our domain matches the wildcard
+        if (d.startsWith("*.")) {
+          const domainRoot = d.slice(2);
+          return origin.endsWith(domainRoot);
+        }
+
+        // If there's no wildcard, we'll check for an exact match
+        return d === origin;
+      })
+    ) {
+      return {
+        authorized: true,
+        apiKeyMeta,
+      };
+    }
+
+    return {
+      authorized: false,
+      errorMessage: "The origin is not authorized for this key.",
+      errorCode: "ORIGIN_UNAUTHORIZED",
+      status: 401,
+    };
+  }
+
+  // FIXME: validate bundle id
+  return {
+    authorized: false,
+    errorMessage: "The keys are invalid.",
+    errorCode: "UNAUTHORIZED",
+    status: 401,
+  };
+}
---
+++ packages/service-utils/src/node/index.ts
@@ -0,0 +1,128 @@
+import type { IncomingHttpHeaders, IncomingMessage } from "node:http";
+import { createHash } from "node:crypto";
+import type { AuthorizationInput } from "../core/authorize";
+import type { CoreServiceConfig } from "../core/api";
+import { authorize } from "../core/authorize";
+import type { AuthorizationResult } from "../core/authorize/types";
+
+export * from "../core/services";
+
+type NodeServiceConfig = CoreServiceConfig;
+
+export async function authorizeWorker(
+  authInput: AuthInput & { targetAddress?: string },
+  serviceConfig: NodeServiceConfig,
+): Promise<AuthorizationResult> {
+  let authData;
+  try {
+    authData = await extractAuthorizationData(authInput);
+  } catch (e) {
+    if (e instanceof Error && e.message === "KEY_CONFLICT") {
+      return {
+        authorized: false,
+        status: 400,
+        errorMessage: "Please pass either a client id or a secret key.",
+        errorCode: "KEY_CONFLICT",
+      };
+    }
+    return {
+      authorized: false,
+      status: 500,
+      errorMessage: "Internal Server Error",
+      errorCode: "INTERNAL_SERVER_ERROR",
+    };
+  }
+
+  return await authorize(authData, serviceConfig);
+}
+
+export type AuthInput = {
+  clientId?: string;
+  req: IncomingMessage;
+};
+
+function getHeader(
+  headers: IncomingHttpHeaders,
+  headerName: string,
+): string | null {
+  const header = headers[headerName];
+  if (Array.isArray(header)) {
+    return header[0];
+  }
+  return header ?? null;
+}
+
+export function extractAuthorizationData(
+  authInput: AuthInput,
+): AuthorizationInput {
+  if (!authInput.req.url) {
+    throw new Error("no req.url in authInput.req");
+  }
+  const requestUrl = new URL(authInput.req.url, authInput.req.headers.host);
+  const headers = authInput.req.headers;
+  const secretKey = getHeader(headers, "x-secret-key");
+  // prefer clientId that is explicitly passed in
+  let clientId = authInput.clientId ?? null;
+
+  if (!clientId) {
+    // next preference is clientId from header
+    clientId = getHeader(headers, "x-client-id");
+  }
+
+  // next preference is search param
+  if (!clientId) {
+    clientId = requestUrl.searchParams.get("clientId");
+  }
+  // bundle id from header is first preference
+  let bundleId = getHeader(headers, "x-bundle-id");
+
+  // next preference is search param
+  if (!bundleId) {
+    bundleId = requestUrl.searchParams.get("bundleId");
+  }
+
+  let origin = getHeader(headers, "origin");
+  // if origin header is not available we'll fall back to referrer;
+  if (!origin) {
+    origin = getHeader(headers, "referer");
+  }
+  // if we have an origin at this point, normalize it
+  if (origin) {
+    try {
+      origin = new URL(origin).hostname;
+    } catch (e) {
+      console.warn("failed to parse origin", origin, e);
+    }
+  }
+
+  // handle if we a secret key is passed in the headers
+  let secretKeyHash: string | null = null;
+  if (secretKey) {
+    // hash the secret key
+    secretKeyHash = hashSecretKey(secretKey);
+    // derive the client id from the secret key hash
+    const derivedClientId = deriveClientIdFromSecretKeyHash(secretKeyHash);
+    // if we already have a client id passed in we need to make sure they match
+    if (clientId && clientId !== derivedClientId) {
+      throw new Error("KEY_CONFLICT");
+    }
+    // otherwise set the client id to the derived client id (client id based off of secret key)
+    clientId = derivedClientId;
+  }
+
+  return {
+    secretKeyHash,
+    secretKey,
+    clientId,
+    origin,
+    bundleId,
+  };
+}
+
+export function hashSecretKey(secretKey: string) {
+  return createHash("sha256").update(secretKey).digest("hex");
+}
+
+export function deriveClientIdFromSecretKeyHash(secretKeyHash: string) {
+  return secretKeyHash.slice(0, 32);
+}
