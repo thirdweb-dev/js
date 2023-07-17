b6a0bc057a1268dfb7a4cff4d66aa99d0c30535b
DLHTX <949729789@qq.com>
2023-07-17 19:37:17 +0800

    feat(chain): update iotex chain icon

--- packages/chains/chains/4690.ts
+++ packages/chains/chains/4690.ts
@@ -2,6 +2,12 @@ import type { Chain } from "../src/types";
 export default {
   "name": "IoTeX Network Testnet",
   "chain": "iotex.io",
+  "icon": {
+    "url": "ipfs://QmQKHQrvtyUC5b5B76ke5GPTGXoGTVCubXS6gHgzCAswKo",
+    "width": 250,
+    "height": 250,
+    "format": "png"
+  },
   "rpc": [
     "https://iotex-network-testnet.rpc.thirdweb.com/${THIRDWEB_API_KEY}",
     "https://babel-api.testnet.iotex.io"
@@ -22,7 +28,13 @@ export default {
     {
       "name": "testnet iotexscan",
       "url": "https://testnet.iotexscan.io",
-      "standard": "EIP3091"
+      "standard": "EIP3091",
+      "icon":{
+        "url":"ipfs://QmPYzuYAbVFZHWHyVpqG2P4Rhbr9v4TnLyF5bQjRgeAATN",
+        "width":267,
+        "height":47,
+        "format":"png"
+      }
     }
   ],
   "testnet": true,
