3f71bae917b3db5c438b54f39859cd989ef419f9
Manan Tank <manantankm@gmail.com>
2023-07-18 03:03:36 +0530

    Omit clientId type from smartWallet() options

--- packages/react/src/wallet/wallets/smartWallet/types.ts
+++ packages/react/src/wallet/wallets/smartWallet/types.ts
@@ -4,7 +4,10 @@ import {
   SmartWalletConfig as SmartWalletConfig_,
 } from "@thirdweb-dev/wallets";
 
-export type SmartWalletConfigOptions = Omit<SmartWalletConfig_, "chain"> & {
+export type SmartWalletConfigOptions = Omit<
+  SmartWalletConfig_,
+  "chain" | "clientId"
+> & {
   personalWallets?: WalletConfig<any>[];
 };
 
