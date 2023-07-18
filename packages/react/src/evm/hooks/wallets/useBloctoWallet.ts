f5056db7f5c33da46465af7b21886d7bbd21fd89
Calvin Chang <q20274982@gmail.com>
2023-07-18 17:51:08 +0800

    feat: add useBloctoWallet hook

---
+++ packages/react/src/evm/hooks/wallets/useBloctoWallet.ts
@@ -0,0 +1,20 @@
+import { useConnect } from "@thirdweb-dev/react-core";
+import { useCallback } from "react";
+import { BloctoAdditionalOptions } from '@thirdweb-dev/wallets';
+
+export function useBloctoWallet() {
+  const connect = useConnect();
+  return useCallback(
+    async (options: { chainId?: number } & BloctoAdditionalOptions) => {
+      const { bloctoWallet } = await import(
+        "../../../wallet/wallets/blocto/bloctoWallet"
+      );
+      return connect(bloctoWallet({
+        chain: options.chain,
+        rpc: options?.rpc,
+        appId: options?.appId,
+      }), options);
+    },
+    [connect],
+  );
+}
