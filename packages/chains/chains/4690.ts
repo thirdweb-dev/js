b8086435a4a4f7ce4847cb6dc2eb6d330953ed2a
Jonas Daniels <jonas.daniels@outlook.com>
2023-07-18 21:39:34 -0700

    refactor: Update icon properties in IoTeX Network chains (#1368)

--- packages/chains/chains/4690.ts
+++ packages/chains/chains/4690.ts
@@ -2,12 +2,6 @@ import type { Chain } from "../src/types";
 export default {
   "name": "IoTeX Network Testnet",
   "chain": "iotex.io",
-  "icon": {
-    "url": "ipfs://QmQKHQrvtyUC5b5B76ke5GPTGXoGTVCubXS6gHgzCAswKo",
-    "width": 250,
-    "height": 250,
-    "format": "png"
-  },
   "rpc": [
     "https://iotex-network-testnet.rpc.thirdweb.com/${THIRDWEB_API_KEY}",
     "https://babel-api.testnet.iotex.io"
@@ -28,15 +22,15 @@ export default {
     {
       "name": "testnet iotexscan",
       "url": "https://testnet.iotexscan.io",
-      "standard": "EIP3091",
-      "icon":{
-        "url":"ipfs://QmPYzuYAbVFZHWHyVpqG2P4Rhbr9v4TnLyF5bQjRgeAATN",
-        "width":267,
-        "height":47,
-        "format":"png"
-      }
+      "standard": "EIP3091"
     }
   ],
+  "icon": {
+    "url": "ipfs://QmQKHQrvtyUC5b5B76ke5GPTGXoGTVCubXS6gHgzCAswKo",
+    "width": 250,
+    "height": 250,
+    "format": "png"
+  },
   "testnet": true,
   "slug": "iotex-network-testnet"
 } as const satisfies Chain;
