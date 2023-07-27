cf5bb9382a548c196826b23ebafba812de6bcf0e
ciaranightingale <ciara.nightingale@sky.com>
2023-07-27 21:49:51 +0100

    working code & fix factoryAddress required

--- packages/wallets/src/evm/connectors/token-bound-smart-wallet/index.ts
+++ packages/wallets/src/evm/connectors/token-bound-smart-wallet/index.ts
@@ -1,54 +1,54 @@
-import { TokenBoundSmartWalletConfig } from "./types";
+import { TokenBoundSmartWalletConfig, TokenBoundSmartWalletConfigInput } from "./types";
 import { ethers } from "ethers";
 import { SmartWalletConnector } from "../smart-wallet";
 import {
-  AccountContractInfo,
-  FactoryContractInfo,
+    AccountContractInfo,
+    FactoryContractInfo,
 } from "../smart-wallet/types";
 import { ERC6551_REGISTRY } from "../smart-wallet/lib/constants";
+import { SmartWalletConfig } from "../smart-wallet/types";
 
 export class TokenBoundSmartWalletConnector extends SmartWalletConnector {
-  protected config: TokenBoundSmartWalletConfig;
+    protected config: TokenBoundSmartWalletConfig;
 
-  constructor(config: TokenBoundSmartWalletConfig) {
-    config.factoryAddress = config.factoryAddress || ERC6551_REGISTRY;
+    constructor(input: TokenBoundSmartWalletConfigInput) {
+        input.factoryAddress = input.factoryAddress || ERC6551_REGISTRY;
+        super(input as TokenBoundSmartWalletConfig);
+        this.config = input as TokenBoundSmartWalletConfig;
+    }
 
-    super(config);
-    this.config = config;
-  }
+    protected defaultAccountInfo(): AccountContractInfo {
+        return {
+            execute: async (account, target, value, data) => {
+                return account.prepare("executeCall", [target, value, data]);
+            },
+            getNonce: async (account) => {
+                return account.call("nonce", []);
+            },
+        };
+    }
 
-  protected defaultAccountInfo(): AccountContractInfo {
-    return {
-      execute: async (account, target, value, data) => {
-        return account.prepare("executeCall", [target, value, data]);
-      },
-      getNonce: async (account) => {
-        return account.call("nonce", []);
-      },
-    };
-  }
-
-  protected defaultFactoryInfo(): FactoryContractInfo {
-    return {
-      createAccount: async (factory, owner) => {
-        return factory.prepare("createAccount", [
-          this.config.accountImplementation,
-          this.chainId,
-          this.config.tokenContract,
-          this.config.tokenId,
-          this.config.salt,
-          ethers.utils.toUtf8Bytes(""),
-        ]);
-      },
-      getAccountAddress: async (factory, owner) => {
-        return await factory.call("address", [
-          this.config.accountImplementation,
-          this.chainId,
-          this.config.tokenContract,
-          this.config.tokenId,
-          this.config.salt,
-        ]);
-      },
-    };
-  }
+    protected defaultFactoryInfo(): FactoryContractInfo {
+        return {
+            createAccount: async (factory, owner) => {
+                return factory.prepare("createAccount", [
+                    this.config.accountImplementation,
+                    this.chainId,
+                    this.config.tokenContract,
+                    this.config.tokenId,
+                    this.config.salt,
+                    ethers.utils.toUtf8Bytes(""),
+                ]);
+            },
+            getAccountAddress: async (factory, owner) => {
+                return await factory.call("account", [
+                    this.config.accountImplementation,
+                    this.chainId,
+                    this.config.tokenContract,
+                    this.config.tokenId,
+                    this.config.salt,
+                ]);
+            },
+        };
+    }
 }
--- packages/wallets/src/evm/wallets/token-bound-smart-wallet.ts
+++ packages/wallets/src/evm/wallets/token-bound-smart-wallet.ts
@@ -1,44 +1,44 @@
 import { SmartWallet } from "./smart-wallet";
 import type { TokenBoundSmartWalletConnector as TokenBoundSmartWalletConnectorType } from "../connectors/token-bound-smart-wallet";
 import { walletIds } from "../constants/walletIds";
-import type { TokenBoundSmartWalletConfig } from "../connectors/token-bound-smart-wallet/types";
+import type { TokenBoundSmartWalletConfigInput, TokenBoundSmartWalletConfig } from "../connectors/token-bound-smart-wallet/types";
 import { WalletOptions } from "./base";
 
 /**
  *
  */
 export class TokenBoundSmartWallet extends SmartWallet {
-  connector?: TokenBoundSmartWalletConnectorType;
+    connector?: TokenBoundSmartWalletConnectorType;
 
-  static meta = {
-    name: "Token Bound Smart Wallet",
-    iconURL:
-      "ipfs://QmeAJVqn17aDNQhjEU3kcWVZCFBrfta8LzaDGkS8Egdiyk/token-bound-smart-wallet.svg",
-  };
+    static meta = {
+        name: "Token Bound Smart Wallet",
+        iconURL:
+            "ipfs://QmeAJVqn17aDNQhjEU3kcWVZCFBrfta8LzaDGkS8Egdiyk/token-bound-smart-wallet.svg",
+    };
 
-  static id = walletIds.tokenBoundSmartWallet;
-  public get walletName() {
-    return "Token Bound Smart Wallet";
-  }
-
-  constructor(options: WalletOptions<TokenBoundSmartWalletConfig>) {
-    super(options);
-  }
-
-  async getConnector(): Promise<TokenBoundSmartWalletConnectorType> {
-    if (!this.connector) {
-      if (this.enableConnectApp) {
-        await this.wcWallet.init();
-        this.setupWalletConnectEventsListeners();
-      }
-
-      const { TokenBoundSmartWalletConnector } = await import(
-        "../connectors/token-bound-smart-wallet"
-      );
-      this.connector = new TokenBoundSmartWalletConnector(
-        this.options as TokenBoundSmartWalletConfig,
-      );
+    static id = walletIds.tokenBoundSmartWallet;
+    public get walletName() {
+        return "Token Bound Smart Wallet";
     }
-    return this.connector;
-  }
+
+    constructor(options: WalletOptions<TokenBoundSmartWalletConfigInput>) {
+        super(options as TokenBoundSmartWalletConfig);
+    }
+
+    async getConnector(): Promise<TokenBoundSmartWalletConnectorType> {
+        if (!this.connector) {
+            if (this.enableConnectApp) {
+                await this.wcWallet.init();
+                this.setupWalletConnectEventsListeners();
+            }
+
+            const { TokenBoundSmartWalletConnector } = await import(
+                "../connectors/token-bound-smart-wallet"
+            );
+            this.connector = new TokenBoundSmartWalletConnector(
+                this.options as TokenBoundSmartWalletConfig,
+            );
+        }
+        return this.connector;
+    }
 }
