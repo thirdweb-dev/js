fbde927a0cb36a6269e045d8e577536f23164ef7
iketw <121973632+iketw@users.noreply.github.com>
2023-07-31 22:04:50 -0400

    [Wallets] Add transform private methods plugin for RN (#1421)

---
+++ .changeset/hip-dragons-jog.md
@@ -0,0 +1,5 @@
+---
+"@thirdweb-dev/wallets": patch
+---
+
+Add private methods transform plugin to the wallets package for ReactNative support
--- packages/chains/chains/1666600000.ts
+++ packages/chains/chains/1666600000.ts
@@ -5,17 +5,23 @@ export default {
   "rpc": [
     "https://harmony-shard-0.rpc.thirdweb.com/${THIRDWEB_API_KEY}",
     "https://api.harmony.one",
-    "https://api.s0.t.hmny.io"
+    "https://a.api.s0.t.hmny.io",
+    "https://api.s0.t.hmny.io",
+    "https://rpc.ankr.com/harmony",
+    "https://harmony.api.onfinality.io/public",
+    "https://1rpc.io/one"
   ],
-  "faucets": [
-    "https://free-online-app.com/faucet-for-eth-evm-chains/"
-  ],
+  "faucets": [],
   "nativeCurrency": {
     "name": "ONE",
     "symbol": "ONE",
     "decimals": 18
   },
   "infoURL": "https://www.harmony.one/",
+  "slip44": 1023,
+  "ens": {
+    "registry": "0x4cd2563118e57b19179d8dc033f2b0c5b5d69ff5"
+  },
   "shortName": "hmy-s0",
   "chainId": 1666600000,
   "networkId": 1666600000,
