dfd120a3a9d1582c8b174265c92bf43dbbaf5c86
Adam Majmudar <64697628+adam-maj@users.noreply.github.com>
2023-08-04 15:09:11 -0700

    [Auth] Split into functions & expose all auth functions on client (#1392)

---
+++ .changeset/two-points-shout.md
@@ -0,0 +1,6 @@
+---
+"@thirdweb-dev/react-core": patch
+"@thirdweb-dev/auth": patch
+---
+
+Expose functions from auth and update useAuth
--- packages/react-core/src/evm/hooks/auth/useLogin.ts
+++ packages/react-core/src/evm/hooks/auth/useLogin.ts
@@ -2,12 +2,7 @@ import { useWallet } from "../../../core/hooks/wallet-hooks";
 import { useThirdwebAuthContext } from "../../contexts/thirdweb-auth";
 import { cacheKeys } from "../../utils/cache-keys";
 import { useMutation, useQueryClient } from "@tanstack/react-query";
-import type {
-  LoginOptions,
-  LoginPayload,
-  LoginPayloadData,
-} from "@thirdweb-dev/auth";
-import { GenericAuthWallet } from "@thirdweb-dev/wallets";
+import { signLoginPayload, type LoginPayloadData } from "@thirdweb-dev/auth";
 import invariant from "tiny-invariant";
 import { AUTH_TOKEN_STORAGE_KEY } from "../../../core/constants/auth";
 
@@ -59,7 +54,7 @@ export function useLogin() {
         throw new Error(`Failed to get payload`);
       }
 
-      const payload = await doLoginWithPayload(wallet, payloadData);
+      const payload = await signLoginPayload({ wallet, payload: payloadData });
 
       res = await fetch(`${authConfig.authUrl}/login`, {
         method: "POST",
@@ -92,107 +87,4 @@ export function useLogin() {
     login: () => login.mutateAsync(),
     isLoading: login.isLoading,
   };
-}
-
-export async function doLoginWithPayload(
-  wallet: GenericAuthWallet,
-  payload: LoginPayloadData,
-): Promise<LoginPayload> {
-  const message = generateMessage(payload);
-  const signature = await wallet.signMessage(message);
-
-  return {
-    payload,
-    signature,
-  };
-}
-
-export async function doLogin(
-  wallet: GenericAuthWallet,
-  options?: LoginOptions,
-): Promise<LoginPayload> {
-  let chainId: string | undefined = options?.chainId;
-  if (!chainId && wallet.getChainId) {
-    try {
-      chainId = (await wallet.getChainId()).toString();
-    } catch {
-      // ignore error
-    }
-  }
-
-  invariant(options?.domain, "Please specify a domain to login with.");
-  // generate a pseudo-random nonce if none is provided
-  const nonce =
-    options?.nonce || Math.floor(Math.random() * 1000000000).toString();
-
-  const payloadData: LoginPayloadData = {
-    type: wallet.type,
-    domain: options.domain,
-    address: await wallet.getAddress(),
-    statement:
-      options?.statement ||
-      "Please ensure that the domain above matches the URL of the current website.",
-    version: options?.version || "1",
-    uri: options?.uri,
-    chain_id: chainId,
-    nonce: options?.nonce || nonce,
-    issued_at: new Date().toISOString(),
-    expiration_time: new Date(
-      options?.expirationTime || Date.now() + 1000 * 60 * 10,
-    ).toISOString(),
-    invalid_before: new Date(
-      options?.invalidBefore || Date.now() - 1000 * 60 * 10,
-    ).toISOString(),
-    resources: options?.resources,
-  };
-
-  return doLoginWithPayload(wallet, payloadData);
-}
-
-// generate straight from auth
-function generateMessage(payload: LoginPayloadData): string {
-  const typeField = payload.type === "evm" ? "Ethereum" : "Solana";
-  const header = `${payload.domain} wants you to sign in with your ${typeField} account:`;
-  let prefix = [header, payload.address].join("\n");
-  prefix = [prefix, payload.statement].join("\n\n");
-  if (payload.statement) {
-    prefix += "\n";
-  }
-
-  const suffixArray = [];
-  if (payload.uri) {
-    const uriField = `URI: ${payload.uri}`;
-    suffixArray.push(uriField);
-  }
-
-  const versionField = `Version: ${payload.version}`;
-  suffixArray.push(versionField);
-
-  if (payload.chain_id) {
-    const chainField = `Chain ID: ` + payload.chain_id || "1";
-    suffixArray.push(chainField);
-  }
-
-  const nonceField = `Nonce: ${payload.nonce}`;
-  suffixArray.push(nonceField);
-
-  const issuedAtField = `Issued At: ${payload.issued_at}`;
-  suffixArray.push(issuedAtField);
-
-  const expiryField = `Expiration Time: ${payload.expiration_time}`;
-  suffixArray.push(expiryField);
-
-  if (payload.invalid_before) {
-    const invalidBeforeField = `Not Before: ${payload.invalid_before}`;
-    suffixArray.push(invalidBeforeField);
-  }
-
-  if (payload.resources) {
-    suffixArray.push(
-      [`Resources:`, ...payload.resources.map((x) => `- ${x}`)].join("\n"),
-    );
-  }
-
-  const suffix = suffixArray.join("\n");
-  return [prefix, suffix].join("\n");
 }
