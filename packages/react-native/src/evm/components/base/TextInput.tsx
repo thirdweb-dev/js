04e01e61ba619eefb98ea7361789b9b8d9addf2e
ikethirdweb <jorge@thirdweb.com>
2023-08-16 12:26:55 -0400

    add left/right element to textinput

--- packages/react-native/src/evm/components/base/TextInput.tsx
+++ packages/react-native/src/evm/components/base/TextInput.tsx
@@ -1,12 +1,19 @@
 import { StyleSheet, TextInput as TextInputRN } from "react-native";
 import Box from "./Box";
 import { useTheme } from "@shopify/restyle";
+import { ReactNode } from "react";
 
-type TextInputProps = (typeof Box)["arguments"] & TextInputRN["props"];
+type TextInputProps = (typeof Box)["arguments"] &
+  TextInputRN["props"] & {
+    icon: ReactNode;
+    position: "left" | "right";
+  };
 
 export const TextInput = ({
   onChangeText,
   placeholder,
+  position,
+  icon,
   ...props
 }: TextInputProps) => {
   const theme = useTheme();
@@ -16,11 +23,13 @@ export const TextInput = ({
       flexDirection="row"
       alignItems="center"
       borderColor="border"
+      justifyContent="center"
       borderWidth={1}
       borderRadius="md"
       pr="xs"
       {...props}
     >
+      {position === "left" && icon}
       <TextInputRN
         style={{ ...styles.textInput, color: theme.colors.textPrimary }}
         returnKeyType={"done"}
@@ -31,6 +40,7 @@ export const TextInput = ({
         onChangeText={onChangeText}
         {...props}
       />
+      {position === "right" && icon}
     </Box>
   );
 };
