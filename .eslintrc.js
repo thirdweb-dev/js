0f0dae7574c18ba3d1537cb2682f99e412fcc0a3
Jonas Daniels <jonas.daniels@outlook.com>
2023-08-18 20:32:32 -0700

    Jd/upgrade deps 5 (#1571)
    * upgrade dependencies
    
    * more upgrades
    
    * refactor: remove unused rule from .eslintrc.js

--- .eslintrc.js
+++ .eslintrc.js
@@ -30,7 +30,6 @@ module.exports = {
     "@typescript-eslint/no-empty-interface": "off",
     "@typescript-eslint/no-explicit-any": "off",
     "@typescript-eslint/no-non-null-assertion": "error",
-    "@typescript-eslint/no-parameter-properties": "error",
     "@typescript-eslint/no-unused-vars": "warn",
     // import
     "import/first": "error",
