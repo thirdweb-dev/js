528401ba42c06e2d0ab268a7c1048ca5002bb210
Nacho Iacovino <nacho.iacovino@gmail.com>
2023-08-21 22:06:29 +0200

    Try/catch parseSnapshotInputs

--- packages/sdk/src/evm/common/parseSnapshotInputs.ts
+++ packages/sdk/src/evm/common/parseSnapshotInputs.ts
@@ -2,16 +2,23 @@ import { SnapshotInputSchema } from "../schema/contracts/common/snapshots";
 import { SnapshotInput } from "../types/claim-conditions/claim-conditions";
 
 export async function parseSnapshotInputs(inputs: SnapshotInput) {
-  const chunkSize = 25000;
-  const chunks = Array.from(
-    { length: Math.ceil(inputs.length / chunkSize) },
-    (_, i) => inputs.slice(i * chunkSize, i * chunkSize + chunkSize),
-  );
+  try {
+    const chunkSize = 25000;
 
-  const results = [];
-  for (const chunk of chunks) {
-    results.push(...(await SnapshotInputSchema.parseAsync(chunk)));
+    const chunks = Array.from(
+      { length: Math.ceil(inputs.length / chunkSize) },
+      (_, i) => inputs.slice(i * chunkSize, i * chunkSize + chunkSize),
+    );
+
+    const results = [];
+
+    for (const chunk of chunks) {
+      results.push(...(await SnapshotInputSchema.parseAsync(chunk)));
+    }
+
+    return results;
+
+  } catch (err: any) {
+    throw new Error(`Failed to parse snapshot inputs: ${err.message}`);
   }
-
-  return results;
 }
