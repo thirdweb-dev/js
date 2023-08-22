a4eaa3521acaa217025e40c4192e6f25ab66522f
Yash <kumaryashcse@gmail.com>
2023-08-23 01:29:17 +0530

    parse async

--- packages/sdk/src/evm/core/classes/contract-publisher.ts
+++ packages/sdk/src/evm/core/classes/contract-publisher.ts
@@ -459,7 +459,7 @@ export class ContractPublisher extends RPCConnectionHandler {
       const bytecodeHash = utils.solidityKeccak256(["bytes"], [bytecode]);
       const contractId = predeployMetadata.name;
 
-      const fullMetadata = FullPublishMetadataSchemaInput.parse({
+      const fullMetadata = FullPublishMetadataSchemaInput.parseAsync({
         ...extraMetadata,
         metadataUri: predeployMetadata.metadataUri,
         bytecodeUri: predeployMetadata.bytecodeUri,
