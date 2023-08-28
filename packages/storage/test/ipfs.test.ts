4e849eedf94e3598703c69d0f67da41c10566660
Joaquim Verges <joaquim.verges@gmail.com>
2023-08-28 16:20:01 -0700

    fix storage test

--- packages/storage/test/ipfs.test.ts
+++ packages/storage/test/ipfs.test.ts
@@ -426,7 +426,7 @@ describe("IPFS", async () => {
       getGatewayUrlForCid(
         authorizedUrls["ipfs://"][0],
         // CID changes based on file contents (prod gateway vs staging gateway since they get written)
-        `bafybeie4otkctycxgas34kjsvapcbqspqfj2xzjvyaet26i3nvlxv56oxy/0`,
+        `bafybeibihsvjwauk2ekbp32tpe73oeaoby57osxi7glx5onlsxqpr4nyhu/0`,
       ),
     );
   });
