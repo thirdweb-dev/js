2088de1cacbc903d4f18a84c21a8f27af8d06b29
Joaquim Verges <joaquim.verges@gmail.com>
2023-09-01 11:28:46 -0700

    [SDK] Dynamic gas fee estimation (#1572)

---
+++ .changeset/tidy-hairs-drive.md
@@ -0,0 +1,6 @@
+---
+"@thirdweb-dev/wallets": patch
+"@thirdweb-dev/sdk": patch
+---
+
+Dynamic gas fee estimations
--- packages/sdk/src/evm/constants/chains/supportedChains.ts
+++ packages/sdk/src/evm/constants/chains/supportedChains.ts
@@ -1,13 +1,5 @@
 import type { ChainInfo } from "../../schema/shared/ChainInfo";
-import {
-  Base,
-  BaseGoerli,
-  Optimism,
-  OptimismGoerli,
-  Zora,
-  ZoraTestnet,
-  defaultChains,
-} from "@thirdweb-dev/chains";
+import { defaultChains } from "@thirdweb-dev/chains";
 
 // @ts-expect-error - readonly vs not
 let supportedChains: ChainInfo[] = defaultChains;
@@ -30,12 +22,3 @@ export function setSupportedChains(chains: ChainInfo[] | undefined) {
 export function getSupportedChains() {
   return supportedChains;
 }
-
-export const OP_STACK_CHAINS = [
-  Optimism,
-  Base,
-  Zora,
-  OptimismGoerli,
-  BaseGoerli,
-  ZoraTestnet,
-];
