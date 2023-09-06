8913e694df8cd19c173c721f4fce4c2ba2b0b837
ikethirdweb <jorge@thirdweb.com>
2023-09-06 16:03:46 -0400

    [RN] Sets correct UI types to some compoments

--- packages/react-native/.eslintrc.cjs
+++ packages/react-native/.eslintrc.cjs
@@ -1,4 +1,8 @@
 module.exports = {
   root: true,
   extends: ["thirdweb"],
+  settings: {
+    // https://github.com/facebook/react-native/issues/28549
+    "import/ignore": ["react-native"],
+  },
 };
