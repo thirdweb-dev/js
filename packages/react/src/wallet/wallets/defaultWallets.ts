b92142d8c2c0669671950b4dff56010d68849216
Manan Tank <manantankm@gmail.com>
2023-09-08 17:45:12 +0530

    update default wallets

--- packages/react/src/wallet/wallets/defaultWallets.ts
+++ packages/react/src/wallet/wallets/defaultWallets.ts
@@ -2,9 +2,15 @@ import { WalletConfig } from "@thirdweb-dev/react-core";
 import { coinbaseWallet } from "./coinbase/coinbaseWallet";
 import { metamaskWallet } from "./metamask/metamaskWallet";
 import { walletConnect } from "./walletConnect";
+import { trustWallet } from "./trustWallet/TrustWallet";
+import { zerionWallet } from "./zerion/zerionWallet";
+import { rainbowWallet } from "./rainbow/RainbowWallet";
 
 export const defaultWallets: WalletConfig<any>[] = /* @__PURE__ */ (() => [
   metamaskWallet(),
   coinbaseWallet(),
   walletConnect(),
+  rainbowWallet(),
+  zerionWallet(),
+  trustWallet(),
 ])();
