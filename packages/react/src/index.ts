5838297d30d9dcaf64a175ecad777e9b1c955acc
ElasticBottle <winstonyeo99@yahoo.com>
2023-09-09 01:01:11 -0700

    chore(react): add embeddedWallet export

--- packages/react/src/index.ts
+++ packages/react/src/index.ts
@@ -1,18 +1,19 @@
 export { useSmartWallet } from "./evm/hooks/wallets/useSmartWallet";
-export { localWallet } from "./wallet/wallets/localWallet/localWallet";
+export { bloctoWallet } from "./wallet/wallets/blocto/bloctoWallet";
 export { coinbaseWallet } from "./wallet/wallets/coinbase/coinbaseWallet";
+export { embeddedWallet } from "./wallet/wallets/embeddedWallet";
 export { frameWallet } from "./wallet/wallets/frame/frameWallet";
+export { localWallet } from "./wallet/wallets/localWallet/localWallet";
+export { magicLink } from "./wallet/wallets/magic/magicLink";
 export { metamaskWallet } from "./wallet/wallets/metamask/metamaskWallet";
 export { paperWallet } from "./wallet/wallets/paperWallet";
 export { rainbowWallet } from "./wallet/wallets/rainbow/RainbowWallet";
 export { safeWallet } from "./wallet/wallets/safe/safeWallet";
+export { smartWallet } from "./wallet/wallets/smartWallet/smartWallet";
 export { trustWallet } from "./wallet/wallets/trustWallet/TrustWallet";
 export { walletConnect } from "./wallet/wallets/walletConnect";
 export { walletConnectV1 } from "./wallet/wallets/walletConnectV1";
-export { smartWallet } from "./wallet/wallets/smartWallet/smartWallet";
-export { magicLink } from "./wallet/wallets/magic/magicLink";
 export { zerionWallet } from "./wallet/wallets/zerion/zerionWallet";
-export { bloctoWallet } from "./wallet/wallets/blocto/bloctoWallet";
 
 // at the moment we'll re-export everything from the evm package
 export * from "./evm";
