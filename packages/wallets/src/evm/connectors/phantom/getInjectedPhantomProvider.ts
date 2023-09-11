7fcb45188fecaebddf3df6894c36ea19c04c7b0e
Manan Tank <manantankm@gmail.com>
2023-09-11 17:42:43 +0530

    fix connection issues, add react integration

--- packages/wallets/src/evm/connectors/phantom/getInjectedPhantomProvider.ts
+++ packages/wallets/src/evm/connectors/phantom/getInjectedPhantomProvider.ts
@@ -1,26 +1,21 @@
 import { Ethereum } from "../injected/types";
-import { assertWindowEthereum } from "../../utils/assertWindowEthereum";
+
+declare global {
+  interface Window {
+    phantom?: {
+      ethereum?: Ethereum;
+    };
+  }
+}
 
 export function getInjectedPhantomProvider(): Ethereum | undefined {
   if (typeof window === "undefined") {
     return;
   }
 
-  function getReady(ethereum?: Ethereum): Ethereum | undefined {
-    const isPhantom = !!ethereum?.isPhantom;
+  const provider = window.phantom?.ethereum;
 
-    if (!isPhantom) {
-      return;
-    }
-
-    return ethereum;
-  }
-
-  if (assertWindowEthereum(globalThis.window)) {
-    if (globalThis.window.ethereum?.providers) {
-      return globalThis.window.ethereum.providers.find(getReady);
-    }
-
-    return getReady(globalThis.window.ethereum);
+  if (provider?.isPhantom) {
+    return provider;
   }
 }
--- packages/wallets/src/evm/wallets/phantom.ts
+++ packages/wallets/src/evm/wallets/phantom.ts
@@ -1,11 +1,11 @@
 import type { WalletConnectConnector as WalletConnectConnectorType } from "../connectors/wallet-connect";
 import { Connector, WagmiAdapter } from "../interfaces/connector";
-import { AbstractClientWallet } from "./base";
+import { AbstractClientWallet, WalletOptions } from "./base";
 import type { PhantomConnector as PhantomConnectorType } from "../connectors/phantom";
 import { walletIds } from "../constants/walletIds";
-import { TW_WC_PROJECT_ID } from "../constants/wc";
 import { getInjectedPhantomProvider } from "../connectors/phantom/getInjectedPhantomProvider";
 
+type PhantomWalletOptions = WalletOptions;
 
 export class PhantomWallet extends AbstractClientWallet {
   connector?: Connector;
@@ -15,7 +15,8 @@ export class PhantomWallet extends AbstractClientWallet {
 
   static meta = {
     name: "Phantom",
-    iconURL: "ipfs://bafybeibkpca5nwxpsjrtuxmz2ckb5lyc2sl2abg5f7dnvxku637vvffjti",
+    iconURL:
+      "ipfs://bafybeibkpca5nwxpsjrtuxmz2ckb5lyc2sl2abg5f7dnvxku637vvffjti",
     urls: {
       chrome:
         "https://chrome.google.com/webstore/detail/phantom/bfnaelmomeimhlpmgjnjophhpkkoljpa",
@@ -37,23 +38,19 @@ export class PhantomWallet extends AbstractClientWallet {
 
   protected async getConnector(): Promise<Connector> {
     if (!this.connector) {
+      const { PhantomConnector } = await import("../connectors/phantom");
+      const phantomConnector = new PhantomConnector({
+        chains: this.chains,
+        connectorStorage: this.walletStorage,
+        options: {
+          shimDisconnect: true,
+        },
+      });
 
-      if (this.isInjected) {
-        // import the connector dynamically
-        const { PhantomConnector } = await import("../connectors/phantom");
-        const phantomConnector = new PhantomConnector({
-          chains: this.chains,
-          connectorStorage: this.walletStorage,
-          options: {
-            shimDisconnect: true,
-          },
-        });
-
-        this.phantomConnector = phantomConnector;
-
-        this.connector = new WagmiAdapter(phantomConnector);
-      }
+      this.phantomConnector = phantomConnector;
+      this.connector = new WagmiAdapter(phantomConnector);
     }
 
     return this.connector;
   }
+}
