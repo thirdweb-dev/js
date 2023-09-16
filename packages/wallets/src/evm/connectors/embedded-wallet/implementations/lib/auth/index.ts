ace0b852b38bde04419a5b451dc883c38c660103
ElasticBottle <winstonyeo99@yahoo.com>
2023-09-15 17:14:57 -0700

    chore(wallets): remove login with google from embeddedWallets

--- packages/wallets/src/evm/connectors/embedded-wallet/implementations/lib/auth/index.ts
+++ packages/wallets/src/evm/connectors/embedded-wallet/implementations/lib/auth/index.ts
@@ -134,9 +134,6 @@ export class Auth {
    * @returns {{user: InitializedUser}} An InitializedUser object. See {@link EmbeddedWalletSdk.getUser} for more
 
    */
-  async loginWithGoogle(): Promise<AuthLoginReturnType> {
-    return this.BaseLogin.loginWithGoogle();
-  }
 
   /**
    * @description
