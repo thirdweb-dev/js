99f347088d6ff2ac8a501b410e28d4bdb8636a81
Jonas Daniels <jonas.daniels@outlook.com>
2023-09-28 13:10:42 -0700

    chore: Update postinstall script for Sentry CLI installation (#1718)
    * chore: Update postinstall script for Sentry CLI installation
    
    * feat(package.json): update next version to 13.5.4-canary.6

--- sentry-postinstall.sh
+++ sentry-postinstall.sh
@@ -1,17 +1,11 @@
 #!/usr/bin/env bash
 
-# check if the install script exists at the vercel path
-if [ -f "./vercel/path0/node_modules/@sentry/cli/scripts/install.js" ]; then
-  # run the install script
-  bun ./vercel/path0/node_modules/@sentry/cli/scripts/install.js
-  # log
-  echo "Sentry CLI installed on Vercel"
-fi
-
-# check if the install scripts at the local path
-if [ -f "./node_modules/@sentry/cli/scripts/install.js" ]; then
-  # run the install script
-  bun ./node_modules/@sentry/cli/scripts/install.js
-  # log
-  echo "Sentry CLI installed"
+# check if the sentry cli is installed
+if ! [ -x "$(command -v sentry-cli)" ]; then
+  # if it's not installed install it
+  curl -sL https://sentry.io/get-cli/ | bash
+  # print that it's installed
+  echo "Sentry CLI installed successfully."
+else
+  echo "Sentry CLI already installed."
 fi
