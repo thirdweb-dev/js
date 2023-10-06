39c10970461dc8817579df3b19c9479cc41bcd0a
0xFirekeeper <0xFirekeeper@gmail.com>
2023-10-07 00:35:43 +0300

    copy style

--- packages/unity-js-bridge/src/thirdweb-bridge.ts
+++ packages/unity-js-bridge/src/thirdweb-bridge.ts
@@ -300,11 +300,7 @@ class ThirdwebBridge implements TWBridge {
         const embeddedWallet = walletInstance as EmbeddedWallet;
 
         if (useGoogle?.toLowerCase() === "true") {
-          const googleWindow = window.open(
-            "",
-            "Login",
-            "width=500,height=600,menubar=no,toolbar=no,location=no",
-          );
+          const googleWindow = this.openGoogleSignInWindow();
           if (!googleWindow) {
             throw new Error("Failed to open google login window");
           }
@@ -644,6 +640,72 @@ class ThirdwebBridge implements TWBridge {
       password,
     });
     return localWallet;
+  }
+
+  public openGoogleSignInWindow() {
+    const win = window.open("", undefined, "width=350, height=500");
+    if (win) {
+      win.document.title = "Sign In - Google Accounts";
+      win.document.body.innerHTML = `
+      <svg class="loader" viewBox="0 0 50 50">
+        <circle
+          cx="25"
+          cy="25"
+          r="20"
+          fill="none"
+          stroke="#000"
+          stroke-width="4"
+        />
+      </svg>
+      
+      <style>
+        body,
+        html {
+          height: 100%;
+          margin: 0;
+          padding: 0;
+        }
+      
+        body {
+          display: flex;
+          justify-content: center;
+          align-items: center;
+        }
+      
+        .loader {
+          width: 15vw;
+          height: 15vw;
+          animation: spin 2s linear infinite;
+        }
+      
+        .loader circle {
+          animation: loading 1.5s linear infinite;
+        }
+      
+        @keyframes loading {
+          0% {
+            stroke-dasharray: 1, 150;
+            stroke-dashoffset: 0;
+          }
+          50% {
+            stroke-dasharray: 90, 150;
+            stroke-dashoffset: -35;
+          }
+          100% {
+            stroke-dasharray: 90, 150;
+            stroke-dashoffset: -124;
+          }
+        }
+      
+        @keyframes spin {
+          100% {
+            transform: rotate(360deg);
+          }
+        }
+      </style>
+      `;
+    }
+    return win;
   }
 }
 
