77d321158ed3170dde203a20d4af255fd90541b8
Yash <kumaryashcse@gmail.com>
2023-10-19 21:26:49 +0530

    lint

--- packages/sdk/src/evm/common/any-evm-utils/deployCreate2Factory.ts
+++ packages/sdk/src/evm/common/any-evm-utils/deployCreate2Factory.ts
@@ -1,6 +1,5 @@
 import { BigNumber, Signer } from "ethers";
 import invariant from "tiny-invariant";
-import { toWei } from "../currency/toWei";
 import type { DeployOptions } from "../../types/deploy";
 import { CUSTOM_GAS_FOR_CHAIN } from "../any-evm-constants";
 import { COMMON_FACTORY } from "./constants";
