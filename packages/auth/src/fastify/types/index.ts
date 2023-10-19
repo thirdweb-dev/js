99d7ec06ec4f31308627edc87d3122c21a884b5a
Adam Majmudar <64697628+adam-maj@users.noreply.github.com>
2023-10-19 16:34:32 -0700

    [Auth] Add fastify extension, onToken callback, & secure option (#1801)

--- packages/auth/src/fastify/types/index.ts
+++ packages/auth/src/fastify/types/index.ts
@@ -1,8 +1,14 @@
-import { ThirdwebAuth } from "../../core";
-import { Json, LoginPayloadOutputSchema, User } from "../../core";
-import type { GenericAuthWallet } from "@thirdweb-dev/wallets";
-import { Request } from "express";
+import { GenericAuthWallet } from "@thirdweb-dev/wallets";
+import { Json, LoginPayloadOutputSchema, ThirdwebAuth, User } from "../../core";
+import {
+  FastifyBaseLogger,
+  FastifyInstance,
+  FastifyRequest,
+  RawServerDefault,
+} from "fastify";
 import { z } from "zod";
+import { IncomingMessage, ServerResponse } from "http";
+import { ZodTypeProvider } from "fastify-type-provider-zod";
 
 export const PayloadBodySchema = z.object({
   address: z.string(),
@@ -17,7 +23,9 @@ export const LoginPayloadBodySchema = z.object({
   payload: LoginPayloadOutputSchema,
 });
 
-export type ThirdwebAuthRoute = "login" | "user" | "logout";
+export const ErrorResponseSchema = z.object({
+  error: z.string(),
+});
 
 export type ThirdwebAuthUser<
   TData extends Json = Json,
@@ -56,17 +64,17 @@ export type ThirdwebAuthConfig<
   };
   callbacks?: {
     onLogin?:
-      | ((address: string, req: Request) => void | TSession)
-      | ((address: string, req: Request) => Promise<void | TSession>);
+      | ((address: string, req: FastifyRequest) => void | TSession)
+      | ((address: string, req: FastifyRequest) => Promise<void | TSession>);
     onToken?:
-      | ((token: string, req: Request) => void)
-      | ((token: string, req: Request) => Promise<void>);
+      | ((token: string, req: FastifyRequest) => void)
+      | ((token: string, req: FastifyRequest) => Promise<void>);
     onUser?:
-      | ((user: User<TSession>, req: Request) => void | TData)
-      | ((user: User<TSession>, req: Request) => Promise<void | TData>);
+      | ((user: User<TSession>, req: FastifyRequest) => void | TData)
+      | ((user: User<TSession>, req: FastifyRequest) => Promise<void | TData>);
     onLogout?:
-      | ((user: User, req: Request) => void)
-      | ((user: User, req: Request) => Promise<void>);
+      | ((user: User, req: FastifyRequest) => void)
+      | ((user: User, req: FastifyRequest) => Promise<void>);
   };
 };
 
@@ -76,3 +84,11 @@ export type ThirdwebAuthContext<
 > = Omit<Omit<ThirdwebAuthConfig<TData, TSession>, "wallet">, "domain"> & {
   auth: ThirdwebAuth;
 };
+
+export type FastifyInstanceWithZod = FastifyInstance<
+  RawServerDefault,
+  IncomingMessage,
+  ServerResponse<IncomingMessage>,
+  FastifyBaseLogger,
+  ZodTypeProvider
+>;
