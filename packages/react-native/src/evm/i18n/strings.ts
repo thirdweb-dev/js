11a01f34ec25ddbdd70ddaa41deea1157a9d7781
ikethirdweb <jorge@thirdweb.com>
2023-10-18 20:09:23 -0400

    locale

--- packages/react-native/src/evm/i18n/strings.ts
+++ packages/react-native/src/evm/i18n/strings.ts
@@ -1,65 +1,27 @@
-export const en = {
-  connect_wallet: {
-    label: "Connect Wallet",
-  },
-  connect_wallet_details: {
-    additional_actions: "Additional Actions",
-    backup_wallet: "Backup Wallet",
-    import_wallet: "Import Wallet",
-    connected_to_smart_wallet: "Connected to a Smart Wallet",
-    current_network: "Current Network",
-    wallet_address: "Wallet Address",
-    backup: "Backup",
-    connect_to_app: "Connect to App",
-    guest: "Guest",
-    connect: "Connect",
-    new_to_wallets: "New to wallets?",
-    get_started: "Get started",
-    connect_a_wallet: "Connect a wallet",
-    continue_as_guest: "Continue as guest",
-    tos_intro: "By connecting, you agree to the",
-    tos: "Terms of Service",
-    privacy_policy: "Privacy Policy",
-    copy_address_or_scan:
-      "Copy the wallet address or scan the QR code to send funds to this wallet.",
-    your_address: "Your address",
-    qr_code: "QR Code",
-    select_token: "Select Token",
-    send_to: "Send to",
-    no_tokens_found: "No Tokens found",
-    confirm_in_wallet: "Confirm in your wallet",
-    select_network: "Select Network",
-    no_supported_chains_detected: "No supported chains detected",
-    recommended: "Recommended",
-  },
-  connecting_wallet: {
-    local_wallet_message:
-      "Creating, encrypting and securing your device wallet.",
-    connecting_your_wallet: "Connecting your wallet",
-  },
-  local_wallet: {
-    create_new_wallet: "Create new wallet",
-    private_key_mnemonic: "Or Private key or Mnemonic",
-  },
-  embedded_wallet: {
-    request_new_code: "Request new code",
-    sign_in: "Sign In",
-  },
-  wallet_connect: {
-    no_results_found: "No results found",
-  },
-  common: {
-    password: "Password",
-    reject: "Reject",
-    approve: "Approve",
-    switch_network: "Switch Network",
-    import: "Import",
-    username: "Username",
-    amount: "Amount",
-    send: "Send",
-    continue: "Continue",
-    error_switching_network: "Error switching network",
-  },
+import { DeepPartial } from "../types/deepPartial";
+import { Locale, LocaleType, _en, _es } from "./types";
+
+export const setLocale = (locale: Locale): LocaleType => {
+  if (typeof locale === "string") {
+    if (locale === "es") {
+      return es();
+    } else if (locale === "en") {
+      return en();
+    }
+  }
+  return locale;
 };
 
-export const fr = {};
+export const en = (locale?: DeepPartial<LocaleType>): LocaleType => {
+  return {
+    ..._en,
+    ...locale,
+  } as LocaleType;
+};
+
+export const es = (locale?: DeepPartial<LocaleType>): LocaleType => {
+  return {
+    ..._es,
+    ...locale,
+  } as LocaleType;
+};
---
+++ packages/react-native/src/evm/i18n/types.ts
@@ -0,0 +1,131 @@
+export type Locale = LocaleType | "en" | "es";
+
+export type LocaleType = typeof _en;
+
+export const _en = {
+  connect_wallet: {
+    label: "Connect Wallet",
+  },
+  connect_wallet_details: {
+    additional_actions: "Additional Actions",
+    backup_wallet: "Backup Wallet",
+    import_wallet: "Import Wallet",
+    connected_to_smart_wallet: "Connected to a Smart Wallet",
+    current_network: "Current Network",
+    wallet_address: "Wallet Address",
+    backup: "Backup",
+    connect_to_app: "Connect to App",
+    guest: "Guest",
+    connect: "Connect",
+    new_to_wallets: "New to wallets?",
+    get_started: "Get started",
+    connect_a_wallet: "Connect a wallet",
+    continue_as_guest: "Continue as guest",
+    tos_intro: "By connecting, you agree to the",
+    tos: "Terms of Service",
+    privacy_policy: "Privacy Policy",
+    copy_address_or_scan:
+      "Copy the wallet address or scan the QR code to send funds to this wallet.",
+    your_address: "Your address",
+    qr_code: "QR Code",
+    select_token: "Select Token",
+    send_to: "Send to",
+    no_tokens_found: "No Tokens found",
+    confirm_in_wallet: "Confirm in your wallet",
+    select_network: "Select Network",
+    no_supported_chains_detected: "No supported chains detected",
+    recommended: "Recommended",
+  },
+  connecting_wallet: {
+    local_wallet_message:
+      "Creating, encrypting and securing your device wallet.",
+    connecting_your_wallet: "Connecting your wallet",
+  },
+  local_wallet: {
+    create_new_wallet: "Create new wallet",
+    private_key_mnemonic: "Or Private key or Mnemonic",
+  },
+  embedded_wallet: {
+    request_new_code: "Request new code",
+    sign_in: "Sign In",
+  },
+  wallet_connect: {
+    no_results_found: "No results found",
+  },
+  common: {
+    password: "Password",
+    reject: "Reject",
+    approve: "Approve",
+    switch_network: "Switch Network",
+    import: "Import",
+    username: "Username",
+    amount: "Amount",
+    send: "Send",
+    continue: "Continue",
+    error_switching_network: "Error switching network",
+  },
+};
+
+export const _es = {
+  connect_wallet: {
+    label: "Conectar Cartera",
+  },
+  connect_wallet_details: {
+    additional_actions: "Acciones Adicionales",
+    backup_wallet: "Respaldar Cartera",
+    import_wallet: "Importar Cartera",
+    connected_to_smart_wallet: "Conectado a una Cartera Inteligente",
+    current_network: "Red Actual",
+    wallet_address: "Dirección de Cartera",
+    backup: "Respaldo",
+    connect_to_app: "Conectar a la App",
+    guest: "Invitado",
+    connect: "Conectar",
+    new_to_wallets: "¿Nuevo en carteras?",
+    get_started: "Comenzar",
+    connect_a_wallet: "Conectar una cartera",
+    continue_as_guest: "Continuar como invitado",
+    tos_intro: "Al conectar, aceptas los",
+    tos: "Términos de Servicio",
+    privacy_policy: "Política de Privacidad",
+    copy_address_or_scan:
+      "Copia la dirección de la cartera o escanea el código QR para enviar fondos a esta cartera.",
+    your_address: "Tu dirección",
+    qr_code: "Código QR",
+    select_token: "Seleccionar Token",
+    send_to: "Enviar a",
+    no_tokens_found: "No se encontraron tokens",
+    confirm_in_wallet: "Confirma en tu cartera",
+    select_network: "Seleccionar Red",
+    no_supported_chains_detected: "No se detectaron cadenas compatibles",
+    recommended: "Recomendado",
+  },
+  connecting_wallet: {
+    local_wallet_message:
+      "Creando, cifrando y asegurando tu cartera de dispositivo.",
+    connecting_your_wallet: "Conectando tu cartera",
+  },
+  local_wallet: {
+    create_new_wallet: "Crear nueva cartera",
+    private_key_mnemonic: "O Clave Privada o Mnemónico",
+  },
+  embedded_wallet: {
+    request_new_code: "Solicitar nuevo código",
+    sign_in: "Iniciar Sesión",
+  },
+  wallet_connect: {
+    no_results_found: "No se encontraron resultados",
+  },
+  common: {
+    password: "Contraseña",
+    reject: "Rechazar",
+    approve: "Aprobar",
+    switch_network: "Cambiar Red",
+    import: "Importar",
+    username: "Nombre de usuario",
+    amount: "Cantidad",
+    send: "Enviar",
+    continue: "Continuar",
+    error_switching_network: "Error al cambiar de red",
+  },
+};
