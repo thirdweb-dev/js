b2910f6bf885c876976d373968ff6c3d567d2c3c
Adam Majmudar <mr.adam.maj@gmail.com>
2023-10-18 18:30:20 -0700

    Update fastify auth

--- packages/auth/src/fastify/index.ts
+++ packages/auth/src/fastify/index.ts
@@ -1,13 +1,35 @@
-import { FastifyInstance } from "fastify";
-import { ThirdwebAuth as ThirdwebAuthSDK } from "../core";
-import fastifyCookie from "@fastify/cookie";
+import { FastifyInstance, FastifyRequest } from "fastify";
+import { Json, ThirdwebAuth as ThirdwebAuthSDK } from "../core";
+import fastifyCookie, { FastifyCookie } from "@fastify/cookie";
+import payloadHandler from "./routes/payload";
 import loginHandler from "./routes/login";
+import userHandler from "./routes/user";
+import switchAccountHandler from "./routes/switch-account";
+import logoutHandler from "./routes/logout";
+import { getUser } from "./helpers/user";
+import {
+  ThirdwebAuthConfig,
+  ThirdwebAuthContext,
+  ThirdwebAuthUser,
+} from "./types";
 
-type ThirdwebAuthReturnType = {
+type ThirdwebAuthReturnType<
+  TData extends Json = Json,
+  TSession extends Json = Json,
+> = {
   authRouter: (fastify: FastifyInstance) => void;
+  authMiddleware: FastifyCookie;
+  getUser: (
+    req: FastifyRequest,
+  ) => Promise<ThirdwebAuthUser<TData, TSession> | null>;
 };
 
-export function ThirdwebAuth(cfg: any): ThirdwebAuthReturnType {
+export function ThirdwebAuth<
+  TData extends Json = Json,
+  TSession extends Json = Json,
+>(
+  cfg: ThirdwebAuthConfig<TData, TSession>,
+): ThirdwebAuthReturnType<TData, TSession> {
   const ctx = {
     ...cfg,
     auth: new ThirdwebAuthSDK(cfg.wallet, cfg.domain),
@@ -16,12 +38,32 @@ export function ThirdwebAuth(cfg: any): ThirdwebAuthReturnType {
   const authRouter = async (fastify: FastifyInstance) => {
     await fastify.register(fastifyCookie);
 
+    fastify.post("/payload", async (req, res) => {
+      payloadHandler(req, res, ctx as ThirdwebAuthContext);
+    });
+
     fastify.post("/login", async (req, res) => {
-      loginHandler(req, res, ctx);
+      loginHandler(req, res, ctx as ThirdwebAuthContext);
+    });
+
+    fastify.get("/user", async (req, res) => {
+      userHandler(req, res, ctx as ThirdwebAuthContext);
+    });
+
+    fastify.post("/logout", async (req, res) => {
+      logoutHandler(req, res, ctx as ThirdwebAuthContext);
+    });
+
+    fastify.post("/switch-account", async (req, res) => {
+      switchAccountHandler(req, res, ctx as ThirdwebAuthContext);
     });
   };
 
   return {
     authRouter,
+    authMiddleware: fastifyCookie,
+    getUser: (req: FastifyRequest) => {
+      return getUser<TData, TSession>(req, ctx);
+    },
   };
 }
