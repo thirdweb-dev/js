6d1eabe9f9818ee2a79ce5bf6aa74417dbfd0558
Kien Ngo <kenseverus@gmail.com>
2023-10-20 00:46:50 -0400

    [SDK] Some more small improvements (#1813)

--- packages/sdk/src/evm/common/legacy.ts
+++ packages/sdk/src/evm/common/legacy.ts
@@ -7,12 +7,13 @@ export async function getPrebuiltInfo(
 ): Promise<{ type: string; version: number } | undefined> {
   try {
     const contract = new Contract(address, IThirdwebContractABI, provider);
-    const [type, version] = await Promise.all([
-      utils
-        .toUtf8String(await contract.contractType()) // eslint-disable-next-line no-control-regex
-        .replace(/\x00/g, ""),
-      await contract.contractVersion(),
+    const [_type, version] = await Promise.all([
+      contract.contractType(),
+      contract.contractVersion(),
     ]);
+    const type = utils
+      .toUtf8String(_type) // eslint-disable-next-line no-control-regex
+      .replace(/\x00/g, "");
     return {
       type,
       version,
