641b59a2a8b3051738ac083cdcab41f10d18ef01
Joaquim Verges <joaquim.verges@gmail.com>
2023-10-20 16:14:50 -0700

    implement multidimensional nonces for smart wallets

--- packages/wallets/src/evm/connectors/smart-wallet/lib/utils.ts
+++ packages/wallets/src/evm/connectors/smart-wallet/lib/utils.ts
@@ -1,4 +1,4 @@
-import { ethers, utils } from "ethers";
+import { BigNumber, ethers, utils } from "ethers";
 import { UserOperationStruct } from "@account-abstraction/contracts";
 
 export function toJSON(op: Partial<UserOperationStruct>): Promise<any> {
@@ -76,3 +76,31 @@ export async function getUserOpHashV06(
   );
   return utils.keccak256(enc);
 }
+
+const generateRandomUint192 = (): bigint => {
+  const rand1 = BigInt(Math.floor(Math.random() * 0x100000000));
+  const rand2 = BigInt(Math.floor(Math.random() * 0x100000000));
+  const rand3 = BigInt(Math.floor(Math.random() * 0x100000000));
+  const rand4 = BigInt(Math.floor(Math.random() * 0x100000000));
+  const rand5 = BigInt(Math.floor(Math.random() * 0x100000000));
+  const rand6 = BigInt(Math.floor(Math.random() * 0x100000000));
+  return (
+    (rand1 << 160n) |
+    (rand2 << 128n) |
+    (rand3 << 96n) |
+    (rand4 << 64n) |
+    (rand5 << 32n) |
+    rand6
+  );
+};
+
+export const randomNonce = () => {
+  let hexString = generateRandomUint192().toString(16);
+  if (hexString.length % 2 !== 0) {
+    hexString = "0" + hexString;
+  }
+  hexString = "0x" + hexString;
+  return ethers.BigNumber.from(
+    ethers.utils.concat([hexString, "0x0000000000000000"]),
+  );
+};
