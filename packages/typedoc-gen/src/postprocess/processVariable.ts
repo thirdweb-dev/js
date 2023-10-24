85ae18b817a3b13677d780f9c6ac919febf29fbe
Manan Tank <manantankm@gmail.com>
2023-10-25 03:46:04 +0530

    update getVariableDoc

--- packages/typedoc-gen/src/postprocess/processVariable.ts
+++ packages/typedoc-gen/src/postprocess/processVariable.ts
@@ -15,47 +15,49 @@ export function getVariableDoc(
     summary: data.comment?.summary,
     source: data.sources?.[0]?.url,
     type: data.type ? getReadableType(data.type) : undefined,
-    typeDeclaration: getDeclaration(data.type),
+    typeDeclaration: data.type ? getDeclaration(data.type) : undefined,
   };
 }
 
 function getDeclaration(
-  typeObj?: JSONOutput.SomeType,
+  typeObj: JSONOutput.SomeType,
 ): VariableTypeDeclaration[] | undefined {
-  if (typeObj?.type !== "reflection") {
-    return undefined;
+  if (typeObj.type === "reflection") {
+    return typeObj.declaration.children?.map((child) => {
+      if (child.signatures) {
+        const output: VariableTypeDeclaration = getFunctionDoc(child);
+        return output;
+      }
+
+      // when property is a assigned arrow function
+      // Example: { bar: (a: number) => a + 2 }
+      if (
+        child.type?.type === "reflection" &&
+        child.type.declaration.signatures
+      ) {
+        const output: VariableTypeDeclaration = getFunctionDoc(
+          child.type.declaration,
+        );
+        // fix wrong name ( this is kinda hacky )
+        output.name = child.name;
+        return output;
+      }
+
+      if (child.type) {
+        const output: VariableTypeDeclaration = {
+          name: child.name,
+          type: getReadableType(child.type),
+          summary: child.comment?.summary,
+        };
+
+        return output;
+      }
+
+      throw new Error(`Unknown type declaration node ${child.name}`);
+    });
   }
 
-  return typeObj.declaration.children?.map((child) => {
-    if (child.signatures) {
-      const output: VariableTypeDeclaration = getFunctionDoc(child);
-      return output;
-    }
-
-    // when property is a assigned arrow function
-    // Example: { bar: (a: number) => a + 2 }
-    if (
-      child.type?.type === "reflection" &&
-      child.type.declaration.signatures
-    ) {
-      const output: VariableTypeDeclaration = getFunctionDoc(
-        child.type.declaration,
-      );
-      // fix wrong name ( this is kinda hacky )
-      output.name = child.name;
-      return output;
-    }
-
-    if (child.type) {
-      const output: VariableTypeDeclaration = {
-        name: child.name,
-        type: getReadableType(child.type),
-        summary: child.comment?.summary,
-      };
-
-      return output;
-    }
-
-    throw new Error(`Unknown type declaration node ${child.name}`);
-  });
+  if (typeObj.type === "array") {
+    return getDeclaration(typeObj.elementType);
+  }
 }
