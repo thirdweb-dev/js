5db30f7568782eeaacda0ac317bfc0c0da5a5df6
Jonas Daniels <jonas.daniels@outlook.com>
2023-10-26 19:42:05 -0700

    feat: Add YourFilesSection to DashboardStorage (#1827)
    Signed-off-by: Jonas Daniels <jonas.daniels@outlook.com>

--- pages/dashboard/infrastructure/storage.tsx
+++ pages/dashboard/infrastructure/storage.tsx
@@ -4,6 +4,7 @@ import { CodeSegment } from "components/contract-tabs/code/CodeSegment";
 import { CodeEnvironment } from "components/contract-tabs/code/types";
 import { RelevantDataSection } from "components/dashboard/RelevantDataSection";
 import { IpfsUploadDropzone } from "components/ipfs-upload/dropzone";
+import { YourFilesSection } from "components/storage/your-files";
 import { InfrastructureSidebar } from "core-ui/sidebar/infrastructure";
 import { NextSeo } from "next-seo";
 import { PageId } from "page-id";
@@ -67,8 +68,8 @@ const DashboardStorage: ThirdwebNextPage = () => {
               <IpfsUploadDropzone />
             </Flex>
           </Flex>
-          {/* TODO: uncomment this when API is ready */}
-          {/* <YourFilesSection /> */}
+
+          <YourFilesSection />
           <Flex flexDir="column" w="full" gap={4}>
             <Heading size="title.md" as="h2">
               Gateway
