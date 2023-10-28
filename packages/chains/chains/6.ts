f6b49c703b2aee33d8a5353cd4180f4995e47b3f
Jonas Daniels <jonas.daniels@outlook.com>
2023-10-27 18:44:02 -0700

    chore(chains): sync chains (#1865)

---
+++ packages/chains/chains/6.ts
@@ -0,0 +1,24 @@
+import type { Chain } from "../src/types";
+export default {
+  "chain": "ETC",
+  "chainId": 6,
+  "explorers": [],
+  "faucets": [],
+  "infoURL": "https://explorer.jade.builders/?network=kotti",
+  "name": "Ethereum Classic Testnet Kotti",
+  "nativeCurrency": {
+    "name": "Kotti Ether",
+    "symbol": "KOT",
+    "decimals": 18
+  },
+  "networkId": 6,
+  "rpc": [
+    "https://ethereum-classic-testnet-kotti.rpc.thirdweb.com/${THIRDWEB_API_KEY}",
+    "https://6.rpc.thirdweb.com/${THIRDWEB_API_KEY}",
+    "https://www.ethercluster.com/kotti"
+  ],
+  "shortName": "kot",
+  "slug": "ethereum-classic-testnet-kotti",
+  "status": "deprecated",
+  "testnet": true
+} as const satisfies Chain;
