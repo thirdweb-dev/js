e166ec565ef745405f76b9b08f7c927a2454ab1e
Jonas Daniels <jonas.daniels@outlook.com>
2023-10-29 10:32:32 -0700

    refactor: Update keccak256 implementation and tests

--- packages/crypto/src/sha256.ts
+++ packages/crypto/src/sha256.ts
@@ -1,6 +1,11 @@
 import { sha256 as sha256Noble } from "@noble/hashes/sha256";
 import { uint8ArrayToHex } from "uint8array-extras";
 
+/**
+ * Hash a string or Uint8Array using sha256.
+ * @param value - Value to be hashed.
+ * @returns A promise that resolves to the hash of the value as Uint8Array.
+ */
 export async function sha256(
   value: string | BufferSource,
 ): Promise<Uint8Array> {
@@ -9,14 +14,29 @@ export async function sha256(
   return new Uint8Array(await crypto.subtle.digest("SHA-256", encodedValue));
 }
 
+/**
+ * Hash a string or Uint8Array using sha256 and returns the result as a hex string.
+ * @param value - Value to be hashed.
+ * @returns A promise that resolves to the hash of the value as a hex string.
+ */
 export async function sha256Hex(value: string | BufferSource): Promise<string> {
-  return "0x" + uint8ArrayToHex(await sha256(value));
+  return uint8ArrayToHex(await sha256(value));
 }
 
+/**
+ * Hash a string or Uint8Array using sha256.
+ * @param value - Value to be hashed.
+ * @returns The hash of the value as Uint8Array.
+ */
 export function sha256Sync(value: string | Uint8Array): Uint8Array {
   return sha256Noble(value);
 }
 
+/**
+ * Hash a string or Uint8Array using sha256.
+ * @param value - Value to be hashed.
+ * @returns The hash of the value as a hex string.
+ */
 export function sha256HexSync(value: string | Uint8Array): string {
-  return "0x" + uint8ArrayToHex(sha256Sync(value));
+  return uint8ArrayToHex(sha256Sync(value));
 }
--- packages/crypto/test/keccak256.test.ts
+++ packages/crypto/test/keccak256.test.ts
@@ -1,4 +1,8 @@
-import { keccak256SyncHex, keccak256Sync } from "../src";
+import {
+  keccak256SyncHex,
+  keccak256SyncHexPrefixed,
+  keccak256Sync,
+} from "../src";
 import { utils } from "ethers";
 import { keccak_256 } from "@noble/hashes/sha3";
 import { uint8ArrayToHex } from "uint8array-extras";
@@ -6,15 +10,15 @@ import { uint8ArrayToHex } from "uint8array-extras";
 const STRING_TO_HASH = "hello world";
 
 describe("keccak256", () => {
-  it("keccak256SyncHex === ethers.utils.keccak256", async () => {
-    const out = keccak256SyncHex(STRING_TO_HASH);
+  it("keccak256SyncHexPrefixed === ethers.utils.keccak256", async () => {
+    const out = keccak256SyncHexPrefixed(STRING_TO_HASH);
     const ethersOut = utils.keccak256(Buffer.from(STRING_TO_HASH));
 
     expect(out).toEqual(ethersOut);
   });
   it("keccak256SyncHex === @noble/hashes: keccak_256 -> hex", async () => {
     const out = keccak256SyncHex(STRING_TO_HASH);
-    const nobleOut = "0x" + uint8ArrayToHex(keccak_256(STRING_TO_HASH));
+    const nobleOut = uint8ArrayToHex(keccak_256(STRING_TO_HASH));
     expect(out).toEqual(nobleOut);
   });
 
