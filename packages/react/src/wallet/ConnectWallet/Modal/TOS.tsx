b9f637d46f4f78ff74e9b96973f9e803019b1487
Manan Tank <manantankm@gmail.com>
2023-10-31 21:02:17 +0530

    Fix welcomeScreen "&"

--- packages/react/src/wallet/ConnectWallet/Modal/TOS.tsx
+++ packages/react/src/wallet/ConnectWallet/Modal/TOS.tsx
@@ -42,7 +42,7 @@ export function TOS(props: {
             {locale.termsOfService}{" "}
           </Link>
         )}
-        {bothGiven && "& "}
+        {bothGiven && locale.and + " "}
         {privacyPolicyUrl && (
           <Link inline size="xs" href={privacyPolicyUrl} target="_blank">
             {locale.privacyPolicy}
