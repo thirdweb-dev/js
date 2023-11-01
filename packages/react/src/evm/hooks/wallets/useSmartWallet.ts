cf91bad38d6727c472b1397164022b29b8a6b771
Joaquim Verges <joaquim.verges@gmail.com>
2023-10-31 21:35:51 -0700

    Improved `useSmartWallet` hook

--- packages/react/src/evm/hooks/wallets/useSmartWallet.ts
+++ packages/react/src/evm/hooks/wallets/useSmartWallet.ts
@@ -1,20 +1,84 @@
-import { type WalletConfig, useConnect } from "@thirdweb-dev/react-core";
+import {
+  type WalletConfig,
+  WalletInstance,
+  useCreateWalletInstance,
+  useSetConnectedWallet,
+  useSetConnectionStatus,
+  useWalletContext,
+} from "@thirdweb-dev/react-core";
 import type { SmartWalletConfigOptions } from "../../../wallet/wallets/smartWallet/types";
-import type { SmartWalletConnectionArgs } from "@thirdweb-dev/wallets/evm/wallets/smart-wallet";
+import {
+  getSmartWalletAddress,
+  type SmartWalletConnectionArgs,
+} from "@thirdweb-dev/wallets/evm/wallets/smart-wallet";
 import { useCallback } from "react";
+import type { BytesLike } from "ethers";
 
-export function useSmartWallet() {
-  const connect = useConnect();
-  return useCallback(
-    async (
-      wallet: WalletConfig<any>,
-      options: SmartWalletConnectionArgs & SmartWalletConfigOptions,
-    ) => {
+export function useSmartWallet<W extends WalletInstance>(
+  personalWallet: WalletConfig<W>,
+  options: SmartWalletConfigOptions,
+) {
+  const create = useCreateWalletInstance();
+  const setStatus = useSetConnectionStatus();
+  const setWallet = useSetConnectedWallet();
+  const context = useWalletContext();
+
+  const predictAddress = useCallback(
+    async (args: { personalWalletAddress: string; data?: BytesLike }) => {
+      return getSmartWalletAddress(
+        context.activeChain,
+        options.factoryAddress,
+        args.personalWalletAddress,
+        args.data,
+      );
+    },
+    [context.activeChain, options.factoryAddress],
+  );
+
+  const connect = useCallback(
+    async (args?: {
+      connectPersonalWallet?: (wallet: W) => Promise<void>;
+      connectionArgs?: Omit<SmartWalletConnectionArgs, "personalWallet">;
+    }) => {
       const { smartWallet } = await import(
         "../../../wallet/wallets/smartWallet/smartWallet"
       );
-      return connect(smartWallet(wallet, options), options);
+      setStatus("connecting");
+      const pw = create(personalWallet);
+      const sw = create(smartWallet(personalWallet, options));
+      try {
+        if (args?.connectPersonalWallet) {
+          // if passed in, use custom function to connect personal wallet
+          await args.connectPersonalWallet(pw);
+        } else {
+          // otherwise default to auto-connecting personal wallet with chainId
+          await pw.connect({
+            chainId: context.activeChain.chainId,
+          });
+        }
+        await sw.connect({
+          ...args?.connectionArgs,
+          personalWallet: pw,
+        });
+        setStatus("connected");
+        setWallet(sw);
+      } catch (e) {
+        console.error("Error connecting to smart wallet", e);
+        setStatus("disconnected");
+      }
+      return sw;
     },
-    [connect],
+    [
+      context.activeChain.chainId,
+      create,
+      options,
+      personalWallet,
+      setStatus,
+      setWallet,
+    ],
   );
+  return {
+    connect,
+    predictAddress,
+  };
 }
