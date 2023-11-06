88081f32282c93363f9d633c4e07f7eace5a80b9
Kien Ngo <kienngo@skiff.com>
2023-11-06 17:41:18 -0500

    update

--- packages/react/src/wallet/ConnectWallet/Details.tsx
+++ packages/react/src/wallet/ConnectWallet/Details.tsx
@@ -327,7 +327,7 @@ export const ConnectedWalletDetails: React.FC<{
 
       <ConnectedToSmartWallet />
 
-      {/* Send and Recive */}
+      {/* Send and Receive */}
       <Container
         style={{
           display: "grid",
