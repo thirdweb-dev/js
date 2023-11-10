3dd671ab63018e122b21ceb8c21f3c48b6b89d97
ikethirdweb <jorge@thirdweb.com>
2023-11-10 16:34:27 -0500

    apple icon

--- packages/react-native/src/evm/wallets/types/embedded-wallet.ts
+++ packages/react-native/src/evm/wallets/types/embedded-wallet.ts
@@ -1,20 +1,14 @@
-import {
-  APPLE_ICON,
-  EMAIL_WALLET_ICON,
-  FACEBOOK_ICON,
-  GOOGLE_ICON,
-} from "../../assets";
+import { APPLE_ICON, EMAIL_WALLET_ICON, GOOGLE_ICON } from "../../assets";
 import { LocaleType } from "../../i18n/types";
 
 export type AuthOption = "email" | SocialLogin;
 
-export type SocialLogin = "google" | "facebook" | "apple";
+export type SocialLogin = "google" | "apple";
 
 export type AuthOptionDictionary = Record<AuthOption, string>;
 
 export const AUTH_OPTIONS_ICONS: AuthOptionDictionary = {
   google: GOOGLE_ICON,
-  facebook: FACEBOOK_ICON,
   email: EMAIL_WALLET_ICON,
   apple: APPLE_ICON,
 };
@@ -24,7 +18,6 @@ export const AUTH_OPTIONS_TEXT: Record<
   keyof LocaleType["embedded_wallet"]
 > = {
   google: "sign_in_google",
-  facebook: "sign_in_facebook",
   email: "sign_in_email",
   apple: "sign_in_apple",
 };
