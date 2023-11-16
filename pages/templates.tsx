564b90da611be882a889c461c526236d17b189b5
Avneesh Agarwal <avneeshagarwal0612@gmail.com>
2023-11-17 01:11:35 +0530

    feat: add engine nft checkout template (#1971)
    Co-authored-by: Juan Leal <114959779+juandolealt@users.noreply.github.com>

--- pages/templates.tsx
+++ pages/templates.tsx
@@ -48,6 +48,19 @@ export const templates: TemplateCardProps[] = [
     authorIcon: "/assets/templates/thirdweb-eth.png",
   },
   {
+    id: "engine-nft-checkout",
+    title: "Fiat NFT checkout",
+    homepage: "https://engine-nft-checkout.thirdweb-example.com/",
+    repo: "https://github.com/thirdweb-example/engine-nft-checkout",
+    description:
+      "Allow users to buy an NFT via a fiat checkout flow using thirdweb engine.",
+    img: "/assets/templates/engine-nft-checkout.png",
+    hoverBorderColor: "hsl(248deg 89% 79% / 15%)",
+    tags: ["Engine", "ERC721"],
+    authorENS: "thirdweb.eth",
+    authorIcon: "/assets/templates/thirdweb-eth.png",
+  },
+  {
     id: "loyalty-card",
     title: "Loyalty Card",
     homepage: "https://loyalty-card.thirdweb-example.com",
