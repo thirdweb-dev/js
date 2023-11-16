c0f71185b41bcef41b2a787ffc52dd697a14b1d9
Jonas Daniels <jonas.daniels@outlook.com>
2023-11-16 23:15:01 +0100

    refactor: Improve code readability and remove unnecessary string concatenations

--- packages/crypto/src/utils/universal-crypto.ts
+++ packages/crypto/src/utils/universal-crypto.ts
@@ -6,6 +6,6 @@ export async function universalCrypto(): Promise<Crypto> {
   // otherwise we are in node 18 so we can use `webcrypto` off of the "node:crypto" package and treat it as native
   // trick bundlers so that they leave this alone :)
   const pto = "pto";
-  const nodeCryptoNameSpace = "node:" + "cry" + pto;
-  return (await import(nodeCryptoNameSpace)).webcrypto as Crypto;
+  // this becomes `node:crypto` at runtime
+  return (await import("node" + ":cry" + pto)).webcrypto as Crypto;
 }
