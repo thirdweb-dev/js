61eee131e9d4044b29573d4103cf49c8ecd950be
Jonas Daniels <jonas.daniels@outlook.com>
2023-11-16 21:57:03 +0100

    refactor: Simplify logic for removing paths from `exports` in package.json

--- packages/contracts-js/scripts/revert-exports.ts
+++ packages/contracts-js/scripts/revert-exports.ts
@@ -1,27 +1,14 @@
+import {  readFile, writeFile } from "node:fs/promises";
 
-import {resolve} from "node:path";
-import { readdir, readFile, writeFile } from "node:fs/promises";
-
-
-// enumerate all files in `dist/abis/*.json`
-async function getPaths() {
-  
-  const dir = resolve(__dirname, '../dist/abis');
-  const paths = await readdir(dir);
-  const jsonPaths = paths.filter(p => p.endsWith('.json'));
-  return jsonPaths.map(p => `./dist/abis/${p}`);
-}
-
-
-const [paths, pkg] = await Promise.all([getPaths(), readFile('./package.json', "utf-8")]);
-
-const pkgJson = JSON.parse(pkg);
+const pkgJson = JSON.parse(await readFile('./package.json', "utf-8"));
 
 // for each path add to the `exports` field in `package.json`
 const pkgExports = pkgJson.exports || {};
-for(const path of paths){
-  delete pkgExports[path];
-}
+Object.keys(pkgExports).forEach(key => {
+  if(key.endsWith(".json") && key !== './package.json'){
+    delete pkgExports[key];
+  }
+})
 pkgJson.exports = pkgExports;
 // write the updated `package.json` to disk
 
