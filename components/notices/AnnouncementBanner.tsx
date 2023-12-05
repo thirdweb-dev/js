7a40bc136066c8011cb380313b41e3bcdef8cf8a
Edward Sun <eedwardsun@gmail.com>
2023-12-04 17:22:15 -0800

    Update announcement banner (#2036)
    * Update announcement banner
    
    * Update label

--- components/notices/AnnouncementBanner.tsx
+++ components/notices/AnnouncementBanner.tsx
@@ -26,7 +26,11 @@ export const AnnouncementBanner = () => {
         px={4}
       >
         <Box display={{ base: "none", md: "block" }} />
-        <TrackedLink href="/engine" category="announcement" label="engine">
+        <TrackedLink
+          href="https://blog.thirdweb.com/security-vulnerability"
+          category="announcement"
+          label="security"
+        >
           <Container maxW="container.page" display="flex" px={0}>
             <Flex
               cursor="pointer"
@@ -42,8 +46,8 @@ export const AnnouncementBanner = () => {
                 color="white"
                 fontWeight={500}
               >
-                Introducing Engine: The open-source HTTP server for scaling web3
-                apps. Start building
+                Smart contract security vulnerability - Potential action
+                required
               </Heading>
               <Icon display={{ base: "none", md: "block" }} as={FiArrowRight} />
             </Flex>
