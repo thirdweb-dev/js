26446665ce58f1509469f029e45494721d3cab65
ikethirdweb <jorge@thirdweb.com>
2023-12-18 19:03:28 -0500

    [RN] Update coinbase wallet SDK to fix bug

--- packages/react-native/src/evm/wallets/hooks/useCoinbaseWalletListener.ts
+++ packages/react-native/src/evm/wallets/hooks/useCoinbaseWalletListener.ts
@@ -13,20 +13,16 @@ export function useCoinbaseWalletListener(enable: boolean, callbackURL?: URL) {
       return;
     }
 
-    const sub = Linking.addEventListener("url", async ({ url }) => {
-      console.log(
-        "CoinbaseWalletListener: Setting up listener for",
-        callbackURL?.toString(),
-        url,
-      );
-      // const incomingUrl = new URL(url);
-      if (callbackURL && url.includes("wsegue?")) {
-        try {
-          // @ts-expect-error - Passing a URL object to handleResponse crashes the function
-          handleResponse(url);
-        } catch (error) {
-          console.error("CoinbaseWallet not initialized", error);
-        }
+    const sub = Linking.addEventListener("url", ({ url }) => {
+      const incomingUrl = new URL(url);
+      if (
+        callbackURL &&
+        incomingUrl.host === callbackURL.host &&
+        incomingUrl.protocol === callbackURL.protocol &&
+        incomingUrl.hostname === callbackURL.hostname
+      ) {
+        // @ts-expect-error - Passing a URL object to handleResponse crashes the function
+        handleResponse(url);
       }
     });
     return () => sub?.remove();
