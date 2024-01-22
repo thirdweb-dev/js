d2d925972b692adf914b4ad5f815039796755640
ikethirdweb <jorge@thirdweb.com>
2024-01-22 13:26:22 -0500

    chore: synced chains with db

--- packages/chains/chains/7341.ts
+++ packages/chains/chains/7341.ts
@@ -10,12 +10,6 @@ export default {
     }
   ],
   "faucets": [],
-  "icon": {
-    "url": "ipfs://QmUkFZC2ZmoYPTKf7AHdjwRPZoV2h1MCuHaGM4iu8SNFpi",
-    "width": 400,
-    "height": 400,
-    "format": "svg"
-  },
   "infoURL": "https://shyft.network",
   "name": "Shyft Mainnet",
   "nativeCurrency": {
