92c599ed00743b3dbc0ac9118a015cefc514a47f
Winston Yeo <winstonyeo99@yahoo.com>
2024-01-24 22:36:52 +0900

    chore(thirdweb): add error name for authentication errors

--- packages/thirdweb/src/wallets/embedded-wallet/core/authentication.error.ts
+++ packages/thirdweb/src/wallets/embedded-wallet/core/authentication.error.ts
@@ -3,10 +3,12 @@ export const USER_ABORT_AUTHENTICATION_ERROR_MESSAGE =
 export class UserAbortError extends Error {
   constructor() {
     super(USER_ABORT_AUTHENTICATION_ERROR_MESSAGE);
+    this.name = "UserAbortError";
   }
 }
 export class AuthenticationError extends Error {
   constructor(message: string) {
     super(message);
+    this.name = "AuthenticationError";
   }
 }
