de756eee0cc23971dbdfabaa9307097815666253
Jonas Daniels <jonas.daniels@outlook.com>
2024-01-28 22:20:26 -0800

    add inline docs to the blocknumber polling logic

--- packages/thirdweb/src/rpc/watchBlockNumber.ts
+++ packages/thirdweb/src/rpc/watchBlockNumber.ts
@@ -5,6 +5,7 @@ import type { ThirdwebClient } from "../client/client.js";
 const MAX_POLL_DELAY = 5000; // 5 seconds
 const DEFAULT_POLL_DELAY = 1000; // 1 second
 const MIN_POLL_DELAY = 100; // 100 milliseconds
+const DEFAULT_OVERPOLL_RATIO = 2; // poll twice as often as the average block time by default
 
 const SLIDING_WINDOW_SIZE = 10; // always keep track of the last 10 block times
 
@@ -14,17 +15,7 @@ function getAverageBlockTime(blockTimes: number[]): number {
     blockTimes.unshift(DEFAULT_POLL_DELAY);
   }
 
-  const sum = blockTimes.reduce((acc, blockTime) => {
-    // never let the blockTime be less than our minimum
-    if (blockTime <= MIN_POLL_DELAY) {
-      return acc + MIN_POLL_DELAY;
-    }
-    // never let the blockTime be more than our maximum
-    if (blockTime >= MAX_POLL_DELAY) {
-      return acc + MAX_POLL_DELAY;
-    }
-    return acc + blockTime;
-  }, 0);
+  const sum = blockTimes.reduce((acc, blockTime) => acc + blockTime, 0);
   return sum / blockTimes.length;
 }
 
@@ -68,27 +59,43 @@ function createBlockNumberPoller(
         blockTimesWindow = blockTimesWindow.slice(-SLIDING_WINDOW_SIZE);
       }
       lastBlockAt = currentTime;
+      // for all new blockNumbers...
       newBlockNumbers.forEach((b) => {
-        subscribers.forEach((fn) => fn(b));
+        // ... call all current subscribers
+        subscribers.forEach((subscriberCallback) => subscriberCallback(b));
       });
     }
     const currentApproximateBlockTime = getAverageBlockTime(blockTimesWindow);
 
-    // sleep for the average block time for this chain (divided by the overPollRatio, which defaults to 2)
-    await sleep(currentApproximateBlockTime / (overPollRatio ?? 2));
+    // make sure we never poll faster than our minimum poll delay or slower than our maximum poll delay
+    const pollDelay = Math.max(
+      MIN_POLL_DELAY,
+      Math.min(
+        MAX_POLL_DELAY,
+        Math.max(MIN_POLL_DELAY, currentApproximateBlockTime),
+      ),
+    );
+
+    // sleep for the pollDelay for this chain (divided by the overPollRatio, which defaults to 2)
+    await sleep(pollDelay / (overPollRatio ?? DEFAULT_OVERPOLL_RATIO));
     // poll again
     poll();
   }
 
-  return function (callBack: (blockNumber: bigint) => void) {
+  // return the "subscribe" function
+  return function subscribe(callBack: (blockNumber: bigint) => void) {
     subscribers.push(callBack);
+    // if we are currently not active -> start polling
     if (!isActive) {
       isActive = true;
       poll();
     }
 
+    // return the "unsubscribe" function (meaning the caller can unsubscribe)
     return function unSubscribe() {
+      // filter out the callback from the subscribers
       subscribers = subscribers.filter((fn) => fn !== callBack);
+      // if the new subscribers Array is empty (aka we were the last subscriber) -> stop polling
       if (subscribers.length === 0) {
         isActive = false;
       }
@@ -112,10 +119,14 @@ export function watchBlockNumber(opts: {
   overPollRatio?: number;
 }) {
   const { client, chainId, onNewBlockNumber, overPollRatio } = opts;
+  // if we already have a poller for this chainId -> use it
   let poller = existingPollers.get(chainId);
+  // otherwise create a new poller
   if (!poller) {
     poller = createBlockNumberPoller(client, chainId, overPollRatio);
+    // and store it for later use
     existingPollers.set(chainId, poller);
   }
+  // subscribe to the poller and return the unSubscribe function to the caller
   return poller(onNewBlockNumber);
 }
