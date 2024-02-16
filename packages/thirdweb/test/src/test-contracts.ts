bc907c4de272e2b06602e71cf791a16818cdecf0
Jonas Daniels <jonas.daniels@outlook.com>
2024-02-15 23:29:12 -0800

    style: Remove unnecessary code and comments

--- packages/thirdweb/test/src/test-contracts.ts
+++ packages/thirdweb/test/src/test-contracts.ts
@@ -5,8 +5,7 @@ import { getContract } from "../../src/contract/index.js";
 
 // ERC20
 
-const USDC_CONTRACT_ADDRESS =
-  "0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48";
+const USDC_CONTRACT_ADDRESS = "0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48";
 
 export const USDC_CONTRACT = getContract({
   client: TEST_CLIENT,
@@ -29,11 +28,4 @@ export const DOODLES_CONTRACT = getContract({
   client: TEST_CLIENT,
   address: DOODLES_ADDRESS,
   chain: FORKED_ETHEREUM_CHAIN,
-});
-
-const DOODLES_CONTRACT_WITH_ABI = getContract({
-  client: TEST_CLIENT,
-  address: DOODLES_ADDRESS,
-  chain: FORKED_ETHEREUM_CHAIN,
-  abi: USDC_ABI,
 });
