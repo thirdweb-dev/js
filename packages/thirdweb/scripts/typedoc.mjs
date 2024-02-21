e3810ab4d255f158b8949cf404824a3b0d127ef8
Jonas Daniels <jonas.daniels@outlook.com>
2024-02-21 04:32:48 -0800

    chore: Update typedoc entry points

--- packages/thirdweb/scripts/typedoc.mjs
+++ packages/thirdweb/scripts/typedoc.mjs
@@ -4,19 +4,6 @@
 import { typedoc } from "typedoc-gen";
 
 typedoc({
-  entryPoints: [
-    "src/index.ts",
-    "src/react/index.ts",
-    "src/rpc/index.ts",
-    "src/transaction/index.ts",
-    "src/event/index.ts",
-    "src/storage/index.ts",
-    "src/wallets/index.ts",
-    "src/utils/index.ts",
-    "src/contract/index.ts",
-    "src/wallets/{smart,injected,wallet-connect}/index.ts",
-    "src/extensions/*.ts",
-    "src/adapters/*.ts",
-  ],
+  entryPoints: ["src/exports/**/*.ts"],
   exclude: ["test/**/*", "src/**/*.test.ts", "src/**/*.bench.ts"],
 });
