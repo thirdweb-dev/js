78b04ed72a58bc4e0c3dd92368800d90e8aa819a
Joaquim Verges <joaquim.verges@gmail.com>
2024-02-27 11:11:23 -0800

    better error parsing

--- packages/thirdweb/src/transaction/extract-error.ts
+++ packages/thirdweb/src/transaction/extract-error.ts
@@ -19,14 +19,49 @@ export async function extractError<abi extends Abi>(args: {
       data?: Hex;
     };
     if (errorObj.data) {
-      const parsedError = decodeErrorResult({
-        data: errorObj.data,
-        abi: contract ? await resolveContractAbi(contract) : undefined,
-      });
-      return new Error(
-        `Execution reverted: ${parsedError.errorName} - ${parsedError.args}`,
-      );
+      if (errorObj.data !== "0x") {
+        const parsedError = decodeErrorResult({
+          data: errorObj.data,
+          abi: contract?.abi
+            ? contract.abi
+            : contract
+              ? await resolveContractAbi(contract)
+              : undefined,
+        });
+        return new TransactionError(
+          `${parsedError.errorName} - ${parsedError.args}`,
+          contract,
+        );
+      } else {
+        return new TransactionError("Execution Reverted", contract);
+      }
     }
   }
   return error;
+}
+
+// eslint-disable-next-line turbo/no-undeclared-env-vars, better-tree-shaking/no-top-level-side-effects
+export const __DEV__ = process.env.NODE_ENV !== "production";
+
+class TransactionError<abi extends Abi> extends Error {
+  public contractAddress: string | undefined;
+  public chainId: number | undefined;
+
+  constructor(reason: string, contract?: ThirdwebContract<abi>) {
+    super();
+    this.name = "TransactionError";
+    this.contractAddress = contract?.address;
+    this.chainId = contract?.chain?.id;
+    if (__DEV__ && contract) {
+      // show more infor in dev
+      this.message = [
+        reason,
+        "",
+        `contract: ${this.contractAddress}`,
+        `chainId: ${this.chainId}`,
+      ].join("\n");
+    } else {
+      this.message = reason;
+    }
+  }
 }
