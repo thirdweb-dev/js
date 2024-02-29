3540c446570b6c1ae930a15a8f7829afce8824ff
Jonas Daniels <jonas.daniels@outlook.com>
2024-02-29 11:27:55 -0800

    feat: Add ability to download wallets as .csv (#2274)

--- components/embedded-wallets/Users/index.tsx
+++ components/embedded-wallets/Users/index.tsx
@@ -3,11 +3,12 @@ import { Flex, Switch } from "@chakra-ui/react";
 import { createColumnHelper } from "@tanstack/react-table";
 import { TWTable } from "components/shared/TWTable";
 import { format } from "date-fns/format";
-import { useMemo, useState } from "react";
-import { Text } from "tw-components";
+import { useCallback, useMemo, useState } from "react";
+import { Button, Text } from "tw-components";
 import { withinDays } from "utils/date-utils";
 import { AddressCopyButton } from "tw-components/AddressCopyButton";
 import { Analytics } from "./Analytics";
+import Papa from "papaparse";
 
 const ACTIVE_THRESHOLD_DAYS = 30;
 
@@ -78,21 +79,57 @@ export const Users: React.FC<UsersProps> = ({
     });
   }, [wallets]);
 
+  const theWalletsWeWant = useMemo(() => {
+    return (onlyActive ? activeWallets : wallets) ?? [];
+  }, [activeWallets, onlyActive, wallets]);
+
+  const downloadCSV = useCallback(() => {
+    if (theWalletsWeWant.length === 0) {
+      return;
+    }
+    const csv = Papa.unparse(
+      theWalletsWeWant.map((row) => ({
+        email: row.ews_authed_user[0].email,
+        address: row.embedded_wallet[0].address,
+        created: format(new Date(row.created_at), "MMM dd, yyyy"),
+        last_login: format(new Date(row.last_accessed_at), "MMM dd, yyyy"),
+      })),
+    );
+    const csvUrl = URL.createObjectURL(
+      new Blob([csv], { type: "text/csv;charset=utf-8;" }),
+    );
+    const tempLink = document.createElement("a");
+    tempLink.href = csvUrl;
+    tempLink.setAttribute("download", "download.csv");
+    tempLink.click();
+  }, [theWalletsWeWant]);
+
   return (
     <Flex flexDir="column" gap={10}>
       <Flex flexDir="column" gap={6}>
-        <Flex gap={2} alignItems="center" justifyContent="flex-end">
-          <Text>Active last {ACTIVE_THRESHOLD_DAYS} days</Text>
-          <Switch
-            isChecked={onlyActive}
-            onChange={() => setOnlyActive(!onlyActive)}
-            disabled={wallets.length === 0}
-          />
+        <Flex dir="row" justify="space-between" align="center">
+          <Button
+            isDisabled={theWalletsWeWant.length === 0}
+            variant="outline"
+            onClick={downloadCSV}
+            size="sm"
+          >
+            Download as .csv
+          </Button>
+
+          <Flex gap={2} alignItems="center" justifyContent="flex-end">
+            <Text>Active last {ACTIVE_THRESHOLD_DAYS} days</Text>
+            <Switch
+              isChecked={onlyActive}
+              onChange={() => setOnlyActive(!onlyActive)}
+              disabled={wallets.length === 0}
+            />
+          </Flex>
         </Flex>
 
         <TWTable
           title="active embedded wallets"
-          data={onlyActive ? activeWallets : wallets}
+          data={theWalletsWeWant}
           columns={columns}
           isLoading={isLoading}
           isFetched={isFetched}
