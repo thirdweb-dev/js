6fcd7e232cd797ea14f4fd8a81d42db8c36b65e7
Jonas Daniels <jonas.daniels@outlook.com>
2024-02-28 21:40:12 -0800

    feat(test): add secret key to fork URL

--- packages/thirdweb/test/globalSetup.ts
+++ packages/thirdweb/test/globalSetup.ts
@@ -1,15 +1,25 @@
+import { sha256 } from "@noble/hashes/sha256";
 import { createAnvil } from "@viem/anvil";
 
 // eslint-disable-next-line @typescript-eslint/no-var-requires
 require("dotenv-mono").load();
 
+const SECRET_KEY = process.env.TW_SECRET_KEY as string;
+// eslint-disable-next-line no-restricted-globals
+const clientId = Buffer.from(sha256(SECRET_KEY)).toString("utf-8").slice(0, 32);
+
 export default async function globalSetup() {
   const anvil = createAnvil({
-    forkUrl: "https://1.rpc.thirdweb.com",
+    chainId: 1,
+    forkUrl: `https://1.rpc.thirdweb.com/${clientId}`,
+    // TODO: add the secret key headers here once we have the ability to pass headers
+    // see: https://github.com/wevm/anvil.js/pull/44
+    forkChainId: 1,
     forkBlockNumber: 19139495n,
     port: 8555,
     noMining: true,
   });
+
   await anvil.start();
 
   return async () => {
