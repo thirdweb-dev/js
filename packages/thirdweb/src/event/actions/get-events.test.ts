5182f2a0b1fb748b3dc223c0bd076173af0596f8
Joaquim Verges <joaquim.verges@gmail.com>
2024-03-04 11:23:45 -0800

    add TODO event test

--- packages/thirdweb/src/event/actions/get-events.test.ts
+++ packages/thirdweb/src/event/actions/get-events.test.ts
@@ -29,13 +29,53 @@ describe("getEvents", () => {
     expect(events.length).toBe(2);
   });
 
+  // TODO make this test pass
+  it.todo("should get specified events", async () => {
+    const events = await getContractEvents({
+      contract: USDC_CONTRACT,
+      fromBlock: 19139495n - 10n,
+      events: [
+        prepareEvent({
+          signature: "event Burn(address indexed burner, uint256 amount)",
+        }),
+        prepareEvent({
+          signature: {
+            anonymous: false,
+            inputs: [
+              {
+                indexed: true,
+                internalType: "address",
+                name: "owner",
+                type: "address",
+              },
+              {
+                indexed: true,
+                internalType: "address",
+                name: "spender",
+                type: "address",
+              },
+              {
+                indexed: false,
+                internalType: "uint256",
+                name: "value",
+                type: "uint256",
+              },
+            ],
+            name: "Approval",
+            type: "event",
+          },
+        }),
+      ],
+    });
+    expect(events.length).toBe(15);
+  });
+
   it("should get individual events with extension", async () => {
     const events = await getContractEvents({
       contract: DOODLES_CONTRACT,
       fromBlock: 19139495n - 1000n,
       events: [transferEvent()],
     });
-    console.log(events.length);
     expect(events.length).toBe(38);
   });
 });
