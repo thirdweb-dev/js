b5a6f8596642974bd82c502f6aa38c9e0f84d29c
greg <gregfromstl@gmail.com>
2024-03-03 20:48:48 -0800

    feat(actions): sendAndConfirmTransaction (#2386)
    Co-authored-by: Joaquim Verges <joaquim.verges@gmail.com>

---
+++ packages/thirdweb/src/transaction/actions/send-and-confirm-transaction.ts
@@ -0,0 +1,29 @@
+import { waitForReceipt } from "./wait-for-tx-receipt.js";
+import type { TransactionReceipt } from "../types.js";
+import {
+  sendTransaction,
+  type SendTransactionOptions,
+} from "./send-transaction.js";
+
+/**
+ * Sends a transaction using the provided wallet.
+ * @param options - The options for sending the transaction.
+ * @returns A promise that resolves to the confirmed transaction receipt.
+ * @throws An error if the wallet is not connected.
+ * @transaction
+ * @example
+ * ```ts
+ * import { sendAndConfirmTransaction } from "thirdweb";
+ *
+ * const transactionReceipt = await sendAndConfirmTransaction({
+ *  wallet,
+ *  transaction
+ * });
+ * ```
+ */
+export async function sendAndConfirmTransaction(
+  options: SendTransactionOptions,
+): Promise<TransactionReceipt> {
+  const submittedTx = await sendTransaction(options);
+  return waitForReceipt(submittedTx);
+}
