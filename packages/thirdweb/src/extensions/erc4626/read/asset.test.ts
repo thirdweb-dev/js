9e754f4af51f928fd40a3e1046aa0a04f339096f
gregfromstl <gregfromstl@gmail.com>
2024-03-04 20:12:52 -0800

    fix(extensions): ERC4626 assets fn cleanup

--- packages/thirdweb/src/extensions/erc4626/read/asset.test.ts
+++ packages/thirdweb/src/extensions/erc4626/read/asset.test.ts
@@ -2,7 +2,6 @@ import { describe, it, expect, vi, afterEach } from "vitest";
 
 import { asset } from "./asset.js";
 import { FRAX_ETHER_CONTRACT } from "~test/test-contracts.js";
-import { VITALIK_WALLET } from "~test/addresses.js";
 
 const fetchSpy = vi.spyOn(globalThis, "fetch");
 
@@ -13,13 +12,8 @@ describe("erc4626.asset", () => {
   it("should return the balanceOf result", async () => {
     const underlyingAsset = await asset({
       contract: FRAX_ETHER_CONTRACT,
-      vaultAddress: VITALIK_WALLET,
     });
-    expect(underlyingAsset).toMatchInlineSnapshot(`
-      {
-        "address": "0x5E8422345238F34275888049021821E8E08CAa1f",
-      }
-    `);
+    expect(underlyingAsset).toBe("0x5E8422345238F34275888049021821E8E08CAa1f");
     expect(fetchSpy).toHaveBeenCalledTimes(1);
   });
 });
--- packages/thirdweb/src/extensions/erc4626/read/asset.ts
+++ packages/thirdweb/src/extensions/erc4626/read/asset.ts
@@ -3,37 +3,18 @@ import type { BaseTransactionOptions } from "../../../transaction/types.js";
 import type { Address } from "abitype";
 
 /**
- * Represents the parameters for retrieving the underlying asset of a tokenized vault.
- */
-type AssetParams = {
-  /**
-   * The token vault address for which to retrieve the base asset.
-   */
-  vaultAddress: string;
-};
-
-/**
- * Represents the result of a balance query for an ERC20 token.
- */
-type AssetResult = {
-  address: Address;
-};
-
-/**
  * Retrieves the underlying ERC20 used in a tokenized vault.
- * @param options - The transaction options including the address.
- * @returns An object containing the vault's underlying token address.
+ * @param options - The transaction options.
+ * @returns The vault's underlying token address.
  * @extension ERC4626
  * @example
  * ```ts
- * import { asset } from "thirdweb/extensions/erc20";
+ * import { asset } from "thirdweb/extensions/erc4626";
  *
- * const underlyingAsset = await asset({ contract, vaultAddress: "0x..." });
+ * const underlyingAsset = await asset({ contract });
  * ```
  */
-export async function asset(
-  options: BaseTransactionOptions<AssetParams>,
-): Promise<AssetResult> {
+export async function asset(options: BaseTransactionOptions): Promise<Address> {
   const address = await readContract({
     ...options,
     method: [
@@ -48,7 +29,5 @@ export async function asset(
     params: [],
   });
 
-  return {
-    address,
-  };
+  return address;
 }
