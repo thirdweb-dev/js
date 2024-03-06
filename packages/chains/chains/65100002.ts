78c664f25dac3df427022a243c691d7b86fc73c2
GitHub Action <action@github.com>
2024-03-06 01:21:16 +0000

    chore(chains): sync from db

--- packages/chains/chains/65100002.ts
+++ packages/chains/chains/65100002.ts
@@ -1,7 +1,7 @@
 import type { Chain } from "../src/types";
 export default {
   "chain": "AUT",
-  "chainId": 65100001,
+  "chainId": 65100002,
   "explorers": [
     {
       "name": "autonity-blockscout",
@@ -17,17 +17,20 @@ export default {
     "format": "png"
   },
   "infoURL": "https://autonity.org/",
-  "name": "Autonity Piccadilly (Barada) Testnet",
+  "name": "Autonity Piccadilly (Sumida) Testnet",
   "nativeCurrency": {
     "name": "Piccadilly Auton",
     "symbol": "ATN",
     "decimals": 18
   },
-  "networkId": 65100001,
-  "rpc": [],
-  "shortName": "piccadilly-01",
+  "networkId": 65100002,
+  "rpc": [
+    "https://65100002.rpc.thirdweb.com/${THIRDWEB_API_KEY}",
+    "https://rpc1.piccadilly.autonity.org/",
+    "wss://rpc1.piccadilly.autonity.org/ws/"
+  ],
+  "shortName": "piccadilly-02",
   "slip44": 1,
-  "slug": "autonity-piccadilly-barada-testnet",
-  "status": "deprecated",
+  "slug": "autonity-piccadilly-sumida-testnet",
   "testnet": true
 } as const satisfies Chain;
