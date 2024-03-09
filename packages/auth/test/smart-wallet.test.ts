111de17549c83a24a31be2132e9e5466c1e3bfe5
0xFirekeeper <0xFirekeeper@gmail.com>
2024-03-09 23:53:37 +0300

    add jest for auth sw test // timeout 240

--- packages/auth/test/smart-wallet.test.ts
+++ packages/auth/test/smart-wallet.test.ts
@@ -5,6 +5,7 @@ import { expect } from "chai";
 import { Wallet } from "ethers";
 
 require("dotenv-mono").load();
+jest.setTimeout(240_000);
 
 describe("Wallet Authentication - EVM - Smart Wallet", async () => {
   let adminWallet: any, signerWallet: any, attackerWallet: any;
