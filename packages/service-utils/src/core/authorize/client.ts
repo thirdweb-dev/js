f1bfca46ed13443b696f1206ab0e38fdfe609ca9
Winston Yeo <winstonyeo99@yahoo.com>
2024-03-11 17:55:09 -0400

    chore: whitelist embedded-wallet.thirdweb.com in service utils

--- packages/service-utils/src/core/authorize/client.ts
+++ packages/service-utils/src/core/authorize/client.ts
@@ -52,7 +52,9 @@ export function authorizeClient(
   if (origin) {
     if (
       // find matching domain, or if all domains allowed
-      domains.find((d) => {
+      // embedded-wallet.thirdweb.com is automatically allowed
+      // because the rpc is passed from user's doamin to embedded-wallet.thirdweb.com iframe for use.
+      [...domains, "embedded-wallet.thirdweb.com"].find((d) => {
         // if any domain is allowed, we'll return true
         if (d === "*") {
           return true;
