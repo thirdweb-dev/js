11b87b7caed95e4d3043da7a4e7c114ce5cd1b8c
Kien Ngo <kienngo@skiff.com>
2024-03-14 05:09:44 +0700

    Update

--- packages/wallets/src/evm/wallets/binance-wallet.ts
+++ packages/wallets/src/evm/wallets/binance-wallet.ts
@@ -1,12 +1,44 @@
+import type { WalletConnectConnector as WalletConnectConnectorType } from "../connectors/wallet-connect";
+import type { QRModalOptions } from "../connectors/wallet-connect/qrModalOptions";
 import { Connector, WagmiAdapter } from "../interfaces/connector";
 import { AbstractClientWallet, WalletOptions } from "./base";
+import type { BinanceConnector as BinanceWalletConnectorType } from "../connectors/binance-wallet";
 import { walletIds } from "../constants/walletIds";
+import { TW_WC_PROJECT_ID } from "../constants/wc";
 import { getInjectedBinanceProvider } from "../connectors/binance-wallet/getInjectedBinanceProvider";
 
-export type BinanceOptions = WalletOptions;
+export type BinanceWalletAdditionalOptions = {
+  /**
+   * Whether to open the default Wallet Connect QR code Modal for connecting to Binance app on mobile if Binance wallet is not injected when calling connect().
+   */
+  qrcode?: boolean;
+
+  /**
+   * When connecting Binance wallet using the QR Code - Wallet Connect connector is used which requires a project id.
+   * This project id is Your project’s unique identifier for wallet connect that can be obtained at cloud.walletconnect.com.
+   *
+   * https://docs.walletconnect.com/2.0/web3modal/options#projectid-required
+   */
+  projectId?: string;
+
+  /**
+   * options to customize the Wallet Connect QR Code Modal ( only relevant when qrcode is true )
+   *
+   * https://docs.walletconnect.com/2.0/web3modal/options
+   */
+  qrModalOptions?: QRModalOptions;
+};
+
+export type BinanceWalletOptions = WalletOptions<BinanceWalletAdditionalOptions>;
+
+type ConnectWithQrCodeArgs = {
+  chainId?: number;
+  onQrCodeUri: (uri: string) => void;
+  onConnected: (accountAddress: string) => void;
+};
 
 /**
- * Wallet interface to connect [Binance wallet](https://binance.com/en/web3wallet) extension or mobile app
+ * Wallet interface to connect [Binance wallet](https://binance.com/en/web3wallet) extension or mobile app.
  *
  * @example
  * ```ts
@@ -19,11 +51,19 @@ export type BinanceOptions = WalletOptions;
  *
  * @wallet
  */
-export class BinanceWallet extends AbstractClientWallet {
+export class BinanceWallet extends AbstractClientWallet<BinanceWalletAdditionalOptions> {
   /**
    * @internal
    */
   connector?: Connector;
+  /**
+   * @internal
+   */
+  walletConnectConnector?: WalletConnectConnectorType;
+  /**
+   * @internal
+   */
+  BinanceWalletConnector?: BinanceWalletConnectorType;
   /**
    * @internal
    */
@@ -36,7 +76,7 @@ export class BinanceWallet extends AbstractClientWallet {
    * @internal
    */
   public get walletName() {
-    return "Binance" as const;
+    return "Binance Wallet" as const;
   }
 
   /**
@@ -48,6 +88,12 @@ export class BinanceWallet extends AbstractClientWallet {
    * Provide `clientId` to use the thirdweb RPCs for given `chains`
    *
    * You can create a client ID for your application from [thirdweb dashboard](https://thirdweb.com/create-api-key).
+   *
+   * ### projectId (recommended)
+   *
+   * This is only relevant if you want to use [WalletConnect](https://walletconnect.com/) for connecting to Zerion wallet mobile app when MetaMask is not injected.
+   *
+   * This `projectId` can be obtained at [cloud.walletconnect.com](https://cloud.walletconnect.com/). It is highly recommended to use your own project id and only use the default one for testing purposes.
    *
    * ### chains (optional)
    * Provide an array of chains you want to support.
@@ -61,38 +107,115 @@ export class BinanceWallet extends AbstractClientWallet {
    *
    * Must be an object containing `name`, `url`, and optionally `description` and `logoUrl` properties.
    *
-   * ```javascript
-   * import { PhantomWallet } from "@thirdweb-dev/wallets";
+   * ### qrcode
+   * Whether to display the Wallet Connect QR code Modal or not.
    *
-   * const wallet = new BinanceWallet({
-   *   dappMetadata: {
-   *     name: "thirdweb powered dApp",
-   *     url: "https://thirdweb.com",
-   *     description: "thirdweb powered dApp",
-   *     logoUrl: "https://thirdweb.com/favicon.ico",
-   *   },
-   * });
-   * ```
+   * Must be a `boolean`. Defaults to `true`.
+   *
+   * ### qrModalOptions
+   * WalletConnect's [options](https://docs.walletconnect.com/advanced/walletconnectmodal/options) to customize the QR Code Modal.
+   *
    */
-  constructor(options: BinanceOptions) {
+  constructor(options: BinanceWalletOptions) {
     super(BinanceWallet.id, options);
     this.isInjected = !!getInjectedBinanceProvider();
   }
 
   protected async getConnector(): Promise<Connector> {
     if (!this.connector) {
-      const { BinanceConnector } = await import("../connectors/binance-wallet");
-      const connector = new BinanceConnector({
-        chains: this.chains,
-        connectorStorage: this.walletStorage,
-        options: {
-          shimDisconnect: true,
-        },
-      });
+      // if Binance wallet is injected, use the injected connector
+      // otherwise, use the wallet connect connector for using the Binance app on mobile via QR code scan
 
-      this.connector = new WagmiAdapter(connector);
+      if (this.isInjected) {
+        // import the connector dynamically
+        const { BinanceConnector } = await import(
+          "../connectors/binance-wallet"
+        );
+        this.BinanceWalletConnector = new BinanceConnector({
+          chains: this.chains,
+          connectorStorage: this.walletStorage,
+          options: {
+            shimDisconnect: true,
+          },
+        });
+
+        this.connector = new WagmiAdapter(this.BinanceWalletConnector);
+      } else {
+        const { WalletConnectConnector } = await import(
+          "../connectors/wallet-connect"
+        );
+
+        const walletConnectConnector = new WalletConnectConnector({
+          chains: this.chains,
+          options: {
+            projectId: this.options?.projectId || TW_WC_PROJECT_ID, // TODO,
+            storage: this.walletStorage,
+            qrcode: this.options?.qrcode,
+            dappMetadata: this.dappMetadata,
+            qrModalOptions: this.options?.qrModalOptions,
+          },
+        });
+
+        walletConnectConnector.getProvider().then((provider) => {
+          provider.signer.client.on("session_request_sent", () => {
+            this.emit("wc_session_request_sent");
+          });
+        });
+
+        // need to save this for getting the QR code URI
+        this.walletConnectConnector = walletConnectConnector;
+        this.connector = new WagmiAdapter(walletConnectConnector);
+      }
     }
 
     return this.connector;
+  }
+
+  /**
+   * Connect to the wallet using a QR code.
+   * You can use this method to display a QR code. The user can scan this QR code using the Binance Wallet mobile app to connect to your dapp.
+   *
+   * @example
+   * ```typescript
+   * wallet.connectWithQrCode({
+   *  chainId: 1,
+   *  onQrCodeUri(qrCodeUri) {
+   *    // render the QR code with `qrCodeUri`
+   *  },
+   *  onConnected(accountAddress)  {
+   *    // update UI to show connected state
+   *  },
+   * })
+   * ```
+   *
+   * @param options -
+   * The options object contains the following properties/method:
+   *
+   * ### chainId (optional)
+   * If provided, wallet will prompt the user to switch to the network with the given `chainId` after connecting.
+   *
+   * ### onQrCodeUri
+   * A callback to get the QR code URI to display to the user.
+   *
+   * ### onConnected
+   * A callback that is called when the user has connected their wallet using the QR code.
+   */
+  async connectWithQrCode(options: ConnectWithQrCodeArgs) {
+    await this.getConnector();
+    const wcConnector = this.walletConnectConnector;
+
+    if (!wcConnector) {
+      throw new Error("WalletConnect connector not found");
+    }
+
+    const wcProvider = await wcConnector.getProvider();
+
+    // set a listener for display_uri event
+    wcProvider.on("display_uri", (uri) => {
+      options.onQrCodeUri(uri);
+    });
+
+    // trigger connect flow
+    this.connect({ chainId: options.chainId }).then(options.onConnected);
   }
 }
