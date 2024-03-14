1229f1211eee105195b2c9901a61f8a2e9cff519
ikethirdweb <jorge@thirdweb.com>
2024-03-13 23:10:29 -0400

    storage

--- packages/thirdweb/src/react/storage.ts
+++ packages/thirdweb/src/react/storage.ts
@@ -2,6 +2,14 @@ import type { AsyncStorage } from "../wallets/storage/AsyncStorage.js";
 
 let storage: AsyncStorage;
 
+/**
+ * Gets the storage to be used in react.
+ * @example
+ * ```
+ * const storage = getStorage();
+ * ```
+ * @returns The storage to use
+ */
 export function getStorage() {
   if (storage) {
     return storage;
@@ -9,6 +17,14 @@ export function getStorage() {
   return localStorage;
 }
 
-export function setConnectionManager(_storage: AsyncStorage) {
+/**
+ * Sets the storage to be used in react.
+ * @example
+ * ```
+ * setStorage(storage);
+ * ```
+ * @param _storage - The storage to use
+ */
+export function setStorage(_storage: AsyncStorage) {
   storage = _storage;
 }
