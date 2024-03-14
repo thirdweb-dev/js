c494db87cbfa4b6fc4013abb01936e86a4350ea8
Joaquim Verges <joaquim.verges@gmail.com>
2024-03-14 01:01:38 -0700

    sync chains (#2484)

--- packages/chains/chains/813.ts
+++ packages/chains/chains/813.ts
@@ -5,8 +5,19 @@ export default {
   "explorers": [
     {
       "name": "meerscan",
+      "url": "https://qng.qitmeer.io",
+      "standard": "EIP3091",
+      "icon": {
+        "url": "ipfs://QmWSbMuCwQzhBB6GRLYqZ87n5cnpzpYCehCAMMQmUXj4mm",
+        "width": 512,
+        "height": 512,
+        "format": "png"
+      }
+    },
+    {
+      "name": "meerscan",
       "url": "https://qng.meerscan.io",
-      "standard": "none",
+      "standard": "EIP3091",
       "icon": {
         "url": "ipfs://QmWSbMuCwQzhBB6GRLYqZ87n5cnpzpYCehCAMMQmUXj4mm",
         "width": 512,
@@ -36,8 +47,6 @@ export default {
     "https://evm-dataseed2.meerscan.io",
     "https://evm-dataseed3.meerscan.io",
     "https://evm-dataseed.meerscan.com",
-    "https://evm-dataseed1.meerscan.com",
-    "https://evm-dataseed2.meerscan.com",
     "https://qng.rpc.qitmeer.io",
     "https://mainnet.meerlabs.com",
     "https://rpc.dimai.ai",
