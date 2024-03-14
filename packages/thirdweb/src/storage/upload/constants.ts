d8cebb08f374f002059be632caca43e0b4aa5ab1
Winston Yeo <winstonyeo99@yahoo.com>
2024-03-14 13:25:40 -0400

    docs: add doc string

--- packages/thirdweb/src/storage/upload/constants.ts
+++ packages/thirdweb/src/storage/upload/constants.ts
@@ -1,4 +1,8 @@
 import type { ThirdwebClient } from "../../client/client.js";
 
+/**
+ * Constructs the endpoint to upload a file to ipfs
+ * @internal
+ */
 export const getUploadServerUrl = (client: ThirdwebClient) =>
   client.config?.baseUrls?.storage ?? "storage.thirdweb.com";
