e6dc7120d24e3688778de895e9f51c4d2a245aa9
gregfromstl <gregfromstl@gmail.com>
2024-03-15 13:35:50 -0700

    test(extensions/farcaster): getRegistrationPrice extra storage

--- packages/thirdweb/src/extensions/farcaster/read/getRegistrationPrice.test.ts
+++ packages/thirdweb/src/extensions/farcaster/read/getRegistrationPrice.test.ts
@@ -9,11 +9,21 @@ describe("farcaster.getRegistrationPrice", () => {
   afterEach(() => {
     fetchSpy.mockClear();
   });
+
   it("should return the price to register a new fid", async () => {
     const price = await getRegistrationPrice({
       client: TEST_CLIENT,
     });
     expect(price).toBe(814540083517510n);
+    expect(fetchSpy).toHaveBeenCalledTimes(1);
+  });
+
+  it("should return the price to register a new fid with extra storage", async () => {
+    const price = await getRegistrationPrice({
+      client: TEST_CLIENT,
+      extraStorage: 3,
+    });
+    expect(price).toBe(814540083517510n * 4n);
     expect(fetchSpy).toHaveBeenCalledTimes(1);
   });
 });
