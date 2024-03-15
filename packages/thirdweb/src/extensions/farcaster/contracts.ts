05c59136a52bc65736635db2f8bf2a045a5fe02b
gregfromstl <gregfromstl@gmail.com>
2024-03-14 22:16:46 -0700

    fix(extensions/farcaster): remove unused generic

--- packages/thirdweb/src/extensions/farcaster/contracts.ts
+++ packages/thirdweb/src/extensions/farcaster/contracts.ts
@@ -1,16 +1,15 @@
 import { getContract, type ThirdwebContract } from "src/contract/contract.js";
 import { ID_GATEWAY_ADDRESS } from "./constants.js";
 import type { ThirdwebClient } from "src/client/client.js";
-import type { Abi } from "abitype";
 import { optimism } from "src/exports/chains.js";
 
 export type FarcasterContractOptions = {
   client: ThirdwebClient;
 };
 
-export function getIdGateway<const abi extends Abi = []>({
+export function getIdGateway({
   client,
-}: FarcasterContractOptions): ThirdwebContract<abi> {
+}: FarcasterContractOptions): ThirdwebContract {
   return getContract({
     client,
     address: ID_GATEWAY_ADDRESS,
