8a6dc331a77d247eb572cbec35df32ee2bff3709
Juan Leal <114959779+juandolealt@users.noreply.github.com>
2024-03-19 10:34:33 -0400

    desc fix (#2372)
    Signed-off-by: Juan Leal <114959779+juandolealt@users.noreply.github.com>

--- components/homepage/sections/HeroSection.tsx
+++ components/homepage/sections/HeroSection.tsx
@@ -88,8 +88,8 @@ export const HeroSection = ({ TRACKING_CATEGORY }: HeroSectionProps) => {
             </Heading>
           </Flex>
           <Text size="body.xl" mr={6}>
-            Frontend, backend, and onchain tools for end-to-end web3 apps — on
-            any EVM chain.
+            Frontend, backend, and onchain tools to build complete web3 apps —
+            on any EVM chain.
           </Text>
 
           <Flex
