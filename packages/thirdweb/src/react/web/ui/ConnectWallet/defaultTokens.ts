06a7697f7a09db0e52361bedbdcc1e758bad03ca
Manan Tank <manantankm@gmail.com>
2024-03-19 18:20:33 +0530

    Remove usdc bridged

--- packages/thirdweb/src/react/web/ui/ConnectWallet/defaultTokens.ts
+++ packages/thirdweb/src/react/web/ui/ConnectWallet/defaultTokens.ts
@@ -152,12 +152,6 @@ export const defaultTokens: SupportedTokens = {
       icon: usdcIcon,
     },
     {
-      address: "0x2791Bca1f2de4661ED88A30C99A7a9449Aa84174",
-      name: "USD Coin (Bridged)",
-      symbol: "USDC.e",
-      icon: usdcIcon,
-    },
-    {
       address: "0x0d500B1d8E8eF31E21C99d1Db9A6444d3ADf1270",
       name: "Wrapped Matic",
       symbol: "WMATIC",
