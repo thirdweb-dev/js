79bafa250f71ecb151316db11df681843addef32
Manan Tank <manantankm@gmail.com>
2024-03-22 00:45:06 +0530

    JSDoc improvements

--- packages/thirdweb/src/react/core/hooks/connection/useAutoConnect.ts
+++ packages/thirdweb/src/react/core/hooks/connection/useAutoConnect.ts
@@ -94,20 +94,29 @@ export type AutoConnectProps = {
 };
 
 /**
- * AutoConnect Last connected wallet.
- * Note: If you are using `ConnectButton`, You don't need to use this component as it is already included in `ConnectButton`.
+ * AutoConnect last connected wallet on page reload or revisit.
+ * Note: If you are using `ConnectButton` or `ConnectEmbed` components, You don't need to use this component as it is already included.
+ *
+ * This is useful if you are manually connecting the wallets using the [`useConnect`](https://portal.thirdweb.com/references/typescript/v5/useConnect) hook and want to auto connect the last connected wallets on page reload or revisit.
  * @param props - Object of type `AutoConnectProps`. Refer to [`AutoConnectProps`](https://portal.thirdweb.com/references/typescript/v5/AutoConnectProps)
  * @example
  * ```tsx
  * import { AutoConnect } from "@thirdweb/react";
  *
+ * // list of wallets that your app uses
  * const wallets = [
  *  metamaskConfig(),
  *  coinbaseConfig(),
  * ]
  *
  * function Example() {
- *  return <AutoConnect wallets={wallets} client={client} appMetadata={appMetadata} />;
+ *  return (
+ *    <AutoConnect
+ *      wallets={wallets}
+ *      client={client}
+ *      appMetadata={appMetadata}
+ *    />
+ *  );
  * }
  * ```
  * @component
