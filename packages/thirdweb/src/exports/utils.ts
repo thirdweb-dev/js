a859b70464a884ec090968e36d54ec7571b93091
gregfromstl <gregfromstl@gmail.com>
2024-03-20 21:53:26 -0700

    fix removed line

--- packages/thirdweb/src/exports/utils.ts
+++ packages/thirdweb/src/exports/utils.ts
@@ -38,6 +38,7 @@ export {
   signTypedData,
 } from "../utils/signatures/sign-typed-data.js";
 
+// ------------------------------------------------
 // encoding
 // ------------------------------------------------
 
