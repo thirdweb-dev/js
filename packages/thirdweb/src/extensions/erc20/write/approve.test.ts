af7fc144caa2f01002ada685afc449298fab0d4b
Jonas Daniels <jonas.daniels@outlook.com>
2024-03-30 16:59:37 -0700

    test: estimates erc20 approval gas correctly

--- packages/thirdweb/src/extensions/erc20/write/approve.test.ts
+++ packages/thirdweb/src/extensions/erc20/write/approve.test.ts
@@ -4,15 +4,18 @@ import { USDC_CONTRACT } from "~test/test-contracts.js";
 import { VITALIK_WALLET } from "~test/addresses.js";
 import { estimateGas } from "../../../transaction/actions/estimate-gas.js";
 
-it("estimate erc20 approve", async () => {
-  const transaction = approve({
-    contract: USDC_CONTRACT,
-    amount: 100,
-    spender: VITALIK_WALLET,
-  });
-  const result = await estimateGas({
-    transaction,
-    from: VITALIK_WALLET,
-  });
-  expect(result).toMatchInlineSnapshot(`55937n`);
-});
+it.runIf(process.env.TW_SECRET_KEY)(
+  "estimates erc20 approval gas correctly",
+  async () => {
+    const transaction = approve({
+      contract: USDC_CONTRACT,
+      amount: 100,
+      spender: VITALIK_WALLET,
+    });
+    const result = await estimateGas({
+      transaction,
+      from: VITALIK_WALLET,
+    });
+    expect(result).toMatchInlineSnapshot(`55937n`);
+  },
+);
