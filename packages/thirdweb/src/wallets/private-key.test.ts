9425ff7ae785c25aa4ce3511ba55228a9ed8ce62
gregfromstl <gregfromstl@gmail.com>
2024-04-01 12:50:34 -0700

    test(private-key): use internal unit parsers

--- packages/thirdweb/src/wallets/private-key.test.ts
+++ packages/thirdweb/src/wallets/private-key.test.ts
@@ -1,23 +1,22 @@
 import { expect, test } from "vitest";
-import { parseEther, parseGwei } from "viem/utils";
 
 import { privateKeyAccount } from "./private-key.js";
 import { ANVIL_PKEY_A } from "~test/test-wallets.js";
 import { TEST_CLIENT } from "~test/test-clients.js";
 import { typedData } from "~test/typed-data.js";
+import { toUnits } from "../utils/units.js";
 
 test("default", () => {
   expect(privateKeyAccount({ privateKey: ANVIL_PKEY_A, client: TEST_CLIENT }))
     .toMatchInlineSnapshot(`
     {
       "address": "0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266",
-      "publicKey": "0x048318535b54105d4a7aae60c08fc45f9687181b4fdfc625bd1a753fa7397fed753547f11ca8696646f2f3acb08e31016afac23e630c5d11f59f61fef57b0d2aa5",
       "sendTransaction": [Function],
       "signMessage": [Function],
       "signTransaction": [Function],
       "signTypedData": [Function],
-      "source": "privateKey",
-      "type": "local",
+      Symbol(type): "local",
+      Symbol(publicKey): "0x048318535b54105d4a7aae60c08fc45f9687181b4fdfc625bd1a753fa7397fed753547f11ca8696646f2f3acb08e31016afac23e630c5d11f59f61fef57b0d2aa5",
     }
   `);
 });
@@ -39,13 +38,15 @@ test("sign transaction", async () => {
     privateKey: ANVIL_PKEY_A,
     client: TEST_CLIENT,
   });
+
+  expect(account.signTransaction).toBeDefined();
   expect(
-    await account.signTransaction({
+    await account.signTransaction!({
       chainId: 1,
-      maxFeePerGas: parseGwei("20"),
+      maxFeePerGas: toUnits("20", 9),
       gas: 21000n,
       to: "0x70997970c51812dc3a010c7d01b50e0d17dc79c8",
-      value: parseEther("1"),
+      value: toUnits("1", 18),
     }),
   ).toMatchInlineSnapshot(
     '"0x02f86f0180808504a817c8008252089470997970c51812dc3a010c7d01b50e0d17dc79c8880de0b6b3a764000080c001a0f40a2d2ae9638056cafbe9083c7125edc8555e0e715db0984dd859a5c6dfac57a020f36fd0b32bef4d6d75c62f220e59c5fb60c244ca3b361e750985ee5c3a0931"',
