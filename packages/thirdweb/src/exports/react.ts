9e23c333d763721b76668c67befa8f6f783471f1
Joaquim Verges <joaquim.verges@gmail.com>
2024-04-03 21:07:15 +0200

    export

--- packages/thirdweb/src/exports/react.ts
+++ packages/thirdweb/src/exports/react.ts
@@ -53,6 +53,7 @@ export {
 // contract related
 export { useReadContract } from "../react/core/hooks/contract/useReadContract.js";
 export { useSendTransaction } from "../react/core/hooks/contract/useSendTransaction.js";
+export { useSendAndConfirmTransaction } from "../react/core/hooks/contract/useSendAndConfirmTransaction.js";
 export { useEstimateGas } from "../react/core/hooks/contract/useEstimateGas.js";
 export { useEstimateGasCost } from "../react/core/hooks/contract/useEstimateGasCost.js";
 export { useWaitForReceipt } from "../react/core/hooks/contract/useWaitForReceipt.js";
