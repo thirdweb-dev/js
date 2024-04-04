b3d01402dd93c24f6b16aaa55674c2c10eb33be5
Manan Tank <manantankm@gmail.com>
2024-04-05 00:48:42 +0530

    [v4 wallets] Start new session for walletConnect on connect method call (#2683)

--- legacy_packages/wallets/src/evm/connectors/wallet-connect/index.ts
+++ legacy_packages/wallets/src/evm/connectors/wallet-connect/index.ts
@@ -124,15 +124,9 @@ export class WalletConnectConnector extends WagmiConnector<
       const provider = await this.getProvider();
       this.setupListeners();
 
-      const isChainsStale = await this._isChainsStale();
-
-      // If there is an active session with stale chains, disconnect the current session.
-      if (provider.session && isChainsStale) {
+      if (provider.session) {
         await provider.disconnect();
-      }
 
-      // If there no active session, or the chains are stale, connect.
-      if (!provider.session || isChainsStale) {
         const optionalChains = this.filteredChains
           .filter((chain) => chain.chainId !== targetChainId)
           .map((optionalChain) => optionalChain.chainId);
