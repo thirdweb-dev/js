bbd7d81e3a7bc9bd9356dab269b4a52ed60106ff
Yash <kumaryashcse@gmail.com>
2024-04-05 20:03:12 +0530

    custom gas bins for v5 sdk create2 factory deployment

--- packages/thirdweb/src/contract/deployment/utils/create-2-factory.ts
+++ packages/thirdweb/src/contract/deployment/utils/create-2-factory.ts
@@ -13,6 +13,7 @@ import type {
   ClientAndChain,
   ClientAndChainAndAccount,
 } from "../../../utils/types.js";
+import { getGasPrice } from "../../../gas/get-gas-price.js";
 
 const COMMON_FACTORY_ADDRESS = "0x4e59b44847b379578588920cA78FbF26c0B4956C"; // for pre-eip-155 supporting chains
 
@@ -40,12 +41,12 @@ export async function computeCreate2FactoryAddress(
   options: ClientAndChain,
 ): Promise<string> {
   // TODO add LRU cache
-  const commonFactory = getContract({
+  let factory = getContract({
     ...options,
     address: COMMON_FACTORY_ADDRESS,
   });
   // if the common factory is deployed, return the address
-  if (await isContractDeployed(commonFactory)) {
+  if (await isContractDeployed(factory)) {
     return COMMON_FACTORY_ADDRESS;
   }
 
@@ -54,12 +55,27 @@ export async function computeCreate2FactoryAddress(
   const custom = CUSTOM_GAS_FOR_CHAIN[chainId.toString()];
   const eipChain = enforceEip155 ? chainId : 0;
 
-  const deploymentInfo = custom
+  let deploymentInfo = custom
     ? await _getCreate2FactoryDeploymentInfo(eipChain, {
         gasPrice: custom.gasPrice,
         gasLimit: custom.gasLimit,
       })
     : await _getCreate2FactoryDeploymentInfo(eipChain, {});
+
+  factory = getContract({
+    ...options,
+    address: deploymentInfo.predictedAddress,
+  });
+
+  let bin;
+  if (!(await isContractDeployed(factory))) {
+    const gasPriceFetched = await getGasPrice(options);
+    bin = CUSTOM_GAS_BINS.find((e) => e >= gasPriceFetched) || gasPriceFetched;
+
+    deploymentInfo = await _getCreate2FactoryDeploymentInfo(eipChain, {
+      gasPrice: bin,
+    });
+  }
 
   return deploymentInfo.predictedAddress;
 }
@@ -88,18 +104,19 @@ export async function deployCreate2Factory(options: ClientAndChainAndAccount) {
   const { client, chain, account } = options;
   const enforceEip155 = await isEIP155Enforced(options);
   const chainId = options.chain.id;
-  const custom = CUSTOM_GAS_FOR_CHAIN[chainId.toString()];
   const eipChain = enforceEip155 ? chainId : 0;
 
-  const deploymentInfo = custom
-    ? await _getCreate2FactoryDeploymentInfo(eipChain, {
-        gasPrice: custom.gasPrice,
-        gasLimit: custom.gasLimit,
-      })
-    : await _getCreate2FactoryDeploymentInfo(eipChain, {});
   const rpcRequest = getRpcClient({
     client: client,
     chain,
+  });
+
+  const gasPriceFetched = await getGasPrice(options);
+  const bin =
+    CUSTOM_GAS_BINS.find((e) => e >= gasPriceFetched) || gasPriceFetched;
+
+  const deploymentInfo = await _getCreate2FactoryDeploymentInfo(eipChain, {
+    gasPrice: bin,
   });
 
   const balance = await eth_getBalance(rpcRequest, {
@@ -214,3 +231,44 @@ const CUSTOM_GAS_FOR_CHAIN: Record<string, CustomChain> = {
     gasLimit: 200000n,
   },
 };
+
+const CUSTOM_GAS_BINS = [
+  1n,
+  10n,
+  10n ** 2n,
+  10n ** 3n,
+  10n ** 4n,
+  10n ** 5n,
+  10n ** 6n,
+  10n ** 7n,
+  10n ** 8n,
+  10n ** 9n,
+  10n * 10n ** 9n,
+  100n * 10n ** 9n,
+  500n * 10n ** 9n,
+  1000n * 10n ** 9n,
+  2000n * 10n ** 9n,
+  3000n * 10n ** 9n,
+  4000n * 10n ** 9n,
+  5000n * 10n ** 9n,
+  6000n * 10n ** 9n,
+  7000n * 10n ** 9n,
+  10_000n * 10n ** 9n,
+  20_000n * 10n ** 9n,
+  30_000n * 10n ** 9n,
+  40_000n * 10n ** 9n,
+  50_000n * 10n ** 9n,
+  60_000n * 10n ** 9n,
+  70_000n * 10n ** 9n,
+  80_000n * 10n ** 9n,
+  90_000n * 10n ** 9n,
+  100_000n * 10n ** 9n,
+  150_000n * 10n ** 9n,
+  200_000n * 10n ** 9n,
+  250_000n * 10n ** 9n,
+  300_000n * 10n ** 9n,
+  350_000n * 10n ** 9n,
+  400_000n * 10n ** 9n,
+  500_000n * 10n ** 9n,
+  1000_000n * 10n ** 9n,
+];
