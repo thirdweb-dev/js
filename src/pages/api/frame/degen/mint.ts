efb7048ae026553a23a7b5b68b2fadc5f41ff500
easyrun42 <136458308+easyrun42@users.noreply.github.com>
2024-04-05 18:22:31 +0100

    feat: nft drop (transaction frame on degen chain) (#2444)
    * chore: degen drop
    
    * chore: change NFT contract address

---
+++ src/pages/api/frame/degen/mint.ts
@@ -0,0 +1,44 @@
+import { CoinbaseKit } from "classes/CoinbaseKit";
+import { getFrameHtmlResponse } from "@coinbase/onchainkit";
+import { errorResponse } from "utils/api";
+import { getFarcasterAccountAddress } from "utils/tx-frame";
+import { ThirdwebDegenEngine } from "classes/ThirdwebDegenEngine";
+import { getAbsoluteUrl } from "lib/vercel-utils";
+import { shortenAddress } from "@thirdweb-dev/react";
+import { NextApiRequest, NextApiResponse } from "next";
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse,
+) {
+  if (req.method !== "POST") {
+    return errorResponse("Invalid method", 400);
+  }
+
+  const { isValid, message } = await CoinbaseKit.validateMessage(req.body);
+
+  if (!isValid || !message) {
+    return res.status(400).send("Invalid message");
+  }
+
+  const faracsterAddress = getFarcasterAccountAddress(message.interactor);
+
+  const isNftOwned = await ThirdwebDegenEngine.isNFTOwned(faracsterAddress);
+
+  if (isNftOwned) {
+    throw new Error("Already owns an NFT");
+  }
+
+  await ThirdwebDegenEngine.mint(faracsterAddress);
+
+  const htmlResponse = getFrameHtmlResponse({
+    buttons: [
+      {
+        label: `Successfully minted to address ${shortenAddress(faracsterAddress)}`,
+      },
+    ],
+    image: `${getAbsoluteUrl()}/assets/og-image/degen-enchine-frame.png`,
+  });
+
+  return res.status(200).send(htmlResponse);
+}
---
+++ src/pages/frame/degen/mint.tsx
@@ -0,0 +1,51 @@
+import { useEffect } from "react";
+import Head from "next/head";
+import { getAbsoluteUrl } from "lib/vercel-utils";
+import { NextSeo } from "next-seo";
+import { useRouter } from "next/router";
+
+const title = "Certified Degen NFT";
+const description = "Mint Certified Degen NFT using thirdweb engine";
+
+const ogImageUrl = `${getAbsoluteUrl()}/assets/og-image/degen-enchine-frame.png`;
+
+const BaseFramePage = () => {
+  const router = useRouter();
+
+  useEffect(() => {
+    router.push("https://frames.thirdweb.com/mint");
+  }, [router]);
+
+  return (
+    <>
+      <NextSeo
+        title={title}
+        description={description}
+        openGraph={{
+          title,
+          description,
+          images: [
+            {
+              url: ogImageUrl,
+              width: 1200,
+              height: 630,
+              alt: title,
+            },
+          ],
+        }}
+      />
+      <Head>
+        <meta property="fc:frame" content="vNext" />
+        <meta name="fc:frame:image" content={ogImageUrl} />
+        <meta property="fc:frame:button:1" content="Claim NFT on Degen Chain" />
+        <meta property="fc:frame:button:1:action" content="post" />
+        <meta
+          property="fc:frame:button:1:target"
+          content={`${getAbsoluteUrl()}/api/frame/degen/mint`}
+        />
+      </Head>
+    </>
+  );
+};
+
+export default BaseFramePage;
