9611b4cbbbe8f447300e44359cc3f3057f153b94
Yash <kumaryashcse@gmail.com>
2024-04-05 19:19:57 +0530

    fix view method

--- legacy_packages/sdk/src/evm/common/any-evm-utils/getCreate2FactoryAddress.ts
+++ legacy_packages/sdk/src/evm/common/any-evm-utils/getCreate2FactoryAddress.ts
@@ -1,5 +1,5 @@
 import { providers } from "ethers";
-import { CUSTOM_GAS_FOR_CHAIN } from "../any-evm-constants";
+import { CUSTOM_GAS_BINS, CUSTOM_GAS_FOR_CHAIN } from "../any-evm-constants";
 import { COMMON_FACTORY } from "./constants";
 import { isContractDeployed } from "./isContractDeployed";
 import { isEIP155Enforced } from "./isEIP155Enforced";
@@ -16,23 +16,32 @@ import { getCreate2FactoryDeploymentInfo } from "./getCreate2FactoryDeploymentIn
 export async function getCreate2FactoryAddress(
   provider: providers.Provider,
 ): Promise<string> {
-  const commonFactoryExists = await isContractDeployed(
-    COMMON_FACTORY,
-    provider,
-  );
-  if (commonFactoryExists) {
+  let factoryExists = await isContractDeployed(COMMON_FACTORY, provider);
+  if (factoryExists) {
     return COMMON_FACTORY;
   }
 
   const enforceEip155 = await isEIP155Enforced(provider);
   const networkId = (await provider.getNetwork()).chainId;
   const chainId = enforceEip155 ? networkId : 0;
-  const deploymentInfo = CUSTOM_GAS_FOR_CHAIN[networkId]
+  let deploymentInfo = CUSTOM_GAS_FOR_CHAIN[networkId]
     ? getCreate2FactoryDeploymentInfo(chainId, {
         gasPrice: CUSTOM_GAS_FOR_CHAIN[networkId].gasPrice,
         gasLimit: CUSTOM_GAS_FOR_CHAIN[networkId].gasLimit,
       })
     : getCreate2FactoryDeploymentInfo(chainId, {});
+
+  factoryExists = await isContractDeployed(deploymentInfo.deployment, provider);
+
+  let bin;
+  if (!factoryExists) {
+    const gasPriceFetched = (await provider.getGasPrice()).toBigInt();
+    bin = CUSTOM_GAS_BINS.find((e) => e >= gasPriceFetched) || gasPriceFetched;
+
+    deploymentInfo = getCreate2FactoryDeploymentInfo(chainId, {
+      gasPrice: bin,
+    });
+  }
 
   return deploymentInfo.deployment;
 }
