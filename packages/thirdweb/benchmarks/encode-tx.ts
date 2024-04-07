767bc89aa34a641b920112f979e177598615e9c2
Jonas Daniels <jonas.daniels@outlook.com>
2024-04-06 19:37:14 -0700

    refactor: Refactor benchmark code for readability

--- packages/thirdweb/benchmarks/encode-tx.ts
+++ packages/thirdweb/benchmarks/encode-tx.ts
@@ -13,125 +13,125 @@ const USDC_CONTRACT_ADDRESS = "0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48";
 const VITALIK_WALLET = "0xAb5801a7D398351b8bE11C439e05C5B3259aeC9B";
 
 const client = createThirdwebClient({
-	clientId: "BENCH",
+  clientId: "BENCH",
 });
 
 const USDC_CONTRACT = getContract({
-	chain: defineChain({
-		id: 1,
-		rpc: LOCAL_RPC,
-	}),
-	client,
-	address: USDC_CONTRACT_ADDRESS,
+  chain: defineChain({
+    id: 1,
+    rpc: LOCAL_RPC,
+  }),
+  client,
+  address: USDC_CONTRACT_ADDRESS,
 });
 
 const ABI = [
-	{
-		inputs: [
-			{
-				internalType: "address",
-				name: "to",
-				type: "address",
-			},
-			{
-				internalType: "uint256",
-				name: "value",
-				type: "uint256",
-			},
-		],
-		name: "transfer",
-		outputs: [
-			{
-				internalType: "uint256",
-				name: "",
-				type: "uint256",
-			},
-		],
-		stateMutability: "view",
-		type: "function",
-	},
+  {
+    inputs: [
+      {
+        internalType: "address",
+        name: "to",
+        type: "address",
+      },
+      {
+        internalType: "uint256",
+        name: "value",
+        type: "uint256",
+      },
+    ],
+    name: "transfer",
+    outputs: [
+      {
+        internalType: "uint256",
+        name: "",
+        type: "uint256",
+      },
+    ],
+    stateMutability: "view",
+    type: "function",
+  },
 ] as const;
 
 const ethers6Contract = new ethers.Contract(
-	USDC_CONTRACT_ADDRESS,
-	ABI,
-	ethers.getDefaultProvider(LOCAL_RPC),
+  USDC_CONTRACT_ADDRESS,
+  ABI,
+  ethers.getDefaultProvider(LOCAL_RPC),
 );
 
 const transfer_viem = viem.prepareEncodeFunctionData({
-	abi: ABI,
-	functionName: "transfer",
+  abi: ABI,
+  functionName: "transfer",
 });
 
 function randomBigint() {
-	return BigInt(Math.floor(Math.random() * 1000));
+  return BigInt(Math.floor(Math.random() * 1000));
 }
 
 group("encode transfer (warm cache)", () => {
-	bench("thirdweb", async () => {
-		const tx = transfer({
-			contract: USDC_CONTRACT,
-			to: VITALIK_WALLET,
-			value: randomBigint(),
-		});
+  bench("thirdweb", async () => {
+    const tx = transfer({
+      contract: USDC_CONTRACT,
+      to: VITALIK_WALLET,
+      value: randomBigint(),
+    });
 
-		await encode(tx);
-	});
+    await encode(tx);
+  });
 
-	bench("ethers", async () => {
-		ethers6Contract.interface.encodeFunctionData("transfer", [
-			VITALIK_WALLET,
-			randomBigint(),
-		]);
-	});
+  bench("ethers", async () => {
+    ethers6Contract.interface.encodeFunctionData("transfer", [
+      VITALIK_WALLET,
+      randomBigint(),
+    ]);
+  });
 
-	bench("viem", async () => {
-		viem.encodeFunctionData({
-			...transfer_viem,
-			args: [VITALIK_WALLET, randomBigint()],
-		});
-	});
+  bench("viem", async () => {
+    viem.encodeFunctionData({
+      ...transfer_viem,
+      args: [VITALIK_WALLET, randomBigint()],
+    });
+  });
 });
 
 group("encode transfer (cold cache)", () => {
-	bench("thirdweb", async () => {
-		// define chain
-		const chain = defineChain({
-			id: 1,
-			rpc: LOCAL_RPC,
-		});
-		// get contract
-		const contract = getContract({
-			chain,
-			client,
-			address: USDC_CONTRACT_ADDRESS,
-		});
+  bench("thirdweb", async () => {
+    // define chain
+    const chain = defineChain({
+      id: 1,
+      rpc: LOCAL_RPC,
+    });
+    // get contract
+    const contract = getContract({
+      chain,
+      client,
+      address: USDC_CONTRACT_ADDRESS,
+    });
 
-		const tx = transfer({
-			contract: contract,
-			to: VITALIK_WALLET,
-			value: randomBigint(),
-		});
-		await encode(tx);
-	});
+    const tx = transfer({
+      contract: contract,
+      to: VITALIK_WALLET,
+      value: randomBigint(),
+    });
+    await encode(tx);
+  });
 
-	bench("ethers", async () => {
-		new ethers.Contract(
-			USDC_CONTRACT_ADDRESS,
-			ABI,
-			ethers.getDefaultProvider(LOCAL_RPC),
-		).interface.encodeFunctionData("transfer", [
-			VITALIK_WALLET,
-			randomBigint(),
-		]);
-	});
+  bench("ethers", async () => {
+    new ethers.Contract(
+      USDC_CONTRACT_ADDRESS,
+      ABI,
+      ethers.getDefaultProvider(LOCAL_RPC),
+    ).interface.encodeFunctionData("transfer", [
+      VITALIK_WALLET,
+      randomBigint(),
+    ]);
+  });
 
-	bench("viem", async () => {
-		viem.encodeFunctionData({
-			...transfer_viem,
-			args: [VITALIK_WALLET, randomBigint()],
-		});
-	});
+  bench("viem", async () => {
+    viem.encodeFunctionData({
+      ...transfer_viem,
+      args: [VITALIK_WALLET, randomBigint()],
+    });
+  });
 });
 
 await run();
--- packages/thirdweb/benchmarks/send-transaction.ts
+++ packages/thirdweb/benchmarks/send-transaction.ts
@@ -15,68 +15,68 @@ const LOCAL_RPC = "http://127.0.0.1:8545";
 const VITALIK_WALLET = "0xAb5801a7D398351b8bE11C439e05C5B3259aeC9B";
 
 const client = createThirdwebClient({
-	clientId: "BENCH",
-	config: { rpc: { maxBatchSize: 1 } },
+  clientId: "BENCH",
+  config: { rpc: { maxBatchSize: 1 } },
 });
 
 const TEST_CHAIN = defineChain({
-	id: 1,
-	rpc: LOCAL_RPC,
+  id: 1,
+  rpc: LOCAL_RPC,
 });
 
 const PKEY =
-	"0xac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80";
+  "0xac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80";
 
 const account = privateKeyAccount({
-	privateKey: PKEY,
-	client,
+  privateKey: PKEY,
+  client,
 });
 
 // viem setup
 const privateKeyAccountViem = privateKeyToAccount(PKEY);
 const walletClient = viem.createWalletClient({
-	account: privateKeyAccountViem,
-	transport: viem.http(LOCAL_RPC),
+  account: privateKeyAccountViem,
+  transport: viem.http(LOCAL_RPC),
 });
 
 // ethers setup
 const provider = new ethers.JsonRpcProvider(LOCAL_RPC, 1, {
-	staticNetwork: true,
-	batchMaxCount: 1,
-	cacheTimeout: 0,
+  staticNetwork: true,
+  batchMaxCount: 1,
+  cacheTimeout: 0,
 });
 const wallet = new ethers.Wallet(PKEY, provider);
 
 function randomBigint() {
-	return BigInt(Math.floor(Math.random() * 1000));
+  return BigInt(Math.floor(Math.random() * 1000));
 }
 
 group("transfer native tokens", () => {
-	bench("thirdweb", async () => {
-		const transaction = prepareTransaction({
-			client,
-			chain: TEST_CHAIN,
-			value: randomBigint(),
-			to: VITALIK_WALLET,
-		});
+  bench("thirdweb", async () => {
+    const transaction = prepareTransaction({
+      client,
+      chain: TEST_CHAIN,
+      value: randomBigint(),
+      to: VITALIK_WALLET,
+    });
 
-		await sendTransaction({ transaction, account });
-	});
+    await sendTransaction({ transaction, account });
+  });
 
-	bench("viem", async () => {
-		await walletClient.sendTransaction({
-			chain: mainnet,
-			value: randomBigint(),
-			to: VITALIK_WALLET,
-		});
-	});
+  bench("viem", async () => {
+    await walletClient.sendTransaction({
+      chain: mainnet,
+      value: randomBigint(),
+      to: VITALIK_WALLET,
+    });
+  });
 
-	bench("ethers", async () => {
-		await wallet.sendTransaction({
-			to: VITALIK_WALLET,
-			value: randomBigint(),
-		});
-	});
+  bench("ethers", async () => {
+    await wallet.sendTransaction({
+      to: VITALIK_WALLET,
+      value: randomBigint(),
+    });
+  });
 });
 
 await run();
