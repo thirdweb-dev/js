82d0b07fcbe43c404f55313631366367c3ae8f22
Jonas Daniels <jonas.daniels@outlook.com>
2024-04-11 01:06:39 -0700

    extensions(permissions): add support for initial permissions

---
+++ .changeset/stupid-parrots-pretend.md
@@ -0,0 +1,5 @@
+---
+"thirdweb": minor
+---
+
+add extensions for contract roles in the `thirdweb/extensions/permissions` namespace
---
+++ packages/thirdweb/src/extensions/permissions/__generated__/IPermissionsEnumerable/events/RoleGranted.ts
@@ -0,0 +1,53 @@
+import { prepareEvent } from "../../../../../event/prepare-event.js";
+import type { AbiParameterToPrimitiveType } from "abitype";
+
+/**
+ * Represents the filters for the "RoleGranted" event.
+ */
+export type RoleGrantedEventFilters = Partial<{
+  role: AbiParameterToPrimitiveType<{
+    type: "bytes32";
+    name: "role";
+    indexed: true;
+  }>;
+  account: AbiParameterToPrimitiveType<{
+    type: "address";
+    name: "account";
+    indexed: true;
+  }>;
+  sender: AbiParameterToPrimitiveType<{
+    type: "address";
+    name: "sender";
+    indexed: true;
+  }>;
+}>;
+
+/**
+ * Creates an event object for the RoleGranted event.
+ * @param filters - Optional filters to apply to the event.
+ * @returns The prepared event object.
+ * @extension PERMISSIONS
+ * @example
+ * ```ts
+ * import { getContractEvents } from "thirdweb";
+ * import { roleGrantedEvent } from "thirdweb/extensions/permissions";
+ *
+ * const events = await getContractEvents({
+ * contract,
+ * events: [
+ *  roleGrantedEvent({
+ *  role: ...,
+ *  account: ...,
+ *  sender: ...,
+ * })
+ * ],
+ * });
+ * ```
+ */
+export function roleGrantedEvent(filters: RoleGrantedEventFilters = {}) {
+  return prepareEvent({
+    signature:
+      "event RoleGranted(bytes32 indexed role, address indexed account, address indexed sender)",
+    filters,
+  });
+}
