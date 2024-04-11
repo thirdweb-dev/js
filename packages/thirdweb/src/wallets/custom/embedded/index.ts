007770cd635fba5d38eca5efccc55592c85a106f
Manan Tank <manantankm@gmail.com>
2024-04-11 22:17:39 +0530

    feat: add alias for inApp wallet, show ENS in Connect (#2748)

---
+++ packages/thirdweb/src/wallets/custom/embedded/index.ts
@@ -0,0 +1,27 @@
+export const wallet = {
+  id: "embedded",
+  name: "Social Login",
+  homepage: "https://thirdweb.com",
+  app: {
+    browser: null,
+    ios: null,
+    android: null,
+    mac: null,
+    windows: null,
+    linux: null,
+    chrome: null,
+    firefox: null,
+    safari: null,
+    edge: null,
+    opera: null,
+  },
+  rdns: null,
+  mobile: {
+    native: null,
+    universal: null,
+  },
+  desktop: {
+    native: null,
+    universal: null,
+  },
+} as const;
