7506996d981143433b7a480f08e7539d8715513a
Jonas Daniels <jonas.daniels@outlook.com>
2024-04-12 16:57:54 -0700

    chore: Update vitest.config.ts

--- packages/thirdweb/test/vitest.config.ts
+++ packages/thirdweb/test/vitest.config.ts
@@ -1,7 +1,7 @@
 import { join } from "node:path";
-import { defineConfig } from "vitest/config";
 // @ts-expect-error - no types
 import codspeedPlugin from "@codspeed/vitest-plugin";
+import { defineConfig } from "vitest/config";
 
 const plugins = process.env.CI ? [codspeedPlugin()] : [];
 
@@ -36,6 +36,7 @@ export default defineConfig({
     globalSetup: [join(__dirname, "./globalSetup.ts")],
     testTimeout: 30_000,
     retry: process.env.CI ? 0 : 3,
+    maxWorkers: process.env.CI ? 2 : undefined,
     // clear any mocks between any tests
     clearMocks: true,
     pool: "forks",
