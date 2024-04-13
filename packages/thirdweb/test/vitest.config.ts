4b6911f3cf0523a71fa7fb5fa6b94a5418f37ddc
Jonas Daniels <jonas.daniels@outlook.com>
2024-04-12 17:02:37 -0700

    refactor: Update vitest.config.ts minWorkers and maxWorkers

--- packages/thirdweb/test/vitest.config.ts
+++ packages/thirdweb/test/vitest.config.ts
@@ -36,7 +36,8 @@ export default defineConfig({
     globalSetup: [join(__dirname, "./globalSetup.ts")],
     testTimeout: 30_000,
     retry: process.env.CI ? 0 : 3,
-    maxWorkers: process.env.CI ? 2 : undefined,
+    minWorkers: process.env.CI ? 2 : undefined,
+    maxWorkers: process.env.CI ? 8 : undefined,
     // clear any mocks between any tests
     clearMocks: true,
     pool: "forks",
