29cc8f4ae2490cf1784de58f213a9c874892bfb1
Jonas Daniels <jonas.daniels@outlook.com>
2024-04-17 12:29:46 -0700

    test: fix bid amount validation errors

--- packages/thirdweb/src/extensions/marketplace/marketplace.test.ts
+++ packages/thirdweb/src/extensions/marketplace/marketplace.test.ts
@@ -376,71 +376,73 @@ describe.runIf(process.env.TW_SECRET_KEY)(
         }
       `);
 
-
-      // check for a winning bid
-      await expect(
-        getWinningBid({ contract: marketplaceContract, auctionId: listing.id }),
-      ).resolves.toBeUndefined();
-
-      // invalid bid amount 1: 0 bid (0 is not allowed)
-      await expect(
-        sendAndConfirmTransaction({
-          account: TEST_ACCOUNT_B,
-          transaction: bidInAuction({
-            auctionId: listing.id,
+        // check for a winning bid
+        await expect(
+          getWinningBid({
             contract: marketplaceContract,
-            bidAmount: "0",
-          }),
-        }),
-      ).rejects.toThrowErrorMatchingInlineSnapshot(
-        "[Error: Bid amount is zero]",
-      );
-      // invalid bid amount 2: 11 bid (over buyout)
-      await expect(
-        sendAndConfirmTransaction({
-          account: TEST_ACCOUNT_B,
-          transaction: bidInAuction({
             auctionId: listing.id,
-            contract: marketplaceContract,
-            bidAmount: "11",
           }),
-        }),
-      ).rejects.toThrowErrorMatchingInlineSnapshot(
-        "[Error: Bid amount is above the buyout amount]",
-      );
-      // invalid bid amount 3: below minimum bid (but not 0)
-      await expect(
-        sendAndConfirmTransaction({
-          account: TEST_ACCOUNT_B,
-          transaction: bidInAuction({
-            auctionId: listing.id,
-            contract: marketplaceContract,
-            bidAmount: "0.5",
-          }),
-        }),
-      ).rejects.toThrowErrorMatchingInlineSnapshot(
-        "[Error: Bid amount is below the minimum bid amount]",
-      );
+        ).resolves.toBeUndefined();
 
-      // valid bid amount: "2"
-      await expect(
-        sendAndConfirmTransaction({
-          account: TEST_ACCOUNT_B,
-          transaction: bidInAuction({
-            auctionId: listing.id,
-            contract: marketplaceContract,
-            bidAmount: "2",
+        // invalid bid amount 1: 0 bid (0 is not allowed)
+        await expect(
+          sendAndConfirmTransaction({
+            account: TEST_ACCOUNT_B,
+            transaction: bidInAuction({
+              auctionId: listing.id,
+              contract: marketplaceContract,
+              bidAmount: "0",
+            }),
           }),
-        }),
-      ).resolves.toBeDefined();
+        ).rejects.toThrowErrorMatchingInlineSnapshot(
+          "[Error: Bid amount is zero]",
+        );
+        // invalid bid amount 2: 11 bid (over buyout)
+        await expect(
+          sendAndConfirmTransaction({
+            account: TEST_ACCOUNT_B,
+            transaction: bidInAuction({
+              auctionId: listing.id,
+              contract: marketplaceContract,
+              bidAmount: "11",
+            }),
+          }),
+        ).rejects.toThrowErrorMatchingInlineSnapshot(
+          "[Error: Bid amount is above the buyout amount]",
+        );
+        // invalid bid amount 3: below minimum bid (but not 0)
+        await expect(
+          sendAndConfirmTransaction({
+            account: TEST_ACCOUNT_B,
+            transaction: bidInAuction({
+              auctionId: listing.id,
+              contract: marketplaceContract,
+              bidAmount: "0.5",
+            }),
+          }),
+        ).rejects.toThrowErrorMatchingInlineSnapshot(
+          "[Error: Bid amount is below the minimum bid amount]",
+        );
 
-      // check for a winning bid
-      const winningBid = await getWinningBid({
-        contract: marketplaceContract,
-        auctionId: listing.id,
-      });
-      expect(winningBid).toBeDefined();
-      expect(winningBid).toMatchInlineSnapshot(`
+        // valid bid amount: "2"
+        await expect(
+          sendAndConfirmTransaction({
+            account: TEST_ACCOUNT_B,
+            transaction: bidInAuction({
+              auctionId: listing.id,
+              contract: marketplaceContract,
+              bidAmount: "2",
+            }),
+          }),
+        ).resolves.toBeDefined();
+
+        // check for a winning bid
+        const winningBid = await getWinningBid({
+          contract: marketplaceContract,
+          auctionId: listing.id,
+        });
+        expect(winningBid).toBeDefined();
+        expect(winningBid).toMatchInlineSnapshot(`
         {
           "bidAmountWei": 2000000000000000000n,
           "bidderAddress": "0x70997970C51812dc3A010C7d01b50e0d17dc79C8",
@@ -455,54 +457,54 @@ describe.runIf(process.env.TW_SECRET_KEY)(
         }
       `);
 
-      // invalid bid amount, above minimum but below existing winning bid
-      await expect(
-        sendAndConfirmTransaction({
-          account: TEST_ACCOUNT_B,
-          transaction: bidInAuction({
-            auctionId: listing.id,
-            contract: marketplaceContract,
-            bidAmount: "1.5",
+        // invalid bid amount, above minimum but below existing winning bid
+        await expect(
+          sendAndConfirmTransaction({
+            account: TEST_ACCOUNT_B,
+            transaction: bidInAuction({
+              auctionId: listing.id,
+              contract: marketplaceContract,
+              bidAmount: "1.5",
+            }),
           }),
-        }),
-      ).rejects.toThrowErrorMatchingInlineSnapshot(
-        "[Error: Bid amount is too low to outbid the existing winning bid]",
-      );
+        ).rejects.toThrowErrorMatchingInlineSnapshot(
+          "[Error: Bid amount is too low to outbid the existing winning bid]",
+        );
 
-      // invalid bid amount, above winning bit but below bid + bidBuffer (default 500bps)
-      await expect(
-        sendAndConfirmTransaction({
-          account: TEST_ACCOUNT_B,
-          transaction: bidInAuction({
-            auctionId: listing.id,
-            contract: marketplaceContract,
-            // 2 * 1.05 = 2.1, so 2.05 is invalid
-            bidAmount: "2.05",
+        // invalid bid amount, above winning bit but below bid + bidBuffer (default 500bps)
+        await expect(
+          sendAndConfirmTransaction({
+            account: TEST_ACCOUNT_B,
+            transaction: bidInAuction({
+              auctionId: listing.id,
+              contract: marketplaceContract,
+              // 2 * 1.05 = 2.1, so 2.05 is invalid
+              bidAmount: "2.05",
+            }),
           }),
-        }),
-      ).rejects.toThrowErrorMatchingInlineSnapshot(
-        "[Error: Bid amount is too low to outbid the existing winning bid]",
-      );
+        ).rejects.toThrowErrorMatchingInlineSnapshot(
+          "[Error: Bid amount is too low to outbid the existing winning bid]",
+        );
 
-      // actually outbid the winning bid
-      await expect(
-        sendAndConfirmTransaction({
-          account: TEST_ACCOUNT_B,
-          transaction: bidInAuction({
-            auctionId: listing.id,
-            contract: marketplaceContract,
-            bidAmount: "3",
+        // actually outbid the winning bid
+        await expect(
+          sendAndConfirmTransaction({
+            account: TEST_ACCOUNT_B,
+            transaction: bidInAuction({
+              auctionId: listing.id,
+              contract: marketplaceContract,
+              bidAmount: "3",
+            }),
           }),
-        }),
-      ).resolves.toBeDefined();
+        ).resolves.toBeDefined();
 
-      // check for a new winning bid
-      const newWinningBid = await getWinningBid({
-        contract: marketplaceContract,
-        auctionId: listing.id,
-      });
-      expect(newWinningBid).toBeDefined();
-      expect(newWinningBid).toMatchInlineSnapshot(`
+        // check for a new winning bid
+        const newWinningBid = await getWinningBid({
+          contract: marketplaceContract,
+          auctionId: listing.id,
+        });
+        expect(newWinningBid).toBeDefined();
+        expect(newWinningBid).toMatchInlineSnapshot(`
         {
           "bidAmountWei": 3000000000000000000n,
           "bidderAddress": "0x70997970C51812dc3A010C7d01b50e0d17dc79C8",
@@ -516,6 +518,7 @@ describe.runIf(process.env.TW_SECRET_KEY)(
           },
         }
       `);
+      });
     });
   },
 );
