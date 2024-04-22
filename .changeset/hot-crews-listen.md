ed33a8d76b944351d4adbccf184b51196cc41c98
Kien Ngo <kientngo@icloud.com>
2024-04-22 18:03:21 +0700

    Add exports and changeset

---
+++ .changeset/hot-crews-listen.md
@@ -0,0 +1,5 @@
+---
+"thirdweb": patch
+---
+
+Add multicall3 extension
---
+++ packages/thirdweb/src/exports/extensions/multicall3.ts
@@ -0,0 +1,43 @@
+// READ
+export { getBasefee } from "../../extensions/multicall3/__generated__/IMulticall3/read/getBasefee.js";
+export {
+  getBlockHash,
+  type GetBlockHashParams,
+} from "../../extensions/multicall3/__generated__/IMulticall3/read/getBlockHash.js";
+export { getBlockNumber } from "../../extensions/multicall3/__generated__/IMulticall3/read/getBlockNumber.js";
+export { getChainId } from "../../extensions/multicall3/__generated__/IMulticall3/read/getChainId.js";
+export { getCurrentBlockCoinbase } from "../../extensions/multicall3/__generated__/IMulticall3/read/getCurrentBlockCoinbase.js";
+export { getCurrentBlockDifficulty } from "../../extensions/multicall3/__generated__/IMulticall3/read/getCurrentBlockDifficulty.js";
+export { getCurrentBlockGasLimit } from "../../extensions/multicall3/__generated__/IMulticall3/read/getCurrentBlockGasLimit.js";
+export { getCurrentBlockTimestamp } from "../../extensions/multicall3/__generated__/IMulticall3/read/getCurrentBlockTimestamp.js";
+export {
+  getEthBalance,
+  type GetEthBalanceParams,
+} from "../../extensions/multicall3/__generated__/IMulticall3/read/getEthBalance.js";
+export { getLastBlockHash } from "../../extensions/multicall3/__generated__/IMulticall3/read/getLastBlockHash.js";
+
+// WRITE
+export {
+  aggregate,
+  type AggregateParams,
+} from "../../extensions/multicall3/__generated__/IMulticall3/write/aggregate.js";
+export {
+  aggregate3,
+  type Aggregate3Params,
+} from "../../extensions/multicall3/__generated__/IMulticall3/write/aggregate3.js";
+export {
+  aggregate3Value,
+  type Aggregate3ValueParams,
+} from "../../extensions/multicall3/__generated__/IMulticall3/write/aggregate3Value.js";
+export {
+  blockAndAggregate,
+  type BlockAndAggregateParams,
+} from "../../extensions/multicall3/__generated__/IMulticall3/write/blockAndAggregate.js";
+export {
+  tryAggregate,
+  type TryAggregateParams,
+} from "../../extensions/multicall3/__generated__/IMulticall3/write/tryAggregate.js";
+export {
+  tryBlockAndAggregate,
+  type TryBlockAndAggregateParams,
+} from "../../extensions/multicall3/__generated__/IMulticall3/write/tryBlockAndAggregate.js";
