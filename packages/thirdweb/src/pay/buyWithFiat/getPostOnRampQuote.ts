ad8a72840959b31481b0d0bf80bbde00cca64584
Isaac Dubuque <idubuque@umich.edu>
2024-04-24 13:29:39 -0700

    update docs

--- packages/thirdweb/src/pay/buyWithFiat/getPostOnRampQuote.ts
+++ packages/thirdweb/src/pay/buyWithFiat/getPostOnRampQuote.ts
@@ -8,31 +8,72 @@ import {
   type BuyWithFiatStatus,
 } from "./getStatus.js";
 
+/**
+ * The parameters for [`getPostOnRampQuote`](https://portal.thirdweb.com/references/typescript/v5/getPostOnRampQuote) function
+ * Used post onramp to complete get the next quote to execute to fulfill the BuyWithFiat intent
+ */
 export type GetPostOnRampQuoteParams = {
+  /**
+   * A client is the entry point to the thirdweb SDK. It is required for all other actions.
+   *
+   * You can create a client using the `createThirdwebClient` function.
+   * Refer to the [Creating a Client](https://portal.thirdweb.com/typescript/v5/client) documentation for more information.
+   *
+   */
   client: ThirdwebClient;
-  onrampStatus: BuyWithFiatStatus;
+  /**
+   * The BuyWithFiatStatus returned by getBuyWithFiatStatus that includes details on next steps
+   *
+   * You can get a BuyWithFiatStatus using the `getBuyWithFiatStatus` function.
+   * Refer to the [`getBuyWithFiatStatus`](https://portal.thirdweb.com/typescript/v5/getBuyWithFiatStatus) documentation for more information.
+   *
+   */
+  buyWithFiatStatus: BuyWithFiatStatus;
 };
 
+/**
+ * Get a quote of type [`BuyWithCryptoQuote`](https://portal.thirdweb.com/references/typescript/v5/BuyWithCryptoQuote) to fulfill the BuyWithFiat intent
+ * This quote contains the information about the swap such as token amounts, processing fees, estimated time etc.
+ *
+ * Once you have the quote, you can use `prepareTransaction` and prepare the transaction for submission.
+ * @param params - object of type [`GetPostOnRampQuoteParams`](https://portal.thirdweb.com/references/typescript/v5/GetPostOnRampQuoteParams)
+ * @returns Object of type [`BuyWithCryptoQuote`](https://portal.thirdweb.com/references/typescript/v5/BuyWithCryptoQuote) which contains the information about the quote such as processing fees, estimated time, converted token amounts, etc.
+ * @example
+ *
+ * ```ts
+ * import { getPostOnRampQuote, getBuyWithFiatStatus } from "thirdweb/pay";
+ *
+ * const intentId = "abc-123";
+ *
+ * const buyWithFiatStatus = await getBuyWithFiatStatus({ client, intentId });
+ * const buyWithCryptoQuote = await getPostOnRampQuote({
+ *  client,
+ *  buyWithFiatStatus
+ * });
+ * ```
+ * @buyCrypto
+ */
 export async function getPostOnRampQuote({
   client,
-  onrampStatus,
+  buyWithFiatStatus,
 }: GetPostOnRampQuoteParams): Promise<BuyWithCryptoQuote | null> {
-  if (!("intentId" in onrampStatus)) {
+  if (!("intentId" in buyWithFiatStatus)) {
     // transaction not found
     return null;
   }
 
   if (
-    onrampStatus.status === BuyWithFiatTransactionStatus.CRYPTO_SWAP_REQUIRED
+    buyWithFiatStatus.status ===
+    BuyWithFiatTransactionStatus.CRYPTO_SWAP_REQUIRED
   ) {
     return getBuyWithCryptoQuote({
       client,
-      fromAddress: onrampStatus.toAddress,
-      fromChainId: onrampStatus.quote.onRampToken.chainId,
-      fromTokenAddress: onrampStatus.quote.onRampToken.tokenAddress,
-      toChainId: onrampStatus.quote.toToken.chainId,
-      toTokenAddress: onrampStatus.quote.toToken.tokenAddress,
-      toAmount: onrampStatus.quote.estimatedToTokenAmount,
+      fromAddress: buyWithFiatStatus.toAddress,
+      fromChainId: buyWithFiatStatus.quote.onRampToken.chainId,
+      fromTokenAddress: buyWithFiatStatus.quote.onRampToken.tokenAddress,
+      toChainId: buyWithFiatStatus.quote.toToken.chainId,
+      toTokenAddress: buyWithFiatStatus.quote.toToken.tokenAddress,
+      toAmount: buyWithFiatStatus.quote.estimatedToTokenAmount,
     });
   }
 
