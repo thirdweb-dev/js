81c297faeb698503cf5e2716d604c9f0c18ad668
Firekeeper <0xFirekeeper@gmail.com>
2024-04-25 10:44:08 +0300

    Unity 4.12.3 (#2854)

--- legacy_packages/unity-js-bridge/src/thirdweb-bridge.ts
+++ legacy_packages/unity-js-bridge/src/thirdweb-bridge.ts
@@ -171,7 +171,7 @@ class ThirdwebBridge implements TWBridge {
       // biome-ignore lint/suspicious/noExplicitAny: TODO: fix use of any
       (globalThis as any).X_SDK_PLATFORM = "unity";
       // biome-ignore lint/suspicious/noExplicitAny: TODO: fix use of any
-      (globalThis as any).X_SDK_VERSION = "4.12.2";
+      (globalThis as any).X_SDK_VERSION = "4.12.3";
       // biome-ignore lint/suspicious/noExplicitAny: TODO: fix use of any
       (globalThis as any).X_SDK_OS = browser?.os ?? "unknown";
     }
