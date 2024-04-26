c488d266decd0742ad3e3d84821a2e0f47c9168b
Manan Tank <manantankm@gmail.com>
2024-04-26 23:15:29 +0530

    fix popup

--- packages/thirdweb/src/react/web/ui/ConnectWallet/screens/Buy/openOnRamppopup.tsx
+++ packages/thirdweb/src/react/web/ui/ConnectWallet/screens/Buy/openOnRamppopup.tsx
@@ -1,11 +1,11 @@
 export function openOnrampPopup(link: string, theme: string) {
-  const height = 700;
-  const width = 600;
+  const height = 750;
+  const width = 500;
   const top = (window.innerHeight - height) / 2;
   const left = (window.innerWidth - width) / 2;
 
   return window.open(
-    `${link}@theme=${theme}`,
+    `${link}&theme=${theme}`,
     "Buy",
     `width=${width}, height=${height}, top=${top}, left=${left}`,
   );
