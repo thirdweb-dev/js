fc71112ddacd4390731ebb915a537699e8327115
Manan Tank <manantankm@gmail.com>
2024-04-30 01:06:09 +0530

    Update Fiat steps UI

--- packages/thirdweb/src/react/web/ui/ConnectWallet/screens/Buy/fiat/FiatSteps.tsx
+++ packages/thirdweb/src/react/web/ui/ConnectWallet/screens/Buy/fiat/FiatSteps.tsx
@@ -1,10 +1,11 @@
-import { ChevronRightIcon } from "@radix-ui/react-icons";
+import { TriangleDownIcon } from "@radix-ui/react-icons";
 import { useMemo } from "react";
 import { defineChain } from "../../../../../../../chains/utils.js";
 import type { ThirdwebClient } from "../../../../../../../client/client.js";
 import { NATIVE_TOKEN_ADDRESS } from "../../../../../../../constants/addresses.js";
 import type { BuyWithFiatQuote } from "../../../../../../../exports/pay.js";
 import { formatNumber } from "../../../../../../../utils/formatNumber.js";
+import { useChainQuery } from "../../../../../../core/hooks/others/useChainQuery.js";
 import { Spacer } from "../../../../components/Spacer.js";
 import { TokenIcon } from "../../../../components/TokenIcon.js";
 import { Container, Line, ModalHeader } from "../../../../components/basic.js";
@@ -67,49 +68,49 @@ export function FiatSteps(props: {
     return tokenInfo;
   }, [onRampTokenMeta]);
 
-  const fiat = (
-    <Container flex="row" gap="sm" center="y">
-      <currency.icon size={iconSize.sm} />
-      <Text color="primaryText" size="sm">
-        {props.quote.fromCurrency.amount}{" "}
-        {props.quote.fromCurrency.currencySymbol}
-      </Text>
-    </Container>
-  );
+  const onRampChainMetaQuery = useChainQuery(onRampChain);
+  const toChainMetaQuery = useChainQuery(toChain);
 
   const onRampTokenInfo = (
-    <Container flex="row" gap="sm" center="y">
-      <TokenIcon
-        token={onRampToken}
-        chain={onRampChain}
-        size="sm"
-        client={props.client}
-      />
-      <div>
-        <Text color="primaryText" inline size="sm">
-          {formatNumber(Number(onRampTokenMeta.amount), 4)}
-        </Text>{" "}
+    <div>
+      <Text color="primaryText" size="sm">
+        {formatNumber(Number(onRampTokenMeta.amount), 4)}{" "}
         <TokenSymbol token={onRampToken} chain={onRampChain} size="sm" inline />
-      </div>
-    </Container>
+      </Text>
+    </div>
+  );
+
+  const fiatIcon = <currency.icon size={iconSize.sm} />;
+
+  const onRampTokenIcon = (
+    <TokenIcon
+      token={onRampToken}
+      chain={onRampChain}
+      size="sm"
+      client={props.client}
+    />
+  );
+  const toTokenIcon = (
+    <TokenIcon
+      token={toToken}
+      chain={toChain}
+      size="sm"
+      client={props.client}
+    />
+  );
+
+  const onRampChainInfo = (
+    <Text size="xs">{onRampChainMetaQuery.data?.name}</Text>
   );
 
   const toTokenInfo = (
-    <Container flex="row" gap="sm" center="y">
-      <TokenIcon
-        token={toToken}
-        chain={toChain}
-        size="sm"
-        client={props.client}
-      />
-      <div>
-        <Text color="primaryText" inline>
-          {props.quote.estimatedToAmountMin}{" "}
-        </Text>
-        <TokenSymbol token={toToken} chain={toChain} size="sm" inline />
-      </div>
-    </Container>
+    <Text color="primaryText" size="sm">
+      {props.quote.estimatedToAmountMin}{" "}
+      <TokenSymbol token={toToken} chain={toChain} size="sm" inline />
+    </Text>
   );
+
+  const toTokehChainInfo = <Text size="xs">{toChainMetaQuery.data?.name}</Text>;
 
   return (
     <Container p="lg">
@@ -118,10 +119,8 @@ export function FiatSteps(props: {
       <Spacer y="xl" />
 
       {/* Step 1 */}
-      <StepContainer isActive={step === 1} isDone={step === 2}>
-        <Container>
-          <Text size="sm">Step 1</Text>
-          <Spacer y="xs" />
+      <PaymentStep
+        title={
           <Text color="primaryText" size="md">
             Get{" "}
             <TokenSymbol
@@ -132,27 +131,30 @@ export function FiatSteps(props: {
             />{" "}
             with {props.quote.fromCurrency.currencySymbol}
           </Text>
+        }
+        step={1}
+        isActive={step === 1}
+        isDone={step === 2}
+        from={{
+          icon: fiatIcon,
+          primaryText: (
+            <Text color="primaryText" size="sm">
+              {props.quote.fromCurrency.amount}{" "}
+              {props.quote.fromCurrency.currencySymbol}
+            </Text>
+          ),
+        }}
+        to={{
+          icon: onRampTokenIcon,
+          primaryText: onRampTokenInfo,
+          secondaryText: onRampChainInfo,
+        }}
+      />
 
-          <Spacer y="sm" />
-          <Line />
-          <Spacer y="sm" />
-          <Container flex="column" gap="xs">
-            <Container flex="row" gap="xxs" center="y">
-              {fiat}
-              <ChevronRightIcon width={iconSize.sm} />
-            </Container>
-            {onRampTokenInfo}
-          </Container>
-        </Container>
-      </StepContainer>
+      <Spacer y="sm" />
 
-      <Spacer y="md" />
-
-      {/* Step 2 */}
-      <StepContainer isActive={step === 2} isDone={false}>
-        <Container>
-          <Text size="sm">Step 2</Text>
-          <Spacer y="xs" />
+      <PaymentStep
+        title={
           <Text color="primaryText" size="md">
             Convert{" "}
             <TokenSymbol
@@ -163,26 +165,116 @@ export function FiatSteps(props: {
             />{" "}
             to <TokenSymbol token={toToken} chain={toChain} size="md" inline />
           </Text>
+        }
+        step={2}
+        isActive={step === 2}
+        isDone={false}
+        from={{
+          icon: onRampTokenIcon,
+          primaryText: onRampTokenInfo,
+          secondaryText: onRampChainInfo,
+        }}
+        to={{
+          icon: toTokenIcon,
+          primaryText: toTokenInfo,
+          secondaryText: toTokehChainInfo,
+        }}
+      />
 
-          <Spacer y="sm" />
-          <Line />
-          <Spacer y="sm" />
-
-          <Container flex="column" gap="xs">
-            <Container flex="row" gap="xxs" center="y">
-              {onRampTokenInfo}
-              <ChevronRightIcon width={iconSize.sm} />
-            </Container>
-            {toTokenInfo}
-          </Container>
-        </Container>
-      </StepContainer>
-
-      <Spacer y="lg" />
+      <Spacer y="md" />
 
       <Button variant="accent" onClick={props.onContinue} fullWidth>
         Continue
       </Button>
+    </Container>
+  );
+}
+
+function PaymentStep(props: {
+  step: number;
+  title: React.ReactNode;
+  isActive: boolean;
+  isDone: boolean;
+  from: {
+    icon: React.ReactNode;
+    primaryText: React.ReactNode;
+    secondaryText?: React.ReactNode;
+  };
+  to: {
+    icon: React.ReactNode;
+    primaryText: React.ReactNode;
+    secondaryText?: React.ReactNode;
+  };
+}) {
+  return (
+    <StepContainer isActive={props.isActive} isDone={props.isDone}>
+      <Text size="sm">Step {props.step}</Text>
+      <Spacer y="xs" />
+      {props.title}
+      <Spacer y="sm" />
+      <Line />
+      <Spacer y="md" />
+
+      <PaymentSubStep {...props.from} />
+
+      <Container
+        color="borderColor"
+        style={{
+          paddingLeft: "18px",
+          position: "relative",
+          marginBlock: "3px",
+        }}
+      >
+        {/* TODO - replace this with SVG  */}
+        <div
+          style={{
+            height: "18px",
+            width: "2px",
+            backgroundColor: "currentColor",
+            transform: "translateX(-50%)",
+          }}
+        />
+        <TriangleDownIcon
+          width={iconSize.sm}
+          height={iconSize.sm}
+          style={{
+            position: "absolute",
+            bottom: "0",
+            transform: "translate(-50%, 50%)",
+          }}
+        />
+      </Container>
+
+      <PaymentSubStep {...props.to} />
+    </StepContainer>
+  );
+}
+
+function PaymentSubStep(props: {
+  icon: React.ReactNode;
+  primaryText: React.ReactNode;
+  secondaryText?: React.ReactNode;
+}) {
+  return (
+    <Container flex="row" gap="sm" center="y">
+      {/* icon */}
+      <Container
+        p="xs"
+        borderColor="borderColor"
+        flex="row"
+        center="both"
+        style={{
+          borderStyle: "solid",
+          borderWidth: "1.5px",
+          borderRadius: radius.lg,
+        }}
+      >
+        {props.icon}
+      </Container>
+      <Container flex="column" gap="xxs">
+        {props.primaryText}
+        {props.secondaryText}
+      </Container>
     </Container>
   );
 }
