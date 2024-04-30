1c89c275ab3118ef8ea430d59ae2788789218537
Joaquim Verges <joaquim.verges@gmail.com>
2024-05-01 09:37:18 +1200

    iter

--- packages/thirdweb/src/wallets/in-app/implementations/lib/auth/passkeys.ts
+++ packages/thirdweb/src/wallets/in-app/implementations/lib/auth/passkeys.ts
@@ -5,26 +5,37 @@ import type { AsyncStorage } from "../../../../storage/AsyncStorage.js";
 async function registerPasskey(options: {
   client: ThirdwebClient;
   storage: AsyncStorage;
+  authenticatorType?: string;
+  username?: string;
 }) {
   if (!client.isAvailable()) {
     throw new Error("Passkeys are not available on this device");
   }
-  // 1. request challenge from  server/iframe
+  // 1. request challenge from  server
+  // TODO enpoint to req challenge
+
   const challenge = "a7c61ef9-dc23-4806-b486-2428938a547e";
   // 2. initiate registration
-  const generatedName = `in-app-wallet-${options.client.clientId}`;
+  const generatedName =
+    options.username ?? `in-app-wallet-${options.client.clientId}`;
   const registration = await client.register(generatedName, challenge, {
-    authenticatorType: "auto",
+    authenticatorType: "roaming",
     userVerification: "required",
     attestation: true,
     debug: false,
   });
-  // 3. send the registration object to the server/iframe
-  // 4. store the credentialId in local storage
+  // 3. store the credentialId in local storage
   await options.storage.setItem(
     `in-app-wallet-credential-id-${options.client.clientId}`,
     registration.credential.id,
   );
+
+  // 4. send the registration object to the server
+  // TODO endpoint to send registration data
+
+  // 5. returns back the IAW authentication token
+  // 6. pass it to the iframe and call postLogin to store the auth token
+
   return registration;
 }
 
@@ -44,7 +55,7 @@ async function loginWithPasskey(options: {
   const credentials = credentialId ? [credentialId] : [];
   // 2. initiate login
   const authentication = await client.authenticate(credentials, challenge, {
-    authenticatorType: "auto",
+    authenticatorType: "roaming",
     userVerification: "required",
   });
   // 3. send the authentication object to the server/iframe
