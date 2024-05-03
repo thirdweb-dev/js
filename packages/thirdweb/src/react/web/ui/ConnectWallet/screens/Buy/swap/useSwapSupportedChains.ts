75346fb6087c0fa123020be7477c94a5929881f4
Isaac Dubuque <idubuque@umich.edu>
2024-05-03 11:37:58 -0700

    add supported chains

--- packages/thirdweb/src/react/web/ui/ConnectWallet/screens/Buy/swap/useSwapSupportedChains.ts
+++ packages/thirdweb/src/react/web/ui/ConnectWallet/screens/Buy/swap/useSwapSupportedChains.ts
@@ -5,14 +5,35 @@ import { getPayChainsEndpoint } from "../../../../../../../pay/utils/definitions
 import { getClientFetch } from "../../../../../../../utils/fetch.js";
 import { withCache } from "../../../../../../../utils/promise/withCache.js";
 
+export interface PaySupportedChainsResponse {
+  result: {
+    sourceChains: {
+      name: string;
+      chain: string;
+      chainId: number;
+    }[];
+    destinationChains: {
+      name: string;
+      chain: string;
+      chainId: number;
+    }[]
+  }
+}
+
 export async function fetchBuySupportedChains(client: ThirdwebClient) {
   return withCache(
     async () => {
       const fetchWithHeaders = getClientFetch(client);
       const res = await fetchWithHeaders(getPayChainsEndpoint());
-      const data = await res.json();
-      const chainIds = data.result.chainIds as number[];
-      return chainIds.map(defineChain);
+      const data = await res.json() as PaySupportedChainsResponse;
+
+      const sourceChains: number[] = data.result.sourceChains.map(chain => chain.chainId);
+      const destinationChains: number[] = data.result.destinationChains.map(chain => chain.chainId);
+
+      return {
+        sourceChains: sourceChains.map(defineChain),
+        destinationChains: destinationChains.map(defineChain);
+      }
     },
     {
       cacheKey: "buy-supported-chains",
