52d7f490569c7cf63ffc1432539150606e0d01fb
gregfromstl <gregfromstl@gmail.com>
2024-05-07 10:35:10 -0700

    feat(in-app): export getUserPhoneNumber and getAuthenticatedUser

--- packages/thirdweb/src/exports/wallets/in-app.ts
+++ packages/thirdweb/src/exports/wallets/in-app.ts
@@ -4,5 +4,7 @@ export {
   preAuthenticate,
   authenticate,
   getUserEmail,
+  getUserPhoneNumber,
+  getAuthenticatedUser,
   type GetAuthenticatedUserParams,
 } from "../../wallets/in-app/core/authentication/index.js";
