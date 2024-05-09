d660b6ce3249bdcd1ff57719ecee7db5c7d47718
greg <gregfromstl@gmail.com>
2024-05-09 16:58:20 -0700

    React tests & deterministic deploy cleanup (#2969)
    Co-authored-by: Jonas Daniels <jonas.daniels@outlook.com>

--- packages/thirdweb/src/react/web/ui/ConnectWallet/ConnectButton.test.tsx
+++ packages/thirdweb/src/react/web/ui/ConnectWallet/ConnectButton.test.tsx
@@ -1,11 +1,53 @@
-import { describe, expect, it } from "vitest";
+import {
+  afterAll,
+  beforeAll,
+  beforeEach,
+  describe,
+  expect,
+  it,
+  vi,
+} from "vitest";
 import { render, screen } from "../../../../../test/src/react-render.js";
 import { TEST_CLIENT } from "../../../../../test/src/test-clients.js";
 import { ConnectButton } from "./ConnectButton.js";
 
 describe("ConnectButton", () => {
-  it("renders", () => {
-    render(<ConnectButton client={TEST_CLIENT} />);
-    expect(screen.getByRole("button")).toBeInTheDocument();
+  beforeAll(() => {
+    vi.mock("./locale/getConnectLocale.js", () => ({
+      useConnectLocale: vi.fn().mockReturnValue({
+        data: {
+          defaultButtonTitle: "Connect Wallet",
+        },
+        isLoading: false,
+      }),
+    }));
+  });
+
+  afterAll(() => {
+    vi.clearAllMocks();
+  });
+
+  describe("defaults", () => {
+    beforeEach(() => {
+      render(<ConnectButton client={TEST_CLIENT} />);
+    });
+
+    it("should render", () => {
+      expect(screen.getByRole("button")).toBeInTheDocument();
+    });
+
+    it("should display 'Connect Wallet' on initial render", () => {
+      expect(screen.getByText("Connect Wallet")).toBeInTheDocument();
+    });
+  });
+
+  it("should override default text", () => {
+    render(
+      <ConnectButton
+        client={TEST_CLIENT}
+        connectButton={{ label: "Sign In" }}
+      />,
+    );
+    expect(screen.getByText("Sign In")).toBeInTheDocument();
   });
 });
---
+++ packages/thirdweb/src/react/web/ui/TransactionButton/TransactionButton.test.tsx
@@ -0,0 +1,95 @@
+import {
+  afterAll,
+  afterEach,
+  beforeAll,
+  beforeEach,
+  describe,
+  expect,
+  it,
+  vi,
+} from "vitest";
+import { ANVIL_CHAIN } from "../../../../../test/src/chains.js";
+import { render, screen } from "../../../../../test/src/react-render.js";
+import { TEST_CLIENT } from "../../../../../test/src/test-clients.js";
+import { TEST_ACCOUNT_A } from "../../../../../test/src/test-wallets.js";
+import { prepareTransaction, toWei } from "../../../../exports/thirdweb.js";
+import { TransactionButton } from "./index.js";
+
+vi.mock("../../../../transaction/actions/send-transaction.js", () => ({
+  sendTransaction: vi.fn(),
+}));
+
+const TRANSFER_TX = prepareTransaction({
+  client: TEST_CLIENT,
+  chain: ANVIL_CHAIN,
+  to: TEST_ACCOUNT_A.address,
+  value: BigInt(toWei("100")),
+});
+
+const mocks = vi.hoisted(() => {
+  return {
+    activeAccount: vi.fn(),
+    activeWallet: vi.fn(),
+    switchActiveWalletChain: vi.fn(),
+    transaction: vi.fn(),
+  };
+});
+
+describe.sequential("TransactionButton", () => {
+  beforeAll(() => {
+    vi.mock("../../../core/hooks/wallets/wallet-hooks.js", () => ({
+      useActiveAccount: mocks.activeAccount,
+      useActiveWallet: mocks.activeWallet,
+      useSwitchActiveWalletChain: mocks.switchActiveWalletChain,
+    }));
+  });
+
+  afterAll(() => {
+    vi.clearAllMocks();
+  });
+
+  afterEach(() => {
+    vi.restoreAllMocks();
+  });
+
+  beforeEach(() => {
+    mocks.transaction.mockReturnValue(TRANSFER_TX);
+    render(
+      <TransactionButton transaction={mocks.transaction}>
+        Send ETH
+      </TransactionButton>,
+    );
+  });
+
+  describe("defaults", () => {
+    it("should render", () => {
+      expect(screen.getByRole("button")).toBeInTheDocument();
+    });
+
+    it("should display text on initial render", () => {
+      expect(screen.getByText("Send ETH")).toBeInTheDocument();
+    });
+  });
+
+  describe("with account", () => {
+    beforeAll(() => {
+      mocks.activeAccount.mockReturnValue(TEST_ACCOUNT_A);
+    });
+
+    it("should be enabled", () => {
+      const button = screen.getByRole("button", { name: "Send ETH" });
+      expect(button).not.toBeDisabled();
+    });
+  });
+
+  describe("without account", () => {
+    beforeAll(() => {
+      mocks.activeAccount.mockReturnValue(undefined);
+    });
+
+    it("should be disabled", () => {
+      const button = screen.getByRole("button", { name: "Send ETH" });
+      expect(button).toBeDisabled();
+    });
+  });
+});
