e08993cf662656bad118609c7ffe6363dc688a3f
gregfromstl <gregfromstl@gmail.com>
2024-05-08 22:35:08 -0700

    fix(cli): hardhat hanging process

--- legacy_packages/cli/src/core/helpers/exec.ts
+++ legacy_packages/cli/src/core/helpers/exec.ts
@@ -4,15 +4,21 @@ export async function execute(
   command: string,
   projectPath: string,
   options = { log: false, cwd: projectPath },
+  timeout = 300_000, // 5 minute timeout by default
 ): Promise<{ stdout: string; stderr: string }> {
   return new Promise((resolve, reject) => {
-    exec(command, { ...options }, (err, stdout, stderr) => {
+    const process = exec(command, { ...options }, (err, stdout, stderr) => {
       if (err) {
         reject(err);
         return;
       }
-
+      clearTimeout(timeoutId);
       resolve({ stdout, stderr });
     });
+
+    const timeoutId = setTimeout(() => {
+      process.kill();  // Attempt to kill the process
+      reject(new Error('Command timeout'));
+    }, timeout);
   });
 }
