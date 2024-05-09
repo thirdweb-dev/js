8a7e00767cda5ea76cedb2083d26838a99211530
gregfromstl <gregfromstl@gmail.com>
2024-05-09 10:02:31 -0700

    test(TransactionButton): baseline tests

--- packages/thirdweb/src/react/web/ui/ConnectWallet/ConnectButton.test.tsx
+++ packages/thirdweb/src/react/web/ui/ConnectWallet/ConnectButton.test.tsx
@@ -1,29 +1,42 @@
-import { beforeEach, describe, expect, it, suite, test, vi } from "vitest";
+import {
+  afterAll,
+  beforeAll,
+  beforeEach,
+  describe,
+  expect,
+  it,
+  vi,
+} from "vitest";
 import { render, screen } from "../../../../../test/src/react-render.js";
 import { TEST_CLIENT } from "../../../../../test/src/test-clients.js";
 import { ConnectButton } from "./ConnectButton.js";
-import * as connectLocale from "./locale/getConnectLocale.js";
 
-vi.mock("./locale/getConnectLocale.js", () => ({
-  useConnectLocale: vi.fn().mockReturnValue({
-    data: {
-      defaultButtonTitle: "Connect Wallet",
-    },
-    isLoading: false,
-  }),
-}));
+describe("ConnectButton", () => {
+  beforeAll(() => {
+    vi.mock("./locale/getConnectLocale.js", () => ({
+      useConnectLocale: vi.fn().mockReturnValue({
+        data: {
+          defaultButtonTitle: "Connect Wallet",
+        },
+        isLoading: false,
+      }),
+    }));
+  });
 
-suite("ConnectButton", () => {
+  afterAll(() => {
+    vi.clearAllMocks();
+  });
+
   describe("defaults", () => {
     beforeEach(() => {
       render(<ConnectButton client={TEST_CLIENT} />);
     });
 
-    it("renders", () => {
+    it("should render", () => {
       expect(screen.getByRole("button")).toBeInTheDocument();
     });
 
-    it("displays 'Connect Wallet' on initial render", () => {
+    it("should display 'Connect Wallet' on initial render", () => {
       expect(screen.getByText("Connect Wallet")).toBeInTheDocument();
     });
   });
