d17d68c0f2b9380ec9d8367d6264022ee0adf72d
Joaquim Verges <joaquim.verges@gmail.com>
2024-05-16 12:38:19 +1200

    fix test

--- packages/thirdweb/src/auth/core/generate-login-payload.test.ts
+++ packages/thirdweb/src/auth/core/generate-login-payload.test.ts
@@ -77,7 +77,9 @@ describe("generateLoginPayload", () => {
 
     const { nonce, ...restResult } = result;
 
-    expect(nonce).toContain("-");
+    expect(nonce).toContain(
+      "0x18f68cae0efd8818fbfc163f27070442407b4ef21716ad36112dc2001946c0b2",
+    );
 
     expect(restResult).toMatchInlineSnapshot(`
       {
