692779566efce795ebf821b84bed3ae69efee653
Yash <kumaryashcse@gmail.com>
2024-05-17 19:05:00 +0530

    export functions

---
+++ packages/thirdweb/src/exports/extensions/modular.ts
@@ -0,0 +1,85 @@
+/**
+ * READ
+ */
+export {
+  getInstalledExtensions,
+  isGetInstalledExtensionsSupported,
+} from "../../extensions/modular/__generated__/ModularCoreUpgradeable/read/getInstalledExtensions.js";
+export { erc1967FactoryAddress } from "../../extensions/modular/__generated__/ModularCoreUpgradeable/read/erc1967FactoryAddress.js";
+export {
+  getSupportedCallbackFunctions,
+  isGetSupportedCallbackFunctionsSupported,
+} from "../../extensions/modular/__generated__/ModularCoreUpgradeable/read/getSupportedCallbackFunctions.js";
+export {
+  hasAnyRole,
+  type HasAnyRoleParams,
+} from "../../extensions/modular/__generated__/ModularCoreUpgradeable/read/hasAnyRole.js";
+export {
+  hasAllRoles,
+  type HasAllRolesParams,
+} from "../../extensions/modular/__generated__/ModularCoreUpgradeable/read/hasAllRoles.js";
+export { INSTALLER_ROLE } from "../../extensions/modular/__generated__/ModularCoreUpgradeable/read/INSTALLER_ROLE.js";
+export { owner } from "../../extensions/modular/__generated__/ModularCoreUpgradeable/read/owner.js";
+export {
+  ownershipHandoverExpiresAt,
+  type OwnershipHandoverExpiresAtParams,
+} from "../../extensions/modular/__generated__/ModularCoreUpgradeable/read/ownershipHandoverExpiresAt.js";
+export {
+  rolesOf,
+  type RolesOfParams,
+} from "../../extensions/modular/__generated__/ModularCoreUpgradeable/read/rolesOf.js";
+
+/**
+ * Write
+ */
+export { cancelOwnershipHandover } from "../../extensions/modular/__generated__/ModularCoreUpgradeable/write/cancelOwnershipHandover.js";
+export {
+  completeOwnershipHandover,
+  type CompleteOwnershipHandoverParams,
+} from "../../extensions/modular/__generated__/ModularCoreUpgradeable/write/completeOwnershipHandover.js";
+export {
+  grantRoles,
+  type GrantRolesParams
+} from "../../extensions/modular/__generated__/ModularCoreUpgradeable/write/grantRoles.js";
+export {
+  installExtension,
+  isInstallExtensionSupported,
+  type InstallExtensionParams
+} from "../../extensions/modular/__generated__/ModularCoreUpgradeable/write/installExtension.js";
+export {
+  renounceOwnership
+} from "../../extensions/modular/__generated__/ModularCoreUpgradeable/write/renounceOwnership.js";
+export {
+  renounceRoles,
+  type RenounceRolesParams
+} from "../../extensions/modular/__generated__/ModularCoreUpgradeable/write/renounceRoles.js";
+export {
+  revokeRoles,
+  type RevokeRolesParams
+} from "../../extensions/modular/__generated__/ModularCoreUpgradeable/write/revokeRoles.js";
+export {
+  transferOwnership,
+  type TransferOwnershipParams
+} from "../../extensions/modular/__generated__/ModularCoreUpgradeable/write/transferOwnership.js";
+export {
+  uninstallExtension,
+  isUninstallExtensionSupported,
+  type UninstallExtensionParams
+} from "../../extensions/modular/__generated__/ModularCoreUpgradeable/write/uninstallExtension.js";
+export {
+  updateExtension,
+  isUpdateExtensionSupported,
+  type UpdateExtensionParams
+} from "../../extensions/modular/__generated__/ModularCoreUpgradeable/write/updateExtension.js";
+export {
+  installPublishedExtension,
+  type InstallPublishedExtensionOptions
+} from "../../extensions/modular/ModularCoreUpgradeable/write/installPublishedExtension.js";
+export {
+  uninstallPublishedExtension,
+  type UninstallPublishedExtensionOptions
+} from "../../extensions/modular/ModularCoreUpgradeable/write/uninstallPublishedExtension.js";
+export {
+  uninstallExtensionByProxy,
+  type UninstallExtensionByProxyOptions
+} from "../../extensions/modular/ModularCoreUpgradeable/write/uninstallExtensionByProxy.js";
