73130003938eda4f679e533d3a9f3604547e8e88
Kien Ngo <github@kien.ngo>
2024-05-19 06:08:29 -0400

    Add test for encodeAbiParameters > prepareParam

--- packages/thirdweb/src/utils/abi/encode-abi-parameters.test.ts
+++ packages/thirdweb/src/utils/abi/encode-abi-parameters.test.ts
@@ -1,5 +1,5 @@
 import { describe, expect, it } from "vitest";
-import { encodeAbiParameters } from "./encodeAbiParameters.js";
+import { encodeAbiParameters, prepareParam } from "./encodeAbiParameters.js";
 
 describe("encodeAbiParameters", () => {
   it("should throw an error if params.length !== values.length", async () => {
@@ -10,6 +10,14 @@ describe("encodeAbiParameters", () => {
     const values = [123, "hello", "this should not be here"];
     expect(() => encodeAbiParameters(params, values)).toThrowError(
       "The number of parameters and values must match.",
+    );
+  });
+
+  it("should throw an error with unsupported parameter type", () => {
+    const param = { name: "param1", type: "some-unknown-type" };
+    const value = [""];
+    expect(() => prepareParam({ param, value })).toThrowError(
+      "Unsupported parameter type",
     );
   });
 });
--- packages/thirdweb/src/utils/abi/encodeAbiParameters.ts
+++ packages/thirdweb/src/utils/abi/encodeAbiParameters.ts
@@ -80,7 +80,10 @@ function prepareParams<const TParams extends readonly AbiParameter[]>({
   return preparedParams;
 }
 
-function prepareParam<const TParam extends AbiParameter>({
+/**
+ * @internal Export for unit test
+ */
+export function prepareParam<const TParam extends AbiParameter>({
   param,
   value,
 }: {
