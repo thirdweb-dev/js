278eb95ed90d8bbdd74c9c562d2fe5bdd8c33218
Phillip Ho <arcoraven@gmail.com>
2024-05-22 00:33:26 +0800

    chore: support split native currency for all chains (#2583)
    * chore: support split native currency for all chains
    
    * rename

--- src/pages/api/moralis/balances.ts
+++ src/pages/api/moralis/balances.ts
@@ -1,7 +1,9 @@
 import { SUPPORTED_CHAIN_ID } from "@thirdweb-dev/sdk";
-import { CURRENCIES } from "constants/currencies";
 import { constants, utils } from "ethers";
+import { thirdwebClient } from "lib/thirdweb-client";
 import { NextApiRequest, NextApiResponse } from "next";
+import { defineChain } from "thirdweb";
+import { getWalletBalance } from "thirdweb/wallets";
 
 export type BalanceQueryRequest = {
   chainId: SUPPORTED_CHAIN_ID;
@@ -27,58 +29,55 @@ const handler = async (req: NextApiRequest, res: NextApiResponse) => {
     return res.status(400).json({ error: "invalid address" });
   }
 
-  const _chain = encodeURIComponent(`0x${chainId?.toString(16)}`);
-  const _address = encodeURIComponent(address);
+  const getNativeBalance = async (): Promise<BalanceQueryResponse> => {
+    const chain = defineChain(chainId);
+    const balance = await getWalletBalance({
+      address,
+      chain,
+      client: thirdwebClient,
+    });
+    return [
+      {
+        token_address: constants.AddressZero,
+        symbol: balance.symbol,
+        name: "Native Token",
+        decimals: balance.decimals,
+        balance: balance.value.toString(),
+        display_balance: balance.displayValue,
+      },
+    ];
+  };
 
-  const tokenBalanceEndpoint = `https://deep-index.moralis.io/api/v2/${_address}/erc20?chain=${_chain}`;
-  const nativeBalanceEndpoint = `https://deep-index.moralis.io/api/v2/${_address}/balance?chain=${_chain}`;
+  const getTokenBalances = async (): Promise<BalanceQueryResponse> => {
+    const _chain = encodeURIComponent(`0x${chainId?.toString(16)}`);
+    const _address = encodeURIComponent(address);
+    const tokenBalanceEndpoint = `https://deep-index.moralis.io/api/v2/${_address}/erc20?chain=${_chain}`;
+
+    const resp = await fetch(tokenBalanceEndpoint, {
+      method: "GET",
+      headers: {
+        "x-api-key": process.env.MORALIS_API_KEY || "",
+      },
+    });
+    if (!resp.ok) {
+      resp.body?.cancel();
+      return [];
+    }
+    const json = await resp.json();
+    return json.map((balance: any) => ({
+      ...balance,
+      display_balance: utils
+        .formatUnits(balance.balance, balance.decimals)
+        .toString(),
+    }));
+  };
 
   const [nativeBalance, tokenBalances] = await Promise.all([
-    fetch(nativeBalanceEndpoint, {
-      method: "GET",
-      headers: {
-        "Content-Type": "application/json",
-        "x-api-key": process.env.MORALIS_API_KEY || "",
-      },
-    }).then((nRes) => nRes.json()),
-    fetch(tokenBalanceEndpoint, {
-      method: "GET",
-      headers: {
-        "Content-Type": "application/json",
-        "x-api-key": process.env.MORALIS_API_KEY || "",
-      },
-    }).then((tRes) => tRes.json()),
+    getNativeBalance(),
+    getTokenBalances(),
   ]);
 
-  const native = CURRENCIES[chainId]?.find(
-    (c) => c.address === constants.AddressZero,
-  );
-
-  if (tokenBalances?.message) {
-    return res.status(400).json({ error: tokenBalances.message });
-  }
-
-  if (nativeBalance?.message) {
-    return res.status(400).json({ error: nativeBalance.message });
-  }
-
-  const balances = [
-    ...(tokenBalances || []),
-    {
-      token_address: native?.address,
-      symbol: native?.symbol,
-      name: "Native Token",
-      decimals: 18,
-      balance: nativeBalance.balance,
-    },
-  ].map((balance) => ({
-    ...balance,
-    display_balance: utils
-      .formatUnits(balance.balance, balance.decimals)
-      .toString(),
-  }));
-
-  return res.status(200).json(balances);
+  return res.status(200).json([...nativeBalance, ...tokenBalances]);
 };
 
 export default handler;
