7d051d00086f213ce73f44e42f5e2790ab706bd8
Jonas Daniels <jonas.daniels@outlook.com>
2024-05-23 14:12:20 -0700

    feat: add metadataBase to page components

--- apps/playground-web/src/app/connect/pay/page.tsx
+++ apps/playground-web/src/app/connect/pay/page.tsx
@@ -1,9 +1,11 @@
 import { StyledPayEmbed } from "@/components/styled-pay-embed";
 import { Button } from "@/components/ui/button";
+import { metadataBase } from "@/lib/constants";
 import type { Metadata } from "next";
 import Link from "next/link";
 
 export const metadata: Metadata = {
+  metadataBase,
   title: "Integrate Fiat & Cross-Chain Crypto Payments | thirdweb Pay",
   description:
     "The easiest way for users to transact in your app. Onramp users in clicks and generate revenue for each user transaction. Integrate for free.",
