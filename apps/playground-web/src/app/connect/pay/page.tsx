606bef57aaf7ff41e2a5d3f35fbfec2b672b8612
Jonas Daniels <jonas.daniels@outlook.com>
2024-05-22 22:19:55 -0700

    update playground (#3121)

---
+++ apps/playground-web/src/app/connect/pay/page.tsx
@@ -0,0 +1,70 @@
+import { StyledPayEmbed } from "@/components/styled-pay-embed";
+import { Button } from "@/components/ui/button";
+import { getBaseUrl } from "@/lib/getBaseUrl";
+import type { Metadata } from "next";
+import Link from "next/link";
+
+export const metadata: Metadata = {
+  metadataBase: new URL(getBaseUrl()),
+  title: "Integrate Fiat & Cross-Chain Crypto Payments | thirdweb Pay",
+  description:
+    "The easiest way for users to transact in your app. Onramp users in clicks and generate revenue for each user transaction. Integrate for free.",
+  openGraph: {
+    images: [
+      {
+        url: "/og-image-pay.png",
+        width: 1200,
+        height: 630,
+        alt: "Integrate Fiat & Cross-Chain Crypto Payments | thirdweb Pay",
+      },
+    ],
+  },
+};
+
+export default function Page() {
+  return (
+    <main className="flex-1 content-center relative">
+      <section className="w-full py-12 md:py-24 lg:py-32 xl:py-48 z-10">
+        <div className="container px-4 md:px-6">
+          <div className="grid gap-10 lg:grid-cols-[1fr_400px] lg:gap-12 xl:grid-cols-[1fr_600px]">
+            <div className="flex flex-col justify-center space-y-4 min-h-[100%]">
+              <div className="space-y-2">
+                <h1 className="text-3xl font-bold tracking-tighter sm:text-5xl xl:text-6xl/none font-inter mb-6 text-balance">
+                  The easiest way for users to transact in your app
+                </h1>
+                <p className="max-w-[600px] text-gray-500 md:text-xl dark:text-gray-300 mb-6 font-inter">
+                  Onramp users in clicks with credit card &amp; cross-chain
+                  crypto payments — and generate revenue for each user
+                  transaction. Integrate for free.
+                </p>
+              </div>
+              <div className="flex flex-col gap-4 min-[400px]:flex-row">
+                <Button asChild size="lg">
+                  <Link
+                    target="_blank"
+                    href="https://portal.thirdweb.com/connect/pay/get-started"
+                  >
+                    Get Started
+                  </Link>
+                </Button>
+                <Button asChild variant="outline" size="lg">
+                  <Link target="_blank" href="https://thirdweb.com/contact-us">
+                    Book a Demo
+                  </Link>
+                </Button>
+              </div>
+            </div>
+            <div className="w-full mx-auto my-auto sm:w-full lg:order-last relative flex flex-col space-y-2">
+              <div className="shadow-xl md:w-[400px] md:mx-auto">
+                <StyledPayEmbed />
+              </div>
+              <p className="md:text-xl text-center text-muted-foreground">
+                <small>👆 This is live, try it out! 👆</small>
+              </p>
+            </div>
+          </div>
+        </div>
+      </section>
+    </main>
+  );
+}
---
+++ apps/playground-web/src/components/code/code.tsx
@@ -0,0 +1,42 @@
+// enforces this only gets rendered on the server
+import "server-only";
+
+import { transformerTwoslash } from "@shikijs/twoslash";
+import "@shikijs/twoslash/style-rich.css";
+import * as parserBabel from "prettier/plugins/babel";
+import * as estree from "prettier/plugins/estree";
+import { format } from "prettier/standalone";
+import { type BundledLanguage, codeToHtml } from "shiki";
+
+export type CodeProps = {
+  code: string;
+  lang: BundledLanguage;
+};
+
+export const Code: React.FC<CodeProps> = async ({ code, lang }) => {
+  const formattedCode = await format(
+    `// @noErrors
+  ${code}`,
+    {
+      parser: "babel-ts",
+      plugins: [parserBabel, estree],
+      printWidth: 50,
+    },
+  );
+  const html = await codeToHtml(formattedCode, {
+    lang: lang,
+    themes: {
+      light: "github-light",
+      dark: "github-dark-default",
+    },
+    transformers: [transformerTwoslash()],
+  });
+
+  return (
+    <div
+      className="*:pointer-events-none *:md:pointer-events-auto *:w-full *:p-4 *:md:p-8 *:h-full h-full text-xs md:text-sm *:rounded-3xl *:max-sm:rounded-b-none *:md:rounded-r-none *:overflow-clip *:md:overflow-visible"
+      // biome-ignore lint/security/noDangerouslySetInnerHtml: we know what we're doing here
+      dangerouslySetInnerHTML={{ __html: html }}
+    />
+  );
+};
