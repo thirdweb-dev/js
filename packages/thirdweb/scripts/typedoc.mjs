076c6045ad9abc0759d767964f747342b9c45ab4
Joaquim Verges <joaquim.verges@gmail.com>
2024-05-24 20:49:46 +1200

    exclude react-native exports from tsdoc

--- packages/thirdweb/scripts/typedoc.mjs
+++ packages/thirdweb/scripts/typedoc.mjs
@@ -4,6 +4,7 @@ import { typedoc } from "typedoc-gen";
 typedoc({
   entryPoints: ["src/exports/**/*.ts"],
   exclude: [
+    "src/exports/react-native.ts",
     "test/**/*",
     "src/**/*.test.ts",
     "src/**/*.test.tsx",
