d7ec32ab05559e12e4d6dc3125c99c18c68cdf04
Kien Ngo <github@kien.ngo>
2024-05-29 23:34:36 -0400

    Add extenstion tests

---
+++ packages/thirdweb/src/extensions/erc721/read/getOwnedTokenIds.test.ts
@@ -0,0 +1,17 @@
+import { describe, expect, it } from "vitest";
+import { DOODLES_CONTRACT } from "~test/test-contracts.js";
+import { getOwnedTokenIds } from "./getOwnedTokenIds.js";
+
+describe.runIf(process.env.TW_SECRET_KEY)("erc721.getAllOwners", () => {
+  it("works for a contract with 0 indexed NFTs", async () => {
+    const owner = "0x3010775D16E7B79AF280035c64a1Df5F705CfdDb";
+    const tokenIds = await getOwnedTokenIds({
+      contract: DOODLES_CONTRACT,
+      owner,
+    });
+
+    // The following code is based on the test result from "./getAllOwners.test.ts"
+    expect(tokenIds.length).toBe(1);
+    expect(tokenIds[0]).toBe(0n);
+  });
+});
