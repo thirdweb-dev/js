9a84b759494dc4bec32f7b9307d7b6a1d568bc36
Joaquim Verges <joaquim.verges@gmail.com>
2024-06-04 14:50:32 +1200

    fix jsdoc

--- packages/thirdweb/src/transaction/actions/to-serializable-transaction.ts
+++ packages/thirdweb/src/transaction/actions/to-serializable-transaction.ts
@@ -26,7 +26,7 @@ export type ToSerializableTransactionOptions = {
  * @note For easier transaction sending, {@see sendTransaction}
  * @example
  * ```ts
- * import { prepareTransaction, populateTransaction } from "thirdweb";
+ * import { prepareTransaction, toSerializableTransaction } from "thirdweb";
  *
  * const transaction = await prepareTransaction({
  *   transaction: {
