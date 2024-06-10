8848a9bd15ad7b0e8096ced72c27d71976eb6572
Manan Tank <manantankm@gmail.com>
2024-06-11 04:23:12 +0530

    Fix lint

--- packages/thirdweb/src/react/web/ui/components/TokenIcon.tsx
+++ packages/thirdweb/src/react/web/ui/components/TokenIcon.tsx
@@ -2,6 +2,7 @@
 import { useMemo } from "react";
 import type { Chain } from "../../../../chains/types.js";
 import type { ThirdwebClient } from "../../../../client/client.js";
+import { NATIVE_TOKEN_ADDRESS } from "../../../../constants/addresses.js";
 import { iconSize } from "../../../core/design-system/index.js";
 import { useChainQuery } from "../../../core/hooks/others/useChainQuery.js";
 import { genericTokenIcon } from "../ConnectWallet/icons/dataUris.js";
@@ -10,7 +11,6 @@ import {
   isNativeToken,
 } from "../ConnectWallet/screens/nativeToken.js";
 import { Img } from "./Img.js";
-import { NATIVE_TOKEN_ADDRESS } from "../../../../constants/addresses.js";
 
 // Note: Must not use useConnectUI here
 
@@ -31,7 +31,10 @@ export function TokenIcon(props: {
   const chainQuery = useChainQuery(props.chain);
 
   const tokenImage = useMemo(() => {
-    if (isNativeToken(props.token) || props.token.address === NATIVE_TOKEN_ADDRESS) {
+    if (
+      isNativeToken(props.token) ||
+      props.token.address === NATIVE_TOKEN_ADDRESS
+    ) {
       return chainQuery.data?.icon?.url;
     }
     return props.token.icon;
