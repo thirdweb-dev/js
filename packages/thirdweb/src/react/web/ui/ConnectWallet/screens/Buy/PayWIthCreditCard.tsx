ce45a793b54ab656dac01f2f99d9db8df52f4e23
Manan Tank <manantankm@gmail.com>
2024-06-11 21:29:33 +0530

    Add currency selector in Pay UI (#3251)
    Co-authored-by: greg <gregfromstl@gmail.com>

--- packages/thirdweb/src/react/web/ui/ConnectWallet/screens/Buy/PayWIthCreditCard.tsx
+++ packages/thirdweb/src/react/web/ui/ConnectWallet/screens/Buy/PayWIthCreditCard.tsx
@@ -26,7 +26,6 @@ export function PayWithCreditCard(props: {
   client: ThirdwebClient;
   currency: CurrencyMeta;
   onSelectCurrency: () => void;
-  disableCurrencySelection?: boolean;
 }) {
   return (
     <Container
@@ -47,7 +46,6 @@ export function PayWithCreditCard(props: {
     >
       {/* Left */}
       <CurrencyButton
-        disabled={props.disableCurrencySelection}
         variant="ghost"
         onClick={props.onSelectCurrency}
         style={{
@@ -58,11 +56,9 @@ export function PayWithCreditCard(props: {
         gap="sm"
       >
         <props.currency.icon size={iconSize.md} />
-        <Container flex="row" center="y" gap="xxs">
+        <Container flex="row" center="y" gap="xxs" color="secondaryText">
           <Text color="primaryText">{props.currency.shorthand}</Text>
-          {!props.disableCurrencySelection && (
-            <ChevronDownIcon width={iconSize.sm} height={iconSize.sm} />
-          )}
+          <ChevronDownIcon width={iconSize.sm} height={iconSize.sm} />
         </Container>
       </CurrencyButton>
 
