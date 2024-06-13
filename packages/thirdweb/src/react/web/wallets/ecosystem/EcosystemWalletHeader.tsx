20f463b8d1ea01eba4550ea4b427179d2f9c9fba
gregfromstl <gregfromstl@gmail.com>
2024-06-12 17:31:07 -0700

    fix(react): ecosystem wallet header spacing

--- packages/thirdweb/src/react/web/wallets/ecosystem/EcosystemWalletHeader.tsx
+++ packages/thirdweb/src/react/web/wallets/ecosystem/EcosystemWalletHeader.tsx
@@ -1,7 +1,11 @@
 "use client";
+import type { ThirdwebClient } from "../../../../client/client.js";
 import type { Wallet } from "../../../../wallets/interfaces/wallet.js";
 import type { EcosystemWalletId } from "../../../../wallets/wallet-types.js";
-import { Container, ModalHeader } from "../../ui/components/basic.js";
+import { iconSize, radius } from "../../../core/design-system/index.js";
+import { Img } from "../../ui/components/Img.js";
+import { Skeleton } from "../../ui/components/Skeleton.js";
+import { ModalHeader } from "../../ui/components/basic.js";
 import { ModalTitle } from "../../ui/components/modalElements.js";
 import { useWalletInfo } from "../../ui/hooks/useWalletInfo.js";
 
@@ -10,16 +14,35 @@ import { useWalletInfo } from "../../ui/hooks/useWalletInfo.js";
  */
 export function EcosystemWalletHeader(props: {
   wallet: Wallet<EcosystemWalletId>;
+  client: ThirdwebClient;
   onBack?: () => void;
 }) {
   const walletInfo = useWalletInfo(props.wallet.id);
+
   return (
     <ModalHeader
       onBack={props.onBack}
       title={
-        <Container>
-          <ModalTitle>{walletInfo.data?.name}</ModalTitle>
-        </Container>
+        <>
+          {walletInfo.isLoading ? (
+            <Skeleton height="24px" width="200px" />
+          ) : (
+            <>
+              {!walletInfo.data?.image_id ? null : (
+                <Img
+                  src={walletInfo.data?.image_id}
+                  style={{
+                    borderRadius: radius.sm,
+                  }}
+                  width={iconSize.md}
+                  height={iconSize.md}
+                  client={props.client}
+                />
+              )}
+              <ModalTitle>{walletInfo.data?.name}</ModalTitle>
+            </>
+          )}
+        </>
       }
     />
   );
