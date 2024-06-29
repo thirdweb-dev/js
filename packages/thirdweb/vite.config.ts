e74b9b023d5758617997bed4f35df16ed9d67478
Manan Tank <manantankm@gmail.com>
2024-06-30 04:00:29 +0530

    change vite config

--- packages/thirdweb/vite.config.ts
+++ packages/thirdweb/vite.config.ts
@@ -4,10 +4,5 @@ import { defineConfig } from "vite";
 // This config is for storybook
 
 export default defineConfig({
-  plugins: [
-    react({
-      // Fixes HMR
-      include: "**/*.tsx",
-    }),
-  ],
+  plugins: [react()],
 });
