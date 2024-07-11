4661731561fb6b079c0a8fbcbcb0bc906bb9372d
jnsdls <jonas.daniels@outlook.com>
2024-07-11 03:08:05 +0000

    [Feature] Add /support path to the list of logged in only paths (#3649)

--- apps/dashboard/src/@/constants/auth.ts
+++ apps/dashboard/src/@/constants/auth.ts
@@ -3,5 +3,6 @@ export const LOGGED_IN_ONLY_PATHS = [
   "/dashboard",
   // anything that _starts_ with /cli is logged in only
   "/cli",
+  "/support",
   // TODO: add any other logged in only paths here
 ];
