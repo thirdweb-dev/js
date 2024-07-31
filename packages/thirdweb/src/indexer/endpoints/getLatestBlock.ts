1f99a345a533193f6ead5762133d9062ef9b6d9a
iuwqyir <toomas.oosalu@gmail.com>
2024-07-31 13:37:34 +0300

    implement getLatestBlockNumber for chainsaw

--- packages/thirdweb/src/indexer/endpoints/getLatestBlock.ts
+++
@@ -1,6 +0,0 @@
-// TODO clarify the types, no need to sanitize here because we are already doing it server-side?
-export async function getLatestBlock(client: any, params: any) {
-  const blocksResponse = await client(params);
-  // TODO: Calrify structured return type through the indexer
-  return await blocksResponse.json();
-}
---
+++ packages/thirdweb/src/indexer/endpoints/getLatestBlockNumber.ts
@@ -0,0 +1,30 @@
+import type { ThirdwebClient } from "src/client/client.js";
+import { getClientFetch } from "src/utils/fetch.js";
+import type { ChainsawResponse } from "../types.d.ts";
+import { getLatestBlockNumberEndpoint } from "../urls.js";
+
+export type GetLatestBlockNumberParams = {
+  client: ThirdwebClient;
+  chainId: number;
+};
+
+export async function getLatestBlockNumber(
+  params: GetLatestBlockNumberParams,
+): Promise<ChainsawResponse<number>> {
+  try {
+    const queryParams = new URLSearchParams();
+    queryParams.append("chainId", params.chainId.toString());
+    const url = `${getLatestBlockNumberEndpoint()}?${queryParams.toString()}`;
+
+    const response = await getClientFetch(params.client)(url);
+    if (!response.ok) {
+      response.body?.cancel();
+      throw new Error(`HTTP error! status: ${response.status}`);
+    }
+
+    const data: ChainsawResponse<number> = await response.json();
+    return data;
+  } catch (error) {
+    throw new Error(`Fetch failed: ${error}`);
+  }
+}
