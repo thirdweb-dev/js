49b17790e434905d4941e23c156c9fa33f5a498b
iuwqyir <toomas.oosalu@gmail.com>
2024-07-31 22:12:29 +0300

    fix indexer exports

--- packages/thirdweb/src/exports/indexer.ts
+++ packages/thirdweb/src/exports/indexer.ts
@@ -1,6 +1,1 @@
-export { getIndexerClient } from "../indexer/indexer";
-export { getBlocks } from "../indexer/endpoints/getBlocks";
-export { getEvents } from "../indexer/endpoints/getEvents";
-export { getLatestBlock } from "../indexer/endpoints/getLatestBlock";
-export { getReceipts } from "../indexer/endpoints/getReceipts";
-export { getNFTs } from "../indexer/endpoints/getNFTs";
+export * from "../indexer/index.js";
