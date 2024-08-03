baefa5e9c7d72fd8645b0e33452a83fc0ef229d4
jnsdls <jonas.daniels@outlook.com>
2024-08-03 23:53:16 +0000

    [Chore] Uniform formatting in package.json and update fs imports (#3933)

--- apps/portal/scripts/createEmptySearchIndex.ts
+++ apps/portal/scripts/createEmptySearchIndex.ts
@@ -1,12 +1,12 @@
-import { existsSync, writeFileSync } from "fs";
+import { existsSync, writeFileSync } from "node:fs";
 
 // This is required to load the searchIndex.json on vercel server
 // searchIndex.json needs to exist in file system before we run `next build`
 
 async function main() {
-	if (!existsSync("./searchIndex.json")) {
-		writeFileSync("./searchIndex.json", `[]`);
-	}
+  if (!existsSync("./searchIndex.json")) {
+    writeFileSync("./searchIndex.json", "[]");
+  }
 }
 
 main();
