8f6f7e4a4d9b73704a4bb032be1bb02c8d103181
iuwqyir <toomas.oosalu@gmail.com>
2024-08-08 02:21:12 +0300

    fix const to functions

--- packages/thirdweb/src/chainsaw/urls.ts
+++ packages/thirdweb/src/chainsaw/urls.ts
@@ -3,45 +3,52 @@ import { getThirdwebDomains } from "../utils/domains.js";
 export function getChainsawV2Url(): string {
   const domain: string = getThirdwebDomains().chainsaw;
   return `${domain}/v2`;
-};
+}
 
 /**
  * Endpoint to get block data
  * @internal
  */
-export const getBlockEndpoint = (blockNumber: number) =>
-  `${getChainsawV2Url()}/blocks/${blockNumber}`;
+export function getBlockEndpoint(blockNumber: number) {
+  return `${getChainsawV2Url()}/blocks/${blockNumber}`;
+}
 
 /**
  * Endpoint to get the latest indexed block number
  * @internal
  */
-export const getLatestBlockNumberEndpoint = () =>
-  `${getChainsawV2Url()}/blockNumber/latest`;
+export function getLatestBlockNumberEndpoint() {
+  return `${getChainsawV2Url()}/blockNumber/latest`;
+}
 
 /**
  * Endpoint to get transactions
  * @internal
  */
-export const getTransactionsEndpoint = () =>
-  `${getChainsawV2Url()}/transactions`;
+export function getTransactionsEndpoint() {
+  return `${getChainsawV2Url()}/transactions`;
+}
 
 /**
  * Endpoint to get events
  * @internal
  */
-export const getEventsEndpoint = () => `${getChainsawV2Url()}/events`;
+export function getEventsEndpoint() {
+  return `${getChainsawV2Url()}/events`;
+}
 
 /**
  * Endpoint to get NFTs by collection
  * @internal
  */
 export function getNftsByCollectionEndpoint() {
-  `${getChainsawV2Url()}/nfts/by-collection`;
+  return `${getChainsawV2Url()}/nfts/by-collection`;
+}
 
 /**
  * Endpoint to get NFTs by owner
  * @internal
  */
 export function getNftsByOwnerEndpoint() {
-  `${getChainsawV2Url()}/nfts/by-owner`;
+  return `${getChainsawV2Url()}/nfts/by-owner`;
+}
