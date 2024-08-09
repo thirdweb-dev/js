ed7d153cf6f2a0c6f8bf74b64df2d66c1b1920ec
MananTank <manantankm@gmail.com>
2024-08-09 17:37:42 +0000

    Update Error Handler Modal UI (#4017)
    ## Problem solved
    
    Short description of the bug fixed or feature added
    
    <!-- start pr-codex -->
    
    ---
    
    ## PR-Codex overview
    This PR updates the styling and functionality of error handling components in the dashboard.
    
    ### Detailed summary
    - Updated styling and functionality of error handling components
    - Added `CopyAddressFormControl` component
    - Improved error message copying functionality
    - Updated dialog structure and content
    - Added support link button in the error dialog
    
    > ✨ Ask PR-Codex anything about this PR by commenting with `/codex {your question}`
    
    <!-- end pr-codex -->

--- apps/dashboard/src/contexts/error-handler.tsx
+++ apps/dashboard/src/contexts/error-handler.tsx
@@ -1,10 +1,17 @@
-import { Code, Divider, Flex, Icon, useClipboard } from "@chakra-ui/react";
+import { CopyTextButton } from "@/components/ui/CopyTextButton";
+import { Button } from "@/components/ui/button";
+import {
+  Dialog,
+  DialogContent,
+  DialogHeader,
+  DialogTitle,
+} from "@/components/ui/dialog";
+import { CircleAlertIcon, InfoIcon } from "lucide-react";
+import Link from "next/link";
 import { createContext, useCallback, useContext, useState } from "react";
-import { FiAlertTriangle, FiCheck, FiCopy, FiHelpCircle } from "react-icons/fi";
 import { toast } from "sonner";
-import { Button, Drawer, Heading, LinkButton, Text } from "tw-components";
-import { AddressCopyButton } from "tw-components/AddressCopyButton";
 import type { ComponentWithChildren } from "types/component-with-children";
+import { useDebounce } from "use-debounce";
 import { parseErrorToMessage } from "utils/errorParser";
 
 interface ErrorContext {
@@ -38,7 +45,8 @@ type EnhancedTransactionError = TransactionError & {
 };
 
 export const ErrorProvider: ComponentWithChildren = ({ children }) => {
-  const [currentError, setCurrentError] = useState<EnhancedTransactionError>();
+  const [currentErrorOriginal, setCurrentError] =
+    useState<EnhancedTransactionError>();
   const dismissError = useCallback(() => setCurrentError(undefined), []);
   const onError = useCallback((err: unknown, title = "An error occurred") => {
     if (isTransactionError(err)) {
@@ -50,65 +58,105 @@ export const ErrorProvider: ComponentWithChildren = ({ children }) => {
     }
   }, []);
 
-  const { onCopy, hasCopied } = useClipboard(currentError?.message || "");
+  // to avoid glitch when currentError becomes undefined while the dialog is still closing
+  const [currentErrorDebounced] = useDebounce(currentErrorOriginal, 1000);
+  const currentError = currentErrorDebounced || currentErrorOriginal;
 
   return (
     <>
-      <Drawer size="md" isOpen={!!currentError} onClose={dismissError}>
-        <Flex direction="column" gap={4}>
-          <Flex direction="row" gap={1} align="center">
-            <Icon boxSize={5} as={FiAlertTriangle} color="red.500" />
-            <Heading size="subtitle.md">
-              Error: Failed to send transaction
-            </Heading>
-          </Flex>
-          <Flex direction="column" gap={2}>
-            <Heading size="label.md">From</Heading>
-            <AddressCopyButton
-              title="error"
-              address={currentError?.info?.from}
-            />
-          </Flex>
-          <Flex direction="column" gap={2}>
-            <Heading size="label.md">To</Heading>
-            <AddressCopyButton title="error" address={currentError?.info?.to} />
-          </Flex>
-          <Flex direction="column" gap={2}>
-            <Heading size="label.md">Chain / Chain ID</Heading>
-            <Text>
-              {currentError?.info?.network?.name} (
-              {currentError?.info?.network?.chainId})
-            </Text>
-          </Flex>
-          <Flex direction="column" gap={2}>
-            <Heading size="label.md">Root cause</Heading>
-            <Code px={4} py={2} borderRadius="md" whiteSpace="pre-wrap">
-              {currentError?.reason}
-            </Code>
-          </Flex>
-          <Divider my={2} borderColor="borderColor" />
-          <Heading size="subtitle.md">Need help with this error?</Heading>
-          <LinkButton
-            colorScheme="primary"
-            isExternal
-            noIcon
-            href="/support"
-            leftIcon={<Icon boxSize="1rem" as={FiHelpCircle} />}
-          >
-            Visit our support site
-          </LinkButton>
-          <Button
-            onClick={onCopy}
-            leftIcon={<Icon boxSize={3} as={hasCopied ? FiCheck : FiCopy} />}
-          >
-            {hasCopied ? "Copied!" : "Copy error to clipboard"}
-          </Button>
-          <Text fontStyle="italic">
-            Copying the error message will let you report this error with all
-            its details to our team.
-          </Text>
-        </Flex>
-      </Drawer>
+      <Dialog
+        // Use the original for opening/closing modal
+        open={!!currentErrorOriginal}
+        onOpenChange={(isOpen) => {
+          if (!isOpen) {
+            dismissError();
+          }
+        }}
+      >
+        <DialogContent
+          className="max-w-[480px] gap-0 p-0 z-[10001]"
+          dialogOverlayClassName="z-[10000]"
+        >
+          {/* min-w-0 is actually required here  */}
+          <div className="flex flex-col gap-6 p-6 min-w-0">
+            {/* Header */}
+            <DialogHeader>
+              <div className="flex justify-start mb-1">
+                <div className="p-2 bg-destructive text-destructive-text rounded-xl">
+                  <CircleAlertIcon className="size-6" />
+                </div>
+              </div>
+
+              <DialogTitle className="text-2xl font-semibold tracking-tight">
+                Failed to send transaction
+              </DialogTitle>
+            </DialogHeader>
+
+            {/* min-w-0 is actually required here  */}
+            <div className="flex flex-col gap-5 min-w-0">
+              {/* From */}
+              {currentError?.info?.from && (
+                <CopyAddressFormControl
+                  address={currentError.info.from}
+                  label="From"
+                />
+              )}
+
+              {/* To */}
+              {currentError?.info?.to && (
+                <CopyAddressFormControl
+                  address={currentError?.info?.to}
+                  label="To"
+                />
+              )}
+
+              {/* Chain */}
+              <div className="flex flex-col gap-1.5">
+                <p className="font-semibold text-sm">Chain</p>
+                <p className="text-muted-foreground border border-border px-3 py-2 rounded-lg text-sm">
+                  {currentError?.info?.network?.name} (
+                  {currentError?.info?.network?.chainId})
+                </p>
+              </div>
+
+              {/* Root Cause */}
+              {currentError?.reason && (
+                <div className="flex flex-col gap-1.5">
+                  <p className="font-semibold text-sm">Root Cause</p>
+                  <code className="font-medium max-h-[250px] overflow-auto font-mono p-3 whitespace-pre-wrap border border-border rounded-lg text-sm text-destructive-text">
+                    {currentError.reason}
+                  </code>
+                </div>
+              )}
+
+              <div className="flex gap-2 text-muted-foreground text-sm">
+                <InfoIcon className="size-4 shrink-0 mt-0.5" />
+                Copying the error message will let you report this error with
+                all its details to our team.
+              </div>
+            </div>
+          </div>
+
+          {/* Footer */}
+          <div className="border-t border-border flex justify-end p-6 gap-2">
+            {currentError?.reason && (
+              <CopyTextButton
+                copyIconPosition="left"
+                textToCopy={currentError.reason}
+                textToShow="Copy Error"
+                tooltip="Copy root cause of the error shown above"
+                className="py-2 text-sm px-4"
+              />
+            )}
+
+            <Button variant="primary" asChild className="py-2 text-sm">
+              <Link href="/support" target="_blank">
+                Visit support site
+              </Link>
+            </Button>
+          </div>
+        </DialogContent>
+      </Dialog>
 
       <ErrorContext.Provider
         value={{
@@ -121,6 +169,22 @@ export const ErrorProvider: ComponentWithChildren = ({ children }) => {
     </>
   );
 };
+
+function CopyAddressFormControl(props: { address: string; label: string }) {
+  return (
+    <div className="flex flex-col gap-1.5">
+      <p className="font-semibold text-sm">{props.label}</p>
+      <CopyTextButton
+        textToCopy={props.address}
+        textToShow={props.address}
+        copyIconPosition="left"
+        iconClassName="size-3"
+        tooltip="Copy Address"
+        className="py-2 px-3 text-sm justify-start font-mono text-muted-foreground"
+      />
+    </div>
+  );
+}
 
 export function useErrorHandler() {
   return useContext(ErrorContext);
