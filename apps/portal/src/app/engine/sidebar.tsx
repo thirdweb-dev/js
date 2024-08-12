66906dbef3e9754f663607e27e9c6c343a7bf518
Phillip Ho <arcoraven@gmail.com>
2024-08-12 10:04:29 -0700

    chore: Engine v2-beta in docs (#4049)

--- apps/portal/src/app/engine/sidebar.tsx
+++ apps/portal/src/app/engine/sidebar.tsx
@@ -1,106 +1,101 @@
-import { SideBar } from "@/components/Layouts/DocLayout";
+import type { SideBar } from "@/components/Layouts/DocLayout";
 
 const engineSlug = "/engine";
 
 export const sidebar: SideBar = {
-	name: "Engine",
-	links: [
-		{
-			name: "Overview",
-			href: "/engine",
-		},
-		{
-			name: "Get Started",
-			href: `${engineSlug}/get-started`,
-		},
-		{
-			name: "Production Checklist",
-			href: `${engineSlug}/production-checklist`,
-		},
-		{
-			name: "Self-Host",
-			href: `${engineSlug}/self-host`,
-		},
-		{
-			name: "Features",
-			links: [
-				{
-					name: "Backend Wallets",
-					href: `${engineSlug}/features/backend-wallets`,
-				},
-				{
-					name: "Contracts",
-					href: `${engineSlug}/features/contracts`,
-				},
-				{
-					name: "Admins",
-					href: `${engineSlug}/features/admins`,
-				},
-				{
-					name: "Access Tokens",
-					href: `${engineSlug}/features/access-tokens`,
-				},
-				{
-					name: "Webhooks",
-					href: `${engineSlug}/features/webhooks`,
-				},
-				{
-					name: "Cancelling Transactions",
-					href: `${engineSlug}/features/cancelling-transactions`,
-				},
-				{
-					name: "Preventing Duplicate Transactions",
-					href: `${engineSlug}/features/preventing-duplicate-transactions`,
-				},
-				{
-					name: "Account Abstraction",
-					href: `${engineSlug}/features/account-abstraction`,
-				},
-				{
-					name: "Relayers",
-					href: `${engineSlug}/features/relayers`,
-				},
-				{
-					name: "Gasless Transactions",
-					href: `${engineSlug}/features/gasless-transactions`,
-				},
-				{
-					name: "Contract Subscriptions",
-					href: `${engineSlug}/features/contract-subscriptions`,
-				},
-				{
-					name: "Security",
-					href: `${engineSlug}/features/security`,
-				},
-			],
-		},
-		{
-			name: "Guides",
-			links: [
-				{ name: "Airdrop NFTs", href: `${engineSlug}/guides/airdrop-nfts` },
-				{ name: "NFT Checkout", href: `${engineSlug}/guides/nft-checkout` },
-			],
-		},
-		{
-			name: "References",
-			links: [
-				{
-					name: "API Reference",
-					href: `${engineSlug}/references/api-reference`,
-				},
-				{
-					name: "Typescript SDK",
-					href: `${engineSlug}/references/typescript`,
-				},
-			],
-		},
-		{
-			name: "Security",
-			href: `${engineSlug}/security`,
-		},
-		{
-			name: "FAQ",
-			href: `${engineSlug}/faq`,
-		},
-	],
+  name: "Engine",
+  links: [
+    {
+      name: "Overview",
+      href: "/engine",
+    },
+    {
+      name: "Get Started",
+      href: `${engineSlug}/get-started`,
+    },
+    {
+      name: "Production Checklist",
+      href: `${engineSlug}/production-checklist`,
+    },
+    {
+      name: "Self-Host",
+      href: `${engineSlug}/self-host`,
+    },
+    {
+      name: "Features",
+      links: [
+        {
+          name: "Backend Wallets",
+          href: `${engineSlug}/features/backend-wallets`,
+        },
+        {
+          name: "Contracts",
+          href: `${engineSlug}/features/contracts`,
+        },
+        {
+          name: "Admins",
+          href: `${engineSlug}/features/admins`,
+        },
+        {
+          name: "Access Tokens",
+          href: `${engineSlug}/features/access-tokens`,
+        },
+        {
+          name: "Webhooks",
+          href: `${engineSlug}/features/webhooks`,
+        },
+        {
+          name: "Cancelling Transactions",
+          href: `${engineSlug}/features/cancelling-transactions`,
+        },
+        {
+          name: "Preventing Duplicate Transactions",
+          href: `${engineSlug}/features/preventing-duplicate-transactions`,
+        },
+        {
+          name: "Account Abstraction",
+          href: `${engineSlug}/features/account-abstraction`,
+        },
+        {
+          name: "Relayers",
+          href: `${engineSlug}/features/relayers`,
+        },
+        {
+          name: "Gasless Transactions",
+          href: `${engineSlug}/features/gasless-transactions`,
+        },
+        {
+          name: "Contract Subscriptions",
+          href: `${engineSlug}/features/contract-subscriptions`,
+        },
+        {
+          name: "Security",
+          href: `${engineSlug}/features/security`,
+        },
+      ],
+    },
+    {
+      name: "Guides",
+      links: [
+        { name: "Airdrop NFTs", href: `${engineSlug}/guides/airdrop-nfts` },
+        { name: "NFT Checkout", href: `${engineSlug}/guides/nft-checkout` },
+      ],
+    },
+    {
+      name: "API Reference",
+      href: "https://redocly.github.io/redoc/?url=https://demo.web3api.thirdweb.com/json",
+    },
+    {
+      name: "Typescript SDK",
+      href: `${engineSlug}/references/typescript`,
+    },
+    {
+      name: "Security",
+      href: `${engineSlug}/security`,
+    },
+    {
+      name: "FAQ",
+      href: `${engineSlug}/faq`,
+    },
+  ],
 };
