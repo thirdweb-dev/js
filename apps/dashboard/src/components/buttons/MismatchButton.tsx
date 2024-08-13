13cda91d103677949367c76d6b390db09e86b1a1
MananTank <manantankm@gmail.com>
2024-08-13 17:58:37 +0000

    Remove sign-in requirement for TransactionButton (#4086)
    ## Problem solved
    
    Short description of the bug fixed or feature added
    
    <!-- start pr-codex -->
    
    ---
    
    ## PR-Codex overview
    This PR updates the `MismatchButton` component in the dashboard by adding the `loginRequired` prop to `CustomConnectWallet`.
    
    ### Detailed summary
    - Added `loginRequired={false}` prop to `CustomConnectWallet` in `MismatchButton.tsx`
    
    > ✨ Ask PR-Codex anything about this PR by commenting with `/codex {your question}`
    
    <!-- end pr-codex -->

--- apps/dashboard/src/components/buttons/MismatchButton.tsx
+++ apps/dashboard/src/components/buttons/MismatchButton.tsx
@@ -97,6 +97,7 @@ export const MismatchButton = forwardRef<HTMLButtonElement, ButtonProps>(
       return (
         <CustomConnectWallet
           borderRadius="md"
+          loginRequired={false}
           colorScheme="primary"
           {...props}
         />
