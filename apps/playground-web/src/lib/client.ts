411326a253590a79076c6367507664ddc5d93716
Edward Sun <me@edwardsun.com>
2024-08-20 21:26:40 -0700

    use dev client ID env var

--- apps/playground-web/src/lib/client.ts
+++ apps/playground-web/src/lib/client.ts
@@ -4,6 +4,6 @@ export const THIRDWEB_CLIENT = createThirdwebClient(
   process.env.THIRDWEB_SECRET_KEY
     ? { secretKey: process.env.THIRDWEB_SECRET_KEY }
     : {
-        clientId: process.env.NEXT_PUBLIC_THIRDWEB_CLIENT_ID as string,
+        clientId: process.env.NEXT_PUBLIC_THIRDWEB_CLIENT_ID_DEV as string,
       },
 );
