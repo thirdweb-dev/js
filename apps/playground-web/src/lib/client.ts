e093474638ac36a75f9b4d30587e3d9228e35f3e
Isaac Dubuque <isaacdubuque@gmail.com>
2024-08-21 15:31:08 -0700

    adding demo app

--- apps/playground-web/src/lib/client.ts
+++ apps/playground-web/src/lib/client.ts
@@ -1,4 +1,5 @@
 import { createThirdwebClient } from "thirdweb";
+import { setThirdwebDomains } from "thirdweb/utils";
 
 export const THIRDWEB_CLIENT = createThirdwebClient(
   process.env.THIRDWEB_SECRET_KEY
@@ -7,3 +8,10 @@ export const THIRDWEB_CLIENT = createThirdwebClient(
         clientId: process.env.NEXT_PUBLIC_THIRDWEB_CLIENT_ID as string,
       },
 );
+
+
+setThirdwebDomains({
+  pay: "pay-server-s25c-feature-new-pay-gateway.chainsaw-dev.zeet.app",
+  rpc: "rpc.thirdweb-dev.com",
+});
+
