2973ba1f5463b451f309ebe268b046b2ab41807a
jnsdls <jonas.daniels@outlook.com>
2024-08-23 21:00:49 +0000

    [Update] Make bundle ID regex case-insensitive (#4268)
    BLOCKED BY: https://github.com/thirdweb-dev/api-server/pull/1019

--- apps/dashboard/src/utils/regex.ts
+++ apps/dashboard/src/utils/regex.ts
@@ -6,4 +6,5 @@ export const RE_DOMAIN = new RegExp(
   /(?:[a-z0-9](?:[a-z0-9-]{0,61}[a-z0-9])?\.)+[a-z0-9][a-z0-9-]{0,61}[a-z0-9]/,
 );
 
-export const RE_BUNDLE_ID = new RegExp(/^[a-z0-9.-]{3,64}$/);
+// bundle are case sensitive (/i) and can only contain letters, numbers, hyphens, and periods
+export const RE_BUNDLE_ID = new RegExp(/^[a-z0-9.-]{3,64}$/i);
