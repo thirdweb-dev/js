f383fc74842cfe642cd05d8f33b68139ba9f4f2b
kumaryash90 <kumaryashcse@gmail.com>
2024-09-04 23:55:46 +0000

    Resolve implementation from matic proxy implementation slot (#4393)
    ## Problem solved
    
    keccak256("matic.network.proxy.implementation") - used in polygon USDT proxy: https://polygonscan.com/address/0xc2132d05d31c914a87c6611c10748aeb04b58e8f#code
    
    <!-- start pr-codex -->
    
    ---
    
    ## PR-Codex overview
    This PR focuses on enhancing the Matic (Polygon) proxy contract implementation resolution in the codebase.
    
    ### Detailed summary
    - Added resolution for Matic proxy contract implementation slot
    - Updated test cases for Matic proxy contract implementation
    - Added support for Polygon chain in test setup
    - Defined Polygon chain constants and RPC URLs
    - Updated Ethereum and Polygon chain definitions
    
    > ✨ Ask PR-Codex anything about this PR by commenting with `/codex {your question}`
    
    <!-- end pr-codex -->

--- packages/thirdweb/test/src/chains.ts
+++ packages/thirdweb/test/src/chains.ts
@@ -1,5 +1,6 @@
 import { anvil } from "../../src/chains/chain-definitions/anvil.js";
 import { ethereum } from "../../src/chains/chain-definitions/ethereum.js";
+import { polygon } from "../../src/chains/chain-definitions/polygon.js";
 import { optimism } from "../../src/chains/chain-definitions/optimism.js";
 import { defineChain } from "../../src/chains/utils.js";
 
@@ -9,11 +10,18 @@ export const FORKED_ETHEREUM_RPC = `http://127.0.0.1:8645/${poolId}`;
 export const FORKED_ETHEREUM_WITH_MINING_RPC = `http://127.0.0.1:8646/${poolId}`;
 export const FORKED_OPTIMISM_RPC = `http://127.0.0.1:8647/${poolId}`;
 export const ANVIL_RPC = `http://127.0.0.1:8648/${poolId}`;
+export const FORKED_POLYGON_RPC = `http://127.0.0.1:8649/${poolId}`;
 
 export const FORKED_ETHEREUM_CHAIN = defineChain({
   ...ethereum,
   // override the rpc url
   rpc: FORKED_ETHEREUM_RPC,
+});
+
+export const FORKED_POLYGON_CHAIN = defineChain({
+  ...polygon,
+  // override the rpc url
+  rpc: FORKED_POLYGON_RPC,
 });
 
 export const FORKED_ETHEREUM_CHAIN_WITH_MINING = defineChain({
@@ -36,3 +44,4 @@ export const ANVIL_CHAIN = defineChain({
 
 export const FORK_BLOCK_NUMBER = 19139495n;
 export const OPTIMISM_FORK_BLOCK_NUMBER = 117525204n;
+export const POLYGON_FORK_BLOCK_NUMBER = 61430000n;
