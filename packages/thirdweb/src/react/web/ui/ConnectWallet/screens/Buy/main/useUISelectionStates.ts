d66b58ca739d2a0548771e4ef247decfedb7a0cd
Edward Sun <me@edwardsun.com>
2024-09-04 11:18:49 -0700

    Supported chain and tokens using overriden state

--- packages/thirdweb/src/react/web/ui/ConnectWallet/screens/Buy/main/useUISelectionStates.ts
+++ packages/thirdweb/src/react/web/ui/ConnectWallet/screens/Buy/main/useUISelectionStates.ts
@@ -1,78 +1,71 @@
 import { useState } from "react";
 import { polygon } from "../../../../../../../chains/chain-definitions/polygon.js";
 import type { Chain } from "../../../../../../../chains/types.js";
-import type {
-  FundWalletOptions,
-  PayUIOptions,
-} from "../../../../../../core/hooks/connection/ConnectButtonProps.js";
-import { useActiveWalletChain } from "../../../../../../core/hooks/wallets/useActiveWalletChain.js";
-import { useDebouncedValue } from "../../../../hooks/useDebouncedValue.js";
+import type { PayUIOptions } from "../../../../../../core/hooks/connection/ConnectButtonProps.js";
 import { type ERC20OrNativeToken, NATIVE_TOKEN } from "../../nativeToken.js";
 import {
   type CurrencyMeta,
   currencies,
   usdCurrency,
 } from "../fiat/currencies.js";
-import type { SupportedChainAndTokens } from "../swap/useSwapSupportedChains.js";
+
+type SupportedSourcesInputData = {
+  chain: Chain;
+  tokens: {
+    address: string;
+    buyWithCryptoEnabled: boolean;
+    buyWithFiatEnabled: boolean;
+    name: string;
+    symbol: string;
+  }[];
+};
 
 // handle states for token and chain selection
-
 export function useUISelectionStates(options: {
   payOptions: PayUIOptions;
-  supportedDestinations: SupportedChainAndTokens;
+  supportedSources: SupportedSourcesInputData[];
 }) {
-  const activeChain = useActiveWalletChain();
-  const { payOptions, supportedDestinations } = options;
-
-  // buy token amount ---------------------------------------------------------
-  // NOTE - for transaction / direct payment modes, the token amount is set when the user tap continue
-  const prefillBuy = (payOptions as FundWalletOptions)?.prefillBuy;
-  const initialTokenAmount = prefillBuy?.amount || "";
-
-  const [tokenAmount, setTokenAmount] = useState<string>(initialTokenAmount);
-  const deferredTokenAmount = useDebouncedValue(tokenAmount, 300);
+  const { payOptions, supportedSources } = options;
 
   // --------------------------------------------------------------------------
-
-  // Destination chain and token selection -----------------------------------
-  const [toChain, setToChain] = useState<Chain>(
-    // use prefill chain if available
-    prefillBuy?.chain ||
-      (payOptions.mode === "transaction" && payOptions.transaction?.chain) ||
-      (payOptions.mode === "direct_payment" && payOptions.paymentInfo?.chain) ||
-      // use active chain if its supported as destination
-      supportedDestinations.find((x) => x.chain.id === activeChain?.id)
-        ?.chain ||
-      // default to polygon
-      polygon,
-  );
-
-  const [toToken, setToToken] = useState<ERC20OrNativeToken>(
-    prefillBuy?.token ||
-      (payOptions.mode === "direct_payment" && payOptions.paymentInfo.token) ||
-      NATIVE_TOKEN,
-  );
-  // --------------------------------------------------------------------------
+  const firstSupportedSource = supportedSources?.length
+    ? supportedSources[0]
+    : undefined;
 
   // Source token and chain selection ---------------------------------------------------
-  const [fromChain, setFromChain] = useState<Chain>(
-    // use prefill chain if available
+  const [fromChain_, setFromChain] = useState<Chain>();
+
+  // use prefill chain if available
+  const fromChainDefault =
     (payOptions.buyWithCrypto !== false &&
       payOptions.buyWithCrypto?.prefillSource?.chain) ||
-      (payOptions.mode === "transaction" && payOptions.transaction?.chain) ||
-      (payOptions.mode === "direct_payment" && payOptions.paymentInfo?.chain) ||
-      // default to polygon
-      polygon,
-  );
+    (payOptions.mode === "transaction" && payOptions.transaction?.chain) ||
+    (payOptions.mode === "direct_payment" && payOptions.paymentInfo?.chain) ||
+    // default to polygon
+    polygon;
 
-  const [fromToken, setFromToken] = useState<ERC20OrNativeToken>(
-    // use prefill token if available
+  const fromChainFromApi = firstSupportedSource?.chain
+    ? firstSupportedSource.chain
+    : undefined;
+
+  const fromChain = fromChain_ || fromChainFromApi || fromChainDefault;
+
+  const [fromToken_, setFromToken] = useState<ERC20OrNativeToken>();
+
+  // use prefill token if available
+  const fromTokenDefault =
     (payOptions.buyWithCrypto !== false &&
       payOptions.buyWithCrypto?.prefillSource?.token) ||
-      (payOptions.mode === "direct_payment" && payOptions.paymentInfo.token) ||
-      // default to native token
-      NATIVE_TOKEN,
-  );
+    (payOptions.mode === "direct_payment" && payOptions.paymentInfo.token) ||
+    // default to native token
+    NATIVE_TOKEN;
+
+  const fromTokenFromApi = firstSupportedSource?.chain
+    ? ({ nativeToken: true } as ERC20OrNativeToken)
+    : undefined;
+
+  // supported tokens query in here
+  const fromToken = fromToken_ || fromTokenFromApi || fromTokenDefault;
 
   // --------------------------------------------------------------------------
   const devSpecifiedDefaultCurrency =
@@ -89,20 +82,11 @@ export function useUISelectionStates(options: {
   );
 
   return {
-    tokenAmount,
-    setTokenAmount,
-
-    toChain,
-    setToChain,
-    deferredTokenAmount,
     fromChain,
     setFromChain,
-    toToken,
-    setToToken,
     fromToken,
     setFromToken,
     selectedCurrency,
-
     setSelectedCurrency,
   };
 }
