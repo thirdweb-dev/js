a53e8ea13b6444064f5930bf1e6298d621d189ef
gregfromstl <gregfromstl@gmail.com>
2024-09-17 20:24:32 +0000

    [Wallet UI] Fix: Redirect properly from login page (#4646)
    ## Problem solved
    
    Short description of the bug fixed or feature added
    
    <!-- start pr-codex -->
    
    ---
    
    ## PR-Codex overview
    The focus of this PR is to enhance user authentication and ecosystem info handling in the wallet UI app.
    
    ### Detailed summary
    - Added user authentication check and redirect in `layout.tsx`
    - Improved error handling for fetching ecosystem info in `ecosystems.ts`
    - Refactored middleware logic in `middleware.ts` for better routing
    
    > ✨ Ask PR-Codex anything about this PR by commenting with `/codex {your question}`
    
    <!-- end pr-codex -->

--- apps/wallet-ui/src/middleware.ts
+++ apps/wallet-ui/src/middleware.ts
@@ -16,20 +16,14 @@ export const config = {
 
 const ROOT_DOMAIN = process.env.NEXT_PUBLIC_ROOT_DOMAIN;
 export default async function middleware(req: NextRequest) {
-  const url = req.nextUrl;
-
   // Get the request hostname (e.g. demo.thirdweb.com)
   const hostname = req.headers.get("host");
 
   const searchParams = req.nextUrl.searchParams.toString();
-  // Get the pathname of the request (e.g. /, /assets, /details)
-  const path = `${url.pathname}${searchParams.length > 0 ? `?${searchParams}` : ""}`;
 
   // keep root application at `/`
   if (hostname === ROOT_DOMAIN || hostname === null) {
-    return NextResponse.rewrite(
-      new URL(`/${path === "/" ? "" : path}`, req.url),
-    );
+    return NextResponse.next();
   }
 
   // rewrite everything else to `/[ecosystem]/... dynamic route
