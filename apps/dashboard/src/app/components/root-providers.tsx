e57a79bed6aa746891c7b5473c0ad24ead4e5220
Manan Tank <manantankm@gmail.com>
2024-09-18 22:46:24 +0530

    Add missing posthogCloud.init on app router

--- apps/dashboard/src/app/components/root-providers.tsx
+++ apps/dashboard/src/app/components/root-providers.tsx
@@ -1,9 +1,22 @@
 // app/providers.tsx
 "use client";
+import posthogCloud from "posthog-js";
 import posthog from "posthog-js-opensource";
 import { PostHogProvider as PHProvider } from "posthog-js-opensource/react";
 
 if (typeof window !== "undefined") {
+  posthogCloud.init(
+    process.env.NEXT_PUBLIC_POSTHOG_CLOUD_API_KEY ||
+      "phc_oXH0qpLTaotkIQP5MdaWhtoOXvh1Iba7yNSQrLgWbLN",
+    {
+      api_host: "https://pg.paper.xyz",
+      autocapture: false,
+      debug: false,
+      capture_pageview: false,
+      disable_session_recording: true,
+    },
+  );
+
   posthog.init(
     process.env.NEXT_PUBLIC_POSTHOG_API_KEY ||
       "phc_hKK4bo8cHZrKuAVXfXGpfNSLSJuucUnguAgt2j6dgSV",
