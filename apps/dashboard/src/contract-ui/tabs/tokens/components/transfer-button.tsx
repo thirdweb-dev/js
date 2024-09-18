e5393efcee65f432bf0c33bf5409a9fa8e81978c
kien-ngo <github@kien.ngo>
2024-09-18 10:18:49 +0000

    [Dashboard] erc20 transfer Drawer (#4643)
    chakra-ui Drawer -> Shadcn's Sheet
    
    <!-- start pr-codex -->
    
    ---
    
    ## PR-Codex overview
    This PR introduces a new `Sheet` component for modals and refactors the transfer token functionality to use the `Sheet` component.
    
    ### Detailed summary
    - Added `Sheet`, `SheetContent`, `SheetFooter`, `SheetHeader`, `SheetTitle`, `SheetTrigger` components
    - Refactored token transfer functionality to use the new `Sheet` component
    - Updated imports and dependencies to include new components and utilities
    
    > ✨ Ask PR-Codex anything about this PR by commenting with `/codex {your question}`
    
    <!-- end pr-codex -->

--- apps/dashboard/src/contract-ui/tabs/tokens/components/transfer-button.tsx
+++ apps/dashboard/src/contract-ui/tabs/tokens/components/transfer-button.tsx
@@ -1,49 +1,155 @@
-import { Icon, useDisclosure } from "@chakra-ui/react";
+import {
+  Sheet,
+  SheetContent,
+  SheetFooter,
+  SheetHeader,
+  SheetTitle,
+  SheetTrigger,
+} from "@/components/ui/sheet";
+import { FormControl, Icon, Input } from "@chakra-ui/react";
+import { TransactionButton } from "components/buttons/TransactionButton";
+import { SolidityInput } from "contract-ui/components/solidity-inputs";
+import { useTrack } from "hooks/analytics/useTrack";
+import { useTxNotifications } from "hooks/useTxNotifications";
+import { useState } from "react";
+import { useForm } from "react-hook-form";
 import { FiSend } from "react-icons/fi";
-import type { ThirdwebContract } from "thirdweb";
-import { balanceOf } from "thirdweb/extensions/erc20";
-import { useActiveAccount, useReadContract } from "thirdweb/react";
-import { Button, Drawer } from "tw-components";
-import { TokenTransferForm } from "./transfer-form";
+import { type ThirdwebContract, ZERO_ADDRESS } from "thirdweb";
+import * as ERC20Ext from "thirdweb/extensions/erc20";
+import {
+  useActiveAccount,
+  useReadContract,
+  useSendAndConfirmTransaction,
+} from "thirdweb/react";
+import {
+  Button,
+  FormErrorMessage,
+  FormHelperText,
+  FormLabel,
+} from "tw-components";
 
 interface TokenTransferButtonProps {
   contract: ThirdwebContract;
 }
 
+const TRANSFER_FORM_ID = "token-transfer-form";
+
 export const TokenTransferButton: React.FC<TokenTransferButtonProps> = ({
   contract,
   ...restButtonProps
 }) => {
-  const { isOpen, onOpen, onClose } = useDisclosure();
   const address = useActiveAccount()?.address;
-  const tokenBalanceQuery = useReadContract(balanceOf, {
+  const tokenBalanceQuery = useReadContract(ERC20Ext.balanceOf, {
     contract,
     address: address || "",
     queryOptions: { enabled: !!address },
   });
-
+  const trackEvent = useTrack();
+  const form = useForm({ defaultValues: { amount: "0", to: "" } });
   const hasBalance = tokenBalanceQuery.data && tokenBalanceQuery.data > 0n;
+  const { onSuccess, onError } = useTxNotifications(
+    "Successfully transferred tokens",
+    "Failed to transfer tokens",
+    contract,
+  );
+
+  const decimalsQuery = useReadContract(ERC20Ext.decimals, { contract });
+  const sendConfirmation = useSendAndConfirmTransaction();
+  const [open, setOpen] = useState(false);
 
   return (
-    <>
-      <Drawer
-        allowPinchZoom
-        preserveScrollBarGap
-        size="lg"
-        onClose={onClose}
-        isOpen={isOpen}
-      >
-        <TokenTransferForm contract={contract} />
-      </Drawer>
-      <Button
-        colorScheme="primary"
-        leftIcon={<Icon as={FiSend} />}
-        {...restButtonProps}
-        onClick={onOpen}
-        isDisabled={!hasBalance}
-      >
-        Transfer
-      </Button>
-    </>
+    <Sheet open={open} onOpenChange={setOpen}>
+      <SheetTrigger asChild>
+        <Button
+          colorScheme="primary"
+          leftIcon={<Icon as={FiSend} />}
+          {...restButtonProps}
+          isDisabled={!hasBalance}
+        >
+          Transfer
+        </Button>
+      </SheetTrigger>
+      <SheetContent className="z-[10000]">
+        <SheetHeader>
+          <SheetTitle>Transfer tokens</SheetTitle>
+        </SheetHeader>
+        <form className="mt-10">
+          <div className="flex flex-col gap-6">
+            <FormControl isRequired isInvalid={!!form.formState.errors.to}>
+              <FormLabel>To Address</FormLabel>
+              <SolidityInput
+                formContext={form}
+                solidityType="address"
+                placeholder={ZERO_ADDRESS}
+                {...form.register("to")}
+              />
+              <FormHelperText>Enter the address to transfer to.</FormHelperText>
+              <FormErrorMessage>
+                {form.formState.errors.to?.message}
+              </FormErrorMessage>
+            </FormControl>
+            <FormControl isRequired isInvalid={!!form.formState.errors.amount}>
+              <FormLabel>Amount</FormLabel>
+              <Input
+                type="text"
+                pattern={`^\\d+(\\.\\d{1,${decimalsQuery.data || 18}})?$`}
+                {...form.register("amount")}
+              />
+              <FormHelperText>
+                How many would you like to transfer?
+              </FormHelperText>
+              <FormErrorMessage>
+                {form.formState.errors.amount?.message}
+              </FormErrorMessage>
+            </FormControl>
+          </div>
+        </form>
+        <SheetFooter className="mt-10">
+          <TransactionButton
+            transactionCount={1}
+            form={TRANSFER_FORM_ID}
+            isLoading={sendConfirmation.isPending}
+            type="submit"
+            colorScheme="primary"
+            isDisabled={!form.formState.isDirty}
+            onClick={form.handleSubmit((d) => {
+              trackEvent({
+                category: "token",
+                action: "transfer",
+                label: "attempt",
+              });
+              const transaction = ERC20Ext.transfer({
+                contract,
+                amount: d.amount,
+                to: d.to,
+              });
+              sendConfirmation.mutate(transaction, {
+                onSuccess: () => {
+                  trackEvent({
+                    category: "token",
+                    action: "transfer",
+                    label: "success",
+                  });
+                  form.reset({ amount: "0", to: "" });
+                  onSuccess();
+                  setOpen(false);
+                },
+                onError: (error) => {
+                  trackEvent({
+                    category: "token",
+                    action: "transfer",
+                    label: "error",
+                    error,
+                  });
+                  onError(error);
+                },
+              });
+            })}
+          >
+            Transfer Tokens
+          </TransactionButton>
+        </SheetFooter>
+      </SheetContent>
+    </Sheet>
   );
 };
