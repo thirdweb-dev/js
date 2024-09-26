d0616c4486b62d8c39e496cb61698a1497f359d6
kumaryash90 <kumaryashcse@gmail.com>
2024-09-26 22:13:03 +0000

    Fix Cronos zkevm deployments (#4815)
    ## Problem solved
    
    Short description of the bug fixed or feature added
    
    <!-- start pr-codex -->
    
    ---
    
    ## PR-Codex overview
    This PR adds support for the `cronos zkevm` by updating the `isZkSyncChain` check to include additional chain IDs for both the testnet and mainnet.
    
    ### Detailed summary
    - Updated the `isZkSyncChain` function in the file `packages/thirdweb/src/utils/any-evm/zksync/isZkSyncChain.ts`.
    - Added chain ID `282` for `cronos zkevm testnet`.
    - Added chain ID `388` for `cronos zkevm mainnet`.
    
    > ✨ Ask PR-Codex anything about this PR by commenting with `/codex {your question}`
    
    <!-- end pr-codex -->

---
+++ .changeset/cold-dolphins-know.md
@@ -0,0 +1,5 @@
+---
+"thirdweb": patch
+---
+
+Add cronos zkevm to isZkSyncChain check
--- packages/thirdweb/src/utils/any-evm/zksync/isZkSyncChain.ts
+++ packages/thirdweb/src/utils/any-evm/zksync/isZkSyncChain.ts
@@ -5,6 +5,8 @@ export function isZkSyncChain(chain: Chain) {
     chain.id === 324 ||
     chain.id === 300 ||
     chain.id === 302 ||
-    chain.id === 11124
+    chain.id === 11124 ||
+    chain.id === 282 || // cronos zkevm testnet
+    chain.id === 388 // cronos zkevm mainnet
   );
 }
