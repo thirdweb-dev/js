c8aa87437eedd98a0854ac379ac22b07c2b8dd3f
Kien Ngo <github@kien.ngo>
2024-10-24 16:49:38 +0700

    Update

--- packages/thirdweb/src/extensions/vote/vote.test.ts
+++ packages/thirdweb/src/extensions/vote/vote.test.ts
@@ -3,22 +3,22 @@ import { ANVIL_CHAIN } from "~test/chains.js";
 import { TEST_CONTRACT_URI } from "~test/ipfs-uris.js";
 import { TEST_CLIENT } from "~test/test-clients.js";
 import { TEST_ACCOUNT_C } from "~test/test-wallets.js";
-import { getContract } from "../../../contract/contract.js";
-import { delegate } from "../../../extensions/erc20/__generated__/IVotes/write/delegate.js";
-import { mintTo } from "../../../extensions/erc20/write/mintTo.js";
-import { deployERC20Contract } from "../../../extensions/prebuilts/deploy-erc20.js";
-import { deployVoteContract } from "../../../extensions/prebuilts/deploy-vote.js";
-import { sendAndConfirmTransaction } from "../../../transaction/actions/send-and-confirm-transaction.js";
-import { propose } from "../__generated__/Vote/write/propose.js";
-import { getAll } from "./getAll.js";
-import { proposalExists } from "./proposalExists.js";
+import { getContract } from "../../contract/contract.js";
+import { sendAndConfirmTransaction } from "../../transaction/actions/send-and-confirm-transaction.js";
+import { delegate } from "../erc20/__generated__/IVotes/write/delegate.js";
+import { mintTo } from "../erc20/write/mintTo.js";
+import { deployERC20Contract } from "../prebuilts/deploy-erc20.js";
+import { deployVoteContract } from "../prebuilts/deploy-vote.js";
+import { propose } from "./__generated__/Vote/write/propose.js";
+import { getAll } from "./read/getAll.js";
+import { proposalExists } from "./read/proposalExists.js";
 
 const account = TEST_ACCOUNT_C;
 const client = TEST_CLIENT;
 const chain = ANVIL_CHAIN;
 
 describe.runIf(process.env.TW_SECRET_KEY)("proposal exists", () => {
-  it("should return false if Vote doesn't have any proposal", async () => {
+  it("`proposalExists` and `propose` should work", async () => {
     const tokenAddress = await deployERC20Contract({
       client: TEST_CLIENT,
       chain: ANVIL_CHAIN,
@@ -42,47 +42,16 @@ describe.runIf(process.env.TW_SECRET_KEY)("proposal exists", () => {
         minVoteQuorumRequiredPercent: 51,
       },
     });
-
-    const contract = getContract({
+    const voteContract = getContract({
       address,
       chain,
       client,
     });
-
-    const result = await proposalExists({ contract, proposalId: 0n });
+    const result = await proposalExists({
+      contract: voteContract,
+      proposalId: 0n,
+    });
     expect(result).toBe(false);
-  });
-
-  it("should return true if Vote has the proposal (id)", async () => {
-    const tokenAddress = await deployERC20Contract({
-      client: TEST_CLIENT,
-      chain: ANVIL_CHAIN,
-      account,
-      type: "TokenERC20",
-      params: {
-        name: "Token",
-        contractURI: TEST_CONTRACT_URI,
-      },
-    });
-    const address = await deployVoteContract({
-      account,
-      client: TEST_CLIENT,
-      chain: ANVIL_CHAIN,
-      params: {
-        name: "",
-        contractURI: TEST_CONTRACT_URI,
-        tokenAddress: tokenAddress,
-        initialProposalThreshold: "0.5",
-        initialVotingPeriod: 10,
-        minVoteQuorumRequiredPercent: 51,
-      },
-    });
-
-    const contract = getContract({
-      address,
-      chain,
-      client,
-    });
 
     const tokenContract = getContract({
       address: tokenAddress,
@@ -105,19 +74,19 @@ describe.runIf(process.env.TW_SECRET_KEY)("proposal exists", () => {
 
     // step 3: create a proposal
     const transaction = propose({
-      contract,
+      contract: voteContract,
       description: "first proposal",
-      targets: [contract.address],
+      targets: [voteContract.address],
       values: [0n],
       calldatas: ["0x"],
     });
     await sendAndConfirmTransaction({ transaction, account });
-    const allProposals = await getAll({ contract });
+    const allProposals = await getAll({ contract: voteContract });
     expect(allProposals.length).toBe(1);
-    const result = await proposalExists({
-      contract,
+    const exists = await proposalExists({
+      contract: voteContract,
       proposalId: allProposals[0]?.proposalId || -1n,
     });
-    expect(result).toBe(true);
+    expect(exists).toBe(true);
   });
 });
