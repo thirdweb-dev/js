6c37aee81840bab3b301a7b528901b46db1d121e
Joaquim Verges <joaquim.verges@gmail.com>
2024-10-29 12:17:00 +1300

    feat: improve ecosystem smart account handling

---
+++ .changeset/wicked-gifts-joke.md
@@ -0,0 +1,5 @@
+---
+"thirdweb": patch
+---
+
+Better handling of ecosystem smart accounts
--- packages/thirdweb/src/wallets/ecosystem/get-ecosystem-wallet-auth-options.ts
+++ packages/thirdweb/src/wallets/ecosystem/get-ecosystem-wallet-auth-options.ts
@@ -1,8 +1,13 @@
 import { getThirdwebBaseUrl } from "../../utils/domains.js";
+import { withCache } from "../../utils/promise/withCache.js";
 import type { AuthOption } from "../types.js";
 import type { EcosystemWalletId } from "../wallet-types.js";
 
 export type EcosystemOptions = {
+  name: string;
+  imageUrl?: string;
+  slug: string;
+  homepage?: string;
   authOptions: AuthOption[];
   smartAccountOptions: SmartAccountOptions;
 };
@@ -19,32 +24,40 @@ type SmartAccountOptions = {
  * @returns {AuthOption[] | undefined} The auth options for the ecosystem wallet.
  * @internal
  */
-export async function getEcosystemOptions(
+export async function getEcosystemInfo(
   walletId: EcosystemWalletId,
-): Promise<EcosystemOptions | null> {
-  const res = await fetch(
-    `${getThirdwebBaseUrl("inAppWallet")}/api/2024-05-05/ecosystem-wallet`,
+): Promise<EcosystemOptions> {
+  return withCache(
+    async () => {
+      const res = await fetch(
+        `${getThirdwebBaseUrl("inAppWallet")}/api/2024-05-05/ecosystem-wallet`,
+        {
+          headers: {
+            "x-ecosystem-id": walletId,
+          },
+        },
+      );
+
+      const data = await res.json();
+
+      if (!data || data.code === "UNAUTHORIZED") {
+        throw new Error(
+          data.message ||
+            `Could not find ecosystem wallet with id ${walletId}, please check your ecosystem wallet configuration.`,
+        );
+      }
+
+      // siwe is the auth option in the backend, but we want to use wallet as the auth option in the frontend
+      if (data.authOptions?.includes("siwe")) {
+        data.authOptions = data.authOptions.filter((o: string) => o !== "siwe");
+        data.authOptions.push("wallet");
+      }
+
+      return data;
+    },
     {
-      headers: {
-        "x-ecosystem-id": walletId,
-      },
+      cacheKey: `ecosystem-wallet-options-${walletId}`,
+      cacheTime: 1000 * 60 * 5, // 5 mins
     },
   );
-
-  const data = await res.json();
-
-  if (!data || data.code === "UNAUTHORIZED") {
-    throw new Error(
-      data.message ||
-        `Could not find ecosystem wallet with id ${walletId}, please check your ecosystem wallet configuration.`,
-    );
-  }
-
-  // siwe is the auth option in the backend, but we want to use wallet as the auth option in the frontend
-  if (data.authOptions?.includes("siwe")) {
-    data.authOptions = data.authOptions.filter((o: string) => o !== "siwe");
-    data.authOptions.push("wallet");
-  }
-
-  return data ?? null;
 }
