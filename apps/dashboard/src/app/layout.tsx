3658b3fecea7136cd9366f11c13736a5efa11a9b
MananTank <manantankm@gmail.com>
2024-10-30 16:35:51 +0000

    Fix theming class hydration warning on html element (#5233)
    This is the correct way to fix this
    https://github.com/pacocoursey/next-themes?tab=readme-ov-file#with-app
    
    ` suppressHydrationWarning` property only applies one level deep, so it won't block hydration warnings on other child elements of `<html>`

--- apps/dashboard/src/app/layout.tsx
+++ apps/dashboard/src/app/layout.tsx
@@ -55,7 +55,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <html lang="en">
+    <html lang="en" suppressHydrationWarning>
       <head>
         <PlausibleProvider
           domain="thirdweb.com"
--- apps/playground-web/src/app/layout.tsx
+++ apps/playground-web/src/app/layout.tsx
@@ -32,7 +32,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <html lang="en">
+    <html lang="en" suppressHydrationWarning>
       <head>
         <Script
           src="https://thirdweb.com/js/pl.js"
