e739552829fdc2648f610a61583df53e24917925
Joaquim Verges <joaquim.verges@gmail.com>
2024-10-30 14:14:46 +1300

    Revert "[Dashboard] Feature: Adds analytics to in-app-wallets page (#5186) (#5227)"
    This reverts commit 6e87a866b2d0b284539d4a4e8b37da4f3bd067c8.

--- apps/dashboard/src/app/(dashboard)/dashboard/connect/in-app-wallets/layout.tsx
+++ apps/dashboard/src/app/(dashboard)/dashboard/connect/in-app-wallets/layout.tsx
@@ -1,3 +1,4 @@
+import { AnalyticsCallout } from "../../../../team/[team_slug]/[project_slug]/connect/in-app-wallets/_components/AnalyticsCallout";
 import { InAppWaletFooterSection } from "../../../../team/[team_slug]/[project_slug]/connect/in-app-wallets/_components/footer";
 
 export default function Layout(props: {
@@ -8,6 +9,7 @@ export default function Layout(props: {
       {props.children}
       <div className="h-16" />
       {/* Footer */}
+      <AnalyticsCallout trackingCategory="embedded-wallet" />
       <div className="h-5" />
       <InAppWaletFooterSection trackingCategory="embedded-wallet" />
     </div>
