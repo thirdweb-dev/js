70ed73e7050ab6fc0975b1d202956367b0e1a0c9
jnsdls <jonas.daniels@outlook.com>
2024-11-05 01:03:05 +0000

    add new framer rewrite paths and organize with comments (#5299)

--- apps/dashboard/framer-rewrites.js
+++ apps/dashboard/framer-rewrites.js
@@ -1,10 +1,20 @@
 // add framer paths here
 module.exports = [
+  // -- home
   "/",
+  // -- product landing pages --
+  // -- connect
   "/connect/sign-in",
+  // -- contracts
   "/contracts/modular-contracts",
-  "/unlimited-wallets",
+  "/contracts/explore",
+  "/contracts/deployment-tool",
+  // -- engine
   "/engine",
+  // -- solutions pages --
   "/solutions/gaming",
   "/solutions/consumer-apps",
+  "/solutions/defi",
+  // -- campaigns --
+  "/unlimited-wallets",
 ];
