09763b0590d93b412553afc873e8a170cf68753f
samina <samina@thirdweb.com>
2024-12-12 21:26:53 -0700

    fixed camel casing on svg

--- apps/portal/src/icons/products/nebula/NebulaIcon.tsx
+++ apps/portal/src/icons/products/nebula/NebulaIcon.tsx
@@ -12,7 +12,7 @@ export function NebulaIcon(props: { className?: string }) {
       <path
         d="M8.32977 8.52315C10.2028 6.65012 12.3294 5.4367 14.224 4.96306C16.1354 4.4852 17.6707 4.78261 18.5828 5.69472C19.4949 6.60682 19.7923 8.14213 19.3145 10.0536C18.8408 11.9481 17.6274 14.0747 15.7544 15.9478C13.8814 17.8208 11.7547 19.0342 9.86018 19.5078C7.94875 19.9857 6.41345 19.6883 5.50134 18.7762C4.58924 17.8641 4.29183 16.3288 4.76969 14.4174C5.24332 12.5228 6.45674 10.3962 8.32977 8.52315Z"
         stroke="white"
-        stroke-width="1.5"
+        strokeWidth="1.5"
       />
       <circle
         cx="5.55863"
@@ -21,7 +21,7 @@ export function NebulaIcon(props: { className?: string }) {
         transform="rotate(-90 5.55863 12.2355)"
         fill="#1E1E1E"
         stroke="white"
-        stroke-width="1.5"
+        strokeWidth="1.5"
       />
       <circle
         cx="18.5251"
@@ -30,7 +30,7 @@ export function NebulaIcon(props: { className?: string }) {
         transform="rotate(-90 18.5251 12.2355)"
         fill="#1E1E1E"
         stroke="white"
-        stroke-width="1.5"
+        strokeWidth="1.5"
       />
       <circle
         cx="12.0421"
