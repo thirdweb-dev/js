80faf3479e3cf9704557ef4fbf3a1d094fd1aae3
Joaquim Verges <joaquim.verges@gmail.com>
2024-12-19 09:57:46 +1300

    feat: Reorganize portal landing page sections (#5745)

--- apps/portal/src/components/Document/Cards/ConnectCard.tsx
+++ apps/portal/src/components/Document/Cards/ConnectCard.tsx
@@ -5,9 +5,14 @@ export function ConnectCard(props: {
   title: string;
   href: string;
   iconUrl: string;
+  isExternal?: boolean;
 }) {
   return (
-    <Link href={props.href} target="_blank" className="flex cursor-default">
+    <Link
+      href={props.href}
+      target={props.isExternal ? "_blank" : undefined}
+      className="flex cursor-default"
+    >
       <article className="group/article flex w-full items-center overflow-hidden rounded-lg border bg-b-800 transition-colors duration-300 hover:border-accent-500 hover:bg-accent-900">
         <div className="flex w-full items-center gap-4 p-4">
           <Image src={props.iconUrl} width={40} height={40} alt="" />
