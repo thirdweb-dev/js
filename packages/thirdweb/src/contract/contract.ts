c4fafc5128abbf40d6cff2bae3b6ab850634a5cf
greg <gregfromstl@gmail.com>
2025-01-03 12:28:19 -1000

    Update contract.ts
    Signed-off-by: greg <gregfromstl@gmail.com>

--- packages/thirdweb/src/contract/contract.ts
+++ packages/thirdweb/src/contract/contract.ts
@@ -11,7 +11,7 @@ export type ContractOptions<
   address extends string = string,
 > = {
   client: ThirdwebClient;
-  address: Address;
+  address: address;
   chain: Chain;
   readonly abi?: abi;
 };
