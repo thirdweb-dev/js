65eac4e870892bfbfe116ea05fa45b5376608d81
jcheese1 <jcheese@treasure.lol>
2025-02-07 21:40:39 +0900

    remove duplicate

--- packages/thirdweb/src/utils/any-evm/zksync/isZkSyncChain.ts
+++ packages/thirdweb/src/utils/any-evm/zksync/isZkSyncChain.ts
@@ -27,8 +27,7 @@ export async function isZkSyncChain(chain: Chain) {
     chain.id === 2741 ||
     chain.id === 240 ||
     chain.id === 555271 ||
-    chain.id === 61166 ||
-    chain.id === 978658
+    chain.id === 61166
   ) {
     return true;
   }
