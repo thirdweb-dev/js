c0bbc21a84782317c12f6c8a5988284427095463
kumaryash90 <kumaryashcse@gmail.com>
2025-03-03 22:09:24 +0000

    Update deploy configs (#6299)
    TOOL-3478
    
    <!--
    
    ## title your PR with this format: "[SDK/Dashboard/Portal] Feature/Fix: Concise title for the changes"
    
    If you did not copy the branch name from Linear, paste the issue tag here (format is TEAM-0000):
    
    ## Notes for the reviewer
    
    Anything important to call out? Be sure to also clarify these in your comments.
    
    ## How to test
    
    Unit tests, playground, etc.
    
    -->
    
    <!-- start pr-codex -->
    
    ---
    
    ## PR-Codex overview
    This PR focuses on updating the platform fee configuration in the contract deployment process, introducing a new fee constant, and modifying the logic to handle different fee scenarios based on the publisher address.
    
    ### Detailed summary
    - Added `THIRDWEB_PUBLISHER_ADDRESS` constant in `addresses.ts`.
    - Introduced `DEFAULT_FEE_BPS_NEW` constant in `addresses.ts`.
    - Updated `PlatformFeeFieldset` to destructure `isMarketplace`.
    - Modified `CustomContractForm` to conditionally use `DEFAULT_FEE_BPS_NEW` based on the publisher address.
    - Adjusted imports in `custom-contract.tsx` to include new constants.
    
    > ✨ Ask PR-Codex anything about this PR by commenting with `/codex {your question}`
    
    <!-- end pr-codex -->

--- apps/dashboard/src/components/contract-functions/contract-function.tsx
+++ apps/dashboard/src/components/contract-functions/contract-function.tsx
@@ -285,6 +285,7 @@ export const ContractFunctionsPanel: React.FC<ContractFunctionsPanelProps> = ({
   twAccount,
 }) => {
   // TODO: clean this up
+
   const functionsWithExtension = useMemo(() => {
     const allFunctions = fnsOrEvents.filter((f) => f.type === "function");
     const results: ExtensionFunctions[] = [];
