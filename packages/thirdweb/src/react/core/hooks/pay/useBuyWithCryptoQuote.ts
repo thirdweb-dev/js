ce9b42bee4124d47db5ad5007758318f36e0b2f2
gregfromstl <gregfromstl@gmail.com>
2025-03-07 13:28:44 -0800

    feat: adds quote refetch interval

--- packages/thirdweb/src/react/core/hooks/pay/useBuyWithCryptoQuote.ts
+++ packages/thirdweb/src/react/core/hooks/pay/useBuyWithCryptoQuote.ts
@@ -80,6 +80,7 @@ export function useBuyWithCryptoQuote(
   return useQuery({
     ...queryParams,
     queryKey: ["buyWithCryptoQuote", params],
+    refetchInterval: 30_000,
     queryFn: () => {
       if (!params) {
         throw new Error("Swap params are required");
