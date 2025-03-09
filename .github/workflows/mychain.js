31a0e60551fd951e2379799e401bd4c727b660bb
BOOM TOKEN <marinachris112@outlook.com>
2025-03-09 18:27:10 +0100

    Update and rename RPC to mychain.js
    Signed-off-by: BOOM TOKEN <marinachris112@outlook.com>

--- .github/workflows/mychain.js
+++ .github/workflows/mychain.js
@@ -1,20 +1,11 @@
 import { defineChain } from "@thirdweb-dev/sdk";
 
 const myChain = defineChain({
-  id: 56, // Binance Smart Chain Mainnet ID
+  id: 56,
   name: "Binance Smart Chain Virtual",
   rpc: ["https://virtual.binance.rpc.tenderly.co/add95b93-f1d4-4d71-a63d-0e89712c101d"],
-  nativeCurrency: {
-    name: "BNB",
-    symbol: "BNB",
-    decimals: 18,
-  },
-  blockExplorers: [
-    {
-      name: "BscScan",
-      url: "https://bscscan.com",
-    },
-  ],
+  nativeCurrency: { name: "BNB", symbol: "BNB", decimals: 18 },
+  blockExplorers: [{ name: "BscScan", url: "https://bscscan.com" }],
 });
 
 export default myChain;
