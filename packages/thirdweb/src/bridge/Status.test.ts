4846bae9e396ffed76daddf75f9f5c5995d948c1
gregfromstl <gregfromstl@gmail.com>
2025-03-12 17:35:28 -0700

    fix tests

--- packages/thirdweb/src/bridge/Status.test.ts
+++ packages/thirdweb/src/bridge/Status.test.ts
@@ -3,16 +3,36 @@ import { TEST_CLIENT } from "~test/test-clients.js";
 import { status } from "./Status.js";
 
 describe("Bridge.status", () => {
-  it("should handle invalid transaction hash without throwing", async () => {
+  it("should handle successful status", async () => {
     const result = await status({
       transactionHash:
         "0xe199ef82a0b6215221536e18ec512813c1aa10b4f5ed0d4dfdfcd703578da56d",
-      chainId: 1,
+      chainId: 8453,
       client: TEST_CLIENT,
     });
 
     expect(result).toBeDefined();
-    expect(result.status).toBe("failed");
-    expect(result.transactions).toBeDefined();
+    expect(result.status).toBe("completed");
+    expect(result).toMatchInlineSnapshot(`
+      {
+        "destinationAmount": 188625148000000n,
+        "destinationChainId": 2741,
+        "destinationTokenAddress": "0xEeeeeEeeeEeEeeEeEeEeeEEEeeeeEeeeeeeeEEeE",
+        "originAmount": 200000000000000n,
+        "originChainId": 8453,
+        "originTokenAddress": "0xEeeeeEeeeEeEeeEeEeEeeEEEeeeeEeeeeeeeEEeE",
+        "status": "completed",
+        "transactions": [
+          {
+            "chainId": 8453,
+            "transactionHash": "0xe199ef82a0b6215221536e18ec512813c1aa10b4f5ed0d4dfdfcd703578da56d",
+          },
+          {
+            "chainId": 2741,
+            "transactionHash": "0xa70a82f42330f54be95a542e1fcfe6ed2dd9f07fb8c82ae67afb4342319f7433",
+          },
+        ],
+      }
+    `);
   });
 });
