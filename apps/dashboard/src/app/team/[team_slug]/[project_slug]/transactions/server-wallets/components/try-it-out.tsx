1d531b6d595946358565ae53d1ab1674a4ed6d9a
Joaquim Verges <joaquim.verges@gmail.com>
2025-04-09 13:58:44 +1200

    UI polish

---
+++ apps/dashboard/src/app/team/[team_slug]/[project_slug]/transactions/server-wallets/components/try-it-out.tsx
@@ -0,0 +1,49 @@
+import { Button } from "@/components/ui/button";
+import { CodeServer } from "../../../../../../../@/components/ui/code/code.server";
+
+export function TryItOut() {
+  return (
+    <div className="flex flex-col gap-6 overflow-hidden rounded-lg border border-border bg-card p-6">
+      <div className="flex flex-row items-center gap-4">
+        <div className="flex flex-1 flex-col gap-4 rounded-lg rounded-b-none lg:flex-row lg:justify-between">
+          <div>
+            <h2 className="font-semibold text-xl tracking-tight">Usage</h2>
+            <p className="text-muted-foreground text-sm">
+              Simple http API to send transactions to the blockchain
+            </p>
+          </div>
+        </div>
+        <Button variant={"primary"}>View API reference</Button>
+      </div>
+      <div>
+        <CodeServer
+          lang="ts"
+          code={typescriptCodeExample()}
+          className="bg-background"
+        />
+      </div>
+    </div>
+  );
+}
+const typescriptCodeExample = () => `\
+const response = fetch("https://wallet.thirdweb.com/v1/account/send-transaction", {
+  method: "POST",
+  headers: {
+    "Content-Type": "application/json",
+    "x-secret-key": <your-project-secret-key>,
+  },
+  body: JSON.stringify({
+    "executionOptions": {
+        "type": "AA",
+        "signerAddress": <your-server-wallet-address>
+    },
+    "transactionParams": [
+      {
+        "to": "0xeb0effdfb4dc5b3d5d3ac6ce29f3ed213e95d675",
+        "value": "0"
+      }
+    ],
+    "vaultAccessToken": <your-wallet-access-token>,
+    "chainId": "84532"
+  }),
+});`;
--- apps/dashboard/src/app/team/[team_slug]/[project_slug]/transactions/server-wallets/wallet-table/wallet-table.tsx
+++ apps/dashboard/src/app/team/[team_slug]/[project_slug]/transactions/server-wallets/wallet-table/wallet-table.tsx
@@ -1,6 +1,27 @@
+import { TryItOut } from "../components/try-it-out";
 import type { Wallet } from "./types";
 import { ServerWalletsTableUI } from "./wallet-table-ui";
 
-export function ServerWalletsTable({ wallets }: { wallets: Wallet[] }) {
-  return <ServerWalletsTableUI wallets={wallets} />;
+export function ServerWalletsTable({
+  wallets,
+  projectId,
+  teamId,
+  managementAccessToken,
+}: {
+  wallets: Wallet[];
+  projectId: string;
+  teamId: string;
+  managementAccessToken: string | undefined;
+}) {
+  return (
+    <div className="flex flex-col gap-8">
+      <ServerWalletsTableUI
+        wallets={wallets}
+        projectId={projectId}
+        teamId={teamId}
+        managementAccessToken={managementAccessToken}
+      />
+      <TryItOut />
+    </div>
+  );
 }
