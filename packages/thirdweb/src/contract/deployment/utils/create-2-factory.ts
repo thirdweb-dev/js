66a50e6d02eb7ac5686ba539833b32661c2b1c91
Yash <kumaryashcse@gmail.com>
2025-04-23 16:26:04 +0530

    Custom factory addr for Chiliz mainnet

--- packages/thirdweb/src/contract/deployment/utils/create-2-factory.ts
+++ packages/thirdweb/src/contract/deployment/utils/create-2-factory.ts
@@ -322,6 +322,7 @@ const CUSTOM_GAS_FOR_CHAIN: Record<string, CustomChain> = {
 const FACTORIES: Record<string, string> = {
   "420120000": COMMON_FACTORY_ADDRESS,
   "420120001": COMMON_FACTORY_ADDRESS,
+  "88888": "0xc501b9abf5540de1dd24f66633b1ecf35ff7101f", // EIP155 is enforced, but the check fails, hence we hardcode the address here instead of computing dynamically
 };
 
 const CUSTOM_GAS_BINS = [
