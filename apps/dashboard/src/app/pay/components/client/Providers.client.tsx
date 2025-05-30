fff8016b614c1d7f3b80d67590e249c30760e56d
gregfromstl <gregfromstl@gmail.com>
2025-05-30 13:01:35 -0700

    Adds posthog to payment links

--- apps/dashboard/src/app/pay/components/client/Providers.client.tsx
+++ apps/dashboard/src/app/pay/components/client/Providers.client.tsx
@@ -1,12 +1,17 @@
 "use client";
 import { Toaster } from "sonner";
 import { ThirdwebProvider } from "thirdweb/react";
+import { PHProvider } from "../../../../lib/posthog/Posthog";
+import { PostHogPageView } from "../../../../lib/posthog/PosthogPageView";
 
 export function Providers({ children }: { children: React.ReactNode }) {
   return (
     <ThirdwebProvider>
-      {children}
-      <Toaster richColors theme="dark" />
+      <PHProvider>
+        <PostHogPageView />
+        {children}
+        <Toaster richColors theme="dark" />
+      </PHProvider>
     </ThirdwebProvider>
   );
 }
