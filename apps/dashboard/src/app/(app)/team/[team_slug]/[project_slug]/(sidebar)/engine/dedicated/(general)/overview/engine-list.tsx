a1c875c5969a39eba72a844d3cb5b849fb50133b
Jonas Daniels <jonas.daniels@outlook.com>
2025-06-11 19:32:08 -0700

    [Dashboard] Add self-service dedicated engine deployment option (#7325)

--- apps/dashboard/src/app/(app)/team/[team_slug]/[project_slug]/(sidebar)/engine/dedicated/(general)/overview/engine-list.tsx
+++ apps/dashboard/src/app/(app)/team/[team_slug]/[project_slug]/(sidebar)/engine/dedicated/(general)/overview/engine-list.tsx
@@ -4,28 +4,25 @@ import { EngineFooterCard } from "../_components";
 import { EngineInstancesTable } from "./engine-instances-table";
 
 export const EngineInstancesList = (props: {
-  team_slug: string;
-  project_slug: string;
+  team: Team;
+  projectSlug: string;
   instances: EngineInstance[];
-  teamPlan: Team["billingPlan"];
 }) => {
-  const engineLinkPrefix = `/team/${props.team_slug}/${props.project_slug}/engine/dedicated`;
+  const engineLinkPrefix = `/team/${props.team.slug}/${props.projectSlug}/engine/dedicated`;
 
   return (
     <div className="flex grow flex-col">
       <EngineInstancesTable
-        teamSlug={props.team_slug}
-        projectSlug={props.project_slug}
+        team={props.team}
+        projectSlug={props.projectSlug}
         instances={props.instances}
         engineLinkPrefix={engineLinkPrefix}
-        teamPlan={props.teamPlan}
       />
 
       <div className="h-40" />
       <EngineFooterCard
-        teamPlan={props.teamPlan}
-        team_slug={props.team_slug}
-        project_slug={props.project_slug}
+        teamSlug={props.team.slug}
+        projectSlug={props.projectSlug}
       />
     </div>
   );
