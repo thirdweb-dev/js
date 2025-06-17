6d62f056c9b541d271fa751cd76952cc3caec2ad
Manan Tank <manantankm@gmail.com>
2025-06-17 21:53:07 +0530

    Add Empty Nebula project shell

---
+++ apps/nebula/src/app/layout.tsx
@@ -0,0 +1,12 @@
+export default function Layout(props: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="en" suppressHydrationWarning>
+      <head>
+        <link rel="icon" href="/favicon.ico" />
+      </head>
+      <body>{props.children}</body>
+    </html>
+  );
+}
