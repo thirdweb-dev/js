06c6c6f7579b26d31532d286a84bfba9064fe482
PaoloRollo <paolo.rollo1997@gmail.com>
2025-06-19 17:02:52 +0200

    Revert "feat: add @thirdweb-dev/ai package."
    This reverts commit 72141c40f1dda361da11b1816216658292c89bca.

--- packages/ai/CHANGELOG.md
+++
@@ -1,7 +0,0 @@
-# @thirdweb-dev/ai
-
-## v1.0.0
-
-### Major Changes
-
-- Initial release of the `@thirdweb-dev/ai` package.
--- packages/ai/src/exports/thirdweb.ts
+++
@@ -1,8 +0,0 @@
-export * from "../client/index.js";
-export type { CreateClientConfig } from "../client/client.gen.js";
-export {
-  configure,
-  configureWithClient,
-  getNebulaClient,
-  type NebulaClientOptions,
-} from "../configure.js";
