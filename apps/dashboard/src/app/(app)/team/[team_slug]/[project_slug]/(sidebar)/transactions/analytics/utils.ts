b8fdf4a70f93b280c0737046264dfc314608e827
Manan Tank <manantankm@gmail.com>
2025-06-20 23:17:49 +0530

    Dashboard: Add redirects for project pages (#7386)

--- apps/dashboard/src/app/(app)/team/[team_slug]/[project_slug]/(sidebar)/transactions/analytics/utils.ts
+++ apps/dashboard/src/app/(app)/team/[team_slug]/[project_slug]/(sidebar)/transactions/analytics/utils.ts
@@ -6,6 +6,9 @@ export function storeUserAccessToken(projectId: string, accessToken: string) {
 }
 
 export function getUserAccessToken(projectId: string) {
+  if (typeof localStorage === "undefined") {
+    return null;
+  }
   return localStorage.getItem(
     `thirdweb:engine-cloud-user-access-token-${projectId}`,
   );
