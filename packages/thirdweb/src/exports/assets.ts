82cc1160ec551288e975aa2e42d0e14654add310
Manan Tank <manantankm@gmail.com>
2025-06-23 23:53:30 +0530

    remove export *

--- packages/thirdweb/src/exports/assets.ts
+++ packages/thirdweb/src/exports/assets.ts
@@ -1,1 +1,22 @@
-export * from "../assets/index.js";
+export {
+  deployAssetFactory,
+  deployFeeManager,
+  deployRewardLocker,
+  deployRouter,
+  getDeployedAssetFactory,
+  getDeployedFeeManager,
+  getDeployedRewardLocker,
+  getDeployedRouter,
+} from "../assets/bootstrap.js";
+export { createToken } from "../assets/create-token.js";
+export { createTokenByImplConfig } from "../assets/create-token-by-impl-config.js";
+export { distributeToken } from "../assets/distribute-token.js";
+export { getDeployedEntrypointERC20 } from "../assets/get-entrypoint-erc20.js";
+export { isRouterEnabled } from "../assets/is-router-enabled.js";
+export type {
+  CreateTokenOptions,
+  DistributeContent,
+  MarketConfig,
+  PoolConfig,
+  TokenParams,
+} from "../assets/types.js";
