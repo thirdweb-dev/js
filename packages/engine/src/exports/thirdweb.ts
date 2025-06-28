1098ddf64dcc899f5785c2f0b3be91c2c842dc08
Joaquim Verges <joaquim.verges@gmail.com>
2025-06-28 13:10:00 +1200

    lint

--- packages/engine/src/exports/thirdweb.ts
+++ packages/engine/src/exports/thirdweb.ts
@@ -1,8 +1,8 @@
 export type { CreateClientConfig } from "../client/client.gen.js";
 export * from "../client/index.js";
 export {
-  configure,
-  type EngineClientOptions,
-  isErrorResponse,
-  isSuccessResponse,
+	configure,
+	type EngineClientOptions,
+	isErrorResponse,
+	isSuccessResponse,
 } from "../configure.js";
