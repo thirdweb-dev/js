ef9e47fb1e3d79572b1b9584a827bebbb9a27cf6
Joaquim Verges <joaquim.verges@gmail.com>
2025-06-29 21:38:41 +1200

    [Dashboard] Add client ID header to embedded wallets fetch request

--- apps/dashboard/src/@/hooks/useEmbeddedWallets.ts
+++ apps/dashboard/src/@/hooks/useEmbeddedWallets.ts
@@ -22,6 +22,7 @@ const fetchAccountList = ({
       headers: {
         Authorization: `Bearer ${jwt}`,
         "Content-Type": "application/json",
+        "x-client-id": clientId,
       },
       method: "GET",
     });
