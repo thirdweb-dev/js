b6563b8c4e4482fa31e6542f0db03b4e8acd6267
Manan Tank <manantankm@gmail.com>
2025-07-04 00:27:48 +0530

    Fix createTokenOnUniversalBridge

--- apps/dashboard/src/app/(app)/team/[team_slug]/[project_slug]/(sidebar)/tokens/create/_apis/create-token-on-bridge.ts
+++ apps/dashboard/src/app/(app)/team/[team_slug]/[project_slug]/(sidebar)/tokens/create/_apis/create-token-on-bridge.ts
@@ -7,15 +7,17 @@ export async function createTokenOnUniversalBridge(params: {
   client: ThirdwebClient;
 }) {
   const domain = isProd ? "thirdweb.com" : "thirdweb-dev.com";
-  const res = await fetch(
-    `https://bridge.${domain}/v1/tokens?chainId=${params.chainId}&tokenAddress=${params.tokenAddress}`,
-    {
-      headers: {
-        "x-client-id": params.client.clientId,
-      },
-      method: "POST",
+  const res = await fetch(`https://bridge.${domain}/v1/tokens`, {
+    body: JSON.stringify({
+      chainId: params.chainId.toString(),
+      tokenAddress: params.tokenAddress,
+    }),
+    headers: {
+      "Content-Type": "application/json",
+      "x-client-id": params.client.clientId,
     },
-  );
+    method: "POST",
+  });
 
   return res;
 }
