341b427154785581cb0d2577a33549ace2d31864
nischit <nischitpra@gmail.com>
2025-07-18 13:52:01 +0545

    lint

--- packages/thirdweb/src/gas/fee-data.ts
+++ packages/thirdweb/src/gas/fee-data.ts
@@ -137,11 +137,11 @@ export async function getDefaultGasOverrides(
     resolvedFeeType === "legacy" ||
     FORCE_GAS_PRICE_CHAIN_IDS.includes(chain.id)
   ) {
-    const gasPrice = await getGasPrice({ 
-      chain, 
-      client, 
+    const gasPrice = await getGasPrice({
+      chain,
+      client,
       percentMultiplier: 10,
-    })
+    });
     if (chain.id === 88 || chain.id === 89) {
       return {
         maxPriorityFeePerGas: gasPrice || 0n,
