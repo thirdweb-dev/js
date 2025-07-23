23c470a06b57cb8eaf47c222371bfa07af715b27
samina <samina@thirdweb.com>
2025-07-23 12:20:54 -0600

    fixed SEO issues from ahrefs

--- apps/portal/src/app/chat/page.tsx
+++ apps/portal/src/app/chat/page.tsx
@@ -6,11 +6,11 @@ import { Chat } from "@/components/AI/chat";
 const queryClient = new QueryClient();
 
 export default function ChatPage() {
-  return (
-    <QueryClientProvider client={queryClient}>
-      <div className="m-auto flex h-[calc(100vh-4rem)] w-full flex-col overflow-hidden lg:size-[calc(100vh-8rem)]">
-        <Chat />
-      </div>
-    </QueryClientProvider>
-  );
+	return (
+		<QueryClientProvider client={queryClient}>
+			<div className="m-auto flex h-[calc(100vh-4rem)] w-full flex-col overflow-hidden lg:size-[calc(100vh-8rem)]">
+				<Chat />
+			</div>
+		</QueryClientProvider>
+	);
 }
