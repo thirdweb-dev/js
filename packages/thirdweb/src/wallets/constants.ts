abe9f3b02acf3f9ee2fcb5059ee8888917f3b9c9
Joaquim Verges <joaquim.verges@gmail.com>
2025-09-16 12:12:22 +1200

    [SDK] Filter out AGW from searchable wallets

--- packages/thirdweb/src/wallets/constants.ts
+++ packages/thirdweb/src/wallets/constants.ts
@@ -1,5 +1,15 @@
+import type { WalletId } from "./wallet-types.js";
+
 // Constants for most common wallets
 export const COINBASE = "com.coinbase.wallet";
 export const METAMASK = "io.metamask";
 export const RAINBOW = "me.rainbow";
 export const ZERION = "io.zerion.wallet";
+
+// Wallet IDs that should not appear in searchable wallet lists
+export const NON_SEARCHABLE_WALLETS: WalletId[] = [
+  "inApp",
+  "embedded",
+  "smart",
+  "xyz.abs",
+];
