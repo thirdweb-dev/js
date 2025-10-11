04560599117ec68549e635aeb3316ea4f9513656
Prithvish Baidya <deformercoding@gmail.com>
2025-10-11 06:03:52 +0530

    skip vault tests on CI

--- packages/vault-sdk/tests/env-setup.ts
+++ packages/vault-sdk/tests/env-setup.ts
@@ -3,4 +3,9 @@ import { loadEnvFile } from "node:process";
 import { fileURLToPath } from "node:url";
 
 const __dirname = dirname(fileURLToPath(import.meta.url));
-loadEnvFile(join(__dirname, ".env"));
+
+try {
+  loadEnvFile(join(__dirname, ".env"));
+} catch (_error) {
+  console.warn("No .env file found, using default values");
+}
