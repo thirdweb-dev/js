16fc56dd2451001caad40b31ced67fb9b39df492
Yash <kumaryashcse@gmail.com>
2025-11-10 15:46:35 +0530

    lint

--- packages/thirdweb/src/extensions/dynamic-contracts/write/installPublishedExtension.test.ts
+++ packages/thirdweb/src/extensions/dynamic-contracts/write/installPublishedExtension.test.ts
@@ -11,53 +11,50 @@ import { sendTransaction } from "../../../transaction/actions/send-transaction.j
 import { installPublishedExtension } from "./installPublishedExtension.js";
 
 describe.runIf(process.env.TW_SECRET_KEY)("install extension", () => {
-  it.sequential(
-    "should install extension to a dynamic contract",
-    async () => {
-      await deployCloneFactory({
-        account: TEST_ACCOUNT_A,
-        chain: ANVIL_CHAIN,
-        client: TEST_CLIENT,
-      });
+  it.sequential("should install extension to a dynamic contract", async () => {
+    await deployCloneFactory({
+      account: TEST_ACCOUNT_A,
+      chain: ANVIL_CHAIN,
+      client: TEST_CLIENT,
+    });
 
-      const deployed = await deployPublishedContract({
-        account: TEST_ACCOUNT_A,
-        chain: ANVIL_CHAIN,
-        client: TEST_CLIENT,
-        contractId: "EvolvingNFT",
-        contractParams: {
-          contractURI: "",
-          defaultAdmin: TEST_ACCOUNT_A.address,
-          name: "Evolving nft",
-          royaltyBps: 0n,
-          royaltyRecipient: TEST_ACCOUNT_A.address,
-          saleRecipient: TEST_ACCOUNT_A.address,
-          symbol: "ENFT",
-          trustedForwarders: [],
-        },
-      });
+    const deployed = await deployPublishedContract({
+      account: TEST_ACCOUNT_A,
+      chain: ANVIL_CHAIN,
+      client: TEST_CLIENT,
+      contractId: "EvolvingNFT",
+      contractParams: {
+        contractURI: "",
+        defaultAdmin: TEST_ACCOUNT_A.address,
+        name: "Evolving nft",
+        royaltyBps: 0n,
+        royaltyRecipient: TEST_ACCOUNT_A.address,
+        saleRecipient: TEST_ACCOUNT_A.address,
+        symbol: "ENFT",
+        trustedForwarders: [],
+      },
+    });
 
-      const contract = getContract({
-        address: deployed,
-        chain: ANVIL_CHAIN,
-        client: TEST_CLIENT,
-      });
+    const contract = getContract({
+      address: deployed,
+      chain: ANVIL_CHAIN,
+      client: TEST_CLIENT,
+    });
 
-      const transaction = installPublishedExtension({
-        account: TEST_ACCOUNT_A,
-        contract,
-        extensionName: "DirectListingsLogic",
-      });
+    const transaction = installPublishedExtension({
+      account: TEST_ACCOUNT_A,
+      contract,
+      extensionName: "DirectListingsLogic",
+    });
 
-      await sendTransaction({ account: TEST_ACCOUNT_A, transaction });
+    await sendTransaction({ account: TEST_ACCOUNT_A, transaction });
 
-      const extensions = await readContract({
-        contract,
-        method: resolveMethod("getAllExtensions"),
-        params: [],
-      });
+    const extensions = await readContract({
+      contract,
+      method: resolveMethod("getAllExtensions"),
+      params: [],
+    });
 
-      expect(extensions.length).toEqual(4);
-    },
-  );
+    expect(extensions.length).toEqual(4);
+  });
 });
